3d3b7b6cc71907c5ef0d684878cb2437f8fb6506
Bart Blast <bart@bartblast.com>
2020-07-23 16:53:15 +0200

    Add ex_doc dep

--- mix.exs
+++ mix.exs
@@ -34,6 +34,7 @@ defmodule Reflex.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, "~> 0.22", only: :dev, runtime: false}
     ]
   end
 end
