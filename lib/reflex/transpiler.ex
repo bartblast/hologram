9630a132169a082af33f7f13fa3ef3f18c3a32c6
Bart Blast <bart@bartblast.com>
2020-10-28 18:16:44 +0100

    Add arg for passing vars

--- lib/reflex/transpiler.ex
+++ lib/reflex/transpiler.ex
@@ -15,19 +15,21 @@ defmodule Reflex.Transpiler do
     |> Code.string_to_quoted()
   end
 
-  def transpile(ast) when is_binary(ast) do
+  def transpile(ast, vars \\ [])
+
+  def transpile(ast, _vars) when is_binary(ast) do
     ast
   end
 
-  def transpile(ast) when is_integer(ast) do
+  def transpile(ast, _vars) when is_integer(ast) do
     to_string(ast)
   end
 
-  def transpile(ast) when is_boolean(ast) do
+  def transpile(ast, _vars) when is_boolean(ast) do
     to_string(ast)
   end
 
-  def transpile({:if, _, [condition, [do: do_branch, else: else_branch]]}) do
-    "if (#{transpile(condition)}) { #{transpile(do_branch)} } else { #{transpile(else_branch)} }"
+  def transpile({:if, _, [condition, [do: do_branch, else: else_branch]]}, vars) do
+    "if (#{transpile(condition, vars)}) { #{transpile(do_branch, vars)} } else { #{transpile(else_branch, vars)} }"
   end
 end
