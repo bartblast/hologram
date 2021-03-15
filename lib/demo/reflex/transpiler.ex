f4de7b92459c32fb2a28df5cc89d07bcb915671d
Bart Blast <bart@bartblast.com>
2021-03-15 18:56:05 +0100

    Generate JS assignments

--- lib/demo/reflex/transpiler.ex
+++ lib/demo/reflex/transpiler.ex
@@ -15,6 +15,31 @@ defmodule Reflex.Transpiler do
     []
   end
 
+  def generate({:assignment, left, right}) do
+    Enum.map(left, fn pattern ->
+      case pattern do
+        [var | path] ->
+          "#{var} = #{generate(right)}#{generate_assignment_path(path)}"
+      end
+    end)
+    |> Enum.join("\n")
+  end
+
+  def generate_assignment_path([]) do
+    ""
+  end
+
+  def generate_assignment_path([:map_access, key]) do
+    "['#{key}']"
+  end
+
+  def generate_assignment_path(path) do
+    Enum.map(path, fn access_spec ->
+      generate_assignment_path(access_spec)
+    end)
+    |> Enum.join("")
+  end
+
   def generate({:integer, value}) do
     "#{value}"
   end
--- test/reflex/transpiler_test.exs
+++ test/reflex/transpiler_test.exs
@@ -94,6 +94,31 @@ defmodule Reflex.TranspilerTest do
       result = Transpiler.generate(map)
       assert result == "{ 'a': 1, 'b': { 'c': 2, 'd': { 'e': 3, 'f': 4 } } }"
     end
+
+    test "assignment, simple" do
+      code = "x = 1"
+
+      result =
+        Transpiler.parse!(code)
+        |> Transpiler.transform()
+        |> Transpiler.generate()
+
+      assert result == "x = 1"
+    end
+
+    test "assignment, nested" do
+      left = "%{a: 1, b: %{p: x, r: 4}, c: 3, d: %{m: 0, n: y}}"
+      right = "%{a: 1, b: %{p: 9, r: 4}, c: 3, d: %{m: 0, n: 8}}"
+      code = "#{left} = #{right}"
+
+      result =
+      Transpiler.parse!(code)
+      |> Transpiler.transform()
+      |> Transpiler.generate()
+
+      expected = "x = { 'a': 1, 'b': { 'p': 9, 'r': 4 }, 'c': 3, 'd': { 'm': 0, 'n': 8 } }['b']['p']\ny = { 'a': 1, 'b': { 'p': 9, 'r': 4 }, 'c': 3, 'd': { 'm': 0, 'n': 8 } }['d']['n']"
+      assert result == expected
+    end
   end
 
   describe "parse!/1" do
