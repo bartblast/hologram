a4af709194ee5eae50a76e588e6962218e01f9cc
Bart Blast <bart@bartblast.com>
2021-03-19 13:18:46 +0100

    Pass aliases in args

--- lib/demo/holograf/transpiler/transformer.ex
+++ lib/demo/holograf/transpiler/transformer.ex
@@ -5,48 +5,51 @@ defmodule Holograf.Transpiler.Transformer do
   alias Holograf.Transpiler.AST.MapAccess
   alias Holograf.Transpiler.AST.{Alias, Function, Module, Variable}
 
-  def transform(ast)
+  def transform(ast, aliases \\ [])
 
   # PRIMITIVES
 
   # boolean must be before atom
-  def transform(ast) when is_boolean(ast) do
+  def transform(ast, _aliases) when is_boolean(ast) do
     %BooleanType{value: ast}
   end
 
-  def transform(ast) when is_atom(ast) do
+  def transform(ast, _aliases) when is_atom(ast) do
     %AtomType{value: ast}
   end
 
-  def transform(ast) when is_integer(ast) do
+  def transform(ast, _aliases) when is_integer(ast) do
     %IntegerType{value: ast}
   end
 
-  def transform(ast) when is_binary(ast) do
+  def transform(ast, _aliases) when is_binary(ast) do
     %StringType{value: ast}
   end
 
   # DATA STRUCTURES
 
-  def transform(ast) when is_list(ast) do
-    data = Enum.map(ast, fn v -> transform(v) end)
+  def transform(ast, aliases) when is_list(ast) do
+    data = Enum.map(ast, fn v -> transform(v, aliases) end)
     %ListType{data: data}
   end
 
-  def transform({:%{}, _, ast}) do
-    data = Enum.map(ast, fn {k, v} -> {transform(k), transform(v)} end)
+  def transform({:%{}, _, ast}, aliases) do
+    data = Enum.map(ast, fn {k, v} ->
+      {transform(k, aliases), transform(v, aliases)}
+    end)
+
     %MapType{data: data}
   end
 
   # OPERATORS
 
-  def transform({:=, _, [left, right]}) do
-    left = transform(left)
+  def transform({:=, _, [left, right]}, aliases) do
+    left = transform(left, aliases)
 
     %MatchOperator{
       bindings: bindings(left),
       left: left,
-      right: transform(right)
+      right: transform(right, aliases)
     }
   end
 
@@ -68,12 +71,12 @@ defmodule Holograf.Transpiler.Transformer do
 
   # OTHER
 
-  def transform({:alias, _, [{:__aliases__, _, module}]}) do
+  def transform({:alias, _, [{:__aliases__, _, module}]}, _aliases) do
     %Alias{module: module}
   end
 
-  def transform({:def, _, [{name, _, args}, [do: body]]}) do
-    args = Enum.map(args, fn arg -> transform(arg) end)
+  def transform({:def, _, [{name, _, args}, [do: body]]}, aliases) do
+    args = Enum.map(args, fn arg -> transform(arg, aliases) end)
 
     body =
       case body do
@@ -82,18 +85,18 @@ defmodule Holograf.Transpiler.Transformer do
         expr ->
           [expr]
       end
-      |> Enum.map(fn expr -> transform(expr) end)
+      |> Enum.map(fn expr -> transform(expr, aliases) end)
 
     %Function{name: name, args: args, body: body}
   end
 
-  def transform({:defmodule, _, [{_, _, name}, [do: {_, _, ast}]]}) do
+  def transform({:defmodule, _, [{_, _, name}, [do: {_, _, ast}]]}, _aliases) do
     name =
       Enum.map(name, fn part -> "#{part}" end)
       |> Enum.join(".")
 
     aliases = aggregate_aliases(ast)
-    functions = aggregate_functions(ast)
+    functions = aggregate_functions(ast, aliases.map)
 
     %Module{name: name, aliases: aliases, functions: functions}
   end
@@ -117,18 +120,18 @@ defmodule Holograf.Transpiler.Transformer do
     %{list: list, map: map}
   end
 
-  defp aggregate_functions(ast) do
+  defp aggregate_functions(ast, aliases) do
     Enum.reduce(ast, [], fn expr, acc ->
       case expr do
         {:def, _, _} ->
-          acc ++ [transform(expr)]
+          acc ++ [transform(expr, aliases)]
         _ ->
           acc
       end
     end)
   end
 
-  def transform({name, _, nil}) when is_atom(name) do
+  def transform({name, _, nil}, _aliases) when is_atom(name) do
     %Variable{name: name}
   end
 end
