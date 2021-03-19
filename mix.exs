271ce11d4cf82b31ba54ad8878a19436a9856b5b
Bart Blast <bart@bartblast.com>
2021-03-19 14:28:51 +0100

    Install ex_doc

--- mix.exs
+++ mix.exs
@@ -55,7 +55,9 @@ defmodule Demo.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
   end
 
