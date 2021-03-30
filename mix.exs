7d4197eb43dfbd142ba34201845592d1b3f7addf
Bart Blast <bart@bartblast.com>
2021-03-30 11:11:17 +0200

    Install Floki package

--- mix.exs
+++ mix.exs
@@ -56,6 +56,7 @@ defmodule Demo.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
+      {:floki, "~> 0.30.0"},
 
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
