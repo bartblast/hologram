b6c382963e63763b4ced9276b2a9fc64819022c4
Bart Blast <bart@bartblast.com>
2021-04-01 10:42:43 +0200

    Remove floki package

--- mix.exs
+++ mix.exs
@@ -56,7 +56,6 @@ defmodule Demo.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:floki, "~> 0.30.0"},
       {:saxy, "~> 1.3.0"},
 
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
