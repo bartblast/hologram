e7a3f996222b7e603f20950b710029c6d10aa4ad
Bart Blast <bart@bartblast.com>
2021-04-01 10:38:33 +0200

    Add aliases param and return tag node struct

--- lib/hologram/template_engine/transformer.ex
+++ lib/hologram/template_engine/transformer.ex
@@ -1,12 +1,27 @@
 defmodule Hologram.TemplateEngine.Transformer do
+  alias Hologram.TemplateEngine.AST.ComponentNode
   alias Hologram.TemplateEngine.AST.TagNode
 
-  def transform(ast) when is_list(ast) do
-    Enum.map(ast, fn element -> transform(element) end)
+  def transform(ast, aliases \\ %{})
+
+  def transform(ast, aliases) when is_list(ast) do
+    Enum.map(ast, fn element -> transform(element, aliases) end)
   end
 
-  def transform({type, attrs, children}) do
-    children = Enum.map(children, fn child -> transform(child) end)
-    %TagNode{tag: type, children: children}
+  def transform({type, attrs, children}, aliases) do
+    children = Enum.map(children, fn child -> transform(child, aliases) end)
+
+    case resolve_node_type(type, aliases) do
+      :tag ->
+        %TagNode{tag: type, children: children}
+      # TODO: imlement
+      # :module ->
+      #   %ComponentNode{module: module, children: children}
+    end
+  end
+
+  # TODO: implement
+  defp resolve_node_type(type, aliases) do
+    :tag
   end
 end
