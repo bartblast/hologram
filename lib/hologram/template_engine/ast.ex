38dbf3d0729173dddc84b63eecada996ee4b3a25
Bart Blast <bart@bartblast.com>
2021-04-01 10:32:33 +0200

    Implement ComponentNode struct

--- lib/hologram/template_engine/ast.ex
+++ lib/hologram/template_engine/ast.ex
@@ -1,4 +1,8 @@
 defmodule Hologram.TemplateEngine.AST do
+  defmodule ComponentNode do
+    defstruct module: nil, children: nil
+  end
+
   defmodule TagNode do
     defstruct tag: nil, children: nil
   end
