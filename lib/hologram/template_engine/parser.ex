608a866f0ab73901cd0881cfc834e8e814e92899
Bart Blast <bart@bartblast.com>
2021-04-01 18:06:58 +0200

    Fix quotes in interpolation attribs

--- lib/hologram/template_engine/parser.ex
+++ lib/hologram/template_engine/parser.ex
@@ -2,6 +2,12 @@ defmodule Hologram.TemplateEngine.Parser do
   use Hologram.Parser
 
   def parse(str) do
-    Saxy.SimpleForm.parse_string(str)
+    fix_quotes(str)
+    |> Saxy.SimpleForm.parse_string()
+  end
+
+  defp fix_quotes(str) do
+    regex = ~r/=(\{\{.+\}\})/U
+    Regex.replace(regex, str, "=\"\\1\"")
   end
 end
--- test/hologram/template_engine/parser_test.exs
+++ test/hologram/template_engine/parser_test.exs
@@ -11,5 +11,37 @@ defmodule Hologram.TemplateEngine.ParserTest do
     # test "invalid html" do
     #   assert {:error, _} = Parser.parse("<div ")
     # end
+
+    test "interpolation quotes fixing" do
+      html = """
+        <div class=\"test_class_1\" abc={{ @abc }} id=\"test_id_1\" bcd={{ @bcd }}>
+          <span class=\"test_class_2\" cde={{ @cde }} id=\"test_id_2\" def={{ @def }}></span>
+        </div>
+      """
+
+      result = Parser.parse(html)
+
+      expected = {:ok,
+      {"div",
+       [
+         {"class", "test_class_1"},
+         {"abc", "{{ @abc }}"},
+         {"id", "test_id_1"},
+         {"bcd", "{{ @bcd }}"}
+       ],
+       [
+         "\n    ",
+         {"span",
+          [
+            {"class", "test_class_2"},
+            {"cde", "{{ @cde }}"},
+            {"id", "test_id_2"},
+            {"def", "{{ @def }}"}
+          ], []},
+         "\n  "
+       ]}}
+
+      assert result == expected
+    end
   end
 end
