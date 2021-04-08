9018542b491e46a99af96f493ce6e736adf42b3b
Bart Blast <bart@bartblast.com>
2021-04-08 10:34:26 +0200

    Implement template engine interpolator

---
+++ lib/hologram/template_engine/interpolator.ex
@@ -0,0 +1,62 @@
+defmodule Hologram.TemplateEngine.Interpolator do
+  alias Hologram.TemplateEngine.AST.{Expression, TextNode}
+  alias Hologram.Transpiler.Parser
+
+  def interpolate(nodes) do
+    Enum.reduce(nodes, [], fn node, acc ->
+      case node do
+        %TextNode{text: text} ->
+          acc ++ split_text_node(nodes, node, text)
+
+        _ ->
+          acc ++ [Map.put(node, :children, interpolate(node.children))]
+      end
+    end)
+  end
+
+  defp split_text_node(nodes, node, text) do
+    regex = ~r/(.*)(\{\{(.+)\}\})/U
+
+    case Regex.scan(regex, text) do
+      [] ->
+        [node]
+
+      matches ->
+        handle_matches(text, matches)
+    end
+  end
+
+  defp handle_matches(text, matches) do
+    {char_count, nodes} =
+      Enum.reduce(matches, {0, []}, fn match, {char_count, nodes} ->
+        handle_match(match, char_count, nodes)
+      end)
+
+    length = String.length(text)
+    remainder = String.slice(text, char_count, length - char_count)
+
+    if remainder != "" do
+      nodes ++ [%TextNode{text: remainder}]
+    else
+      nodes
+    end
+  end
+
+  defp handle_match(match, char_count, nodes) do
+    text = Enum.at(match, 1)
+    {char_count, nodes} =
+      if text != "" do
+        {char_count + String.length(text), nodes ++ [%TextNode{text: text}]}
+      else
+        {char_count, nodes}
+      end
+
+    code = Enum.at(match, 3)
+    ast =
+      Parser.parse!(code)
+      |> Hologram.Transpiler.Transformer.transform()
+
+    {char_count, nodes} =
+      {char_count + 4 + String.length(code), nodes ++ [%Expression{ast: ast}]}
+  end
+end
---
+++ test/hologram/template_engine/interpolator_test.exs
@@ -0,0 +1,205 @@
+defmodule Hologram.TemplateEngine.InterpolatorTest do
+  use ExUnit.Case
+
+  alias Hologram.TemplateEngine.AST.{Expression, TagNode, TextNode}
+  alias Hologram.TemplateEngine.Interpolator
+  alias Hologram.Transpiler.AST.ModuleAttribute
+
+  test "multiple text nodes" do
+    nodes = [
+      %TextNode{text: "test_1"},
+      %TextNode{text: "test_2"}
+    ]
+
+    result = Interpolator.interpolate(nodes)
+    assert result == nodes
+  end
+
+  test "text" do
+    nodes = [
+      %TextNode{text: "test"},
+    ]
+
+    result = Interpolator.interpolate(nodes)
+    assert result == nodes
+  end
+
+  test "expression" do
+    nodes = [
+      %TextNode{text: "{{ @abc }}"}
+    ]
+
+    result = Interpolator.interpolate(nodes)
+
+    expected = [
+      %Expression{
+        ast: %ModuleAttribute{name: :abc}
+      }
+    ]
+
+    assert result == expected
+  end
+
+  test "text, expression" do
+    nodes = [
+      %TextNode{text: "bcd{{ @abc }}"}
+    ]
+
+    result = Interpolator.interpolate(nodes)
+
+    expected = [
+      %TextNode{text: "bcd"},
+      %Expression{
+        ast: %ModuleAttribute{name: :abc}
+      }
+    ]
+
+    assert result == expected
+  end
+
+  test "expression, text" do
+    nodes = [
+      %TextNode{text: "{{ @abc }}bcd"}
+    ]
+
+    result = Interpolator.interpolate(nodes)
+
+    expected = [
+      %Expression{
+        ast: %ModuleAttribute{name: :abc}
+      },
+      %TextNode{text: "bcd"}
+    ]
+
+    assert result == expected
+  end
+
+  test "expression, expression" do
+    nodes = [
+      %TextNode{text: "{{ @abc }}{{ @bcd }}"}
+    ]
+
+    result = Interpolator.interpolate(nodes)
+
+    expected = [
+      %Expression{
+        ast: %ModuleAttribute{name: :abc}
+      },
+      %Expression{
+        ast: %ModuleAttribute{name: :bcd}
+      }
+    ]
+
+    assert result == expected
+  end
+
+  test "text, expression, text" do
+    nodes = [
+      %TextNode{text: "cde{{ @abc }}bcd"}
+    ]
+
+    result = Interpolator.interpolate(nodes)
+
+    expected = [
+      %TextNode{text: "cde"},
+      %Expression{
+        ast: %ModuleAttribute{name: :abc}
+      },
+      %TextNode{text: "bcd"}
+    ]
+
+    assert result == expected
+  end
+
+  test "expression, text, expression" do
+    nodes = [
+      %TextNode{text: "{{ @abc }}bcd{{ @cde }}"}
+    ]
+
+    result = Interpolator.interpolate(nodes)
+
+    expected = [
+      %Expression{
+        ast: %ModuleAttribute{name: :abc}
+      },
+      %TextNode{text: "bcd"},
+      %Expression{
+        ast: %ModuleAttribute{name: :cde}
+      }
+    ]
+
+    assert result == expected
+  end
+
+  test "text, expression, text, expression" do
+    nodes = [
+      %TextNode{text: "cde{{ @abc }}bcd{{ @def }}"}
+    ]
+
+    result = Interpolator.interpolate(nodes)
+
+    expected = [
+      %TextNode{text: "cde"},
+      %Expression{
+        ast: %ModuleAttribute{name: :abc}
+      },
+      %TextNode{text: "bcd"},
+      %Expression{
+        ast: %ModuleAttribute{name: :def}
+      },
+    ]
+
+    assert result == expected
+  end
+
+  test "expression, text, expression, text" do
+    nodes = [
+      %TextNode{text: "{{ @abc }}bcd{{ @cde }}def"}
+    ]
+
+    result = Interpolator.interpolate(nodes)
+
+    expected = [
+      %Expression{
+        ast: %ModuleAttribute{name: :abc}
+      },
+      %TextNode{text: "bcd"},
+      %Expression{
+        ast: %ModuleAttribute{name: :cde}
+      },
+      %TextNode{text: "def"},
+    ]
+
+    assert result == expected
+  end
+
+  test "nested text node" do
+    nodes = [
+      %TextNode{text: "abc{{ @bcd }}"},
+      %TagNode{tag: "div", attrs: %{}, children: [
+        %TextNode{text: "cde{{ @def }}"}
+      ]}
+    ]
+
+    result = Interpolator.interpolate(nodes)
+
+    expected = [
+      %TextNode{text: "abc"},
+      %Expression{
+        ast: %ModuleAttribute{name: :bcd}
+      },
+      %TagNode{
+        attrs: %{},
+        children: [
+          %TextNode{text: "cde"},
+          %Expression{
+            ast: %ModuleAttribute{name: :def}
+          }
+        ],
+        tag: "div"
+      }
+    ]
+
+    assert result == expected
+  end
+end
