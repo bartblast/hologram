6a1cec6301180d59004c194126dd7bf86d5108ca
Bart Blast <bart@bartblast.com>
2021-04-12 12:22:04 +0200

    Render tag nodes

--- lib/hologram/template_engine/renderer.ex
+++ lib/hologram/template_engine/renderer.ex
@@ -4,6 +4,18 @@ defmodule Hologram.TemplateEngine.Renderer do
 
   def render(ast, state)
 
+  def render(%TagNode{attrs: attrs, children: children, tag: tag}, state) do
+    attrs_html =
+      Enum.map(attrs, fn {key, value} -> " #{key}=\"#{value}\"" end)
+      |> Enum.join("")
+
+    children_html =
+      Enum.map(children, fn child -> render(child, state) end)
+      |> Enum.join("")
+
+    "<#{tag}#{attrs_html}>#{children_html}</#{tag}>"
+  end
+
   def render(%TextNode{text: text}, _state) do
     text
   end
--- test/hologram/template_engine/renderer_text.exs
+++ test/hologram/template_engine/renderer_text.exs
@@ -1,8 +1,21 @@
 defmodule Hologram.TemplateEngine.RendererTest do
   use ExUnit.Case, async: true
 
+  alias Hologram.TemplateEngine.AST.TagNode
   alias Hologram.TemplateEngine.AST.TextNode
   alias Hologram.TemplateEngine.Renderer
+
+  test "tag node" do
+    ast = %TagNode{attrs: %{attr_1: "test_attr_value_1", attr_2: "test_attr_value_2"}, tag: "div", children: [
+      %TextNode{text: "test_text"},
+      %TagNode{attrs: %{}, children: [], tag: "span"}
+    ]}
+
+    result = Renderer.render(ast, %{})
+    expected = "<div attr_1=\"test_attr_value_1\" attr_2=\"test_attr_value_2\">test_text<span></span></div>"
+
+    assert result == expected
+  end
 
   test "text node" do
     ast = %TextNode{text: "test"}
