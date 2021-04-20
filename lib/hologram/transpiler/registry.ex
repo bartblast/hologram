7ae486ab41f128383cca86872e910b90bfced58f
Bart Blast <bart@bartblast.com>
2021-04-20 16:18:57 +0200

    Remove Registry module

--- lib/hologram/transpiler/registry.ex
+++
@@ -1,6 +0,0 @@
-defmodule Hologram.Transpiler.Registry do
-  def has_function?(registry, module, function, arity) do
-    (registry[module] || [])
-    |> Enum.any?(&(&1.name == function && &1.arity == arity))
-  end
-end
