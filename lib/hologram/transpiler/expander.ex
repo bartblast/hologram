e4a5400d9fc1847928603f378f5b11f781975541
Bart Blast <bart@bartblast.com>
2021-04-22 15:09:24 +0200

    Fix expander

--- lib/hologram/transpiler/expander.ex
+++ lib/hologram/transpiler/expander.ex
@@ -1,13 +1,20 @@
 defmodule Hologram.Transpiler.Expander do
   alias Hologram.Transpiler.Helpers
+  alias Hologram.Transpiler.Normalizer
   alias Hologram.Transpiler.Parser
 
-  def expand({:defmodule, line, [aliases, [do: ast]]}) do
-    {:defmodule, line, [aliases, [do: expand(ast)]]}
-  end
+  def expand({:defmodule, line, [aliases, [do: {:__block__, [], exprs}]]}) do
+    expanded =
+      Enum.reduce(exprs, [], fn expr, acc ->
+        case expand(expr) do
+          e when is_list(e) ->
+            acc ++ e
+          e ->
+            acc ++ [e]
+        end
+      end)
 
-  def expand({:__block__, [], expressions}) do
-    {:__block__, [], Enum.map(expressions, &expand/1)}
+    {:defmodule, line, [aliases, [do: {:__block__, [], expanded}]]}
   end
 
   def expand({:use, _, [{:__aliases__, _, module}]}) do
@@ -15,6 +22,7 @@ defmodule Hologram.Transpiler.Expander do
     |> apply(:module_info, [])
     |> get_in([:compile, :source])
     |> Parser.parse_file!()
+    |> Normalizer.normalize()
     |> aggregate_quotes()
   end
 
@@ -22,19 +30,21 @@ defmodule Hologram.Transpiler.Expander do
     ast
   end
 
-  defp aggregate_quotes({:defmodule, _, [{:__aliases__, _, _}, [do: ast]]}) do
-    aggregate_quotes(ast)
+  defp aggregate_quotes({:defmodule, _, [{:__aliases__, _, _}, [do: {:__block__, [], exprs}]]}) do
+    aggregate_quotes_in_block(exprs)
   end
 
-  defp aggregate_quotes({:defmacro, _, [{:__using__, _, _}, [do: ast]]}) do
-    aggregate_quotes(ast)
+  defp aggregate_quotes({:defmacro, _, [{:__using__, _, _}, [do: {:__block__, [], exprs}]]}) do
+    aggregate_quotes_in_block(exprs)
   end
 
-  defp aggregate_quotes({:quote, _, [[do: ast]]}) do
-    ast
+  defp aggregate_quotes({:quote, _, [[do: {:__block__, [], exprs}]]}) do
+    exprs
   end
 
-  defp aggregate_quotes(ast) do
-    ast
+  defp aggregate_quotes_in_block(exprs) do
+    Enum.reduce(exprs, [], fn expr, acc ->
+      acc ++ aggregate_quotes(expr)
+    end)
   end
 end
--- test/hologram/transpiler/expander_test.exs
+++ test/hologram/transpiler/expander_test.exs
@@ -1,6 +1,5 @@
 defmodule Hologram.Transpiler.ExpanderTest do
   use ExUnit.Case, async: true
-  import Hologram.Transpiler.Parser, only: [parse!: 1]
 
   alias Hologram.Transpiler.Expander
   alias TestModule1
@@ -8,206 +7,168 @@ defmodule Hologram.Transpiler.ExpanderTest do
   alias TestModule3
   alias TestModule5
 
-  test "empty parent module" do
-    code = """
-    defmodule Test do
-    end
-    """
+  test "no use directives" do
+    # normalized AST from:
+    #
+    # defmodule Test do
+    #   def test do
+    #       1
+    #   end
+    # end
 
-    result =
-      parse!(code)
-      |> Expander.expand()
+    ast =
+      {:defmodule, [line: 1],
+      [
+        {:__aliases__, [line: 1], [:Test]},
+        [
+          do: {:__block__, [],
+          [
+            {:def, [line: 2],
+              [{:test, [line: 2], nil}, [do: {:__block__, [], [1]}]]}
+          ]}
+        ]
+      ]}
+
+    result = Expander.expand(ast)
+
+    assert result == ast
+  end
+
+  test "single use directive" do
+    # normalized AST from:
+    #
+    # defmodule Test do
+    #   use TestModule2
+    # end
+
+    ast =
+      {:defmodule, [line: 1],
+        [
+          {:__aliases__, [line: 1], [:Test]},
+          [
+            do: {:__block__, [],
+              [{:use, [line: 2], [{:__aliases__, [line: 2], [:TestModule2]}]}]}
+          ]
+        ]}
+
+    result = Expander.expand(ast)
 
     expected =
-      {:defmodule, [line: 1], [{:__aliases__, [line: 1], [:Test]}, [do: {:__block__, [], []}]]}
+      {:defmodule, [line: 1],
+      [
+        {:__aliases__, [line: 1], [:Test]},
+        [
+          do: {:__block__, [],
+            [{:import, [line: 4], [{:__aliases__, [line: 4], [:TestModule1]}]}]}
+        ]
+      ]}
 
     assert result == expected
   end
 
-  test "parent module with one expression which is not a use directive" do
-    code = """
-    defmodule Test do
-      def test do
-        1
-      end
-    end
-    """
+  test "multiple use directives" do
+    # normalized AST from:
+    #
+    # defmodule Test do
+    #   use TestModule2
+    #   use TestModule4
+    # end
 
-    result =
-      parse!(code)
-      |> Expander.expand()
+    ast =
+      {:defmodule, [line: 1],
+      [
+        {:__aliases__, [line: 1], [:Test]},
+        [
+          do: {:__block__, [],
+           [
+             {:use, [line: 2], [{:__aliases__, [line: 2], [:TestModule2]}]},
+             {:use, [line: 3], [{:__aliases__, [line: 3], [:TestModule4]}]}
+           ]}
+        ]
+      ]}
+
+    result = Expander.expand(ast)
 
     expected =
       {:defmodule, [line: 1],
-       [
-         {:__aliases__, [line: 1], [:Test]},
-         [do: {:def, [line: 2], [{:test, [line: 2], nil}, [do: 1]]}]
-       ]}
-
-    assert result == expected
-  end
-
-  test "parent module with multiple expressions which are not use directives" do
-    code = """
-    defmodule Test do
-      def test_1 do
-        1
-      end
-
-      def test_2 do
-        2
-      end
-    end
-    """
-
-    result =
-      parse!(code)
-      |> Expander.expand()
-
-    expected =
-      {:defmodule, [line: 1],
-       [
-         {:__aliases__, [line: 1], [:Test]},
-         [
-           do:
-             {:__block__, [],
-              [
-                {:def, [line: 2], [{:test_1, [line: 2], nil}, [do: 1]]},
-                {:def, [line: 6], [{:test_2, [line: 6], nil}, [do: 2]]}
-              ]}
-         ]
-       ]}
-
-    assert result == expected
-  end
-
-  test "parent module with one use directive" do
-    code = """
-    defmodule Test do
-      use TestModule2
-    end
-    """
-
-    result =
-      parse!(code)
-      |> Expander.expand()
-
-    expected =
-      {:defmodule, [line: 1],
-       [
-         {:__aliases__, [line: 1], [:Test]},
-         [do: {:import, [line: 4], [{:__aliases__, [line: 4], [:TestModule1]}]}]
-       ]}
-
-    assert result == expected
-  end
-
-  test "parent module with multiple use directives" do
-    code = """
-    defmodule Test do
-      use TestModule2
-      use TestModule4
-    end
-    """
-
-    result =
-      parse!(code)
-      |> Expander.expand()
-
-    expected =
-      {:defmodule, [line: 1],
-       [
-         {:__aliases__, [line: 1], [:Test]},
-         [
-           do:
-             {:__block__, [],
-              [
-                {:import, [line: 4], [{:__aliases__, [line: 4], [:TestModule1]}]},
-                {:import, [line: 4], [{:__aliases__, [line: 4], [:TestModule3]}]}
-              ]}
-         ]
-       ]}
-
-    assert result == expected
-  end
-
-  test "parent module with one use directive and one other expression" do
-    code = """
-    defmodule Test do
-      use TestModule2
-
-      def test do
-        1
-      end
-    end
-    """
-
-    result =
-      parse!(code)
-      |> Expander.expand()
-
-    expected =
-      {:defmodule, [line: 1],
-       [
-         {:__aliases__, [line: 1], [:Test]},
-         [
-           do:
-             {:__block__, [],
-              [
-                {:import, [line: 4], [{:__aliases__, [line: 4], [:TestModule1]}]},
-                {:def, [line: 4], [{:test, [line: 4], nil}, [do: 1]]}
-              ]}
-         ]
-       ]}
+      [
+        {:__aliases__, [line: 1], [:Test]},
+        [
+          do: {:__block__, [],
+           [
+             {:import, [line: 4], [{:__aliases__, [line: 4], [:TestModule1]}]},
+             {:import, [line: 4], [{:__aliases__, [line: 4], [:TestModule3]}]}
+           ]}
+        ]
+      ]}
 
     assert result == expected
   end
 
   test "single quote at the root of __using__ macro with a single expression" do
-    code = """
-    defmodule Test do
-      use TestModule2
-    end
-    """
+    # normalized AST from:
+    #
+    # defmodule Test do
+    #   use TestModule2
+    # end
 
-    result =
-      parse!(code)
-      |> Expander.expand()
+    ast =
+      {:defmodule, [line: 1],
+        [
+          {:__aliases__, [line: 1], [:Test]},
+          [
+            do: {:__block__, [],
+              [{:use, [line: 2], [{:__aliases__, [line: 2], [:TestModule2]}]}]}
+          ]
+        ]}
+
+    result = Expander.expand(ast)
 
     expected =
       {:defmodule, [line: 1],
-       [
-         {:__aliases__, [line: 1], [:Test]},
-         [do: {:import, [line: 4], [{:__aliases__, [line: 4], [:TestModule1]}]}]
-       ]}
+      [
+        {:__aliases__, [line: 1], [:Test]},
+        [
+          do: {:__block__, [],
+            [{:import, [line: 4], [{:__aliases__, [line: 4], [:TestModule1]}]}]}
+        ]
+      ]}
 
     assert result == expected
   end
 
   test "single quote at the root of __using__ macro with multiple expressions" do
-    code = """
-    defmodule Test do
-      use TestModule5
-    end
-    """
+    # normalized AST from:
+    #
+    # defmodule Test do
+    #   use TestModule5
+    # end
 
-    result =
-      parse!(code)
-      |> Expander.expand()
+    ast =
+      {:defmodule, [line: 1],
+        [
+          {:__aliases__, [line: 1], [:Test]},
+          [
+            do: {:__block__, [],
+              [{:use, [line: 2], [{:__aliases__, [line: 2], [:TestModule5]}]}]}
+          ]
+        ]}
+
+    result = Expander.expand(ast)
 
     expected =
       {:defmodule, [line: 1],
-       [
-         {:__aliases__, [line: 1], [:Test]},
-         [
-           do:
-             {:__block__, [],
-              [
-                {:import, [line: 4], [{:__aliases__, [line: 4], [:TestModule1]}]},
-                {:import, [line: 5], [{:__aliases__, [line: 5], [:TestModule3]}]}
-              ]}
-         ]
-       ]}
+      [
+        {:__aliases__, [line: 1], [:Test]},
+        [
+          do: {:__block__, [],
+            [
+              {:import, [line: 4], [{:__aliases__, [line: 4], [:TestModule1]}]},
+              {:import, [line: 5], [{:__aliases__, [line: 5], [:TestModule3]}]}
+            ]
+          }
+        ]
+      ]}
 
     assert result == expected
   end
