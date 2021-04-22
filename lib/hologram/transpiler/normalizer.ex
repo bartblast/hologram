fa15ed935d74c96717d0d601858baea42fb41737
Bart Blast <bart@bartblast.com>
2021-04-22 13:29:09 +0200

    Refactor normalizer

--- lib/hologram/transpiler/normalizer.ex
+++ lib/hologram/transpiler/normalizer.ex
@@ -1,10 +1,10 @@
 defmodule Hologram.Transpiler.Normalizer do
-  def normalize({:defmodule, line, [aliases, [do: {:__block__, [], block}]]}) do
-    {:defmodule, line, [aliases, [do: {:__block__, [], normalize(block)}]]}
+  def normalize({m_1, m_2, [m_3, [do: {:__block__, [], block}]]}) do
+    {m_1, m_2, [m_3, [do: {:__block__, [], normalize(block)}]]}
   end
 
-  def normalize({:defmodule, line, [aliases, [do: block]]}) do
-    {:defmodule, line, [aliases, [do: {:__block__, [], normalize([block])}]]}
+  def normalize({m_1, m_2, [m_3, [do: expr]]}) do
+    {m_1, m_2, [m_3, [do: {:__block__, [], normalize([expr])}]]}
   end
 
   def normalize(ast) do
--- test/hologram/transpiler/normalizer_test.exs
+++ test/hologram/transpiler/normalizer_test.exs
@@ -1,39 +1,10 @@
 defmodule Hologram.Transpiler.NormalizerTest do
   use ExUnit.Case, async: true
-  import Hologram.Transpiler.Parser, only: [parse!: 1]
   alias Hologram.Transpiler.Normalizer
 
-  test "module without block" do
+  test "do expression with block" do
     # AST from:
-    # defmodule Test do
-    #   def test do
-    #     1
-    #   end
-    # end
-
-    ast =
-      {:defmodule, [line: 1], [
-        {:__aliases__, [line: 1], [:Test]},
-        [do: {:def, [line: 2], [{:test, [line: 2], nil}, [do: 1]]}]
-      ]}
-
-    result = Normalizer.normalize(ast)
-
-    expected =
-      {:defmodule, [line: 1], [
-        {:__aliases__, [line: 1], [:Test]},
-        [
-          do: {:__block__, [], [
-            {:def, [line: 2], [{:test, [line: 2], nil}, [do: 1]]}
-          ]}
-        ]
-      ]}
-
-    assert result == expected
-  end
-
-  test "module with block" do
-    # AST from:
+    #
     # defmodule Test do
     #   alias Abc
 
@@ -42,11 +13,12 @@ defmodule Hologram.Transpiler.NormalizerTest do
     #   end
     # end
 
-    ast =
-      {:defmodule, [line: 1], [
+    ast = {:defmodule, [line: 1],
+      [
         {:__aliases__, [line: 1], [:Test]},
         [
-          do: {:__block__, [], [
+          do: {:__block__, [],
+          [
             {:alias, [line: 2], [{:__aliases__, [line: 2], [:Abc]}]},
             {:def, [line: 4], [{:test, [line: 4], nil}, [do: 1]]}
           ]}
@@ -55,5 +27,34 @@ defmodule Hologram.Transpiler.NormalizerTest do
 
     result = Normalizer.normalize(ast)
     assert result == ast
+  end
+
+  test "do expression without block" do
+    # AST from:
+    #
+    # defmodule Test do
+    #   alias Abc
+    # end
+
+    ast =
+      {:defmodule, [line: 1],
+        [
+          {:__aliases__, [line: 1], [:Test]},
+          [do: {:alias, [line: 2], [{:__aliases__, [line: 2], [:Abc]}]}]
+        ]}
+
+    result = Normalizer.normalize(ast)
+
+    expected =
+      {:defmodule, [line: 1],
+        [
+          {:__aliases__, [line: 1], [:Test]},
+          [
+            do: {:__block__, [],
+              [{:alias, [line: 2], [{:__aliases__, [line: 2], [:Abc]}]}]}
+          ]
+        ]}
+
+    assert result == expected
   end
 end
