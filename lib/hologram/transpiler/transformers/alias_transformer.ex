2ee9fd75c225b72d1694d7ed1709af4507126a34
Bart Blast <bart@bartblast.com>
2021-04-27 13:01:10 +0200

    Refactor alias AST transformer

--- lib/hologram/transpiler/transformers/alias_transformer.ex
+++ lib/hologram/transpiler/transformers/alias_transformer.ex
@@ -1,11 +1,11 @@
 defmodule Hologram.Transpiler.AliasTransformer do
   alias Hologram.Transpiler.AST.Alias
 
-  def transform({:alias, _, [{_, _, module}]}) do
+  def transform([{_, _, module}]) do
     %Alias{module: module, as: [List.last(module)]}
   end
 
-  def transform({:alias, _, [{_, _, module}, [as: {_, _, as}]]}) do
+  def transform([{_, _, module}, [as: {_, _, as}]]) do
     %Alias{module: module, as: as}
   end
 end
--- test/hologram/transpiler/transformers/alias_transformer_test.exs
+++ test/hologram/transpiler/transformers/alias_transformer_test.exs
@@ -1,13 +1,12 @@
 defmodule Hologram.Transpiler.AliasTransformerTest do
   use ExUnit.Case, async: true
-  
+
   alias Hologram.Transpiler.AliasTransformer
   alias Hologram.Transpiler.AST.Alias
 
   test "default as" do
     # alias Abc.Bcd
-    ast =
-      {:alias, [line: 1], [{:__aliases__, [line: 1], [:Abc, :Bcd]}]}
+    ast = [{:__aliases__, [line: 1], [:Abc, :Bcd]}]
 
     result = AliasTransformer.transform(ast)
     expected = %Alias{module: [:Abc, :Bcd], as: [:Bcd]}
@@ -18,11 +17,10 @@ defmodule Hologram.Transpiler.AliasTransformerTest do
   test "specified one-part as" do
     # alias Abc.Bcd, as: Xyz
     ast =
-      {:alias, [line: 1],
-        [
-          {:__aliases__, [line: 1], [:Abc, :Bcd]},
-          [as: {:__aliases__, [line: 1], [:Xyz]}]
-        ]}
+      [
+        {:__aliases__, [line: 1], [:Abc, :Bcd]},
+        [as: {:__aliases__, [line: 1], [:Xyz]}]
+      ]
 
     result = AliasTransformer.transform(ast)
     expected = %Alias{module: [:Abc, :Bcd], as: [:Xyz]}
@@ -33,11 +31,10 @@ defmodule Hologram.Transpiler.AliasTransformerTest do
   test "specified multiple-part as" do
     # alias Abc.Bcd, as: Xyz.Kmn
     ast =
-      {:alias, [line: 1],
-        [
-          {:__aliases__, [line: 1], [:Abc, :Bcd]},
-          [as: {:__aliases__, [line: 1], [:Xyz, :Kmn]}]
-        ]}
+      [
+        {:__aliases__, [line: 1], [:Abc, :Bcd]},
+        [as: {:__aliases__, [line: 1], [:Xyz, :Kmn]}]
+      ]
 
     result = AliasTransformer.transform(ast)
     expected = %Alias{module: [:Abc, :Bcd], as: [:Xyz, :Kmn]}
