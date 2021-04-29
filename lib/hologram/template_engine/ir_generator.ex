3d9b55db26f2e7ceadc90dea3d0a80d058c7a2f2
Bart Blast <bart@bartblast.com>
2021-04-29 16:16:51 +0200

    Implement tag node IR generator

--- lib/hologram/template_engine/ir_generator.ex
+++ lib/hologram/template_engine/ir_generator.ex
@@ -1,7 +1,31 @@
 defmodule Hologram.TemplateEngine.IRGenerator do
-  alias Hologram.TemplateEngine.AST.TextNode
+  alias Hologram.TemplateEngine.AST.{TagNode, TextNode}
+  alias Hologram.TemplateEngine.Renderer
 
   def generate(ast)
+
+  def generate(%TagNode{attrs: attrs, children: children, tag: tag}) do
+    attrs_js =
+      if Enum.any?(attrs) do
+        js =
+          Enum.map(attrs, fn {key, value} ->
+            "'#{Renderer.render_attr_name(key)}': '#{value}'"
+          end)
+          |> Enum.join(", ")
+
+        "{ #{js} }"
+      else
+        "{}"
+      end
+
+      children_str =
+        Enum.map(children, &generate(&1))
+        |> Enum.join(", ")
+
+      children_js = "[#{children_str}]"
+
+    "{ type: 'tag_node', tag: '#{tag}', attrs: #{attrs_js}, children: #{children_js} }"
+  end
 
   def generate(%TextNode{text: text}) do
     "{ type: 'text_node', text: '#{text}' }"
--- test/hologram/template_engine/ir_generator_text.exs
+++ test/hologram/template_engine/ir_generator_text.exs
@@ -1,8 +1,50 @@
 defmodule Hologram.TemplateEngine.IRGeneratorTest do
   use ExUnit.Case, async: true
 
-  alias Hologram.TemplateEngine.AST.TextNode
+  alias Hologram.TemplateEngine.AST.{TagNode, TextNode}
   alias Hologram.TemplateEngine.IRGenerator
+
+  describe "tag node" do
+    test "not attrs, no children" do
+      ast = %TagNode{tag: "div", attrs: %{}, children: []}
+
+      result = IRGenerator.generate(ast)
+      expected = "{ type: 'tag_node', tag: 'div', attrs: {}, children: [] }"
+
+      assert result == expected
+    end
+
+    test "has attrs" do
+      ast = %TagNode{tag: "div", attrs: %{"attr_1" => "value_1", "attr_2" => "value_2"}, children: []}
+
+      result = IRGenerator.generate(ast)
+      expected = "{ type: 'tag_node', tag: 'div', attrs: { 'attr_1': 'value_1', 'attr_2': 'value_2' }, children: [] }"
+
+      assert result == expected
+    end
+
+    test "has children" do
+      ast =
+        %TagNode{tag: "div", attrs: %{}, children: [
+          %TagNode{tag: "span", attrs: %{}, children: []},
+          %TagNode{tag: "h1", attrs: %{}, children: []}
+        ]}
+
+      result = IRGenerator.generate(ast)
+      expected = "{ type: 'tag_node', tag: 'div', attrs: {}, children: [{ type: 'tag_node', tag: 'span', attrs: {}, children: [] }, { type: 'tag_node', tag: 'h1', attrs: {}, children: [] }] }"
+
+      assert result == expected
+    end
+
+    test "attr name" do
+      ast = %TagNode{tag: "div", attrs: %{":click" => "test"}, children: []}
+
+      result = IRGenerator.generate(ast)
+      expected = "{ type: 'tag_node', tag: 'div', attrs: { 'holo-click': 'test' }, children: [] }"
+
+      assert result == expected
+    end
+  end
 
   test "text node" do
     ast = %TextNode{text: "test"}
