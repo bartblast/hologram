6a19ad9f78257ccf74f97d3067f1ef995eb62cbf
Bart Blast <bart@bartblast.com>
2021-05-12 12:53:17 +0200

    Refactor function definition generator module

--- lib/hologram/compiler/generators/function_definition_generator.ex
+++ lib/hologram/compiler/generators/function_definition_generator.ex
@@ -1,13 +1,10 @@
-# TODO: refactor & test
-
 defmodule Hologram.Compiler.FunctionDefinitionGenerator do
   alias Hologram.Compiler.AST.{AccessOperator, Variable}
   alias Hologram.Compiler.Generator
 
   def generate(name, variants, context) do
     body =
-      case generate_function_body(variants, context) do
-        # TODO: determine what's this case for, comment and test it
+      case generate_body(variants, context) do
         "" ->
           "{}\n"
 
@@ -18,71 +15,76 @@ defmodule Hologram.Compiler.FunctionDefinitionGenerator do
     "static #{name}() #{body}\n"
   end
 
-  defp generate_function_body(variants, context) do
-    valid_cases =
-      Enum.reduce(variants, "", fn variant, acc ->
-        statement = if acc == "", do: "if", else: "else if"
+  defp generate_body(variants, context) do
+    generate_body_valid_cases(variants, context) <> generate_body_invalid_case()
+  end
 
-        params = generate_function_params(variant, context)
-        vars = generate_function_vars(variant)
-        body = generate_function_expressions(variant, context)
-
-        code = """
-        #{statement} (Hologram.patternMatchFunctionArgs(#{params}, arguments)) {
-        #{vars}
-        #{body}
-        }
-        """
-
-        acc <> code
-      end)
-
-    invalid_case = """
+  defp generate_body_invalid_case do
+    """
     else {
     throw 'No match for the function call'
     }
     """
-
-    valid_cases <> invalid_case
   end
 
-  defp generate_function_expressions(variant, context) do
+  def generate_body_valid_cases(variants, context) do
+    Enum.reduce(variants, "", fn variant, acc ->
+      statement = if acc == "", do: "if", else: "else if"
+      params = generate_params(variant, context)
+      vars = generate_vars(variant)
+      body = generate_exprs(variant, context)
+
+      code = """
+      #{statement} (Hologram.patternMatchFunctionArgs(#{params}, arguments)) {
+      #{vars}
+      #{body}
+      }
+      """
+
+      acc <> code
+    end)
+  end
+
+  defp generate_expr(expr, idx, expr_count, context) do
+    return = if idx == expr_count - 1, do: "return ", else: ""
+    "#{return}#{Generator.generate(expr, context)};"
+  end
+
+  defp generate_exprs(variant, context) do
     expr_count = Enum.count(variant.body)
 
-    Stream.with_index(variant.body)
-    |> Enum.map(fn {expr, idx} ->
-      return = if idx == expr_count - 1, do: "return ", else: ""
-      "#{return}#{Generator.generate(expr, context)};"
-    end)
+    Enum.with_index(variant.body)
+    |> Enum.map(fn {expr, idx} -> generate_expr(expr, idx, expr_count, context) end)
     |> Enum.join("\n")
   end
 
-  defp generate_function_params(variant, context) do
+  defp generate_params(variant, context) do
     params =
-      Enum.map(variant.params, fn param -> Generator.generate(param, context, boxed: true) end)
+      Enum.map(variant.params, &Generator.generate(&1, context, boxed: true))
       |> Enum.join(", ")
 
-    "[ #{params} ]"
+    "[#{params}]"
   end
 
-  defp generate_function_vars(variant) do
-    Enum.map(variant.bindings, fn {name, {idx, binding}} ->
-      acc = "let #{List.last(binding).name} = "
+  defp generate_var({name, {idx, path}}) do
+    acc = "let #{name} = "
 
-      Enum.reduce(binding, acc, fn access, acc ->
-        acc
-        <>
-        case access do
-          %AccessOperator{key: key} ->
-            "['#{key}']"
-
-          %Variable{name: name} ->
-            "arguments[#{idx}]"
-        end
-      end)
+    Enum.reduce(path, acc, fn type, acc ->
+      acc
       <>
-      ";"
+      case type do
+        %AccessOperator{key: key} ->
+          "['#{key}']"
+
+        %Variable{name: name} ->
+          "arguments[#{idx}]"
+      end
     end)
+    <> ";"
+  end
+
+  defp generate_vars(variant) do
+    Enum.map(variant.bindings, &generate_var(&1))
     |> Enum.join("\n")
   end
 end
