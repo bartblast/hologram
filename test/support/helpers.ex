21eda2d01c8a2ba86156eef7b24689252878c9e0
Bart Blast <bart@bartblast.com>
2021-05-12 14:32:20 +0200

    Implement ir test helper

--- test/support/helpers.ex
+++ test/support/helpers.ex
@@ -1,9 +1,13 @@
 defmodule Hologram.Test.Helpers do
-  alias Hologram.Compiler.Normalizer
-  alias Hologram.Compiler.Parser
+  alias Hologram.Compiler.{Normalizer, Parser, Transformer}
 
   def ast(code) do
     Parser.parse!(code)
     |> Normalizer.normalize()
+  end
+
+  def ir(code) do
+    ast(code)
+    |> Transformer.transform()
   end
 end
