792cd6f2909cf222a09b912d2a4079c527e66356
Bart Blast <bart@bartblast.com>
2021-05-21 13:06:09 +0200

    Refactor template parser tests

--- test/hologram/template/parser_test.exs
+++ test/hologram/template/parser_test.exs
@@ -2,50 +2,90 @@ defmodule Hologram.Template.ParserTest do
   use Hologram.TestCase, async: true
   alias Hologram.Template.Parser
 
-  describe "parse/1" do
-    test "valid html" do
-      assert {:ok, _} = Parser.parse("<div></div>")
-    end
+  test "single node" do
+    html = "<div></div>"
 
-    # TODO: find invalid case
-    # test "invalid html" do
-    #   assert {:error, _} = Parser.parse("<div ")
-    # end
+    result = Parser.parse(html)
+    expected = {:ok, [{"div", [], []}]}
 
-    test "interpolation quotes fixing" do
-      html = """
-      <div class=\"test_class_1\" abc={{ @abc }} id=\"test_id_1\" bcd={{ @bcd }}>
-        <span class=\"test_class_2\" cde={{ @cde }} id=\"test_id_2\" def={{ @def }}></span>
-      </div>
-      """
+    assert result == expected
+  end
 
-      result = Parser.parse(html)
+  test "multiple nodes" do
+    html = "<div></div><span></span>"
 
-      expected =
-        {:ok,
-         [
-           {"div",
-            [
-              {"class", "test_class_1"},
-              {"abc", "{{ @abc }}"},
-              {"id", "test_id_1"},
-              {"bcd", "{{ @bcd }}"}
-            ],
-            [
-              "\n  ",
-              {"span",
-               [
-                 {"class", "test_class_2"},
-                 {"cde", "{{ @cde }}"},
-                 {"id", "test_id_2"},
-                 {"def", "{{ @def }}"}
-               ], []},
-              "\n"
-            ]},
-           "\n"
-         ]}
+    result = Parser.parse(html)
+    expected = {:ok, [{"div", [], []}, {"span", [], []}]}
 
-      assert result == expected
-    end
+    assert result == expected
+  end
+
+  test "attrs" do
+    html = "<div class=\"value_class\" id=\"value_id\"></div>"
+
+    result = Parser.parse(html)
+    expected = {:ok, [{"div", [{"class", "value_class", "id", "value_id"}], []}]}
+
+    assert result = expected
+  end
+
+  test "children" do
+    html = "<div><span></span><h1></h1></div>"
+
+    result = Parser.parse(html)
+    expected = {:ok, [{"div", [], [{"span", [], []}, {"h1", [], []}]}]}
+
+    assert result = expected
+  end
+
+  test "interpolation quotes fixing" do
+    html = """
+    <div class=\"test_class_1\" abc={{ @abc }} id=\"test_id_1\" bcd={{ @bcd }}>
+      <span class=\"test_class_2\" cde={{ @cde }} id=\"test_id_2\" def={{ @def }}></span>
+    </div>
+    """
+
+    result = Parser.parse(html)
+
+    expected =
+      {:ok,
+        [
+          {"div",
+          [
+            {"class", "test_class_1"},
+            {"abc", "{{ @abc }}"},
+            {"id", "test_id_1"},
+            {"bcd", "{{ @bcd }}"}
+          ],
+          [
+            "\n  ",
+            {"span",
+              [
+                {"class", "test_class_2"},
+                {"cde", "{{ @cde }}"},
+                {"id", "test_id_2"},
+                {"def", "{{ @def }}"}
+              ], []},
+            "\n"
+          ]},
+          "\n"
+        ]}
+
+    assert result == expected
+  end
+
+  test "invalid html" do
+    html = "<div"
+    result = Parser.parse(html)
+
+    expected =
+      {:error,
+        %Saxy.ParseError{
+          binary: "<root><div</root>",
+          position: 10,
+          reason: {:token, :name_start_char}
+        }}
+
+    assert result == expected
   end
 end
