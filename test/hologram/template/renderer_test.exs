a5bc7e8b8c9cadc9aaca8ba4ad7b01bc91cf77fd
Bart Blast <bart@bartblast.com>
2021-05-24 12:05:34 +0200

    Refactor template renderer tests

--- test/hologram/template/renderer_test.exs
+++ test/hologram/template/renderer_test.exs
@@ -1,23 +1,25 @@
 defmodule Hologram.Template.RendererTest do
   use Hologram.TestCase, async: true
 
-  alias Hologram.Template.VirtualDOM.Expression
-  alias Hologram.Template.VirtualDOM.TagNode
-  alias Hologram.Template.VirtualDOM.TextNode
-  alias Hologram.Template.Renderer
   alias Hologram.Compiler.IR.ModuleAttributeOperator
+  alias Hologram.Template.Renderer
+  alias Hologram.Template.VirtualDOM.{Expression, TagNode, TextNode}
+
+  setup do
+    [
+      state: %{}
+    ]
+  end
 
   describe "render/2" do
+    test "node list", %{state: state} do
+      nodes = [
+        %TextNode{text: "test_1"},
+        %TextNode{text: "test_2"}
+      ]
 
-
-    test "tag node" do
-    end
-
-    test "text node" do
-      virtual_dom = %TextNode{text: "test"}
-
-      result = Renderer.render(virtual_dom, %{})
-      expected = "test"
+      result = Renderer.render(nodes, state)
+      expected = "test_1test_2"
 
       assert result == expected
     end
@@ -32,11 +34,20 @@ defmodule Hologram.Template.RendererTest do
       assert result == expected
     end
 
-    test "attribute name" do
-      node = %TagNode{attrs: %{":click" => "test"}, children: [], tag: "div"}
+    test "tag node", %{state: state} do
+      virtual_dom = %TagNode{tag: "div", attrs: %{}, children: []}
 
-      result = Renderer.render(node, %{})
-      expected = "<div holo-click=\"test\"></div>"
+      result = Renderer.render(virtual_dom, state)
+      expected = "<div></div>"
+
+      assert result == expected
+    end
+
+    test "text node", %{state: state} do
+      virtual_dom = %TextNode{text: "test"}
+
+      result = Renderer.render(virtual_dom)
+      expected = "test"
 
       assert result == expected
     end
