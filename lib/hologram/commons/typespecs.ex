ac89997fc19bd04a2bd4a8fd5268fabfab36b08d
Bart Blast <bart@bartblast.com>
2021-07-01 14:26:55 +0200

    Specify modules_map type

--- lib/hologram/commons/typespecs.ex
+++ lib/hologram/commons/typespecs.ex
@@ -1,6 +1,13 @@
 defmodule Hologram.Typespecs do
+  alias Hologram.Compiler.IR.ModuleDefinition
+
   @typedoc """
-  e.g. [:Hologram, :Compiler, :Helpers]
+  e.g. %{[:Hologram, :Typespecs] => %ModuleDefinition{}}
+  """
+  @type modules_map :: %{module_segments => %ModuleDefinition{}}
+
+  @typedoc """
+  e.g. [:Hologram, :Typespecs]
   """
   @type module_segments :: list(atom())
 end
