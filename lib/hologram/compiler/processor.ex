0b45db7956036499cb557f3e3219f23de1e4dc11
Bart Blast <bart@bartblast.com>
2021-07-01 14:39:26 +0200

    Refactor and document processor module

--- lib/hologram/compiler/processor.ex
+++ lib/hologram/compiler/processor.ex
@@ -1,25 +1,32 @@
 defmodule Hologram.Compiler.Processor do
-  alias Hologram.Compiler.Helpers
-  alias Hologram.Compiler.Normalizer
-  alias Hologram.Compiler.Parser
-  alias Hologram.Compiler.Transformer
+  alias Hologram.Compiler.{Helpers, Normalizer, Parser, Transformer}
+  alias Hologram.Compiler.IR.ModuleDefinition
+  alias Hologram.Typespecs, as: T
+
+  @doc """
+  Creates the module definitions map of modules used by the given module.
+  """
+  @spec compile(T.module_segments, map()) :: T.modules_map
 
   def compile(module, acc \\ %{}) do
-    fully_qualified_module = Helpers.fully_qualified_module(module)
-    source = fully_qualified_module.module_info()[:compile][:source]
-
-    ir =
-      Parser.parse_file!(source)
+    module_definition =
+      Helpers.module_source_path(module)
+      |> Parser.parse_file!()
       |> Normalizer.normalize()
       |> Transformer.transform()
 
-    Map.put(acc, module, ir)
-    |> compile_directives(ir, :imports)
-    |> compile_directives(ir, :aliases)
+    Map.put(acc, module, module_definition)
+    |> compile_referenced_modules(module_definition, :imports)
+    |> compile_referenced_modules(module_definition, :aliases)
   end
 
-  defp compile_directives(acc, current_module, directive_key) do
-    Map.get(current_module, directive_key)
+  @doc """
+  Compiles modules which are referenced by directives of a specific type.
+  """
+  @spec compile_referenced_modules(T.modules_map, %ModuleDefinition{}, :aliases | :imports) :: T.modules_map
+  
+  defp compile_referenced_modules(acc, module_definition, directive_type) do
+    Map.get(module_definition, directive_type)
     |> Enum.reduce(acc, fn directive, acc ->
       if Map.has_key?(acc, directive.module) do
         acc
