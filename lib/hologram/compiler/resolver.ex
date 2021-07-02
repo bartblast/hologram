580bf3ff0ef65ed5085f1e254cc8d4aec6b07e6a
Bart Blast <bart@bartblast.com>
2021-07-02 13:06:42 +0200

    Refactor and document resolver

--- lib/hologram/compiler/resolver.ex
+++ lib/hologram/compiler/resolver.ex
@@ -1,9 +1,15 @@
 defmodule Hologram.Compiler.Resolver do
+  alias Hologram.Compiler.IR.Alias
+  alias Hologram.Compiler.Typespecs, as: T
+
   @doc """
-  Resolves module.
+  Returns the module name segments of a matching aliased module.
+  If no matching alias is found, it returns the given module name segments.
   """
-  def resolve(module, aliases) do
-    resolved = Enum.find(aliases, &(&1.as == module))
-    if resolved, do: resolved.module, else: module
+  @spec resolve(T.module_name_segments, list(%Alias{})) :: T.module_name_segments
+
+  def resolve(module_name_segments, aliases) do
+    resolved = Enum.find(aliases, &(&1.as == module_name_segments))
+    if resolved, do: resolved.module, else: module_name_segments
   end
 end
