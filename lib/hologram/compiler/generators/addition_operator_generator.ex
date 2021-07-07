764188b7982f57fd4936407557ccb2681bef49de
Bart Blast <bart@bartblast.com>
2021-07-07 18:49:46 +0200

    Use default value for opts param

--- lib/hologram/compiler/generators/addition_operator_generator.ex
+++ lib/hologram/compiler/generators/addition_operator_generator.ex
@@ -2,8 +2,8 @@ defmodule Hologram.Compiler.AdditionOperatorGenerator do
   alias Hologram.Compiler.{Context, Generator}
 
   def generate(left, right, %Context{} = context) do
-    left = Generator.generate(left, context, [])
-    right = Generator.generate(right, context, [])
+    left = Generator.generate(left, context)
+    right = Generator.generate(right, context)
 
     "Kernel.$add(#{left}, #{right})"
   end
