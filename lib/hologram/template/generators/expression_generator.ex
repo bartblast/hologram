6efbd39e3d8a3c03e65f3fc75415e42ff4f8b01d
Bart Blast <bart@bartblast.com>
2021-07-08 14:24:38 +0200

    Change a defer into a todo

--- lib/hologram/template/generators/expression_generator.ex
+++ lib/hologram/template/generators/expression_generator.ex
@@ -2,7 +2,7 @@ defmodule Hologram.Template.ExpressionGenerator do
   alias Hologram.Compiler.{Context, Generator}
 
   def generate(ir) do
-    # DEFER: pass actual %Context{} struct received from compiler
+    # TODO: pass actual %Context{} struct received from compiler
     context = %Context{module: [], uses: [], imports: [], aliases: [], attributes: []}
 
     callback_return = Generator.generate(ir, context)
