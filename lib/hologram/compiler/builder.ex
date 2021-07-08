773385f28ac13cce5bb12319e5acd6be27b9dbf5
Bart Blast <bart@bartblast.com>
2021-07-08 15:50:10 +0200

    Fix compiler builder

--- lib/hologram/compiler/builder.ex
+++ lib/hologram/compiler/builder.ex
@@ -1,11 +1,14 @@
 defmodule Hologram.Compiler.Builder do
-  alias Hologram.Compiler.{Generator, Processor, Pruner}
+  alias Hologram.Compiler.{Context, Generator, Processor, Pruner}
 
   def build(module) do
+    # TODO: pass actual %Context{} struct received from compiler
+    context = %Context{module: [], uses: [], imports: [], aliases: [], attributes: []}
+
     Processor.compile(module)
     |> Pruner.prune()
     |> Enum.reduce("", fn {_, ir}, acc ->
-      acc <> "\n" <> Generator.generate(ir)
+      acc <> "\n" <> Generator.generate(ir, context)
     end)
   end
 end
