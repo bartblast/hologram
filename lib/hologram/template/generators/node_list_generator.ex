10a710efb980f52b3109dd827e16730c3f20fe79
Bart Blast <bart@bartblast.com>
2021-07-08 14:15:21 +0200

    Simplify template node list generator

--- lib/hologram/template/generators/node_list_generator.ex
+++ lib/hologram/template/generators/node_list_generator.ex
@@ -1,25 +1,11 @@
 defmodule Hologram.Template.NodeListGenerator do
-  alias Hologram.Compiler.{Normalizer, Transformer}
-  alias Hologram.Compiler.IR.ModuleAttributeDefinition
   alias Hologram.Template.Generator
 
-  def generate(nodes, state) do
-    module_attributes =
-      Enum.map(state, fn {key, value} ->
-        value =
-          Macro.escape(value)
-          |> Normalizer.normalize()
-          |> Transformer.transform()
-
-        %ModuleAttributeDefinition{name: key, value: value}
-      end)
-
-    context = [module_attributes: module_attributes]
-
-    nodes_js =
-      Enum.map(nodes, &Generator.generate(&1, context))
+  def generate(nodes) do
+    js =
+      Enum.map(nodes, &Generator.generate/1)
       |> Enum.join(", ")
 
-    "[#{nodes_js}]"
+    "[#{js}]"
   end
 end
--- test/hologram/template/generators/node_list_generator_test.exs
+++ test/hologram/template/generators/node_list_generator_test.exs
@@ -1,18 +1,16 @@
 defmodule Hologram.Template.NodeListGeneratorTest do
   use Hologram.TestCase, async: true
 
+  alias Hologram.Template.Document.TextNode
   alias Hologram.Template.NodeListGenerator
-  alias Hologram.Template.VirtualDOM.TextNode
 
-  test "generate/2" do
+  test "generate/1" do
     nodes = [
       %TextNode{content: "test_1"},
       %TextNode{content: "test_2"}
     ]
 
-    state = %{}
-
-    result = NodeListGenerator.generate(nodes, state)
+    result = NodeListGenerator.generate(nodes)
     expected = "[{ type: 'text', content: 'test_1' }, { type: 'text', content: 'test_2' }]"
 
     assert result == expected
