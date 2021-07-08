de3a67df383ee3fd7ab6e9c6994af30d798f3cb6
Bart Blast <bart@bartblast.com>
2021-07-08 14:28:29 +0200

    Update transformer doc

--- lib/hologram/template/transformer.ex
+++ lib/hologram/template/transformer.ex
@@ -6,7 +6,7 @@ defmodule Hologram.Template.Transformer do
   alias Hologram.Typespecs, as: T
 
   @doc """
-  Builds virtual DOM from parsed HTML.
+  Builds document tree template from parsed markup.
   Interpolates expression nodes in text nodes and attribute values.
 
   ## Examples
