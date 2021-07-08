50a5447f0b86b68cb71da9de04648b7475ab788f
Bart Blast <bart@bartblast.com>
2021-07-08 15:52:22 +0200

    Fix transformer and generator calls in hydrator

--- lib/hologram/compiler/hydrator.ex
+++ lib/hologram/compiler/hydrator.ex
@@ -1,12 +1,13 @@
 defmodule Hologram.Compiler.Hydrator do
-  alias Hologram.Compiler.Generator
-  alias Hologram.Compiler.Normalizer
-  alias Hologram.Compiler.Transformer
+  alias Hologram.Compiler.{Context, Generator, Normalizer, Transformer}
 
   def hydrate(state) do
+    # TODO: pass actual %Context{} struct received from compiler
+    context = %Context{module: [], uses: [], imports: [], aliases: [], attributes: []}
+
     Macro.escape(state)
     |> Normalizer.normalize()
-    |> Transformer.transform()
-    |> Generator.generate()
+    |> Transformer.transform(context)
+    |> Generator.generate(context)
   end
 end
