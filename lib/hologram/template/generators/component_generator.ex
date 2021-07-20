7a950ea175249c100c23e41e8ff6ad7a0c78306d
Bart Blast <bart@bartblast.com>
2021-07-20 14:28:10 +0200

    Change type of component generator param

--- lib/hologram/template/generators/component_generator.ex
+++ lib/hologram/template/generators/component_generator.ex
@@ -2,10 +2,8 @@ defmodule Hologram.Template.ComponentGenerator do
   alias Hologram.Compiler.Helpers
   alias Hologram.Template.{Builder, Generator}
 
-  def generate(module_name_segs) do
-    # DEFER: allow to use module() type as param to module_name/1
-    module = Helpers.module(module_name_segs)
-    module_name = Helpers.module_name(module_name_segs)
+  def generate(module) do
+    module_name = Helpers.module_name(module)
 
     children_js =
       Builder.build(module)
--- test/hologram/template/generators/component_generator_test.exs
+++ test/hologram/template/generators/component_generator_test.exs
@@ -3,7 +3,7 @@ defmodule Hologram.Template.ComponentGeneratorTest do
   alias Hologram.Template.ComponentGenerator
 
   test "generate/1" do
-    module = [:Hologram, :Test, :Fixtures, :Template, :ComponentGenerator, :Module1]
+    module = Hologram.Test.Fixtures.Template.ComponentGenerator.Module1
 
     result = ComponentGenerator.generate(module)
     expected = "{ type: 'component', module: 'Hologram.Test.Fixtures.Template.ComponentGenerator.Module1', children: [{ type: 'element', tag: 'div', attrs: {}, children: [{ type: 'text', content: 'test' }] }] }"
