1f2e0f363210e0a4219555057b4fd2889fd7c24a
Bart Blast <bart@bartblast.com>
2021-07-23 20:58:03 +0200

    Test nested tuple generation

--- test/hologram/compiler/generators/tuple_type_generator_test.exs
+++ test/hologram/compiler/generators/tuple_type_generator_test.exs
@@ -2,7 +2,7 @@ defmodule Hologram.Compiler.TupleTypeGeneratorTest do
   use Hologram.TestCase, async: true
 
   alias Hologram.Compiler.{Context, TupleTypeGenerator}
-  alias Hologram.Compiler.IR.IntegerType
+  alias Hologram.Compiler.IR.{IntegerType, TupleType}
 
   @context %Context{module: nil, uses: [], imports: [], aliases: [], attributes: []}
   @opts []
@@ -19,6 +19,23 @@ defmodule Hologram.Compiler.TupleTypeGeneratorTest do
 
     result = TupleTypeGenerator.generate(data, @context, @opts)
     expected = "{ type: 'tuple', data: [ { type: 'integer', value: 1 }, { type: 'integer', value: 2 } ] }"
+
+    assert result == expected
+  end
+
+  test "nested tuple" do
+    data = [
+      %IntegerType{value: 1},
+      %TupleType{
+        data: [
+          %IntegerType{value: 2},
+          %IntegerType{value: 3}
+        ]
+      }
+    ]
+
+    result = TupleTypeGenerator.generate(data, @context, @opts)
+    expected = "{ type: 'tuple', data: [ { type: 'integer', value: 1 }, { type: 'tuple', data: [ { type: 'integer', value: 2 }, { type: 'integer', value: 3 } ] } ] }"
 
     assert result == expected
   end
