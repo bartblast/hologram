b5ff4446b7756efb07e8a36277a681efd5ede53f
Bart Blast <bart@bartblast.com>
2021-07-27 20:23:26 +0200

    Decode integers

--- lib/hologram/compiler/decoder.ex
+++ lib/hologram/compiler/decoder.ex
@@ -1,11 +1,15 @@
 defmodule Hologram.Compiler.Decoder do
-  alias Hologram.Compiler.MapTypeDecoder
+  alias Hologram.Compiler.{ListTypeDecoder, MapTypeDecoder}
 
-  def decode(%{"type" => "string", "value" => value}) do
+  def decode(%{"type" => "integer", "value" => value}) do
     value
   end
 
   def decode(%{"type" => "map", "data" => data}) do
     MapTypeDecoder.decode(data)
+  end
+
+  def decode(%{"type" => "string", "value" => value}) do
+    value
   end
 end
--- test/hologram/compiler/decoder_test.exs
+++ test/hologram/compiler/decoder_test.exs
@@ -2,6 +2,11 @@ defmodule Hologram.Compiler.DecoderTest do
   use Hologram.TestCase, async: true
   alias Hologram.Compiler.Decoder
 
+  test "integer" do
+    input = %{"type" => "integer", "value" => 1}
+    assert Decoder.decode(input) == 1
+  end
+
   test "string" do
     input = %{"type" => "string", "value" => "test"}
     assert Decoder.decode(input) == "test"
