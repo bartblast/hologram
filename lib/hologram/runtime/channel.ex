7a7724cf17d84be209ec7632b68221c3350996fd
Bart Blast <bart@bartblast.com>
2021-07-27 15:10:21 +0200

    Return context in channel response

--- lib/hologram/runtime/channel.ex
+++ lib/hologram/runtime/channel.ex
@@ -8,21 +8,21 @@ defmodule Hologram.Runtime.Channel do
     {:ok, socket}
   end
 
-  def handle_in("command", payload, socket) do
+  def handle_in("command", %{"context" => context} = payload, socket) do
     response =
       execute_command(payload)
-      |> build_response()
+      |> build_response(context)
       |> Serializer.serialize()
 
     {:reply, {:ok, response}, socket}
   end
 
-  defp build_response({action, params}) do
-    {action, Enum.into(params, %{})}
+  defp build_response({action, params}, context) do
+    {action, Enum.into(params, %{}), context}
   end
 
-  defp build_response(action) do
-    {action, %{}}
+  defp build_response(action, context) do
+    {action, %{}, context}
   end
 
   defp execute_command(%{"command" => command, "params" => params, "context" => context}) do
--- test/hologram/runtime/channel_test.exs
+++ test/hologram/runtime/channel_test.exs
@@ -12,65 +12,85 @@ defmodule Hologram.Runtime.ChannelTest do
 
   test "command without params that returns action without params", %{socket: socket} do
     page_module = "Elixir_Hologram_Test_Fixtures_Runtime_Module1"
+    scope_module = "Elixir_Hologram_Test_Fixtures_Runtime_Module2"
+    context = %{"page_module" => page_module, "scope_module" => scope_module}
 
     message =
       %{
         command: "test_command",
-        context: %{"page_module" => page_module, "scope_module" => page_module},
+        context: context,
         params: %{}
       }
 
-    expected_response = Serializer.serialize({:test_action, %{}})
-
     ref = push(socket, "command", message)
+
+    expected_response =
+      {:test_action, %{}, context}
+      |> Serializer.serialize()
+
     assert_reply ref, :ok, expected_response
   end
 
   test "command without params that returns action with params", %{socket: socket} do
     page_module = "Elixir_Hologram_Test_Fixtures_Runtime_Module3"
+    scope_module = "Elixir_Hologram_Test_Fixtures_Runtime_Module2"
+    context = %{"page_module" => page_module, "scope_module" => scope_module}
 
     message =
       %{
         command: "test_command",
-        context: %{"page_module" => page_module, "scope_module" => page_module},
+        context: context,
         params: %{}
       }
 
-    expected_response = Serializer.serialize({:test_action, %{a: 1, b: 2}})
-
     ref = push(socket, "command", message)
+
+    expected_response =
+      {:test_action, %{a: 1, b: 2}, context}
+      |> Serializer.serialize()
+
     assert_reply ref, :ok, expected_response
   end
 
   test "command with params that returns action without params", %{socket: socket} do
     page_module = "Elixir_Hologram_Test_Fixtures_Runtime_Module4"
+    scope_module = "Elixir_Hologram_Test_Fixtures_Runtime_Module2"
+    context = %{"page_module" => page_module, "scope_module" => scope_module}
 
     message =
       %{
         command: "test_command",
-        context: %{"page_module" => page_module, "scope_module" => page_module},
+        context: context,
         params: %{"a" => 1, "b" => 2}
       }
 
-    expected_response = Serializer.serialize({:test_action_1, %{}})
-
     ref = push(socket, "command", message)
+
+    expected_response =
+      {:test_action_1, %{}, context}
+      |> Serializer.serialize()
+
     assert_reply ref, :ok, expected_response
   end
 
   test "command with params that returns action with params", %{socket: socket} do
     page_module = "Elixir_Hologram_Test_Fixtures_Runtime_Module2"
+    scope_module = "Elixir_Hologram_Test_Fixtures_Runtime_Module3"
+    context = %{"page_module" => page_module, "scope_module" => scope_module}
 
     message =
       %{
         command: "test_command",
-        context: %{"page_module" => page_module, "scope_module" => page_module},
+        context: context,
         params: %{"a" => 1, "b" => 2}
       }
 
-    expected_response = Serializer.serialize({:test_action, %{a: 10, b: 20}})
-
     ref = push(socket, "command", message)
+
+    expected_response =
+      {:test_action, %{a: 10, b: 20}, context}
+      |> Serializer.serialize()
+
     assert_reply ref, :ok, expected_response
   end
 end
