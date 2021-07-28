52caa49c09a9b0e506179f03c20a2ac5f6af247f
Bart Blast <bart@bartblast.com>
2021-07-28 15:02:50 +0200

    Refactor tuple type generate and its tests

--- lib/hologram/compiler/generators/tuple_type_generator.ex
+++ lib/hologram/compiler/generators/tuple_type_generator.ex
@@ -1,19 +1,8 @@
 defmodule Hologram.Compiler.TupleTypeGenerator do
+  use Hologram.Compiler.TypeEncoder
   alias Hologram.Compiler.{Context, Generator}
 
   def generate(data, %Context{} = context, opts) do
-    "{ type: 'tuple', data: #{generate_data(data, context, opts)} }"
-  end
-
-  def generate_data(data, %Context{} = context, opts) do
-    fields =
-      Enum.map(data, &"#{Generator.generate(&1, context, opts)}")
-      |> Enum.join(", ")
-
-    if fields != "" do
-      "[ #{fields} ]"
-    else
-      "[]"
-    end
+    "{ type: 'tuple', data: #{encode_as_list(data, context, opts)} }"
   end
 end
--- test/hologram/compiler/generators/tuple_type_generator_test.exs
+++ test/hologram/compiler/generators/tuple_type_generator_test.exs
@@ -7,35 +7,11 @@ defmodule Hologram.Compiler.TupleTypeGeneratorTest do
   @context %Context{module: nil, uses: [], imports: [], aliases: [], attributes: []}
   @opts []
 
-  test "empty tuple" do
-    result = TupleTypeGenerator.generate([], @context, @opts)
-    expected = "{ type: 'tuple', data: [] }"
-
-    assert result == expected
-  end
-
-  test "non-empty tuple" do
+  test "generate/3" do
     data = [%IntegerType{value: 1}, %IntegerType{value: 2}]
 
     result = TupleTypeGenerator.generate(data, @context, @opts)
     expected = "{ type: 'tuple', data: [ { type: 'integer', value: 1 }, { type: 'integer', value: 2 } ] }"
-
-    assert result == expected
-  end
-
-  test "nested tuple" do
-    data = [
-      %IntegerType{value: 1},
-      %TupleType{
-        data: [
-          %IntegerType{value: 2},
-          %IntegerType{value: 3}
-        ]
-      }
-    ]
-
-    result = TupleTypeGenerator.generate(data, @context, @opts)
-    expected = "{ type: 'tuple', data: [ { type: 'integer', value: 1 }, { type: 'tuple', data: [ { type: 'integer', value: 2 }, { type: 'integer', value: 3 } ] } ] }"
 
     assert result == expected
   end
