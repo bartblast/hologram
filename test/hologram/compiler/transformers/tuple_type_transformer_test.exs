956e0f7a9426bd1cf38a6e0a0443ae6fb839c0a5
Bart Blast <bart@bartblast.com>
2021-07-28 15:35:44 +0200

    Simplify tuple type transformer tests

--- test/hologram/compiler/transformers/tuple_type_transformer_test.exs
+++ test/hologram/compiler/transformers/tuple_type_transformer_test.exs
@@ -12,27 +12,7 @@ defmodule Hologram.Compiler.TupleTypeTransformerTest do
     attributes: []
   }
 
-  test "empty tuple" do
-    code = "{}"
-    {:{}, _, list} = ast(code)
-
-    result = TupleTypeTransformer.transform(list, @context)
-    expected = %TupleType{data: []}
-
-    assert result == expected
-  end
-
-  test "1 element tuple" do
-    code = "{}"
-    {:{}, _, list} = ast(code)
-
-    result = TupleTypeTransformer.transform(list, @context)
-    expected = %TupleType{data: []}
-
-    assert result == expected
-  end
-
-  test "2 element tuple" do
+  test "transform/2" do
     code = "{1, 2}"
     ast = ast(code)
 
@@ -43,54 +23,6 @@ defmodule Hologram.Compiler.TupleTypeTransformerTest do
         data: [
           %IntegerType{value: 1},
           %IntegerType{value: 2}
-        ]
-      }
-
-    assert result == expected
-  end
-
-  test "3 element tuple" do
-    code = "{1, 2, 3}"
-    {:{}, _, list} = ast(code)
-
-    result = TupleTypeTransformer.transform(list, @context)
-
-    expected =
-      %TupleType{
-        data: [
-          %IntegerType{value: 1},
-          %IntegerType{value: 2},
-          %IntegerType{value: 3}
-        ]
-      }
-
-    assert result == expected
-  end
-
-  test "nested tuple" do
-    code = "{1, {2, {3, 4, 5}, 6}, 7}"
-    {:{}, _, list} = ast(code)
-
-    result = TupleTypeTransformer.transform(list, @context)
-
-    expected =
-      %TupleType{
-        data: [
-          %IntegerType{value: 1},
-          %TupleType{
-            data: [
-              %IntegerType{value: 2},
-              %TupleType{
-                data: [
-                  %IntegerType{value: 3},
-                  %IntegerType{value: 4},
-                  %IntegerType{value: 5}
-                ]
-              },
-              %IntegerType{value: 6}
-            ]
-          },
-          %IntegerType{value: 7}
         ]
       }
 
