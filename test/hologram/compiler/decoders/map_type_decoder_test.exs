439b3cdc1e2db2ee2ef8fd93a00cd02477a6d025
Bart Blast <bart@bartblast.com>
2021-07-28 15:27:39 +0200

    Simplify map type decoder tests

--- test/hologram/compiler/decoders/map_type_decoder_test.exs
+++ test/hologram/compiler/decoders/map_type_decoder_test.exs
@@ -2,14 +2,7 @@ defmodule Hologram.Compiler.MapTypeDecoderTest do
   use Hologram.TestCase, async: true
   alias Hologram.Compiler.MapTypeDecoder
 
-  test "empty map" do
-    input = %{}
-    result = MapTypeDecoder.decode(input)
-
-    assert result == %{}
-  end
-
-  test "non-empty, not-nested map" do
+  test "decode/1" do
     input = %{
       "~string[test_key]" => %{
         "type" => "string",
@@ -19,32 +12,6 @@ defmodule Hologram.Compiler.MapTypeDecoderTest do
 
     result = MapTypeDecoder.decode(input)
     expected = %{"test_key" => "test_value"}
-
-    assert result == expected
-  end
-
-  test "nested map" do
-    input =
-      %{
-        "~string[test_key_1]" => %{
-          "type" => "map",
-          "data" => %{
-            "~string[test_key_2]" => %{
-              "type" => "string",
-              "value" => "test_value"
-            }
-          }
-        }
-      }
-
-    result = MapTypeDecoder.decode(input)
-
-    expected =
-      %{
-        "test_key_1" => %{
-          "test_key_2" => "test_value"
-        }
-      }
 
     assert result == expected
   end
