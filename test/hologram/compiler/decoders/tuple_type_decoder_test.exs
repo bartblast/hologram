fa1a481affcad543c220e35ae7c541e9889e3232
Bart Blast <bart@bartblast.com>
2021-07-28 15:28:52 +0200

    Simplify tuple type decoder tests

--- test/hologram/compiler/decoders/tuple_type_decoder_test.exs
+++ test/hologram/compiler/decoders/tuple_type_decoder_test.exs
@@ -2,14 +2,7 @@ defmodule Hologram.Compiler.TupleTypeDecoderTest do
   use Hologram.TestCase, async: true
   alias Hologram.Compiler.TupleTypeDecoder
 
-  test "empty tuple" do
-    input = []
-    result = TupleTypeDecoder.decode(input)
-
-    assert result == {}
-  end
-
-  test "non-empty, not-nested tuple" do
+  test "decode/1" do
     input = [
       %{"type" => "integer", "value" => 1},
       %{"type" => "atom", "value" => "test"}
@@ -17,24 +10,6 @@ defmodule Hologram.Compiler.TupleTypeDecoderTest do
 
     result = TupleTypeDecoder.decode(input)
     expected = {1, :test}
-
-    assert result == expected
-  end
-
-  test "nested tuple" do
-    input = [
-      %{"type" => "integer", "value" => 1},
-      %{
-        "type" => "tuple",
-        "data" => [
-          %{"type" => "integer", "value" => 2},
-          %{"type" => "integer", "value" => 3}
-        ]
-      }
-    ]
-
-    result = TupleTypeDecoder.decode(input)
-    expected = {1, {2, 3}}
 
     assert result == expected
   end
