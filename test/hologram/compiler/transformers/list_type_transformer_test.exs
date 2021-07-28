464f61e7fb4b6339dabeebff8c6c8106e0f1b5b0
Bart Blast <bart@bartblast.com>
2021-07-28 15:32:06 +0200

    Simplify list type transformer tests

--- test/hologram/compiler/transformers/list_type_transformer_test.exs
+++ test/hologram/compiler/transformers/list_type_transformer_test.exs
@@ -12,17 +12,7 @@ defmodule Hologram.Compiler.ListTypeTransformerTest do
     attributes: []
   }
 
-  test "empty list" do
-    code = "[]"
-    ast = ast(code)
-
-    result = ListTypeTransformer.transform(ast, @context)
-    expected = %ListType{data: []}
-
-    assert result == expected
-  end
-
-  test "non-nested list" do
+  test "transform/2" do
     code = "[1, 2]"
     ast = ast(code)
 
@@ -32,32 +22,6 @@ defmodule Hologram.Compiler.ListTypeTransformerTest do
       data: [
         %IntegerType{value: 1},
         %IntegerType{value: 2}
-      ]
-    }
-
-    assert result == expected
-  end
-
-  test "nested list" do
-    code = "[1, [2, [3, 4]]]"
-    ast = ast(code)
-
-    result = ListTypeTransformer.transform(ast, @context)
-
-    expected = %ListType{
-      data: [
-        %IntegerType{value: 1},
-        %ListType{
-          data: [
-            %IntegerType{value: 2},
-            %ListType{
-              data: [
-                %IntegerType{value: 3},
-                %IntegerType{value: 4}
-              ]
-            }
-          ]
-        }
       ]
     }
 
