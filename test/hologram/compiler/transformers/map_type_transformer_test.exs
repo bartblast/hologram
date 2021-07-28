535b510cec1fe6a45cd991e17e22c588b0e7f762
Bart Blast <bart@bartblast.com>
2021-07-28 15:33:11 +0200

    Simplify map type transformer tests

--- test/hologram/compiler/transformers/map_type_transformer_test.exs
+++ test/hologram/compiler/transformers/map_type_transformer_test.exs
@@ -12,17 +12,7 @@ defmodule Hologram.Compiler.MapTypeTransformerTest do
     attributes: []
   }
 
-  test "empty map" do
-    code = "%{}"
-    {:%{}, _, ast} = ast(code)
-
-    result = MapTypeTransformer.transform(ast, @context)
-    expected = %MapType{data: []}
-
-    assert result == expected
-  end
-
-  test "non-nested map" do
+  test "transform/2" do
     code = "%{a: 1, b: 2}"
     {:%{}, _, ast} = ast(code)
 
@@ -32,34 +22,6 @@ defmodule Hologram.Compiler.MapTypeTransformerTest do
       data: [
         {%AtomType{value: :a}, %IntegerType{value: 1}},
         {%AtomType{value: :b}, %IntegerType{value: 2}}
-      ]
-    }
-
-    assert result == expected
-  end
-
-  test "nested map" do
-    code = "%{a: 1, b: %{c: 2, d: %{e: 3, f: 4}}}"
-    {:%{}, _, ast} = ast(code)
-
-    result = MapTypeTransformer.transform(ast, @context)
-
-    expected = %MapType{
-      data: [
-        {%AtomType{value: :a}, %IntegerType{value: 1}},
-        {%AtomType{value: :b},
-         %MapType{
-           data: [
-             {%AtomType{value: :c}, %IntegerType{value: 2}},
-             {%AtomType{value: :d},
-              %MapType{
-                data: [
-                  {%AtomType{value: :e}, %IntegerType{value: 3}},
-                  {%AtomType{value: :f}, %IntegerType{value: 4}}
-                ]
-              }}
-           ]
-         }}
       ]
     }
 
