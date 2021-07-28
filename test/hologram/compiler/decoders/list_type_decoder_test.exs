c83d08d8dabacf1afd61ccbccd8ac2d282f57bd6
Bart Blast <bart@bartblast.com>
2021-07-28 15:23:58 +0200

    Simplify list type decoder tests

--- test/hologram/compiler/decoders/list_type_decoder_test.exs
+++ test/hologram/compiler/decoders/list_type_decoder_test.exs
@@ -2,14 +2,7 @@ defmodule Hologram.Compiler.ListTypeDecoderTest do
   use Hologram.TestCase, async: true
   alias Hologram.Compiler.ListTypeDecoder
 
-  test "empty list" do
-    input = []
-    result = ListTypeDecoder.decode(input)
-
-    assert result == []
-  end
-
-  test "non-empty, not-nested list" do
+  test "decode/1" do
     input = [
       %{"type" => "integer", "value" => 1},
       %{"type" => "atom", "value" => "test"}
@@ -17,24 +10,6 @@ defmodule Hologram.Compiler.ListTypeDecoderTest do
 
     result = ListTypeDecoder.decode(input)
     expected = [1, :test]
-
-    assert result == expected
-  end
-
-  test "nested list" do
-    input = [
-      %{"type" => "integer", "value" => 1},
-      %{
-        "type" => "list",
-        "data" => [
-          %{"type" => "integer", "value" => 2},
-          %{"type" => "integer", "value" => 3}
-        ]
-      }
-    ]
-
-    result = ListTypeDecoder.decode(input)
-    expected = [1, [2, 3]]
 
     assert result == expected
   end
