48a3602a2c062e22499953d86f6fc679939eb99f
Bart Blast <bart@bartblast.com>
2021-08-02 23:35:28 +0200

    Implement module attribute generator in template mode

--- lib/hologram/compiler/generators/module_attribute_operator_generator.ex
+++ lib/hologram/compiler/generators/module_attribute_operator_generator.ex
@@ -1,8 +1,13 @@
 defmodule Hologram.Compiler.ModuleAttributeOperatorGenerator do
-  alias Hologram.Compiler.{Context, MapKeyGenerator}
+  alias Hologram.Compiler.{Context, Helpers, MapKeyGenerator, Opts}
   alias Hologram.Compiler.IR.AtomType
 
-  def generate(name, %Context{} = context) do
+  def generate(name, %Context{} = context, %Opts{template: true}) do
+    class_name = Helpers.class_name(context.module)
+    "#{class_name}.$#{name}"
+  end
+
+  def generate(name, %Context{} = context, %Opts{} = opts) do
     key = MapKeyGenerator.generate(%AtomType{value: name}, context)
     "$state.data['#{key}']"
   end
--- test/hologram/compiler/generators/module_attribute_operator_generator_test.exs
+++ test/hologram/compiler/generators/module_attribute_operator_generator_test.exs
@@ -1,9 +1,19 @@
 defmodule Hologram.Compiler.ModuleAttributeOperatorGeneratorTest do
   use Hologram.TestCase, async: true
-  alias Hologram.Compiler.{Context, ModuleAttributeOperatorGenerator}
+  alias Hologram.Compiler.{Context, ModuleAttributeOperatorGenerator, Opts}
 
-  test "generate/2" do
-    result = ModuleAttributeOperatorGenerator.generate(:xyz, %Context{})
+  test "template opt" do
+    context = %Context{module: Hologram.Compiler.ModuleAttributeOperatorGeneratorTest}
+    opts = %Opts{template: true}
+
+    result = ModuleAttributeOperatorGenerator.generate(:xyz, context, opts)
+    expected = "Elixir_Hologram_Compiler_ModuleAttributeOperatorGeneratorTest.$xyz"
+
+    assert result == expected
+  end
+
+  test "no template opt" do
+    result = ModuleAttributeOperatorGenerator.generate(:xyz, %Context{}, %Opts{})
     expected = "$state.data['~atom[xyz]']"
 
     assert result == expected
