65c60d7d6ffcbd9c5c95b4a71b4a07bb52108fad
Bart Blast <bart@bartblast.com>
2021-08-03 20:02:32 +0200

    Implement module attribute definition encoder

---
+++ lib/hologram/compiler/encoders/module_attribute_definition_encoder.ex
@@ -0,0 +1,9 @@
+alias Hologram.Compiler.{Context, Encoder, Generator, Opts}
+alias Hologram.Compiler.IR.ModuleAttributeDefinition
+
+defimpl Encoder, for: ModuleAttributeDefinition  do
+  def encode(%ModuleAttributeDefinition{} = ir, %Context{} = context, %Opts{} = opts) do
+    value = Generator.generate(ir.value, context, opts)
+    "static $#{ir.name} = #{value};"
+  end
+end
---
+++ test/hologram/compiler/encoders/module_attribute_definition_encoder_test.exs
@@ -0,0 +1,18 @@
+defmodule Hologram.Compiler.ModuleAttributeDefinitionEncoderTest do
+  use Hologram.TestCase, async: true
+
+  alias Hologram.Compiler.{Context, Encoder, Opts}
+  alias Hologram.Compiler.IR.{IntegerType, ModuleAttributeDefinition}
+
+  test "encode/3" do
+    ir = %ModuleAttributeDefinition{
+      name: :xyz,
+      value: %IntegerType{value: 123}
+    }
+
+    result = Encoder.encode(ir, %Context{}, %Opts{})
+    expected = "static $xyz = { type: 'integer', value: 123 };"
+
+    assert result == expected
+  end
+end
