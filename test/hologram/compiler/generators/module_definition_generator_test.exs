8a9e8d799df373b7ae334fbd68dd11563ac0ae06
Bart Blast <bart@bartblast.com>
2021-08-03 14:43:14 +0200

    Test module without functions encoding

--- test/hologram/compiler/generators/module_definition_generator_test.exs
+++ test/hologram/compiler/generators/module_definition_generator_test.exs
@@ -6,252 +6,259 @@ defmodule Hologram.Compiler.ModuleDefinitionGeneratorTest do
 
   @module Abc.Bcd
 
-  test "single function with single variant" do
-    ir = %ModuleDefinition{
-      functions: [
-        %FunctionDefinition{
-          bindings: [
-            a: {0, [%Variable{name: :a}]}
-          ],
-          body: [
-            %IntegerType{value: 1}
-          ],
-          name: :test,
-          params: [
-            %Variable{name: :a}
-          ]
-        }
-      ],
-      module: @module
-    }
+  describe "functions" do
+    test "no functions" do
+      ir = %ModuleDefinition{functions: []}
+
+      result = ModuleDefinitionGenerator.generate(ir, @module, %Context{}, %Opts{})
+      expected = "window.Elixir_Abc_Bcd = class Elixir_Abc_Bcd {}\n"
+
+      assert result == expected
+    end
+
+    test "single function with single variant" do
+      ir = %ModuleDefinition{
+        functions: [
+          %FunctionDefinition{
+            bindings: [
+              a: {0, [%Variable{name: :a}]}
+            ],
+            body: [
+              %IntegerType{value: 1}
+            ],
+            name: :test,
+            params: [
+              %Variable{name: :a}
+            ]
+          }
+        ],
+        module: @module
+      }
 
 
-    result = ModuleDefinitionGenerator.generate(ir, @module, %Context{}, %Opts{})
+      result = ModuleDefinitionGenerator.generate(ir, @module, %Context{}, %Opts{})
 
-    expected = """
-    window.Elixir_Abc_Bcd = class Elixir_Abc_Bcd {
+      expected = """
+      window.Elixir_Abc_Bcd = class Elixir_Abc_Bcd {
 
-    static test() {
-    if (Hologram.patternMatchFunctionArgs([{ type: 'placeholder' }], arguments)) {
-    let a = arguments[0];
-    return { type: 'integer', value: 1 };
-    }
-    else {
-    console.debug(arguments)
-    throw 'No match for the function call'
-    }
-    }
+      static test() {
+      if (Hologram.patternMatchFunctionArgs([{ type: 'placeholder' }], arguments)) {
+      let a = arguments[0];
+      return { type: 'integer', value: 1 };
+      }
+      else {
+      console.debug(arguments)
+      throw 'No match for the function call'
+      }
+      }
+      }
+      """
 
-    }
-    """
+      assert result == expected
+    end
 
-    assert result == expected
-  end
+    test "single function with multiple variants" do
+      ir = %ModuleDefinition{
+        functions: [
+          %FunctionDefinition{
+            bindings: [
+              a: {0, [%Variable{name: :a}]}
+            ],
+            body: [
+              %IntegerType{value: 1}
+            ],
+            name: :test,
+            params: [
+              %Variable{name: :a}
+            ]
+          },
+          %FunctionDefinition{
+            bindings: [
+              a: {0, [%Variable{name: :a}]},
+              b: {1, [%Variable{name: :b}]}
+            ],
+            body: [
+              %IntegerType{value: 2}
+            ],
+            name: :test,
+            params: [
+              %Variable{name: :a},
+              %Variable{name: :b}
+            ]
+          }
+        ],
+        module: @module
+      }
 
-  test "single function with multiple variants" do
-    ir = %ModuleDefinition{
-      functions: [
-        %FunctionDefinition{
-          bindings: [
-            a: {0, [%Variable{name: :a}]}
-          ],
-          body: [
-            %IntegerType{value: 1}
-          ],
-          name: :test,
-          params: [
-            %Variable{name: :a}
-          ]
-        },
-        %FunctionDefinition{
-          bindings: [
-            a: {0, [%Variable{name: :a}]},
-            b: {1, [%Variable{name: :b}]}
-          ],
-          body: [
-            %IntegerType{value: 2}
-          ],
-          name: :test,
-          params: [
-            %Variable{name: :a},
-            %Variable{name: :b}
-          ]
-        }
-      ],
-      module: @module
-    }
+      result = ModuleDefinitionGenerator.generate(ir, @module, %Context{}, %Opts{})
 
-    result = ModuleDefinitionGenerator.generate(ir, @module, %Context{}, %Opts{})
+      expected = """
+      window.Elixir_Abc_Bcd = class Elixir_Abc_Bcd {
 
-    expected = """
-    window.Elixir_Abc_Bcd = class Elixir_Abc_Bcd {
+      static test() {
+      if (Hologram.patternMatchFunctionArgs([{ type: 'placeholder' }], arguments)) {
+      let a = arguments[0];
+      return { type: 'integer', value: 1 };
+      }
+      else if (Hologram.patternMatchFunctionArgs([{ type: 'placeholder' }, { type: 'placeholder' }], arguments)) {
+      let a = arguments[0];
+      let b = arguments[1];
+      return { type: 'integer', value: 2 };
+      }
+      else {
+      console.debug(arguments)
+      throw 'No match for the function call'
+      }
+      }
+      }
+      """
 
-    static test() {
-    if (Hologram.patternMatchFunctionArgs([{ type: 'placeholder' }], arguments)) {
-    let a = arguments[0];
-    return { type: 'integer', value: 1 };
-    }
-    else if (Hologram.patternMatchFunctionArgs([{ type: 'placeholder' }, { type: 'placeholder' }], arguments)) {
-    let a = arguments[0];
-    let b = arguments[1];
-    return { type: 'integer', value: 2 };
-    }
-    else {
-    console.debug(arguments)
-    throw 'No match for the function call'
-    }
-    }
+      assert result == expected
+    end
 
-    }
-    """
+    test "multiple functions with single variant" do
+      ir = %ModuleDefinition{
+        functions: [
+          %FunctionDefinition{
+            bindings: [
+              a: {0, [%Variable{name: :a}]}
+            ],
+            body: [
+              %IntegerType{value: 1}
+            ],
+            name: :test_1,
+            params: [
+              %Variable{name: :a}
+            ]
+          },
+          %FunctionDefinition{
+            bindings: [
+              a: {0, [%Variable{name: :a}]}
+            ],
+            body: [
+              %IntegerType{value: 2}
+            ],
+            name: :test_2,
+            params: [
+              %Variable{name: :a}
+            ]
+          }
+        ],
+        module: @module
+      }
 
-    assert result == expected
-  end
+      result = ModuleDefinitionGenerator.generate(ir, @module, %Context{}, %Opts{})
 
-  test "multiple functions with single variant" do
-    ir = %ModuleDefinition{
-      functions: [
-        %FunctionDefinition{
-          bindings: [
-            a: {0, [%Variable{name: :a}]}
-          ],
-          body: [
-            %IntegerType{value: 1}
-          ],
-          name: :test_1,
-          params: [
-            %Variable{name: :a}
-          ]
-        },
-        %FunctionDefinition{
-          bindings: [
-            a: {0, [%Variable{name: :a}]}
-          ],
-          body: [
-            %IntegerType{value: 2}
-          ],
-          name: :test_2,
-          params: [
-            %Variable{name: :a}
-          ]
-        }
-      ],
-      module: @module
-    }
+      expected = """
+      window.Elixir_Abc_Bcd = class Elixir_Abc_Bcd {
 
-    result = ModuleDefinitionGenerator.generate(ir, @module, %Context{}, %Opts{})
+      static test_1() {
+      if (Hologram.patternMatchFunctionArgs([{ type: 'placeholder' }], arguments)) {
+      let a = arguments[0];
+      return { type: 'integer', value: 1 };
+      }
+      else {
+      console.debug(arguments)
+      throw 'No match for the function call'
+      }
+      }
 
-    expected = """
-    window.Elixir_Abc_Bcd = class Elixir_Abc_Bcd {
+      static test_2() {
+      if (Hologram.patternMatchFunctionArgs([{ type: 'placeholder' }], arguments)) {
+      let a = arguments[0];
+      return { type: 'integer', value: 2 };
+      }
+      else {
+      console.debug(arguments)
+      throw 'No match for the function call'
+      }
+      }
+      }
+      """
 
-    static test_1() {
-    if (Hologram.patternMatchFunctionArgs([{ type: 'placeholder' }], arguments)) {
-    let a = arguments[0];
-    return { type: 'integer', value: 1 };
-    }
-    else {
-    console.debug(arguments)
-    throw 'No match for the function call'
-    }
-    }
+      assert result == expected
+    end
 
-    static test_2() {
-    if (Hologram.patternMatchFunctionArgs([{ type: 'placeholder' }], arguments)) {
-    let a = arguments[0];
-    return { type: 'integer', value: 2 };
-    }
-    else {
-    console.debug(arguments)
-    throw 'No match for the function call'
-    }
-    }
+    test "multiple functions with multiple variants" do
+      ir = %ModuleDefinition{
+        functions: [
+          %FunctionDefinition{
+            bindings: [
+              a: {0, [%Variable{name: :a}]}
+            ],
+            body: [
+              %IntegerType{value: 1}
+            ],
+            name: :test_1,
+            params: [
+              %Variable{name: :a}
+            ]
+          },
+          %FunctionDefinition{
+            bindings: [
+              a: {0, [%Variable{name: :a}]},
+              b: {1, [%Variable{name: :b}]}
+            ],
+            body: [
+              %IntegerType{value: 2}
+            ],
+            name: :test_1,
+            params: [
+              %Variable{name: :a},
+              %Variable{name: :b}
+            ]
+          },
+          %FunctionDefinition{
+            bindings: [
+              a: {0, [%Variable{name: :a}]}
+            ],
+            body: [
+              %IntegerType{value: 3}
+            ],
+            name: :test_2,
+            params: [
+              %Variable{name: :a}
+            ]
+          }
+        ],
+        module: @module
+      }
 
-    }
-    """
+      result = ModuleDefinitionGenerator.generate(ir, @module, %Context{}, %Opts{})
 
-    assert result == expected
-  end
+      expected = """
+      window.Elixir_Abc_Bcd = class Elixir_Abc_Bcd {
 
-  test "multiple functions with multiple variants" do
-    ir = %ModuleDefinition{
-      functions: [
-        %FunctionDefinition{
-          bindings: [
-            a: {0, [%Variable{name: :a}]}
-          ],
-          body: [
-            %IntegerType{value: 1}
-          ],
-          name: :test_1,
-          params: [
-            %Variable{name: :a}
-          ]
-        },
-        %FunctionDefinition{
-          bindings: [
-            a: {0, [%Variable{name: :a}]},
-            b: {1, [%Variable{name: :b}]}
-          ],
-          body: [
-            %IntegerType{value: 2}
-          ],
-          name: :test_1,
-          params: [
-            %Variable{name: :a},
-            %Variable{name: :b}
-          ]
-        },
-        %FunctionDefinition{
-          bindings: [
-            a: {0, [%Variable{name: :a}]}
-          ],
-          body: [
-            %IntegerType{value: 3}
-          ],
-          name: :test_2,
-          params: [
-            %Variable{name: :a}
-          ]
-        }
-      ],
-      module: @module
-    }
+      static test_1() {
+      if (Hologram.patternMatchFunctionArgs([{ type: 'placeholder' }], arguments)) {
+      let a = arguments[0];
+      return { type: 'integer', value: 1 };
+      }
+      else if (Hologram.patternMatchFunctionArgs([{ type: 'placeholder' }, { type: 'placeholder' }], arguments)) {
+      let a = arguments[0];
+      let b = arguments[1];
+      return { type: 'integer', value: 2 };
+      }
+      else {
+      console.debug(arguments)
+      throw 'No match for the function call'
+      }
+      }
 
-    result = ModuleDefinitionGenerator.generate(ir, @module, %Context{}, %Opts{})
+      static test_2() {
+      if (Hologram.patternMatchFunctionArgs([{ type: 'placeholder' }], arguments)) {
+      let a = arguments[0];
+      return { type: 'integer', value: 3 };
+      }
+      else {
+      console.debug(arguments)
+      throw 'No match for the function call'
+      }
+      }
+      }
+      """
 
-    expected = """
-    window.Elixir_Abc_Bcd = class Elixir_Abc_Bcd {
-
-    static test_1() {
-    if (Hologram.patternMatchFunctionArgs([{ type: 'placeholder' }], arguments)) {
-    let a = arguments[0];
-    return { type: 'integer', value: 1 };
-    }
-    else if (Hologram.patternMatchFunctionArgs([{ type: 'placeholder' }, { type: 'placeholder' }], arguments)) {
-    let a = arguments[0];
-    let b = arguments[1];
-    return { type: 'integer', value: 2 };
-    }
-    else {
-    console.debug(arguments)
-    throw 'No match for the function call'
-    }
-    }
-
-    static test_2() {
-    if (Hologram.patternMatchFunctionArgs([{ type: 'placeholder' }], arguments)) {
-    let a = arguments[0];
-    return { type: 'integer', value: 3 };
-    }
-    else {
-    console.debug(arguments)
-    throw 'No match for the function call'
-    }
-    }
-
-    }
-    """
-
-    assert result == expected
+      assert result == expected
+    end
   end
 end
