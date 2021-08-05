165989ac9c12a82eed051a3fd2dbddeaf13e61db
Bart Blast <bart@bartblast.com>
2021-08-05 13:04:20 +0200

    Refactor module definition transformer

--- lib/hologram/compiler/transformers/module_definiton_transformer.ex
+++ lib/hologram/compiler/transformers/module_definiton_transformer.ex
@@ -6,10 +6,8 @@ defmodule Hologram.Compiler.ModuleDefinitionTransformer do
     {:defmodule, _, [_, [do: {:__block__, _, exprs}]]} = ast
     uses = aggregate_expressions(:use, exprs, %Context{})
 
-    {:defmodule, _, [{:__aliases__, _, module_segs}, [do: {:__block__, _, exprs}]]} =
-      UseDirectiveExpander.expand(ast)
-
-    build_module(exprs, module_segs, uses)
+    UseDirectiveExpander.expand(ast)
+    |> build_module(uses)
   end
 
   defp aggregate_expressions(type, exprs, context) do
@@ -25,13 +23,15 @@ defmodule Hologram.Compiler.ModuleDefinitionTransformer do
   end
 
 
-  defp build_module(ast, module_segs, uses) do
+  defp build_module(ast, uses) do
+    {:defmodule, _, [{:__aliases__, _, module_segs}, [do: {:__block__, _, exprs}]]} = ast
+
     module = Helpers.module(module_segs)
-    imports = aggregate_expressions(:import, ast, %Context{})
-    requires = aggregate_expressions(:require, ast, %Context{})
-    aliases = aggregate_expressions(:alias, ast, %Context{})
-    attributes = aggregate_expressions(:@, ast, %Context{})
-    macros = aggregate_expressions(:defmacro, ast, %Context{})
+    imports = aggregate_expressions(:import, exprs, %Context{})
+    requires = aggregate_expressions(:require, exprs, %Context{})
+    aliases = aggregate_expressions(:alias, exprs, %Context{})
+    attributes = aggregate_expressions(:@, exprs, %Context{})
+    macros = aggregate_expressions(:defmacro, exprs, %Context{})
 
     context = %Context{
       module: module,
@@ -42,7 +42,7 @@ defmodule Hologram.Compiler.ModuleDefinitionTransformer do
       attributes: attributes
     }
 
-    functions = aggregate_expressions(:def, ast, context)
+    functions = aggregate_expressions(:def, exprs, context)
 
     fields =
       Map.from_struct(context)
