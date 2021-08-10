0320aa2ceb3521c16d1fc97b6913d5b2c0700a44
Bart Blast <bart@bartblast.com>
2021-08-10 16:56:50 +0200

    Move renderer code to separate modules

--- lib/hologram/template/renderer.ex
+++ lib/hologram/template/renderer.ex
@@ -1,32 +1,3 @@
-defmodule Hologram.Template.Renderer do
-  alias Hologram.Template.{
-    ComponentRenderer,
-    ExpressionRenderer,
-    NodeListRenderer,
-    ElementNodeRenderer
-  }
-
-  alias Hologram.Template.Document.{Component, Expression, ElementNode, TextNode}
-
-  def render(virtual_dom, state \\ %{})
-
-  def render(nodes, state) when is_list(nodes) do
-    NodeListRenderer.render(nodes, state)
-  end
-
-  def render(%Component{module: module}, state) do
-    ComponentRenderer.render(module, state)
-  end
-
-  def render(%ElementNode{attrs: attrs, children: children, tag: tag}, state) do
-    ElementNodeRenderer.render(tag, attrs, children, state)
-  end
-
-  def render(%Expression{ir: ir}, state) do
-    ExpressionRenderer.render(ir, state)
-  end
-
-  def render(%TextNode{content: content}, _state) do
-    content
-  end
+defprotocol Hologram.Template.Renderer do
+  def render(document, state)
 end
