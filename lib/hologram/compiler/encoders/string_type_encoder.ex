f3107b458f8f9f7b804e059cfb3abfa99f1974bb
Bart Blast <bart@bartblast.com>
2021-08-11 13:36:57 +0200

    Escape newlines in encoded JS strings

--- lib/hologram/compiler/encoders/string_type_encoder.ex
+++ lib/hologram/compiler/encoders/string_type_encoder.ex
@@ -3,7 +3,10 @@ alias Hologram.Compiler.IR.StringType
 
 defimpl Encoder, for: StringType  do
   def encode(%{value: value}, %Context{}, %Opts{}) do
-    value = String.replace(value, "'", "\\'")
+    value =
+      String.replace(value, "'", "\\'", global: true)
+      |> String.replace("\n", "\\n", global: true)
+
     PrimitiveTypeGenerator.generate(:string, "'#{value}'")
   end
 end
--- test/hologram/compiler/encoders/string_type_encoder_test.exs
+++ test/hologram/compiler/encoders/string_type_encoder_test.exs
@@ -5,10 +5,10 @@ defmodule Hologram.Compiler.StrintTypeEncoderTest do
   alias Hologram.Compiler.IR.StringType
 
   test "encode/3" do
-    ir = %StringType{value: "abc'bcd"}
+    ir = %StringType{value: "abc'bcd\ncde"}
 
     result = Encoder.encode(ir, %Context{}, %Opts{})
-    expected = "{ type: 'string', value: 'abc\\'bcd' }"
+    expected = "{ type: 'string', value: 'abc\\'bcd\\ncde' }"
 
     assert result == expected
   end
