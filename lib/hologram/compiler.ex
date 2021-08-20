173f4bbfcbcf2f38cf3fc2abf4d904c50862b4a1
Bart Blast <bart@bartblast.com>
2021-08-20 17:59:37 +0200

    Cleanup

--- lib/hologram/compiler.ex
+++ lib/hologram/compiler.ex
@@ -21,13 +21,11 @@ defmodule Hologram.Compiler do
     |> include_templatables(module_def)
   end
 
-  # TESTED
   defp include_aliases(acc, module_def) do
     module_def.aliases
     |> Enum.reduce(acc, &maybe_include_module(&2, &1.module))
   end
 
-  # TESTED
   defp include_imports(acc, module_def) do
     module_def.imports
     |> Enum.reduce(acc, &maybe_include_module(&2, &1.module))
@@ -42,13 +40,11 @@ defmodule Hologram.Compiler do
     end
   end
 
-  # TESTED
   defp include_used_modules(acc, module_def) do
     module_def.functions
     |> Enum.reduce(acc, &traverse_function_defs(&2, &1))
   end
 
-  # TESTED
   defp maybe_include_module(acc, module) do
     unless acc[module] || Reflection.standard_lib?(module) do
       compile(module, acc)
@@ -57,26 +53,21 @@ defmodule Hologram.Compiler do
     end
   end
 
-  # TESTED
   defp traverse_function_defs(acc, %FunctionDefinition{body: body}) do
     Enum.reduce(body, acc, &traverse_function_defs(&2, &1))
   end
 
-  # TESTED
   defp traverse_function_defs(acc, %FunctionCall{module: module}) do
     maybe_include_module(acc, module)
   end
 
-  # TESTED
   # DEFER: traverse nested code blocks
   defp traverse_function_defs(acc, _), do: acc
 
-  # TESTED
   defp traverse_template(acc, nodes) when is_list(nodes) do
     Enum.reduce(nodes, acc, &traverse_template(&2, &1))
   end
 
-  # TESTED
   defp traverse_template(acc, %Component{module: module, props: props, children: children}) do
     acc = maybe_include_module(acc, module)
 
@@ -88,7 +79,6 @@ defmodule Hologram.Compiler do
     Enum.reduce(children, acc, &traverse_template(&2, &1))
   end
 
-  # TESTED
   defp traverse_template(acc, %ElementNode{attrs: attrs, children: children}) do
     acc =
       Enum.reduce(attrs, acc, fn {_, %{value: value}}, acc ->
@@ -98,17 +88,14 @@ defmodule Hologram.Compiler do
     Enum.reduce(children, acc, &traverse_template(&2, &1))
   end
 
-  # TESTED
   defp traverse_template(acc, %Expression{ir: ir}) do
     traverse_template(acc, ir)
   end
 
-  # TESTED
   defp traverse_template(acc, %FunctionCall{module: module}) do
     maybe_include_module(acc, module)
   end
 
-  # TESTED
   defp traverse_template(acc, %TupleType{data: data}) do
     Enum.reduce(data, acc, &traverse_template(&2, &1))
   end
