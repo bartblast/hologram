56d4ee863d94fdeb6002bb475a7c3271dd599ad6
Bart Blast <bart@bartblast.com>
2021-08-26 14:04:11 +0200

    Implement if expression encoder

---
+++ lib/hologram/compiler/encoders/if_expression_encoder.ex
@@ -0,0 +1,17 @@
+alias Hologram.Compiler.{Context, Encoder, Generator, Opts}
+alias Hologram.Compiler.IR.{AnonymousFunctionType, IfExpression}
+
+defimpl Encoder, for: IfExpression  do
+  def encode(%{condition: condition, do: do_clause, else: else_clause}, %Context{} = context, %Opts{} = opts) do
+    condition = encode_anonymous_function([condition], context, opts)
+    do_clause = encode_anonymous_function(do_clause, context, opts)
+    else_clause = encode_anonymous_function(else_clause, context, opts)
+
+    "Elixir_Kernel.if(#{condition}, #{do_clause}, #{else_clause})"
+  end
+
+  defp encode_anonymous_function(body, context, opts) do
+    %AnonymousFunctionType{body: body}
+    |> Generator.generate(context, opts)
+  end
+end
---
+++ test/hologram/compiler/encoders/if_expression_encoder_test.exs
@@ -0,0 +1,19 @@
+defmodule Hologram.Compiler.IfExpressionEncoderTest do
+  use Hologram.TestCase, async: true
+
+  alias Hologram.Compiler.{Context, Encoder, Opts}
+  alias Hologram.Compiler.IR.{BooleanType, IfExpression, IntegerType}
+
+  test "encode/3" do
+    ir = %IfExpression{
+      condition: %BooleanType{value: true},
+      do: [%IntegerType{value: 1}],
+      else: [%IntegerType{value: 2}]
+    }
+
+    result = Encoder.encode(ir, %Context{}, %Opts{})
+    expected = "Elixir_Kernel.if((function() { return { type: 'boolean', value: true }; }), (function() { return { type: 'integer', value: 1 }; }), (function() { return { type: 'integer', value: 2 }; }))"
+
+    assert result == expected
+  end
+end
