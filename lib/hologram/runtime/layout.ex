dfc0ec2c76790f9a859a2bf19b62fc90a46ff018
Bart Blast <bart@bartblast.com>
2021-08-27 11:57:49 +0200

    Remove alias

--- lib/hologram/runtime/layout.ex
+++ lib/hologram/runtime/layout.ex
@@ -3,7 +3,6 @@ defmodule Hologram.Layout do
     quote do
       import Hologram.Layout
       import Hologram.Runtime.Commons, only: [sigil_H: 2]
-      alias Hologram.UI.Runtime
     end
   end
 end
