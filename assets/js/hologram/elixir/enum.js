9aeabe5d9151a41a8e97a12a8d8aa80ddb3b4d94
Bart Blast <bart@bartblast.com>
2021-08-27 12:36:04 +0200

    Refactor Enum JS module and its tests

--- assets/js/hologram/elixir/enum.js
+++ assets/js/hologram/elixir/enum.js
@@ -1,14 +1,16 @@
 // see: https://www.blazemeter.com/blog/the-correct-way-to-import-lodash-libraries-a-benchmark
 import isEqual from "lodash/isEqual";
 
+import Type from "../type";
+
 export default class Enum {
   static member$question(enumerable, element) {
     switch (enumerable.type) {
       case "list":
         if (enumerable.data.find(el => isEqual(el, element))) {
-          return {type: "boolean", value: true}
+          return Type.boolean(true)
         } else {
-          return {type: "boolean", value: false}
+          return Type.boolean(false)
         }
 
       // DEFER: implement other enumerable types
--- test/js/elixir/enum_test.js
+++ test/js/elixir/enum_test.js
@@ -1,37 +1,32 @@
 import { assert } from "../support/commons"
+
 import Enum from "../../../assets/js/hologram/elixir/enum";
+import Type from "../../../assets/js/hologram/type";
 
 describe("member$question()", () => {
-  it("list has element", () => {
-    const list = {
+  let list;
+  beforeEach(() => {
+    list = {
       type: "list",
       data: [
         {type: "integer", value: 1},
         {type: "integer", value: 2}
       ]
     }
+  })
 
+  it("list has element", () => {
     const element = {type: "integer", value: 2}
-
     const result = Enum.member$question(list, element)
-    const expected = {type: "boolean", value: true}
+    const expected = Type.boolean(true)
 
     assert.deepStrictEqual(result, expected)
   })
 
   it("list doesn't have element", () => {
-    const list = {
-      type: "list",
-      data: [
-        {type: "integer", value: 1},
-        {type: "integer", value: 2}
-      ]
-    }
-
     const element = {type: "integer", value: 3}
-
     const result = Enum.member$question(list, element)
-    const expected = {type: "boolean", value: false}
+    const expected = Type.boolean(false)
 
     assert.deepStrictEqual(result, expected)
   })
