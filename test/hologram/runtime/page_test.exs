c37f4919adc4172e369785b565732dd29c42548d
Bart Blast <bart@bartblast.com>
2021-08-30 13:52:17 +0200

    Test layout macro

--- test/hologram/runtime/page_test.exs
+++ test/hologram/runtime/page_test.exs
@@ -3,6 +3,25 @@ defmodule Hologram.PageTest do
   require Hologram.Page
   alias Hologram.Page
 
+  test "layout/1" do
+    ast =
+      Macro.expand_once(
+        quote do
+          Hologram.Page.layout(Hologram.PageTest)
+        end,
+        __ENV__
+      )
+
+    assert {
+      :def,
+      [context: Hologram.Page, import: Kernel],
+      [
+        {:page_layout, [counter: _, context: Hologram.Page], Hologram.Page},
+        [do: {:__aliases__, [counter: _, alias: false], [:Hologram, :PageTest]}]
+      ]
+    } = ast
+  end
+
   test "route/1" do
     ast =
       Macro.expand_once(
