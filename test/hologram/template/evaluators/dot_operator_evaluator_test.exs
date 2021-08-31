13a45a675c619792535e2c67c60fcb7fd55785fd
Bart Blast <bart@bartblast.com>
2021-08-31 12:14:26 +0200

    Implement dot operator template evaluator

---
+++ test/hologram/template/evaluators/dot_operator_evaluator_test.exs
@@ -0,0 +1,22 @@
+defmodule Hologram.Template.DotOperatorEvaluatorTest do
+  use Hologram.TestCase, async: true
+
+  alias Hologram.Compiler.IR.{AtomType, DotOperator, IntegerType, MapType}
+  alias Hologram.Template.Evaluator
+
+  test "evaluate/2" do
+    ir = %DotOperator{
+      left: %MapType{
+        data: [
+          {%AtomType{value: :a}, %IntegerType{value: 1}}
+        ]
+      },
+      right: %AtomType{value: :a}
+    }
+
+    result = Evaluator.evaluate(ir, %{})
+    expected = 1
+
+    assert result == expected
+  end
+end
