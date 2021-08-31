9aa54487b5b036f2d56f49fbfd837c14efb24ac1
Bart Blast <bart@bartblast.com>
2021-08-31 12:07:53 +0200

    Implement atom type template evaluator

---
+++ lib/hologram/template/evaluators/atom_type_evaluator.ex
@@ -0,0 +1,8 @@
+alias Hologram.Compiler.IR.AtomType
+alias Hologram.Template.Evaluator
+
+defimpl Evaluator, for: AtomType  do
+  def evaluate(%{value: value}, _) do
+    value
+  end
+end
---
+++ test/hologram/template/evaluators/atom_type_evaluator_test.exs
@@ -0,0 +1,15 @@
+defmodule Hologram.Template.AtomTypeEvaluatorTest do
+  use Hologram.TestCase, async: true
+
+  alias Hologram.Compiler.IR.AtomType
+  alias Hologram.Template.Evaluator
+
+  test "evaluate/2" do
+    ir = %AtomType{value: :a}
+
+    result = Evaluator.evaluate(ir, %{})
+    expected = :a
+
+    assert result == expected
+  end
+end
