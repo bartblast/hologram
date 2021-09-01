904601685bd52007a461ab80ad522b1d3345faa4
Bart Blast <bart@bartblast.com>
2021-09-01 12:44:37 +0200

    Implement default E2E layout

--- lib/e2e/layouts/default_layout.ex
+++ lib/e2e/layouts/default_layout.ex
@@ -3,10 +3,21 @@ defmodule Hologram.E2E.DefaultLayout do
 
   def template do
     ~H"""
-    <body>
-      default layout:
-      <slot />
-    </body>
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <title>Hologram Demo</title>
+        <script src="/js/hologram.js"></script>
+        <script src="{@context.__src__}"></script>
+        <script>
+          Hologram.run(window, {@context.__class__}, "{@context.__state__}")
+        </script>
+      </head>
+      <body>
+        default layout:
+        <slot />
+      </body>
+    </html>
     """
   end
 end
