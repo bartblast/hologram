a3bf901ee44c0d6b49eacf3ae878534262e81c46
Bart Blast <bart@bartblast.com>
2021-09-01 12:51:43 +0200

    Cleanup page 1 template

--- lib/e2e/pages/page_1.ex
+++ lib/e2e/pages/page_1.ex
@@ -11,24 +11,22 @@ defmodule Hologram.E2E.Page1 do
 
   def template do
     ~H"""
-    <body>
-      <h1>Page 1</h1>
-      <button id="page-1-action-1-button" on_click="action_1">Action 1</button>
-      <button id="page-1-action-2-button" on_click={:action_2, a: 5, b: 6}>Action 2</button>
-      <button id="page-1-command-3-button" on_click.command="command_3">Command 3</button>
-      <button id="page-1-command-4-button" on_click.command="command_4">Command 4</button>
-      <button id="page-1-command-5-button" on_click.command="command_5">Command 5</button>
-      <button id="page-1-command-6-button" on_click.command={:command_6, a: 1, b: 2}>Command 6</button>
-      <button id="page-1-action-7-button" on_click="action_7">Action 7</button>
-      <button id="page-1-action-8-button" on_click="action_8">Action 8</button>
-      <button id="page-1-forward-button">Forward</button>
-      <div id="text">{@text}</div>
-      <a id="page-2-link" href={Hologram.E2E.Page2.route()} on_click.command={:__redirect__, page: Hologram.E2E.Page2}>Page 2</a>
-      <script>
-        document.getElementById("page-1-forward-button")
-          .addEventListener("click", () => &lcub; history.forward() &rcub;)
-      </script>
-    </body>
+    <h1>Page 1</h1>
+    <button id="page-1-action-1-button" on_click="action_1">Action 1</button>
+    <button id="page-1-action-2-button" on_click={:action_2, a: 5, b: 6}>Action 2</button>
+    <button id="page-1-command-3-button" on_click.command="command_3">Command 3</button>
+    <button id="page-1-command-4-button" on_click.command="command_4">Command 4</button>
+    <button id="page-1-command-5-button" on_click.command="command_5">Command 5</button>
+    <button id="page-1-command-6-button" on_click.command={:command_6, a: 1, b: 2}>Command 6</button>
+    <button id="page-1-action-7-button" on_click="action_7">Action 7</button>
+    <button id="page-1-action-8-button" on_click="action_8">Action 8</button>
+    <button id="page-1-forward-button">Forward</button>
+    <div id="text">{@text}</div>
+    <a id="page-2-link" href={Hologram.E2E.Page2.route()} on_click.command={:__redirect__, page: Hologram.E2E.Page2}>Page 2</a>
+    <script>
+      document.getElementById("page-1-forward-button")
+        .addEventListener("click", () => &lcub; history.forward() &rcub;)
+    </script>
     """
   end
 
