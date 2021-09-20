26d4edd2b50a069b438add324460f5eca9c6271e
Bart Blast <bart@bartblast.com>
2021-09-20 14:56:43 +0200

    Update elixirc path

--- mix.exs
+++ mix.exs
@@ -51,7 +51,7 @@ defmodule Demo.MixProject do
 
   # Specifies which paths to compile per environment.
   defp elixirc_paths(:test), do: ["lib", "test/fixtures", "test/support"]
-  defp elixirc_paths(_), do: ["lib"]
+  defp elixirc_paths(_), do: ["lib/hologram"]
 
   # Specifies your project dependencies.
   #
