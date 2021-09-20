735682a351d84bf120be592776ba3cdceda7062a
Bart Blast <bart@bartblast.com>
2021-09-20 14:16:13 +0200

    Upgrade deps

--- mix.exs
+++ mix.exs
@@ -58,22 +58,22 @@ defmodule Demo.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.5.8"},
-      {:phoenix_ecto, "~> 4.1"},
-      {:ecto_sql, "~> 3.4"},
-      {:postgrex, ">= 0.0.0"},
-      {:phoenix_html, "~> 2.11"},
-      {:phoenix_live_dashboard, "~> 0.4"},
-      {:telemetry_metrics, "~> 0.4"},
-      {:telemetry_poller, "~> 0.4"},
-      {:gettext, "~> 0.11"},
-      {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"},
-      {:saxy, "~> 1.3.0"},
-      {:file_system, "~> 0.2.10"},
+      {:ecto_sql, "~> 3.7"},
+      {:file_system, "~> 0.2"},
+      {:gettext, "~> 0.18"},
+      {:jason, "~> 1.2"},
+      {:phoenix, "~> 1.5"},
+      {:phoenix_ecto, "~> 4.4"},
+      {:phoenix_html, "~> 3.0"},
+      {:phoenix_live_dashboard, "~> 0.5"},
+      {:plug_cowboy, "~> 2.5"},
+      {:postgrex, "~> 0.15"},
+      {:saxy, "~> 1.4"},
+      {:telemetry_metrics, "~> 0.6"},
+      {:telemetry_poller, "~> 0.1"},
 
-      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
-      {:wallaby, "~> 0.28.0", only: :test, runtime: false}
+      {:ex_doc, "~> 0.25", only: :dev, runtime: false},
+      {:wallaby, "~> 0.29", only: :test, runtime: false}
     ]
   end
 
