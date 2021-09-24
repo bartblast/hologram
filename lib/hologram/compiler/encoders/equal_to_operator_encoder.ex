0331a7ccb16e666d9b2ba96d31247a84c8af08e9
Bart Blast <bart@bartblast.com>
2021-09-24 23:23:24 +0200

    Implement equal to operator encoder

---
+++ lib/hologram/compiler/encoders/equal_to_operator_encoder.ex
@@ -0,0 +1,12 @@
+alias Hologram.Compiler.{Context, Encoder, Generator, Helpers, Opts}
+alias Hologram.Compiler.IR.EqualToOperator
+
+defimpl Encoder, for: EqualToOperator  do
+  def encode(%{left: left, right: right}, %Context{} = context, %Opts{} = opts) do
+    left = Generator.generate(left, context, opts)
+    right = Generator.generate(right, context, opts)
+    class_name = Helpers.class_name(Kernel)
+
+    "#{class_name}.$equal_to(#{left}, #{right})"
+  end
+end
