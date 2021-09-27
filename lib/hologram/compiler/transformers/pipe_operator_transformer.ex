381a90ec8cb692c731949c19240ce7202f7af4b9
Bart Blast <bart@bartblast.com>
2021-09-27 16:05:09 +0200

    Format code

--- lib/hologram/compiler/transformers/pipe_operator_transformer.ex
+++ lib/hologram/compiler/transformers/pipe_operator_transformer.ex
@@ -3,12 +3,12 @@ defmodule Hologram.Compiler.PipeOperatorTransformer do
 
   # based on: https://ianrumford.github.io/elixir/pipe/clojure/thread-first/macro/2016/07/24/writing-your-own-elixir-pipe-operator.html
   def transform(ast, %Context{} = context) do
-      [{first_ast, _index} | rest_tuples] = Macro.unpipe(ast)
+    [{first_ast, _index} | rest_tuples] = Macro.unpipe(ast)
 
-      rest_tuples
-      |> Enum.reduce(first_ast, fn {rest_ast, rest_index}, this_ast ->
-        Macro.pipe(this_ast, rest_ast, rest_index)
-      end)
-      |> Transformer.transform(context)
+    rest_tuples
+    |> Enum.reduce(first_ast, fn {rest_ast, rest_index}, this_ast ->
+      Macro.pipe(this_ast, rest_ast, rest_index)
+    end)
+    |> Transformer.transform(context)
   end
 end
