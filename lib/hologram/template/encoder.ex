57390bfbeccfd1f331e8718d293f62c124f38bd2
Bart Blast <bart@bartblast.com>
2021-09-29 14:20:17 +0200

    Refactor template generator into encoder protocol

---
+++ lib/hologram/template/encoder.ex
@@ -0,0 +1,15 @@
+defprotocol Hologram.Template.Encoder do
+  alias Hologram.Typespecs, as: T
+
+  @doc """
+  Given document tree template, generates virtual DOM template JS representation,
+  which can be used by the frontend runtime to re-render the DOM.
+
+  ## Examples
+      iex> encode(%ElementNode{tag: "div", children: [%TextNode{content: "test}]})
+      "{ type: 'element', tag: 'div', attrs: {}, children: [{ type: 'text', content: 'test' }] }"
+  """
+  @spec encode(T.document_node | list(T.document_node)) :: String.t
+
+  def encode(document_tree)
+end
