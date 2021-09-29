e773afbc5a6f5fadb08d1f8a41b250812c8bfa04
Bart Blast <bart@bartblast.com>
2021-09-29 20:53:02 +0200

    Encode component props

--- lib/hologram/template/encoders/component_encoder.ex
+++ lib/hologram/template/encoders/component_encoder.ex
@@ -3,10 +3,24 @@ alias Hologram.Template.Document.Component
 alias Hologram.Template.Encoder
 
 defimpl Encoder, for: Component do
-  def encode(%{module: module, children: children}) do
-    class_name = Helpers.class_name(module)
-    children = Encoder.encode(children)
+  import Hologram.Commons.Encoder
 
-    "{ type: 'component', module: '#{class_name}', children: #{children} }"
+  def encode(%{module: module, children: children, props: props}) do
+    class_name = Helpers.class_name(module)
+    encoded_children = Encoder.encode(children)
+    encoded_props = encode_props(props)
+
+    "{ type: 'component', module: '#{class_name}', children: #{encoded_children}, props: #{encoded_props} }"
+  end
+
+  defp encode_prop(name, value) do
+    "'#{name}': #{Encoder.encode(value)}"
+  end
+
+  defp encode_props(props) do
+    props
+    |> Enum.map(fn {name, value} -> encode_prop(name, value) end)
+    |> Enum.join(", ")
+    |> wrap_with_object()
   end
 end
