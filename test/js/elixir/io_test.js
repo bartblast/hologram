34e685dd2567a81220193688c7ce462e6fba845c
Bart Blast <bart@bartblast.com>
2021-10-05 18:44:18 +0200

    Refactor IO class tests

--- test/js/elixir/io_test.js
+++ test/js/elixir/io_test.js
@@ -2,19 +2,20 @@
 
 import { assert, sinon } from "../support/commons"
 import IO from "../../../assets/js/hologram/elixir/io";
+import Type from "../../../assets/js/hologram/type";
 
 describe("inspect()", () => {
   it("prints debug info for the given value", () => {
     const stub = sinon.stub(console, "debug");
 
-    const val = {type: "integer", value: 1}
+    const val = 123
     IO.inspect(val)
 
     sinon.assert.calledWith(stub, val);
   })
 
   it("returns the value passed as argument", () => {
-    const val = {type: "integer", value: 1}
+    const val = Type.integer(1)
     const result = IO.inspect(val)
 
     assert.equal(result, val)
