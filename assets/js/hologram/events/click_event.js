565058a7bd293f4e92baea3327315423233df409
Bart Blast <bart@bartblast.com>
2021-10-12 13:35:18 +0200

    Freeze returned object

--- assets/js/hologram/events/click_event.js
+++ assets/js/hologram/events/click_event.js
@@ -1,8 +1,10 @@
 "use strict";
 
+import Utils from "../utils"
+
 export default class ClickEvent {
   // TODO: implement & test
   static buildEventData(_event) {
-    return {}
+    return Utils.freeze({})
   }
 }
