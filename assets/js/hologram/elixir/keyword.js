259a6503e78a88a3cf04b8a7eae7d07c40d3cc71
Bart Blast <bart@bartblast.com>
2021-10-13 13:53:42 +0200

    Implement Elixir.Keyword.delete/2 in JavaScript

--- assets/js/hologram/elixir/keyword.js
+++ assets/js/hologram/elixir/keyword.js
@@ -4,8 +4,17 @@
 import isEqual from "lodash/isEqual";
 
 import Type from "../type";
+import Utils from "../utils";
 
 export default class Keyword {
+  static delete(keywords, key) {
+    const newElems = keywords.data.filter((elem) => {
+      return !isEqual(elem.data[0], key)
+    })
+
+    return Utils.freeze(Type.list(newElems))
+  }
+
   static has_key$question(keywords, key) {
     if (keywords.data.find(el => isEqual(el.data[0], key))) {
       return Type.boolean(true)
--- test/js/elixir/keyword_test.js
+++ test/js/elixir/keyword_test.js
@@ -1,8 +1,44 @@
 "use strict";
 
-import { assertBoxedFalse, assertBoxedTrue } from "../support/commons"
+import { assert, assertBoxedFalse, assertBoxedTrue } from "../support/commons"
 import Keyword from "../../../assets/js/hologram/elixir/keyword";
 import Type from "../../../assets/js/hologram/type";
+
+describe("delete()", () => {
+  const key = Type.atom("a")
+
+  const expected = Type.list([
+    Type.tuple([Type.atom("b"), Type.integer(2)])
+  ])
+
+  it("deletes the entry in the keyword list for specific a key when there is one matching entry", () => {
+    const keywords = Type.list([
+      Type.tuple([key, Type.integer(1)]),
+      Type.tuple([Type.atom("b"), Type.integer(2)])
+    ])
+
+    const result = Keyword.delete(keywords, key)
+
+    assert.deepStrictEqual(result, expected)
+  })
+
+  it("deletes the entries in the keyword list for specific a key when there are multiple matching entries", () => {
+    const keywords = Type.list([
+      Type.tuple([key, Type.integer(1)]),
+      Type.tuple([Type.atom("b"), Type.integer(2)]),
+      Type.tuple([key, Type.integer(3)])
+    ])
+
+    const result = Keyword.delete(keywords, key)
+
+    assert.deepStrictEqual(result, expected)
+  })
+
+  it("returns the keyword list unchanged if there are no entries matching the given key", () => {
+    const result = Keyword.delete(expected, key)
+    assert.deepStrictEqual(result, expected)
+  })
+})
 
 describe("has_key$question()", () => {
   let keywords;
