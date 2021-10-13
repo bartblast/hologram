536f3df17dfb383e9e89d75f94a912387991905f
Bart Blast <bart@bartblast.com>
2021-10-13 21:08:11 +0200

    Add TODO

--- assets/js/hologram/events/click_event.js
+++ assets/js/hologram/events/click_event.js
@@ -3,7 +3,7 @@
 import Utils from "../utils"
 
 export default class ClickEvent {
-  // TODO: implement & test
+  // TODO: implement & test (return boxed map)
   static buildEventData(_event) {
     return Utils.freeze({})
   }
