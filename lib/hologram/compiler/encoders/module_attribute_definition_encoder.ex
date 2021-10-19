2d12137c67860721172272a2480db5993d4f9458
Bart Blast <bart@bartblast.com>
2021-10-19 17:37:30 +0200

    Fix module attribute definition encoder

--- lib/hologram/compiler/encoders/module_attribute_definition_encoder.ex
+++ lib/hologram/compiler/encoders/module_attribute_definition_encoder.ex
@@ -1,9 +1,9 @@
-alias Hologram.Compiler.{Context, Encoder, Generator, Opts}
+alias Hologram.Compiler.{Context, Encoder, Opts}
 alias Hologram.Compiler.IR.ModuleAttributeDefinition
 
 defimpl Encoder, for: ModuleAttributeDefinition do
-  def encode(%ModuleAttributeDefinition{} = ir, %Context{} = context, %Opts{} = opts) do
-    value = Generator.generate(ir.value, context, opts)
-    "static $#{ir.name} = #{value};"
+  def encode(%{name: name, value: value}, %Context{} = context, %Opts{} = opts) do
+    value = Encoder.encode(value, context, opts)
+    "static $#{name} = #{value};"
   end
 end
