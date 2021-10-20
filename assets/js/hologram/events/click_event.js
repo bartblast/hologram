7a7186c67ed81d292263605bc2e40e42314abd44
Bart Blast <bart@bartblast.com>
2021-10-20 15:43:45 +0200

    Fix click event data type

--- assets/js/hologram/events/click_event.js
+++ assets/js/hologram/events/click_event.js
@@ -1,10 +1,11 @@
 "use strict";
 
+import Type from "../type"
 import Utils from "../utils"
 
 export default class ClickEvent {
   // TODO: implement & test (return boxed map)
   static buildEventData(_event) {
-    return Utils.freeze({})
+    return Utils.freeze(Type.map({}))
   }
 }
