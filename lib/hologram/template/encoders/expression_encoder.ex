13e959fd1f72abf1647249dd79975a4a37503c3f
Bart Blast <bart@bartblast.com>
2021-10-20 11:31:30 +0200

    Fix expression encoder

--- lib/hologram/template/encoders/expression_encoder.ex
+++ lib/hologram/template/encoders/expression_encoder.ex
@@ -1,11 +1,12 @@
-alias Hologram.Compiler.{Context, Encoder, Opts}
+alias Hologram.Compiler.{Context, Opts}
+alias Hologram.Compiler.Encoder, as: CompilerEncoder
 alias Hologram.Template.Document.Expression
 alias Hologram.Template.Encoder
 
 defimpl Encoder, for: Expression do
   def encode(%{ir: ir}) do
     # DEFER: pass actual %Context{} struct received from compiler
-    callback_return = Encoder.encode(ir, %Context{}, %Opts{template: true})
+    callback_return = CompilerEncoder.encode(ir, %Context{}, %Opts{template: true})
 
     "{ type: 'expression', callback: ($state) => { return #{callback_return} } }"
   end
