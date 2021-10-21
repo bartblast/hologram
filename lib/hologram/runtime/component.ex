f005018f5d3549ac05b9a85eebdbf3a7ab795945
Bart Blast <bart@bartblast.com>
2021-10-21 20:54:06 +0200

    Import update/3 in Component module

--- lib/hologram/runtime/component.ex
+++ lib/hologram/runtime/component.ex
@@ -2,7 +2,7 @@ defmodule Hologram.Component do
   defmacro __using__(_) do
     quote do
       import Hologram.Component
-      import Hologram.Runtime.Commons, only: [sigil_H: 2]
+      import Hologram.Runtime.Commons, only: [sigil_H: 2, update: 3]
     end
   end
 end
