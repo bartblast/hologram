359d2ebd83f97ede6210cb87c1de0951ec73ec6c
Bart Blast <bart@bartblast.com>
2021-10-22 15:28:02 +0200

    Import update/3 in layouts

--- lib/hologram/runtime/layout.ex
+++ lib/hologram/runtime/layout.ex
@@ -2,7 +2,7 @@ defmodule Hologram.Layout do
   defmacro __using__(_) do
     quote do
       import Hologram.Layout
-      import Hologram.Runtime.Commons, only: [sigil_H: 2]
+      import Hologram.Runtime.Commons, only: [sigil_H: 2, update: 3]
     end
   end
 end
