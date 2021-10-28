2d5f4035888052e7616c6be3d21d6ea82efbc488
Bart Blast <bart@bartblast.com>
2021-10-28 13:53:40 +0200

    Implement Traverser.Commons.has_edge?/3

--- lib/hologram/compiler/traversers/commons.ex
+++ lib/hologram/compiler/traversers/commons.ex
@@ -1,6 +1,11 @@
 alias Hologram.Compiler.Reflection
 
 defmodule Hologram.Compiler.Traverser.Commons do
+  def has_edge?(graph, from_vertex, to_vertex) do
+    edges = Graph.edges(graph, from_vertex, to_vertex)
+    Enum.count(edges) == 1
+  end
+
   def maybe_add_module_def(map, module) do
     unless map[module] || Reflection.standard_lib?(module) do
       module_def = Reflection.module_definition(module)
--- test/hologram/compiler/traversers/commons_test.exs
+++ test/hologram/compiler/traversers/commons_test.exs
@@ -5,6 +5,17 @@ defmodule Hologram.Compiler.Traverser.CommonsTest do
   alias Hologram.Compiler.Traverser.Commons
   alias Hologram.Test.Fixtures.PlaceholderModule
 
+  describe "has_edge?/3" do
+    test "returns true if the graph has an edge from from_vertex to to_vertex" do
+      graph = Graph.new() |> Graph.add_edge(:a, :b)
+      assert Commons.has_edge?(graph, :a, :b)
+    end
+
+    test "returns false if the graph doesn't have an edge from from_vertex to to_vertex" do
+      refute Commons.has_edge?(Graph.new(), :a, :b)
+    end
+  end
+
   describe "maybe_add_module_def/2" do
     test "non standard lib module that isn't in the map yet is added" do
       result = Commons.maybe_add_module_def(%{}, PlaceholderModule)
