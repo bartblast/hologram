458e0270f950256d8bf42efd6301198b2e343d70
Bart Blast <bart@bartblast.com>
2021-10-29 17:54:20 +0200

    Simplify module type implementation unit tests of the call graph protocol

--- test/hologram/compiler/call_graph/module_type_test.ex
+++ test/hologram/compiler/call_graph/module_type_test.ex
@@ -14,9 +14,6 @@ defmodule Hologram.Compiler.CallGraph.ModuleTypeTest do
     result = CallGraph.build(ir, call_graph, %{}, from_vertex)
 
     assert Graph.num_vertices(result) == 2
-    assert Graph.has_vertex?(result, from_vertex)
-    assert Graph.has_vertex?(result, PlaceholderModule2)
-
     assert Graph.num_edges(result) == 1
     assert has_edge?(result, PlaceholderModule1, PlaceholderModule2)
   end
