e96fed003df45fce120ae7ad55c1e79b0cafb8d9
Bart Blast <bart@bartblast.com>
2021-10-29 19:51:01 +0200

    Rename maybe_add/2 to maybe_put_module/2

--- lib/hologram/compiler/aggregators/module_type.ex
+++ lib/hologram/compiler/aggregators/module_type.ex
@@ -3,7 +3,7 @@ alias Hologram.Compiler.IR.ModuleType
 
 defimpl Aggregator, for: ModuleType do
   def aggregate(%{module: module}, module_defs) do
-    case maybe_add(module_defs, module) do
+    case maybe_put_module(module_defs, module) do
       ^module_defs ->
         module_defs
 
@@ -13,7 +13,7 @@ defimpl Aggregator, for: ModuleType do
     end
   end
 
-  defp maybe_add(module_defs, module) do
+  defp maybe_put_module(module_defs, module) do
     unless module_defs[module] || Reflection.standard_lib?(module) do
       module_def = Reflection.module_definition(module)
       Map.put(module_defs, module, module_def)
