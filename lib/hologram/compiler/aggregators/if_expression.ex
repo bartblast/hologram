f1449c6f940f8ec3344d7e7f69235d3af4d04ccb
Bart Blast <bart@bartblast.com>
2021-11-05 20:06:00 +0100

    Refactor aggregator protocol implementation for if expressions

--- lib/hologram/compiler/aggregators/if_expression.ex
+++ lib/hologram/compiler/aggregators/if_expression.ex
@@ -3,8 +3,21 @@ alias Hologram.Compiler.IR.IfExpression
 
 defimpl Aggregator, for: IfExpression do
   def aggregate(%{condition: condition, do: do_clause, else: else_clause}, module_defs) do
-    module_defs = Aggregator.aggregate(condition, module_defs)
-    module_defs = Aggregator.aggregate(do_clause, module_defs)
+    module_defs
+    |> aggregate_from_condition(condition)
+    |> aggregate_from_do_clause(do_clause)
+    |> aggregate_from_else_clause(else_clause)
+  end
+
+  defp aggregate_from_condition(module_defs, condition) do
+    Aggregator.aggregate(condition, module_defs)
+  end
+
+  defp aggregate_from_do_clause(module_defs, do_clause) do
+    Aggregator.aggregate(do_clause, module_defs)
+  end
+
+  defp aggregate_from_else_clause(module_defs, else_clause) do
     Aggregator.aggregate(else_clause, module_defs)
   end
 end
