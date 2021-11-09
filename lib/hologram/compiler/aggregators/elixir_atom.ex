bf0c0311b31b0e3d95561f72024985e876b139ad
Bart Blast <bart@bartblast.com>
2021-11-09 17:51:00 +0100

    Detect if the atom is a module

--- lib/hologram/compiler/aggregators/elixir_atom.ex
+++ lib/hologram/compiler/aggregators/elixir_atom.ex
@@ -1,11 +1,15 @@
 # TODO: test
 
-alias Hologram.Compiler.Aggregator
+alias Hologram.Compiler.{Aggregator, Reflection}
 alias Hologram.Compiler.IR.ModuleType
 
 defimpl Aggregator, for: Atom do
   def aggregate(module, module_defs) do
-    %ModuleType{module: module}
-    |> Aggregator.aggregate(module_defs)
+    if Reflection.is_module?(module) do
+      %ModuleType{module: module}
+      |> Aggregator.aggregate(module_defs)
+    else
+      module_defs
+    end
   end
 end
