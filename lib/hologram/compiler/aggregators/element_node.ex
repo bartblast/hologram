beb781aa9a3e1be2054b7d94f5faba58569fcc7e
Bart Blast <bart@bartblast.com>
2021-11-15 14:28:22 +0100

    Simplify aggregator protocol implementation for VDOM element nodes

--- lib/hologram/compiler/aggregators/element_node.ex
+++ lib/hologram/compiler/aggregators/element_node.ex
@@ -1,7 +1,7 @@
 alias Hologram.Compiler.Aggregator
-alias Hologram.Template.VDOM.ElementNode
+alias Hologram.Compiler.IR.DotOperator
 
-defimpl Aggregator, for: ElementNode do
+defimpl Aggregator, for: DotOperator do
   def aggregate(%{attrs: attrs, children: children}, module_defs) do
     module_defs = Aggregator.aggregate(attrs, module_defs)
     Aggregator.aggregate(children, module_defs)
