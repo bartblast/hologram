faa363f37b67951b5a4d098a56e3dc16efc2ff97
Bart Blast <bart@bartblast.com>
2021-11-15 16:03:22 +0100

    Recurse through function call args in call graph protocol implementation for function call

--- lib/hologram/compiler/call_graph/function_call.ex
+++ lib/hologram/compiler/call_graph/function_call.ex
@@ -2,7 +2,9 @@ alias Hologram.Compiler.{CallGraph, Reflection}
 alias Hologram.Compiler.IR.FunctionCall
 
 defimpl CallGraph, for: FunctionCall do
-  def build(%{module: module, function: function}, call_graph, module_defs, from_vertex) do
+  def build(%{module: module, function: function, args: args}, call_graph, module_defs, from_vertex) do
+    call_graph = CallGraph.build(args, call_graph, module_defs, from_vertex)
+
     if Reflection.standard_lib?(module) do
       call_graph
     else
