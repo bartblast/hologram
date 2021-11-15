916b1ddbd8a55b36159e0df3522a736b24d577ab
Bart Blast <bart@bartblast.com>
2021-11-15 14:12:28 +0100

    Add call graph protocol implementation for match operator

---
+++ lib/hologram/compiler/call_graph/match_operator.ex
@@ -0,0 +1,10 @@
+# TODO: test
+
+alias Hologram.Compiler.CallGraph
+alias Hologram.Compiler.IR.MatchOperator
+
+defimpl CallGraph, for: MatchOperator do
+  def build(%{left: left, right: right}, call_graph, module_defs, from_vertex) do
+    CallGraph.build(right, call_graph, module_defs, from_vertex)
+  end
+end
