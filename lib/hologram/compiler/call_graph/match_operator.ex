c8d51cbe836280d6a7b072aec8d15af2a770e99c
Bart Blast <bart@bartblast.com>
2021-11-15 14:50:57 +0100

    Remove redundant map key

--- lib/hologram/compiler/call_graph/match_operator.ex
+++ lib/hologram/compiler/call_graph/match_operator.ex
@@ -4,7 +4,7 @@ alias Hologram.Compiler.CallGraph
 alias Hologram.Compiler.IR.MatchOperator
 
 defimpl CallGraph, for: MatchOperator do
-  def build(%{left: _, right: right}, call_graph, module_defs, from_vertex) do
+  def build(%{right: right}, call_graph, module_defs, from_vertex) do
     CallGraph.build(right, call_graph, module_defs, from_vertex)
   end
 end
