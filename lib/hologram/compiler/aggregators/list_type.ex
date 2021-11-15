79d1755cdc4330b0137f7aab3c7f9271b71120b9
Bart Blast <bart@bartblast.com>
2021-11-15 14:18:16 +0100

    Add aggregator protocol implementation for list type

---
+++ lib/hologram/compiler/aggregators/list_type.ex
@@ -0,0 +1,8 @@
+alias Hologram.Compiler.Aggregator
+alias Hologram.Compiler.IR.ListType
+
+defimpl Aggregator, for: ListType do
+  def aggregate(%{data: data}, module_defs) do
+    Aggregator.aggregate(data, module_defs)
+  end
+end
