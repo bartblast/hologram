4a7bbc485cc680fd27cc82d76f7a1340335bdc40
Bart Blast <bart@bartblast.com>
2021-11-15 14:45:09 +0100

    Add aggregator protocol implementation for module attribute operator

---
+++ lib/hologram/compiler/aggregators/module_attribute_operator.ex
@@ -0,0 +1,6 @@
+alias Hologram.Compiler.Aggregator
+alias Hologram.Compiler.IR.ModuleAttributeOperator
+
+defimpl Aggregator, for: ModuleAttributeOperator do
+  def aggregate(_, module_defs), do: module_defs
+end
