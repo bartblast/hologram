eee63d3962d7c3abf7ccfee91032e509daae1f1f
Bart Blast <bart@bartblast.com>
2021-12-08 15:51:56 +0100

    Use application logger config when logging not supported expressions

--- lib/hologram/compiler/aggregators/not_supported_expression.ex
+++ lib/hologram/compiler/aggregators/not_supported_expression.ex
@@ -1,17 +1,20 @@
 # TODO: test
 
-require Logger
-
 alias Hologram.Compiler.Aggregator
 alias Hologram.Compiler.IR.NotSupportedExpression
 
 defimpl Aggregator, for: NotSupportedExpression do
+  require Logger
+
   def aggregate(ir, module_defs) do
+    logger_config = Application.fetch_env!(:logger, :console)
+    Logger.configure_backend(:console, logger_config)
+
     """
-    Not supported expression (compiler ignored it):
+    Not supported expression (compiler ignored it)
     #{inspect(ir)}
     """
-    |> Logger.debug()
+    |> Logger.notice()
 
     module_defs
   end
