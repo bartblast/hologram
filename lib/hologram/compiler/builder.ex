220e66abeeae4679c1da5ea42a41cd78c7564d03
Bart Blast <bart@bartblast.com>
2021-12-10 13:43:51 +0100

    Remove temporary log messages

--- lib/hologram/compiler/builder.ex
+++ lib/hologram/compiler/builder.ex
@@ -1,17 +1,10 @@
 defmodule Hologram.Compiler.Builder do
-  alias Hologram.Compiler.{Context, ModuleDefStore, JSEncoder, Opts, Pruner}
+  alias Hologram.Compiler.{Context, JSEncoder, Opts, Pruner}
   require Logger
 
   def build(module, module_defs, call_graph) do
-    Logger.debug("Started Pruner.prune/2 for #{module}")
-    pruned_module_defs = Pruner.prune(module, module_defs, call_graph)
-    Logger.debug("Finished Pruner.prune/2 for #{module}")
-
-    Logger.debug("Started encode_module_defs/1 for #{module}")
-    result = encode_module_defs(pruned_module_defs)
-    Logger.debug("Finished encode_module_defs/1 for #{module}")
-
-    result
+    Pruner.prune(module, module_defs, call_graph)
+    |> encode_module_defs()
   end
 
   defp encode_module_defs(module_defs) do
