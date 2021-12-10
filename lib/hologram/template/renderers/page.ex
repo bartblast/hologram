eda5b28f3544ec1eecbc298f09021baa852ef065
Bart Blast <bart@bartblast.com>
2021-12-10 16:02:51 +0100

    Refactor page renderer

--- lib/hologram/template/renderers/page.ex
+++ lib/hologram/template/renderers/page.ex
@@ -3,22 +3,24 @@ alias Hologram.Template.{Builder, Renderer}
 alias Hologram.Utils
 
 defimpl Renderer, for: Atom do
-  def render(module, _params, slots) do
-    bindings = aggregate_bindings(module)
-    layout = module.layout()
+  def render(page_module, _params, _slots) do
+    layout_module = page_module.layout()
+    bindings = aggregate_bindings(page_module, layout_module)
+    page_template = Builder.build(page_module)
+    layout_template = Builder.build(layout_module)
 
-    Builder.build(module, layout)
-    |> Renderer.render(bindings, slots)
+    Renderer.render(layout_template, bindings, default: page_template)
     |> Utils.prepend("<!DOCTYPE html>\n")
   end
 
-  defp aggregate_bindings(module) do
-    class_name = Helpers.class_name(module)
-    digest = Reflection.get_page_digest(module)
+  defp aggregate_bindings(page_module, layout_module) do
+    class_name = Helpers.class_name(page_module)
+    digest = Reflection.get_page_digest(page_module)
 
     # DEFER: pass page params to init function
     bindings =
-      module.init()
+      layout_module.init()
+      |> Map.merge(page_module.init())
       |> Map.put(:context, %{
         __class__: class_name,
         __digest__: digest
--- test/hologram/template/renderers/page_test.exs
+++ test/hologram/template/renderers/page_test.exs
@@ -6,10 +6,12 @@ defmodule Hologram.Template.Renderer.PageTest do
 
   setup_all do
     on_exit(&compile_pages/0)
+    :ok
   end
 
   test "render/2" do
-    compile_pages("test/fixtures/template/renderers/page_renderer")
+    "#{File.cwd!()}/test/fixtures/template/renderers/page_renderer"
+    |> compile_pages()
 
     module = Hologram.Test.Fixtures.Template.PageRenderer.Module1
     digest = Reflection.get_page_digest(module)
@@ -24,7 +26,7 @@ defmodule Hologram.Template.Renderer.PageTest do
         <script src="/hologram/runtime.js"></script>
     <script src="/hologram/page-#{digest}.js"></script>
     <script>
-      Hologram.run(Elixir_Hologram_Test_Fixtures_Template_PageRenderer_Module1, "{ type: 'map', data: { '~atom[a]': { type: 'integer', value: 123 }, '~atom[context]': { type: 'map', data: { '~atom[__class__]': { type: 'string', value: 'Elixir_Hologram_Test_Fixtures_Template_PageRenderer_Module1' }, '~atom[__digest__]': { type: 'string', value: '#{digest}' } } } } }")
+      Hologram.run(Elixir_Hologram_Test_Fixtures_Template_PageRenderer_Module1, "{ type: 'map', data: { '~atom[a]': { type: 'integer', value: 123 }, '~atom[b]': { type: 'integer', value: 987 }, '~atom[context]': { type: 'map', data: { '~atom[__class__]': { type: 'string', value: 'Elixir_Hologram_Test_Fixtures_Template_PageRenderer_Module1' }, '~atom[__digest__]': { type: 'string', value: '#{digest}' } } } } }")
     </script>
       </head>
       <body>
