0396c1e3da558c0ea4ec07fb189986b7a1bd2390
Bart Blast <bart@bartblast.com>
2021-12-10 21:26:43 +0100

    Implement template store gen server

--- lib/hologram/compiler/template_store.ex
+++ lib/hologram/compiler/template_store.ex
@@ -3,23 +3,40 @@
 defmodule Hologram.Compiler.TemplateStore do
   use GenServer
 
+  alias Hologram.Compiler.Reflection
+  alias Hologram.Utils
+
   @table_name :hologram_template_store
 
-  def create do
-    :ets.new(@table_name, [:public, :named_table])
-    start_link()
-  end
-
-  def destroy do
-    Process.whereis(__MODULE__) |> Process.exit(:normal)
-    :ets.delete(@table_name)
-  end
-
   def init(_) do
-    {:ok, %{}}
+    if :ets.whereis(@table_name) == :undefined do
+      :ets.new(@table_name, [:public, :named_table])
+    end
+
+    populate_table()
+
+    {:ok, nil}
   end
 
-  def start_link do
+  def get(module) do
+    GenServer.call(__MODULE__, {:get, module})
+  end
+
+  def handle_call({:get, module}, _, _) do
+    [{^module, vdom}] = :ets.lookup(@table_name, module)
+    {:reply, vdom, nil}
+  end
+
+  defp populate_table do
+    Reflection.template_store_dump_path()
+    |> File.read!()
+    |> Utils.deserialize()
+    |> Enum.each(fn {module, vdom} ->
+      :ets.insert(@table_name, {module, vdom})
+    end)
+  end
+
+  def start_link(_) do
     GenServer.start_link(__MODULE__, [], name: __MODULE__)
   end
 end
