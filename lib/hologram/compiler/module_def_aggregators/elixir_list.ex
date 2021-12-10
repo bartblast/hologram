85d54cb8898fd7d5771a171537ae640c8ced9b28
Bart Blast <bart@bartblast.com>
2021-12-10 01:21:56 +0100

    Do not parallelize Elixir lists in module def aggregator

--- lib/hologram/compiler/module_def_aggregators/elixir_list.ex
+++ lib/hologram/compiler/module_def_aggregators/elixir_list.ex
@@ -1,9 +1,7 @@
 alias Hologram.Compiler.ModuleDefAggregator
-alias Hologram.Utils
 
 defimpl ModuleDefAggregator, for: List do
   def aggregate(list) do
-    Enum.map(list, &Task.async(fn -> ModuleDefAggregator.aggregate(&1) end))
-    |> Utils.await_tasks()
+    Enum.each(list, &ModuleDefAggregator.aggregate/1)
   end
 end
