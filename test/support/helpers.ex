0cf1e3ba01e1fe75543ffd35b5521280c5e591a2
Bart Blast <bart@bartblast.com>
2021-12-11 17:17:52 +0100

    Refaction build_templates_by_path/1 test helper

--- test/support/helpers.ex
+++ test/support/helpers.ex
@@ -1,21 +1,14 @@
 defmodule Hologram.Test.Helpers do
   alias Hologram.Compiler.Reflection
   alias Hologram.Template.Builder, as: TemplateBuilder
-  alias Hologram.Utils
   alias Mix.Tasks.Compile.Hologram, as: Task
 
   @default_pages_path Reflection.pages_path()
 
   def build_templates_by_path(path) do
-    pages = Reflection.list_pages(pages_path: path)
-    components = Reflection.list_components(components_path: path)
-    layouts = Reflection.list_layouts(layouts_path: path)
-
-    # DEFER: consider - this code is very similar to Mix.Tasks.Compile.Hologram.build_templates/1
-    pages ++ components ++ layouts
-    |> Utils.map_async(&{&1, TemplateBuilder.build(&1)})
-    |> Utils.await_tasks()
-    |> Enum.into(%{})
+    [pages_path: path, components_path: path, layouts_path: path]
+    |> Reflection.list_templatables()
+    |> TemplateBuilder.build_all()
   end
 
   # When compile_pages/1 test helper is used, the router is recompiled with the pages found in the given pages_path.
