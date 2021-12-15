68279baadab692273df1c466cbf3c7a8fca6bb98
Bart Blast <bart@bartblast.com>
2021-12-15 15:37:17 +0100

    Move compiler task to mix dir

--- lib/mix/hologram.tmp.ex
+++
@@ -1,9 +0,0 @@
-defmodule Mix.Tasks.Hologram.Tmp do
-  use Mix.Task
-
-  @impl Mix.Task
-  def run(args) do
-    IO.puts("in Mix.Tasks.Hologram.Tmp")
-    Mix.shell().info(Enum.join(args, " "))
-  end
-end
