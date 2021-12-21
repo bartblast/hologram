9252c4c84c27031db4738033c2e3d38616c62ef8
Bart Blast <bart@bartblast.com>
2021-12-21 16:43:49 +0100

    Implement template tokenizer

---
+++ lib/hologram/template/tokenizer.ex
@@ -0,0 +1,60 @@
+# DEFER: test
+
+defmodule Hologram.Template.Tokenizer do
+  def tokenize(""), do: []
+
+  def tokenize(" " <> rest) do
+    [{:whitespace, " "} | tokenize(rest)]
+  end
+
+  def tokenize("\r" <> rest) do
+    [{:whitespace, "\r"} | tokenize(rest)]
+  end
+
+  def tokenize("\n" <> rest) do
+    [{:whitespace, "\n"} | tokenize(rest)]
+  end
+
+  def tokenize("\t" <> rest) do
+    [{:whitespace, "\t"} | tokenize(rest)]
+  end
+
+  def tokenize("<" <> rest) do
+    [{:symbol, :<} | tokenize(rest)]
+  end
+
+  def tokenize(">" <> rest) do
+    [{:symbol, :>} | tokenize(rest)]
+  end
+
+  def tokenize("/" <> rest) do
+    [{:symbol, :/} | tokenize(rest)]
+  end
+
+  def tokenize("=" <> rest) do
+    [{:symbol, :=} | tokenize(rest)]
+  end
+
+  def tokenize("\"" <> rest) do
+    [{:symbol, :"\""} | tokenize(rest)]
+  end
+
+  def tokenize("'" <> rest) do
+    [{:symbol, :"'"} | tokenize(rest)]
+  end
+
+  def tokenize("{" <> rest) do
+    [{:symbol, :"{"} | tokenize(rest)]
+  end
+
+  def tokenize("}" <> rest) do
+    [{:symbol, :"}"} | tokenize(rest)]
+  end
+
+  def tokenize(rest) do
+    excluded_chars = "\s<>/=\""
+    regex = ~r/\A([^#{Regex.escape(excluded_chars)}]+)(.*)\z/msu
+    [_, string, rest] = Regex.run(regex, rest)
+    [{:string, string} | tokenize(rest)]
+  end
+end
