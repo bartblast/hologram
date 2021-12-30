9b302af85b9662d81eff823b5b4b4c81653d2ac1
Bart Blast <bart@bartblast.com>
2021-12-30 19:45:45 +0100

    Implement Runtime.reload/0 helper

---
+++ lib/hologram/runtime.ex
@@ -0,0 +1,10 @@
+# DEFER: test
+
+defmodule Hologram.Runtime do
+  alias Hologram.Runtime.{PageDigestStore, TemplateStore}
+  
+  def reload do
+    PageDigestStore.populate_table()
+    TemplateStore.populate_table()
+  end
+end
