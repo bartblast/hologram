281c463ec77c9e3a2d834247f12cc1e245ed31fa
Bart Blast <bart@bartblast.com>
2021-12-30 17:10:41 +0100

    Add client runtime router stub

--- assets/js/hologram.js
+++ assets/js/hologram.js
@@ -4,6 +4,7 @@ import PatternMatcher from "./hologram/pattern_matcher";
 import Runtime from "./hologram/runtime";
 
 // transpiled Elixir standard library
+
 import Elixir_Ecto_Changeset from "./hologram/elixir/ecto/changeset";
 import Elixir_Enum from "./hologram/elixir/enum";
 import Elixir_IO from "./hologram/elixir/io";
@@ -13,8 +14,22 @@ import Elixir_Map from "./hologram/elixir/map";
 import Elixir_Kernel_SpecialForms from "./hologram/elixir/kernel/special_forms";
 import Elixir_String from "./hologram/elixir/string";
 
+window.Elixir_Ecto_Changeset = Elixir_Ecto_Changeset;
+window.Elixir_Enum = Elixir_Enum;
+window.Elixir_IO = Elixir_IO;
+window.Elixir_Kernel = Elixir_Kernel;
+window.Elixir_Kernel_SpecialForms = Elixir_Kernel_SpecialForms;
+window.Elixir_Keyword = Elixir_Keyword;
+window.Elixir_Map = Elixir_Map;
+window.Elixir_String = Elixir_String;
+
 // transpiled Hologram runtime
+
+import Elixir_Hologram_Router from "./hologram/elixir/hologram/router"
 import Elixir_Hologram_Runtime_JS from "./hologram/elixir/hologram/runtime/js"
+
+window.Elixir_Hologram_Router = Elixir_Hologram_Router
+window.Elixir_Hologram_Runtime_JS = Elixir_Hologram_Runtime_JS
 
 export default class Hologram {
   // Covered implicitely in E2E tests.
@@ -51,18 +66,5 @@ export default class Hologram {
     return PatternMatcher.isFunctionArgsPatternMatched(params, args);
   }
 }
-
-// transpiled Elixir standard library
-window.Elixir_Ecto_Changeset = Elixir_Ecto_Changeset;
-window.Elixir_Enum = Elixir_Enum;
-window.Elixir_IO = Elixir_IO;
-window.Elixir_Kernel = Elixir_Kernel;
-window.Elixir_Kernel_SpecialForms = Elixir_Kernel_SpecialForms;
-window.Elixir_Keyword = Elixir_Keyword;
-window.Elixir_Map = Elixir_Map;
-window.Elixir_String = Elixir_String;
-
-// transpiled Hologram runtime
-window.Elixir_Hologram_Runtime_JS = Elixir_Hologram_Runtime_JS
 
 window.Hologram = Hologram;
---
+++ assets/js/hologram/elixir/hologram/router.js
@@ -0,0 +1,8 @@
+"use strict";
+
+export default class Router {
+  // TODO: implement
+  static static_path(file_path) {
+    return file_path
+  }
+}
