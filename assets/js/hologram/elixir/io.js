133d0db1e2cce0e6a241841398cd71576797bc7e
Bart Blast <bart@bartblast.com>
2022-01-04 15:12:50 +0100

    Add JS implementation of Elixir IO.puts/2

--- assets/js/hologram/elixir/io.js
+++ assets/js/hologram/elixir/io.js
@@ -1,9 +1,17 @@
 "use strict";
 
+import Type from "../type";
+
 export default class IO {
   // DEFER: implement inspect/2 and inspect/3
   static inspect(val) {
     console.debug(val)
     return val
+  }
+
+  // DEFER: test and consider the device param
+  static puts(str) {
+    console.log(str)
+    return Type.atom("ok")
   }
 }
