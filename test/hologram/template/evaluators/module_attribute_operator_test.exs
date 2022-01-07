84ff1589d685bb4db1894a941bbeb94f43641e15
Bart Blast <bart@bartblast.com>
2022-01-08 00:11:01 +0100

    Add additional test case

--- test/hologram/template/evaluators/module_attribute_operator_test.exs
+++ test/hologram/template/evaluators/module_attribute_operator_test.exs
@@ -4,12 +4,22 @@ defmodule Hologram.Template.Evaluator.ModuleAttributeOperatorTest do
   alias Hologram.Compiler.IR.ModuleAttributeOperator
   alias Hologram.Template.Evaluator
 
-  test "evaluate/2" do
+  test "existing binding" do
     ir = %ModuleAttributeOperator{name: :a}
     bindings = %{a: 123}
 
     result = Evaluator.evaluate(ir, bindings)
     expected = 123
+
+    assert result == expected
+  end
+
+  test "non-existing binding" do
+    ir = %ModuleAttributeOperator{name: :b}
+    bindings = %{a: 123}
+
+    result = Evaluator.evaluate(ir, bindings)
+    expected = nil
 
     assert result == expected
   end
