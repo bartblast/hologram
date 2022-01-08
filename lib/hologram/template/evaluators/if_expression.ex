5afd3b2a3fedbd66cad02bbd44a8bde30bcde868
Bart Blast <bart@bartblast.com>
2022-01-08 13:07:34 +0100

    Fix if expression IR evaluation

--- lib/hologram/template/evaluators/if_expression.ex
+++ lib/hologram/template/evaluators/if_expression.ex
@@ -2,11 +2,10 @@ alias Hologram.Compiler.IR.IfExpression
 alias Hologram.Template.Evaluator
 
 defimpl Evaluator, for: IfExpression do
-  def evaluate(%{condition: condition, do: do_expr, else: else_expr}, bindings) do
-    if Evaluator.evaluate(condition, bindings) do
-      Evaluator.evaluate(do_expr, bindings)
-    else
-      Evaluator.evaluate(else_expr, bindings)
-    end
+  def evaluate(%{ast: ast}, bindings) do
+    bindings = Map.to_list(bindings)
+    
+    Code.eval_quoted(ast, bindings)
+    |> elem(0)
   end
 end
--- test/hologram/template/evaluators/if_expression_test.exs
+++ test/hologram/template/evaluators/if_expression_test.exs
@@ -1,34 +1,16 @@
 defmodule Hologram.Template.Evaluator.IfExpressionTest do
   use Hologram.Test.UnitCase, async: true
 
-  alias Hologram.Compiler.IR.{BooleanType, IfExpression, Variable}
+  alias Hologram.Compiler.{Context, Transformer}
   alias Hologram.Template.Evaluator
 
-  @bindings %{a: 1, b: 2}
+  test "evaluate/2" do
+    bindings = %{a: 1, b: 2}
+    code = "if true, do: a, else: b"
+    ast = ast(code)
+    ir = Transformer.transform(ast, %Context{})
+    result = Evaluator.evaluate(ir, %{a: 123, b: 234})
 
-  test "evaluates do expression" do
-    ir = %IfExpression{
-      condition: %BooleanType{value: true},
-      do: %Variable{name: :a},
-      else: %Variable{name: :b}
-    }
-
-    result = Evaluator.evaluate(ir, @bindings)
-    expected = 1
-
-    assert result == expected
-  end
-
-  test "evaluates else expression" do
-    ir = %IfExpression{
-      condition: %BooleanType{value: false},
-      do: %Variable{name: :a},
-      else: %Variable{name: :b}
-    }
-
-    result = Evaluator.evaluate(ir, @bindings)
-    expected = 2
-
-    assert result == expected
+    assert result == 123
   end
 end
