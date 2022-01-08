45ecdfbdab6e513f680ceac2ab1383a7d5439028
Bart Blast <bart@bartblast.com>
2022-01-08 19:11:18 +0100

    Fix if expression IR struct evaluator

--- lib/hologram/template/evaluators/if_expression.ex
+++ lib/hologram/template/evaluators/if_expression.ex
@@ -2,10 +2,9 @@ alias Hologram.Compiler.IR.IfExpression
 alias Hologram.Template.Evaluator
 
 defimpl Evaluator, for: IfExpression do
+  alias Hologram.Compiler.Evaluator, as: ASTEvaluator
+
   def evaluate(%{ast: ast}, bindings) do
-    bindings = Map.to_list(bindings)
-    
-    Code.eval_quoted(ast, bindings)
-    |> elem(0)
+    ASTEvaluator.evaluate(ast, bindings)
   end
 end
--- test/hologram/template/evaluators/if_expression_test.exs
+++ test/hologram/template/evaluators/if_expression_test.exs
@@ -5,12 +5,12 @@ defmodule Hologram.Template.Evaluator.IfExpressionTest do
   alias Hologram.Template.Evaluator
 
   test "evaluate/2" do
-    bindings = %{a: 1, b: 2}
-    code = "if true, do: a, else: b"
+    bindings = %{a: 1, b: 2, c: 3}
+    code = "if @a, do: @b, else: @c"
     ast = ast(code)
     ir = Transformer.transform(ast, %Context{})
-    result = Evaluator.evaluate(ir, %{a: 123, b: 234})
+    result = Evaluator.evaluate(ir, bindings)
 
-    assert result == 123
+    assert result == 2
   end
 end
