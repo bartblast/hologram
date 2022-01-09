4aec5f4bed211607289ae74718aca58a8e70abb6
Bart Blast <bart@bartblast.com>
2022-01-09 13:56:22 +0100

    Make @id and @class props optional in UI Link component

--- test/e2e/ui_test.exs
+++ test/e2e/ui_test.exs
@@ -1,14 +1,18 @@
 defmodule Hologram.Features.UITest do
   use Hologram.Test.E2ECase, async: false
-  
+
   @moduletag :e2e
 
-  feature "link", %{session: session} do
-    session
-    |> visit("/e2e/page-6")
-    |> click(css("#test-id"))
-    |> assert_has(css("h1", text: "Page 7"))
+  describe "link" do
+    feature "redirects to another page", %{session: session} do
+      session
+      |> visit("/e2e/page-6")
+      |> click(css("#test-id"))
+      |> assert_has(css("h1", text: "Page 7"))
 
-    assert current_path(session) == "/e2e/page-7"
+      assert current_path(session) == "/e2e/page-7"
+    end
+
+    # DEFER: test @id and @class optional props
   end
 end
