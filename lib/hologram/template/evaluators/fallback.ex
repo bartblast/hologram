de11314867cf81a15d1be054600920f95ce8cb62
Bart Blast <bart@bartblast.com>
2022-01-10 17:21:05 +0100

    Remove template evaluator protocol fallback implementation

--- lib/hologram/template/evaluators/fallback.ex
+++
@@ -1,5 +0,0 @@
-alias Hologram.Template.Evaluator
-
-defimpl Evaluator, for: Any do
-  def evaluate(value, _), do: value
-end
--- test/hologram/template/evaluators/fallback_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule Hologram.Template.Evaluator.FallbackTest do
-  use Hologram.Test.UnitCase, async: true
-  alias Hologram.Template.Evaluator
-
-  test "evaluate/2" do
-    assert Evaluator.evaluate("str", %{}) == "str"
-  end
-end
