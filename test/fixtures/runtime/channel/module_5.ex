986da800d394fccd02f15298f7a343693a31acff
Bart Blast <bart@bartblast.com>
2022-01-10 17:34:39 +0100

    Fix channel unit test

--- test/fixtures/runtime/channel/module_5.ex
+++ test/fixtures/runtime/channel/module_5.ex
@@ -2,6 +2,7 @@ defmodule Hologram.Test.Fixtures.Runtime.Channel.Module5 do
   use Hologram.Page
 
   route "/test-route-5"
+  layout Hologram.Test.Fixtures.Runtime.Channel.Module8
 
   def init do
     %{}
---
+++ test/fixtures/runtime/channel/module_8.ex
@@ -0,0 +1,10 @@
+defmodule Hologram.Test.Fixtures.Runtime.Channel.Module8 do
+  use Hologram.Layout
+
+  def init, do: %{}
+
+  def template do
+    ~H"""
+    """
+  end
+end
