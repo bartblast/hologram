a75e3b68b52a5a587808869b47c32b8c0567dd08
Bart Blast <bart@bartblast.com>
2022-01-10 15:35:54 +0100

    Fix page renderer test

--- test/hologram/template/renderers/page_test.exs
+++ test/hologram/template/renderers/page_test.exs
@@ -21,8 +21,9 @@ defmodule Hologram.Template.Renderer.PageTest do
     <!DOCTYPE html>
     <html>
       <head>
-        <script src="/hologram/runtime.js"></script>
-    <script src="/hologram/page-#{digest}.js"></script>
+        <script src="/hologram/manifest.js"></script>
+    <script src="/hologram/runtime.js"></script>
+    <script src="/hologram/page-b3c30aef343636aa19c36185d7589ed4.js"></script>
     <script>
       Hologram.run(Elixir_Hologram_Test_Fixtures_Template_PageRenderer_Module1, "{ type: 'map', data: { '~atom[a]': { type: 'integer', value: 123 }, '~atom[b]': { type: 'integer', value: 987 }, '~atom[context]': { type: 'map', data: { '~atom[__class__]': { type: 'string', value: 'Elixir_Hologram_Test_Fixtures_Template_PageRenderer_Module1' }, '~atom[__digest__]': { type: 'string', value: '#{digest}' } } } } }")
     </script>
