cea715f58d2397454b323f0c6b09ca70b0f3fb47
Bart Blast <bart@bartblast.com>
2022-02-11 13:30:14 +0100

    Make sure Hologram runtime is rebuilt before E2E tests

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,4 +1,13 @@
+alias Hologram.Compiler.Reflection
+alias Hologram.E2E.Web.Endpoint
+
 ExUnit.start()
 
+Reflection.release_static_path() <> "/hologram/runtime*"
+|> Path.wildcard()
+|> Enum.each(&File.rm!/1)
+
+Mix.Task.run("assets.build")
+
 {:ok, _} = Application.ensure_all_started(:wallaby)
-Application.put_env(:wallaby, :base_url, Hologram.E2E.Web.Endpoint.url())
+Application.put_env(:wallaby, :base_url, Endpoint.url())
