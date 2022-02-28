fd6641100ba3ab9846e7ab71e6c74644f678928f
Bart Blast <bart@bartblast.com>
2022-02-28 13:56:42 +0100

    Fix holo.assets.build task

--- lib/mix/tasks/holo.assets.build.ex
+++ lib/mix/tasks/holo.assets.build.ex
@@ -1,6 +1,6 @@
 defmodule Mix.Tasks.Holo.Assets.Build do
   def run(_) do
-    Mix.Shell.cmd("npm install", cd: "deps/hologram/assets")
+    Mix.Shell.cmd("npm install", [cd: "deps/hologram/assets", quiet: true], nil)
     Mix.Task.run("esbuild hologram --minify")
   end
 end
