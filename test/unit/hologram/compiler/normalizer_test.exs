60f1b0e28143545b1a51f0b7df2d3b41f415e931
Bart Blast <bart@bartblast.com>
2022-03-07 13:55:36 +0100

    Improve normalizer unit tests

--- test/unit/hologram/compiler/normalizer_test.exs
+++ test/unit/hologram/compiler/normalizer_test.exs
@@ -2,45 +2,6 @@ defmodule Hologram.Compiler.NormalizerTest do
   use Hologram.Test.UnitCase, async: true
   alias Hologram.Compiler.{Normalizer, Parser}
 
-  describe "do expression" do
-    test "without nested block" do
-      code = """
-      def test do
-        :ok
-      end
-      """
-
-      ast = Parser.parse!(code)
-      result = Normalizer.normalize(ast)
-
-      expected = {:def, [line: 1], [{:test, [line: 1], nil}, [do: {:__block__, [], [:ok]}]]}
-
-      assert result == expected
-    end
-
-    test "with nested block" do
-      code = """
-      def test do
-        nested do
-          1
-        end
-      end
-      """
-
-      ast = Parser.parse!(code)
-      result = Normalizer.normalize(ast)
-
-      expected =
-        {:def, [line: 1],
-         [
-           {:test, [line: 1], nil},
-           [do: {:__block__, [], [{:nested, [line: 2], [[do: {:__block__, [], [1]}]]}]}]
-         ]}
-
-      assert result == expected
-    end
-  end
-
   describe "case expression" do
     test "clause with single expression" do
       code = """
@@ -67,7 +28,7 @@ defmodule Hologram.Compiler.NormalizerTest do
       assert result == expected
     end
 
-    test "clause with block" do
+    test "clause with multiple expressions" do
       code = """
       case x do
         1 ->
@@ -90,7 +51,94 @@ defmodule Hologram.Compiler.NormalizerTest do
     end
   end
 
-  test "other expression" do
+  describe "function definition" do
+    test "with single expression in do block" do
+      code = """
+      def test do
+        :ok
+      end
+      """
+
+      ast = Parser.parse!(code)
+      result = Normalizer.normalize(ast)
+
+      expected = {:def, [line: 1], [{:test, [line: 1], nil}, [do: {:__block__, [], [:ok]}]]}
+
+      assert result == expected
+    end
+
+    test "with single expression in do keyword" do
+      code = "def test, do: :ok"
+      ast = Parser.parse!(code)
+      result = Normalizer.normalize(ast)
+
+      expected = {:def, [line: 1], [{:test, [line: 1], nil}, [do: {:__block__, [], [:ok]}]]}
+
+      assert result == expected
+    end
+
+    test "with multiple expressions" do
+      code = """
+      def test do
+        1
+        2
+      end
+      """
+
+      ast = Parser.parse!(code)
+      result = Normalizer.normalize(ast)
+
+      expected = {:def, [line: 1], [{:test, [line: 1], nil}, [do: {:__block__, [], [1, 2]}]]}
+
+      assert result == expected
+    end
+  end
+
+  describe "macro call" do
+    test "with single expression in do block" do
+      code = """
+      test do
+        :ok
+      end
+      """
+
+      ast = Parser.parse!(code)
+      result = Normalizer.normalize(ast)
+
+      expected = {:test, [line: 1], [[do: {:__block__, [], [:ok]}]]}
+
+      assert result == expected
+    end
+
+    test "with single expression in do keyword" do
+      code = "test do: :ok"
+
+      ast = Parser.parse!(code)
+      result = Normalizer.normalize(ast)
+
+      expected = {:test, [line: 1], [[do: {:__block__, [], [:ok]}]]}
+
+      assert result == expected
+    end
+
+    test "with multiple expressions" do
+      code = """
+      test do
+        1
+        2
+      end
+      """
+
+      ast = Parser.parse!(code)
+      result = Normalizer.normalize(ast)
+
+      expected = {:test, [line: 1], [[do: {:__block__, [], [1, 2]}]]}
+
+      assert result == expected
+    end
+  end
+
+  test "other types of expressions" do
     code = "1 + 2"
     ast = Parser.parse!(code)
     result = Normalizer.normalize(ast)
