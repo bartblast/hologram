d1c32e89bb97f642df3f3a46ab5d3df659867254
Bart Blast <bart@bartblast.com>
2022-03-08 13:05:58 +0100

    Simplify if expressions AST to IR transformer and its tests

--- lib/hologram/compiler/transformers/if_expression_transformer.ex
+++ lib/hologram/compiler/transformers/if_expression_transformer.ex
@@ -1,27 +1,13 @@
 defmodule Hologram.Compiler.IfExpressionTransformer do
-  alias Hologram.Compiler.{Context, Helpers, Transformer}
-  alias Hologram.Compiler.IR.{Block, IfExpression}
+  alias Hologram.Compiler.{Context, Transformer}
+  alias Hologram.Compiler.IR.IfExpression
 
-  def transform({:if, _, [condition, [do: do_ast]]} = ast, %Context{} = context) do
-    do_exprs = Helpers.get_block_expressions(do_ast)
-    build_if_exprs(condition, do_exprs, [nil], ast, context)
-  end
-
-  def transform({:if, _, [condition, [do: do_ast, else: else_ast]]} = full_ast, %Context{} = context) do
-    do_exprs = Helpers.get_block_expressions(do_ast)
-    else_exprs = Helpers.get_block_expressions(else_ast)
-    build_if_exprs(condition, do_exprs, else_exprs, full_ast, context)
-  end
-
-  defp build_if_exprs(condition, do_exprs, else_exprs, full_ast, context) do
-    condition = Transformer.transform(condition, context)
-
-    do_exprs = Enum.map(do_exprs, &Transformer.transform(&1, context))
-    do_block = %Block{expressions: do_exprs}
-
-    else_exprs = Enum.map(else_exprs, &Transformer.transform(&1, context))
-    else_block = %Block{expressions: else_exprs}
-
-    %IfExpression{condition: condition, do: do_block, else: else_block, ast: full_ast}
+  def transform({:if, _, [condition, [do: do_block, else: else_block]]} = full_ast, %Context{} = context) do
+    %IfExpression{
+      condition: Transformer.transform(condition, context),
+      do: Transformer.transform(do_block, context),
+      else: Transformer.transform(else_block, context),
+      ast: full_ast
+    }
   end
 end
