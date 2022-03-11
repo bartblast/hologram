3c7a6173e5f60613f6f1d8317a7fbe61c94b1997
Bart Blast <bart@bartblast.com>
2022-03-11 12:09:29 +0100

    Implement binding variable access JS encoding

--- lib/hologram/compiler/js_encoders/binding.ex
+++ lib/hologram/compiler/js_encoders/binding.ex
@@ -1,5 +1,5 @@
 alias Hologram.Compiler.{Context, JSEncoder, MapKeyEncoder, Opts}
-alias Hologram.Compiler.IR.{Binding, MapAccess, ParamAccess}
+alias Hologram.Compiler.IR.{Binding, MapAccess, ParamAccess, VariableAccess}
 
 defimpl JSEncoder, for: Binding do
   def encode(%{access_path: access_path, name: name}, %Context{} = context, %Opts{} = opts) do
@@ -17,5 +17,9 @@ defimpl JSEncoder, for: Binding do
 
   defp encode_part(%ParamAccess{index: index}, _context, _opts) do
     "arguments[#{index}]"
+  end
+
+  defp encode_part(%VariableAccess{name: name}, _context, _opts) do
+    to_string(name)
   end
 end
--- test/unit/hologram/compiler/js_encoders/binding_test.exs
+++ test/unit/hologram/compiler/js_encoders/binding_test.exs
@@ -2,7 +2,7 @@ defmodule Hologram.Compiler.JSEncoder.BindingTest do
   use Hologram.Test.UnitCase, async: true
 
   alias Hologram.Compiler.{Context, JSEncoder, Opts}
-  alias Hologram.Compiler.IR.{AtomType, Binding, MapAccess, ParamAccess}
+  alias Hologram.Compiler.IR.{AtomType, Binding, MapAccess, ParamAccess, VariableAccess}
 
   test "map access" do
     ir = %Binding{access_path: [%MapAccess{key: %AtomType{value: :x}}], name: :abc}
@@ -18,6 +18,15 @@ defmodule Hologram.Compiler.JSEncoder.BindingTest do
 
     result = JSEncoder.encode(ir, %Context{}, %Opts{})
     expected = "let abc = arguments[2];"
+
+    assert result == expected
+  end
+
+  test "variable access" do
+    ir = %Binding{access_path: [%VariableAccess{name: :x}], name: :abc}
+
+    result = JSEncoder.encode(ir, %Context{}, %Opts{})
+    expected = "let abc = x;"
 
     assert result == expected
   end
