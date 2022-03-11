da605eca26e647153a85ee3bc57c852865d61e0e
Bart Blast <bart@bartblast.com>
2022-03-11 14:21:27 +0100

    Add newline

--- lib/hologram/compiler/js_encoders/match_operator.ex
+++ lib/hologram/compiler/js_encoders/match_operator.ex
@@ -17,25 +17,8 @@ defimpl JSEncoder, for: MatchOperator do
     |> Enum.join(";\n")
   end
 
-  defp convert_ir(path, value) when is_list(path) do
-    Enum.reduce(path, value, &convert_ir/2)
-  end
-
-  defp convert_ir(%MapAccess{key: key}, ir) do
-    %DotOperator{left: ir, right: key}
-  end
-
-  defp convert_ir(%TupleAccess{index: index}, ir) do
-    %FunctionCall{
-      module: Kernel,
-      function: :elem,
-      args: [ir, %IntegerType{value: index}]
-    }
-  end
-
   defp encode_binding({name, path}, right, context, opts) do
-    let_statement = if name in context.block_bindings, do: "", else: "let "
-    ir = convert_ir(path, right)
-    let_statement <> "#{name} = " <> JSEncoder.encode(ir, context, opts)
+  #   let_statement = if name in context.block_bindings, do: "", else: "let "
+  #   let_statement <> "#{name} = " <> JSEncoder.encode(ir, context, opts)
   end
 end
