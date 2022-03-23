d43d370730bf487a5ddc5a31a9089da949305043
Bart Blast <bart@bartblast.com>
2022-03-23 13:06:35 +0100

    Cleanup actions E2E tests

--- test/e2e/lib/hologram_e2e_web/pages/runtime/actions_page.ex
+++ test/e2e/lib/hologram_e2e_web/pages/runtime/actions_page.ex
@@ -1,8 +1,8 @@
-defmodule HologramE2E.Page1 do
+defmodule HologramE2E.Runtime.ActionsPage do
   use Hologram.Page
   alias HologramE2E.Component3, warn: false
 
-  route "/e2e/page-1"
+  route "/e2e/runtime/actions"
 
   def init do
     %{
@@ -14,28 +14,26 @@ defmodule HologramE2E.Page1 do
 
   def template do
     ~H"""
-    <h1>Page 1</h1><br />
-
-    <button id="page-1-button-1" on:click="action_1">Action 1</button>
-    <button id="page-1-button-2" on:click={:action_2}>Action 2</button>
-    <button id="page-1-button-3" on:click={:action_3, a: 5, b: 6}>Action 3</button>
-    <button id="page-1-button-4" on:click={:component_3_id, :component_3_action_1}>Component 3 Action 1</button>
-    <button id="page-1-button-5" on:click={:component_3_id, :component_3_action_2, a: 5, b: 6}>Component 3 Action 2</button>
-    <button id="page-1-button-6" on:click="action_4">Action 4</button>
-    <button id="page-1-button-7" on:click="action_5">Action 5</button>
-    <button id="page-1-button-8" on:click="action_6">Action 6</button>
-    <button id="page-1-button-9" on:click="action_7">Action 7</button>
-    <button id="page-1-button-10" on:click="action_8">Action 8</button>
-    <button id="page-1-button-11" on:click="action_9">Action 9</button>
-    <button id="page-1-button-12" on:click="action_10">Action 10</button>
-    <button id="page-1-button-13" on:click={:component_3_id, :component_3_action_3}>Component 3 Action 3</button>
-    <button id="page-1-button-14" on:click={:page, :action_11}>Action 11</button>
-    <button id="page-1-button-15" on:click={:layout, :default_layout_action_1}>Layout Action 1</button>
-    <button id="page-1-button-16" on:click="action_12">Action 12</button>
-    <button id="page-1-button-17" on:click="action_13">Action 13</button>
+    <button id="button-1" on:click="action_1">Action 1</button>
+    <button id="button-2" on:click={:action_2}>Action 2</button>
+    <button id="button-3" on:click={:action_3, a: 5, b: 6}>Action 3</button>
+    <button id="button-4" on:click={:component_3_id, :component_3_action_1}>Component 3 Action 1</button>
+    <button id="button-5" on:click={:component_3_id, :component_3_action_2, a: 5, b: 6}>Component 3 Action 2</button>
+    <button id="button-6" on:click="action_4">Action 4</button>
+    <button id="button-7" on:click="action_5">Action 5</button>
+    <button id="button-8" on:click="action_6">Action 6</button>
+    <button id="button-9" on:click="action_7">Action 7</button>
+    <button id="button-10" on:click="action_8">Action 8</button>
+    <button id="button-11" on:click="action_9">Action 9</button>
+    <button id="button-12" on:click="action_10">Action 10</button>
+    <button id="button-13" on:click={:component_3_id, :component_3_action_3}>Component 3 Action 3</button>
+    <button id="button-14" on:click={:page, :action_11}>Action 11</button>
+    <button id="button-15" on:click={:layout, :default_layout_action_1}>Layout Action 1</button>
+    <button id="button-16" on:click="action_12">Action 12</button>
+    <button id="button-17" on:click="action_13">Action 13</button>
     <br />
 
-    <div id="text-page-1">{@text}</div><br />
+    <div id="text">{@text}</div><br />
 
     <Component3 id="component_3_id" /><br />
     """
--- test/e2e/test/runtime/actions_test.exs
+++ test/e2e/test/runtime/actions_test.exs
@@ -1,31 +1,33 @@
-defmodule HologramE2E.ActionsTest do
+defmodule HologramE2E.Runtime.ActionsTest do
   use HologramE2E.TestCase, async: false
+
+  @page_path "/e2e/runtime/actions"
 
   @moduletag :e2e
 
   describe "action spec" do
     feature "action spec without target and params specified as text", %{session: session} do
       session
-      |> visit("/e2e/page-1")
-      |> click(css("#page-1-button-1"))
-      |> assert_has(css("#text-page-1", text: "text updated by action_1, state.value = p1"))
+      |> visit(@page_path)
+      |> click(css("#button-1"))
+      |> assert_has(css("#text", text: "text updated by action_1, state.value = p1"))
     end
 
     feature "action spec without target and params specified as 1-element tuple", %{
       session: session
     } do
       session
-      |> visit("/e2e/page-1")
-      |> click(css("#page-1-button-2"))
-      |> assert_has(css("#text-page-1", text: "text updated by action_2, state.value = p1"))
+      |> visit(@page_path)
+      |> click(css("#button-2"))
+      |> assert_has(css("#text", text: "text updated by action_2, state.value = p1"))
     end
 
     feature "action spec with params", %{session: session} do
       session
-      |> visit("/e2e/page-1")
-      |> click(css("#page-1-button-3"))
+      |> visit(@page_path)
+      |> click(css("#button-3"))
       |> assert_has(
-        css("#text-page-1",
+        css("#text",
           text: "text updated by action_3, params.a = 5, params.b = 6, state.value = p1"
         )
       )
@@ -33,8 +35,8 @@ defmodule HologramE2E.ActionsTest do
 
     feature "action spec with target ID", %{session: session} do
       session
-      |> visit("/e2e/page-1")
-      |> click(css("#page-1-button-4"))
+      |> visit(@page_path)
+      |> click(css("#button-4"))
       |> assert_has(
         css("#text-component-3", text: "text updated by component_3_action_1, state.value = c3")
       )
@@ -42,8 +44,8 @@ defmodule HologramE2E.ActionsTest do
 
     feature "action spec with targetID and params", %{session: session} do
       session
-      |> visit("/e2e/page-1")
-      |> click(css("#page-1-button-5"))
+      |> visit(@page_path)
+      |> click(css("#button-5"))
       |> assert_has(
         css("#text-component-3",
           text:
@@ -56,24 +58,24 @@ defmodule HologramE2E.ActionsTest do
   describe "action result" do
     feature "action returning state only, not wrapped in tuple", %{session: session} do
       session
-      |> visit("/e2e/page-1")
-      |> click(css("#page-1-button-11"))
-      |> assert_has(css("#text-page-1", text: "text updated by action_9, state.value = p1"))
+      |> visit(@page_path)
+      |> click(css("#button-11"))
+      |> assert_has(css("#text", text: "text updated by action_9, state.value = p1"))
     end
 
     feature "action returning state only, wrapped in tuple", %{session: session} do
       session
-      |> visit("/e2e/page-1")
-      |> click(css("#page-1-button-6"))
-      |> assert_has(css("#text-page-1", text: "text updated by action_4, state.value = p1"))
+      |> visit(@page_path)
+      |> click(css("#button-6"))
+      |> assert_has(css("#text", text: "text updated by action_4, state.value = p1"))
     end
 
     feature "action returning state and command", %{session: session} do
       session
-      |> visit("/e2e/page-1")
-      |> click(css("#page-1-button-7"))
+      |> visit(@page_path)
+      |> click(css("#button-7"))
       |> assert_has(
-        css("#text-page-1",
+        css("#text",
           text: "text updated by action_5_b triggered by command_1, state.value = p1"
         )
       )
@@ -81,10 +83,10 @@ defmodule HologramE2E.ActionsTest do
 
     feature "action returning state, command and params", %{session: session} do
       session
-      |> visit("/e2e/page-1")
-      |> click(css("#page-1-button-8"))
+      |> visit(@page_path)
+      |> click(css("#button-8"))
       |> assert_has(
-        css("#text-page-1",
+        css("#text",
           text:
             "text updated by action_6_b triggered by command_2, params.a = 10, params.b = 20, state.value = p1"
         )
@@ -93,10 +95,10 @@ defmodule HologramE2E.ActionsTest do
 
     feature "action returning state, target ID and command", %{session: session} do
       session
-      |> visit("/e2e/page-1")
-      |> click(css("#page-1-button-9"))
+      |> visit(@page_path)
+      |> click(css("#button-9"))
       |> assert_has(
-        css("#text-page-1",
+        css("#text",
           text: "text updated by action_7_b triggered by component_3_command_1, state.value = p1"
         )
       )
@@ -104,10 +106,10 @@ defmodule HologramE2E.ActionsTest do
 
     feature "action returning state, target ID, command and params", %{session: session} do
       session
-      |> visit("/e2e/page-1")
-      |> click(css("#page-1-button-10"))
+      |> visit(@page_path)
+      |> click(css("#button-10"))
       |> assert_has(
-        css("#text-page-1",
+        css("#text",
           text:
             "text updated by action_8_b triggered by component_3_command_2, params.a = 10, params.b = 20, state.value = p1"
         )
@@ -118,15 +120,15 @@ defmodule HologramE2E.ActionsTest do
   describe "target" do
     feature "default target", %{session: session} do
       session
-      |> visit("/e2e/page-1")
-      |> click(css("#page-1-button-12"))
-      |> assert_has(css("#text-page-1", text: "text updated by action_10, state.value = p1"))
+      |> visit(@page_path)
+      |> click(css("#button-12"))
+      |> assert_has(css("#text", text: "text updated by action_10, state.value = p1"))
     end
 
     feature "component target", %{session: session} do
       session
-      |> visit("/e2e/page-1")
-      |> click(css("#page-1-button-4"))
+      |> visit(@page_path)
+      |> click(css("#button-4"))
       |> assert_has(
         css("#text-component-3", text: "text updated by component_3_action_1, state.value = c3")
       )
@@ -134,15 +136,15 @@ defmodule HologramE2E.ActionsTest do
 
     feature "page target", %{session: session} do
       session
-      |> visit("/e2e/page-1")
-      |> click(css("#page-1-button-14"))
-      |> assert_has(css("#text-page-1", text: "text updated by action_11, state.value = p1"))
+      |> visit(@page_path)
+      |> click(css("#button-14"))
+      |> assert_has(css("#text", text: "text updated by action_11, state.value = p1"))
     end
 
     feature "layout target", %{session: session} do
       session
-      |> visit("/e2e/page-1")
-      |> click(css("#page-1-button-15"))
+      |> visit(@page_path)
+      |> click(css("#button-15"))
       |> assert_has(
         css("#text-default-layout",
           text: "text updated by default_layout_action_1, state.value = dl"
@@ -154,16 +156,16 @@ defmodule HologramE2E.ActionsTest do
   describe "action trigger" do
     feature "action triggered by event", %{session: session} do
       session
-      |> visit("/e2e/page-1")
-      |> click(css("#page-1-button-16"))
-      |> assert_has(css("#text-page-1", text: "text updated by action_12, state.value = p1"))
+      |> visit(@page_path)
+      |> click(css("#button-16"))
+      |> assert_has(css("#text", text: "text updated by action_12, state.value = p1"))
     end
 
     feature "action trigerred by command", %{session: session} do
       session
-      |> visit("/e2e/page-1")
-      |> click(css("#page-1-button-17"))
-      |> assert_has(css("#text-page-1", text: "text updated by action_13_b, state.value = p1"))
+      |> visit(@page_path)
+      |> click(css("#button-17"))
+      |> assert_has(css("#text", text: "text updated by action_13_b, state.value = p1"))
     end
   end
 end
