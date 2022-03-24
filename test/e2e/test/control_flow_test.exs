c2248c6863a2c548bf13794fbd75b3deb7fe6de4
Bart Blast <bart@bartblast.com>
2022-03-24 12:56:33 +0100

    Fix case expression E2E test

--- test/e2e/test/control_flow_test.exs
+++ test/e2e/test/control_flow_test.exs
@@ -5,7 +5,8 @@ defmodule HologramE2E.ControlFlowTest do
 
   feature "case expression", %{session: session} do
     session
-    |> visit("/e2e/operators/addition")
+    |> visit("/e2e/control-flow/case-expression")
     |> click(css("#button"))
-    |> assert_has(css("#text", text: "Result = 3"))
+    |> assert_has(css("#text", text: "Result = 22"))
   end
+end
