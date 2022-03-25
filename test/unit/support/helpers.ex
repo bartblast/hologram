705f7b9b37e22ee4e7e4afce9ff2c2c309e4d38f
Bart Blast <bart@bartblast.com>
2022-03-25 12:34:52 +0100

    Implement seed_template_store/1 test helper

--- test/unit/support/helpers.ex
+++ test/unit/support/helpers.ex
@@ -1,5 +1,6 @@
 defmodule Hologram.Test.Helpers do
   alias Hologram.Compiler.Reflection
+  alias Hologram.Runtime.TemplateStore
   alias Hologram.Template.Builder
 
   @default_app_path Reflection.app_path()
@@ -18,5 +19,11 @@ defmodule Hologram.Test.Helpers do
 
   def md5_hex_regex do
     ~r/^[0-9a-f]{32}$/
+  end
+
+  def seed_template_store(modules) do
+    modules
+    |> Builder.build_all()
+    |> Enum.each(fn {module, template} -> TemplateStore.put(module, template) end)
   end
 end
