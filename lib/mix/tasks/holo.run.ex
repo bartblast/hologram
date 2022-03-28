aa232e72d1d60dcba16f7eed2561b86f9a9cee9e
Bart Blast <bart@bartblast.com>
2022-03-28 12:16:53 +0200

    Implement Mix holo.run task

---
+++ lib/mix/tasks/holo.run.ex
@@ -0,0 +1,12 @@
+defmodule Mix.Tasks.Holo.Run do
+  def run(_) do
+    Mix.Task.run("holo.assets.build", [])
+
+    if Mix.env == :prod do
+      Mix.Task.run("holo.assets.clean")
+      Mix.Task.run("phx.digest")
+    end
+
+    Mix.Task.run("phx.server")
+  end
+end
