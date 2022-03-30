075f1323cb89b3ff9c58f7ff228708722af8a6dc
Bart Blast <bart@bartblast.com>
2022-03-30 13:34:36 +0200

    Load path dynamically and fix missing alias

--- lib/hologram/runtime/template_store.ex
+++ lib/hologram/runtime/template_store.ex
@@ -1,16 +1,17 @@
 defmodule Hologram.Runtime.TemplateStore do
   use Hologram.Commons.MemoryStore
 
-  @dump_path Hologram.Compiler.Reflection.release_template_store_path()
+  alias Hologram.Compiler.Reflection
+  alias Hologram.Utils
 
   @impl true
   def populate_table do
-    @dump_path
+    Reflection.release_template_store_path()
     |> File.read!()
     |> Utils.deserialize()
     |> Enum.each(fn {key, value} -> put(key, value) end)
   end
-  
+
   @impl true
   def table_name, do: :hologram_template_store
 end
