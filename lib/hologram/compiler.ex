3f4360d3f995740fd598e06edaf939a6dd1f5a3f
Bart Blast <bart@bartblast.com>
2022-03-31 13:34:32 +0200

    Extract compiler code

---
+++ lib/hologram/compiler.ex
@@ -0,0 +1,125 @@
+# TODO: refactor & test
+
+defmodule Hologram.Compiler do
+  alias Hologram.Compiler.{
+    Builder,
+    CallGraph,
+    CallGraphBuilder,
+    ModuleDefAggregator,
+    ModuleDefStore,
+    Reflection
+  }
+
+  alias Hologram.Template.Builder, as: TemplateBuilder
+  alias Hologram.Utils
+
+  def compile(opts) do
+    Logger.debug("Hologram compiler started")
+
+    output_path = resolve_output_path()
+    File.mkdir_p!(output_path)
+
+    Reflection.root_priv_path()
+    |> File.mkdir_p!()
+
+    ModuleDefStore.run()
+    CallGraph.run()
+
+    templatables = Reflection.list_templatables(opts)
+    templatables = if opts[:templatables], do: templatables ++ opts[:templatables], else: templatables
+    templates = TemplateBuilder.build_all(templatables)
+    dump_template_store(templates)
+    TemplateStore.run()
+
+    pages = Reflection.list_pages(opts)
+    dump_page_list(pages)
+
+    module_defs = aggregate_module_defs(pages)
+    call_graph = build_call_graph(pages, module_defs, templates)
+
+    build_pages(pages, output_path, module_defs, call_graph)
+    |> dump_page_digest_store()
+
+    TemplateStore.stop()
+    CallGraph.stop()
+    ModuleDefStore.stop()
+
+    Logger.debug("Hologram compiler finished")
+  end
+
+  defp aggregate_module_defs(pages) do
+    pages
+    |> Utils.map_async(&ModuleDefAggregator.aggregate/1)
+    |> Utils.await_tasks()
+
+    ModuleDefStore.get_all()
+  end
+
+  defp build_call_graph(pages, module_defs, templates) do
+    pages
+    |> Utils.map_async(&CallGraphBuilder.build(&1, module_defs, templates, nil))
+    |> Utils.await_tasks()
+
+    CallGraph.get()
+  end
+
+  defp build_page(page, output_path, module_defs, call_graph) do
+    js = Builder.build(page, module_defs, call_graph)
+
+    output = """
+    "use strict";
+
+    #{js}
+
+    window.hologramPageScriptLoaded = true;
+
+    if (window.hologramRuntimeScriptLoaded && window.hologramPageScriptLoaded && !window.hologramPageMounted) {
+      window.hologramPageMounted = true
+      Hologram.run(window.hologramArgs.class, window.hologramArgs.state)
+    }
+    """
+
+    digest =
+      :crypto.hash(:md5, output)
+      |> Base.encode16()
+      |> String.downcase()
+
+    "#{output_path}/page-#{digest}.js"
+    |> File.write!(output)
+
+    {page, digest}
+  end
+
+  defp build_pages(pages, output_path, module_defs, call_graph) do
+    pages
+    |> Utils.map_async(&build_page(&1, output_path, module_defs, call_graph))
+    |> Utils.await_tasks()
+  end
+
+  defp dump_page_digest_store(page_digests) do
+    data =
+      page_digests
+      |> Utils.serialize()
+
+    Reflection.root_page_digest_store_path()
+    |> File.write!(data)
+  end
+  
+  defp dump_page_list(pages) do
+    data = Utils.serialize(pages)
+
+    Reflection.root_page_list_path()
+    |> File.write!(data)
+  end
+
+  defp dump_template_store(templates) do
+    data = Utils.serialize(templates)
+
+    Reflection.root_template_store_path()
+    |> File.write!(data)
+  end
+
+  defp resolve_output_path do
+    Reflection.root_path() <> "/priv/static/hologram"
+  end
+end
--- lib/mix/tasks/compile.hologram.ex
+++ lib/mix/tasks/compile.hologram.ex
@@ -4,18 +4,8 @@ defmodule Mix.Tasks.Compile.Hologram do
   use Mix.Task.Compiler
   require Logger
 
-  alias Hologram.Compiler.{
-    Builder,
-    CallGraph,
-    CallGraphBuilder,
-    ModuleDefAggregator,
-    ModuleDefStore,
-    Reflection
-  }
-
-  alias Hologram.Runtime.TemplateStore
-
-  alias Hologram.Template.Builder, as: TemplateBuilder
+  alias Hologram.Compiler
+  alias Hologram.Compiler.Reflection
   alias Hologram.Utils
 
   def run(opts \\ []) do
@@ -25,95 +15,12 @@ defmodule Mix.Tasks.Compile.Hologram do
         |> digest_files()
 
       if !has_source_digest?() || has_source_changes?(source_digest) || opts[:force] do
-        compile(opts)
+        Compiler.compile(opts)
         save_source_digest(source_digest)
       end
     end
 
     :ok
-  end
-
-  defp compile(opts) do
-    Logger.debug("Hologram compiler started")
-
-    output_path = resolve_output_path()
-    File.mkdir_p!(output_path)
-
-    Reflection.root_priv_path()
-    |> File.mkdir_p!()
-
-    ModuleDefStore.run()
-    CallGraph.run()
-
-    templatables = Reflection.list_templatables(opts)
-    templatables = if opts[:templatables], do: templatables ++ opts[:templatables], else: templatables
-    templates = TemplateBuilder.build_all(templatables)
-    dump_template_store(templates)
-    TemplateStore.run()
-
-    pages = Reflection.list_pages(opts)
-    dump_page_list(pages)
-
-    module_defs = aggregate_module_defs(pages)
-    call_graph = build_call_graph(pages, module_defs, templates)
-
-    build_pages(pages, output_path, module_defs, call_graph)
-    |> dump_page_digest_store()
-
-    TemplateStore.stop()
-    CallGraph.stop()
-    ModuleDefStore.stop()
-
-    Logger.debug("Hologram compiler finished")
-  end
-
-  defp aggregate_module_defs(pages) do
-    pages
-    |> Utils.map_async(&ModuleDefAggregator.aggregate/1)
-    |> Utils.await_tasks()
-
-    ModuleDefStore.get_all()
-  end
-
-  defp build_call_graph(pages, module_defs, templates) do
-    pages
-    |> Utils.map_async(&CallGraphBuilder.build(&1, module_defs, templates, nil))
-    |> Utils.await_tasks()
-
-    CallGraph.get()
-  end
-
-  defp build_page(page, output_path, module_defs, call_graph) do
-    js = Builder.build(page, module_defs, call_graph)
-
-    output = """
-    "use strict";
-
-    #{js}
-
-    window.hologramPageScriptLoaded = true;
-
-    if (window.hologramRuntimeScriptLoaded && window.hologramPageScriptLoaded && !window.hologramPageMounted) {
-      window.hologramPageMounted = true
-      Hologram.run(window.hologramArgs.class, window.hologramArgs.state)
-    }
-    """
-
-    digest =
-      :crypto.hash(:md5, output)
-      |> Base.encode16()
-      |> String.downcase()
-
-    "#{output_path}/page-#{digest}.js"
-    |> File.write!(output)
-
-    {page, digest}
-  end
-
-  defp build_pages(pages, output_path, module_defs, call_graph) do
-    pages
-    |> Utils.map_async(&build_page(&1, output_path, module_defs, call_graph))
-    |> Utils.await_tasks()
   end
 
   defp digest_files(paths) do
@@ -123,29 +30,6 @@ defmodule Mix.Tasks.Compile.Hologram do
       |> :erlang.term_to_binary()
 
     :crypto.hash(:md5, data)
-  end
-
-  defp dump_page_digest_store(page_digests) do
-    data =
-      page_digests
-      |> Utils.serialize()
-
-    Reflection.root_page_digest_store_path()
-    |> File.write!(data)
-  end
-
-  defp dump_page_list(pages) do
-    data = Utils.serialize(pages)
-
-    Reflection.root_page_list_path()
-    |> File.write!(data)
-  end
-
-  defp dump_template_store(templates) do
-    data = Utils.serialize(templates)
-
-    Reflection.root_template_store_path()
-    |> File.write!(data)
   end
 
   defp has_source? do
@@ -173,10 +57,6 @@ defmodule Mix.Tasks.Compile.Hologram do
       Reflection.mix_path(opts),
       Reflection.mix_lock_path(opts)
     ]
-  end
-
-  defp resolve_output_path do
-    Reflection.root_path() <> "/priv/static/hologram"
   end
 
   defp save_source_digest(source_digest) do
