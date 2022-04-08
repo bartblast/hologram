ea7f324835326e575f713cd53a2201724719460a
Bart Blast <bart@bartblast.com>
2022-04-08 13:42:59 +0200

    Implement E2E tests for map pattern matching

---
+++ test/e2e/test/pattern_matching_test.exs
@@ -0,0 +1,28 @@
+defmodule HologramE2E.PatternMatchingTest do
+  use HologramE2E.TestCase, async: false
+
+  @moduletag :e2e
+
+  describe "map" do
+    feature "match expression", %{session: session} do
+      session
+      |> visit("/e2e/pattern-matching/map")
+      |> click(css("#button_match_expression"))
+      |> assert_has(css("#text", text: "Result = 3"))
+    end
+
+    feature "function call", %{session: session} do
+      session
+      |> visit("/e2e/pattern-matching/map")
+      |> click(css("#button_function_call"))
+      |> assert_has(css("#text", text: "Result = 5"))
+    end
+
+    feature "case condition", %{session: session} do
+      session
+      |> visit("/e2e/pattern-matching/map")
+      |> click(css("#button_case_condition"))
+      |> assert_has(css("#text", text: "Result = 7"))
+    end
+  end
+end
