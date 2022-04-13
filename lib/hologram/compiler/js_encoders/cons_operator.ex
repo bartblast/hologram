d4f55517b62486050a7b2bf7e4ec512a30eb1221
Bart Blast <bart@bartblast.com>
2022-04-13 14:16:26 +0200

    Implement JS encoder for cons operator pattern matching

--- lib/hologram/compiler/js_encoders/cons_operator.ex
+++ lib/hologram/compiler/js_encoders/cons_operator.ex
@@ -2,10 +2,20 @@ alias Hologram.Compiler.{Context, JSEncoder, Opts}
 alias Hologram.Compiler.IR.ConsOperator
 
 defimpl JSEncoder, for: ConsOperator do
-  def encode(%{head: head, tail: tail}, %Context{} = context, %Opts{} = opts) do
-    encoded_head = JSEncoder.encode(head, context, opts)
-    encoded_tail = JSEncoder.encode(tail, context, opts)
+  def encode(ir, %Context{} = context, %Opts{placeholder: true} = opts) do
+    {head, tail} = encode_parts(ir, context, opts)
+    "{ type: 'cons_operator_pattern', head: #{head}, tail: #{tail} }"
+  end
 
-    "Hologram.Interpreter.$cons_operator(#{encoded_head}, #{encoded_tail})"
+  def encode(ir, %Context{} = context, %Opts{} = opts) do
+    {head, tail} = encode_parts(ir, context, opts)
+    "Hologram.Interpreter.$cons_operator(#{head}, #{tail})"
+  end
+
+  defp encode_parts(%{head: head, tail: tail}, context, opts) do
+    {
+      JSEncoder.encode(head, context, opts),
+      JSEncoder.encode(tail, context, opts)
+    }
   end
 end
--- test/unit/hologram/compiler/js_encoders/cons_operator_test.exs
+++ test/unit/hologram/compiler/js_encoders/cons_operator_test.exs
@@ -4,7 +4,7 @@ defmodule Hologram.Compiler.JSEncoder.ConsOperatorTest do
   alias Hologram.Compiler.{Context, JSEncoder, Opts}
   alias Hologram.Compiler.IR.{ConsOperator, IntegerType, ListType}
 
-  test "encode/3" do
+  test "expression" do
     ir = %ConsOperator{
       head: %IntegerType{value: 1},
       tail: %ListType{
@@ -23,6 +23,29 @@ defmodule Hologram.Compiler.JSEncoder.ConsOperatorTest do
       "{ type: 'list', data: [ { type: 'integer', value: 2 }, { type: 'integer', value: 3 } ] }"
 
     expected = "Hologram.Interpreter.$cons_operator(#{head}, #{tail})"
+
+    assert result == expected
+  end
+
+  test "pattern matching" do
+    ir = %ConsOperator{
+      head: %IntegerType{value: 1},
+      tail: %ListType{
+        data: [
+          %IntegerType{value: 2},
+          %IntegerType{value: 3}
+        ]
+      }
+    }
+
+    result = JSEncoder.encode(ir, %Context{}, %Opts{placeholder: true})
+
+    head = "{ type: 'integer', value: 1 }"
+
+    tail =
+      "{ type: 'list', data: [ { type: 'integer', value: 2 }, { type: 'integer', value: 3 } ] }"
+
+    expected = "{ type: 'cons_operator_pattern', head: #{head}, tail: #{tail} }"
 
     assert result == expected
   end
