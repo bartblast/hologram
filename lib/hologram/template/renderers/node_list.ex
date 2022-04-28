450b0b3a7f229d3e4ae9d38187b39c9718833464
Bart Blast <bart@bartblast.com>
2022-04-28 11:16:59 +0200

    Return nested component state from node list renderer

--- lib/hologram/template/renderers/node_list.ex
+++ lib/hologram/template/renderers/node_list.ex
@@ -2,7 +2,9 @@ alias Hologram.Template.Renderer
 
 defimpl Renderer, for: List do
   def render(nodes, conn, bindings, slots) do
-    Enum.map(nodes, &Renderer.render(&1, conn, bindings, slots))
-    |> Enum.join("")
+    Enum.reduce(nodes, {"", %{}}, fn node, {html, initial_state} ->
+      {node_html, node_initial_state} = Renderer.render(node, conn, bindings, slots)
+      {html <> node_html, Map.merge(initial_state, node_initial_state)}
+    end)
   end
 end
--- test/unit/hologram/template/renderers/node_list_test.exs
+++ test/unit/hologram/template/renderers/node_list_test.exs
@@ -1,17 +1,59 @@
 defmodule Hologram.Template.Renderer.NodeListTest do
   use Hologram.Test.UnitCase, async: true
 
+  alias Hologram.Conn
+  alias Hologram.Runtime
   alias Hologram.Template.Renderer
+  alias Hologram.Template.VDOM.Component
   alias Hologram.Template.VDOM.TextNode
+  alias Hologram.Test.Fixtures.Template.NodeListRenderer.Module1
+  alias Hologram.Test.Fixtures.Template.NodeListRenderer.Module2
 
-  test "render/2" do
+  setup do
+    [app_path: "#{@fixtures_path}/template/renderers/node_list_renderer"]
+    |> compile()
+
+    Runtime.run()
+
+    :ok
+  end
+
+  test "render/4" do
+    component_1 = %Component{
+      module: Module1,
+      props: %{
+        id: [
+          %TextNode{content: "component_1_id"},
+        ]
+      }
+    }
+
+    component_2 = %Component{
+      module: Module2,
+      props: %{
+        id: [
+          %TextNode{content: "component_2_id"},
+        ]
+      }
+    }
+
     nodes = [
       %TextNode{content: "test_1"},
-      %TextNode{content: "test_2"}
+      component_1,
+      %TextNode{content: "test_2"},
+      component_2
     ]
 
-    result = Renderer.render(nodes, %{})
-    expected = "test_1test_2"
+    bindings = %{}
+    result = Renderer.render(nodes, %Conn{}, bindings)
+
+    expected_initial_state = %{
+      component_1_id: %{component_1_state_key: "component_1_state_value"},
+      component_2_id: %{component_2_state_key: "component_2_state_value"},
+    }
+
+    expected_html = "test_1(in component 1)test_2(in component 2)"
+    expected = {expected_html, expected_initial_state}
 
     assert result == expected
   end
