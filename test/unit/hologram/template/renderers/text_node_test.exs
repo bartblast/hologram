198f8f4fabdfd3a48f2b68944108bf63af06a297
Bart Blast <bart@bartblast.com>
2022-04-28 11:04:44 +0200

    Refactor text node renderer test

--- test/unit/hologram/template/renderers/text_node_test.exs
+++ test/unit/hologram/template/renderers/text_node_test.exs
@@ -1,15 +1,15 @@
 defmodule Hologram.Template.Renderer.TextNodeTest do
   use Hologram.Test.UnitCase, async: true
 
+  alias Hologram.Conn
   alias Hologram.Template.Renderer
   alias Hologram.Template.VDOM.TextNode
 
   test "render/4" do
     content = "a\\{b\\}ca\\{b\\}c"
     text_node = %TextNode{content: content}
-    conn = %Hologram.Conn{}
 
-    result = Renderer.render(text_node, conn, %{})
+    result = Renderer.render(text_node, %Conn{}, %{})
     expected = {"a{b}ca{b}c", %{}}
 
     assert result == expected
