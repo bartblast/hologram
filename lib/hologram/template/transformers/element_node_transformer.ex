ea0df08333e1ab79d516d2d45de88de0ea71c42a
Bart Blast <bart@bartblast.com>
2022-08-01 12:59:13 +0200

    Refactor element node template transformer

--- lib/hologram/template/transformers/element_node_transformer.ex
+++ lib/hologram/template/transformers/element_node_transformer.ex
@@ -1,7 +1,9 @@
 defmodule Hologram.Template.ElementNodeTransformer do
   alias Hologram.Compiler.Context
+  alias Hologram.Compiler.Reflection
   alias Hologram.Template.VDOM.ElementNode
-  alias Hologram.Template.EmbeddedExpressionParser
+  alias Hologram.Template.VDOM.Expression
+  alias Hologram.Template.VDOM.TextNode
 
   def transform(tag, children, attrs, %Context{} = context) do
     attrs = transform_attrs(attrs, context)
@@ -9,26 +11,27 @@ defmodule Hologram.Template.ElementNodeTransformer do
   end
 
   defp transform_attrs(attrs, context) do
-    Enum.map(attrs, fn {type, key, value} ->
+    Enum.map(attrs, fn {key, value} ->
       [name | modifiers] = String.split(key, ".")
 
       name = String.to_atom(name)
+      value = transform_attr_value(value, context)
       modifiers = Enum.map(modifiers, &String.to_atom/1)
-
-      value =
-        case type do
-          :boolean ->
-            nil
-
-          :expression ->
-            "{#{value}}" |> EmbeddedExpressionParser.parse(context)
-
-          :literal ->
-            EmbeddedExpressionParser.parse(value, context)
-        end
 
       {name, %{value: value, modifiers: modifiers}}
     end)
     |> Enum.into(%{})
+  end
+
+  defp transform_attr_value(value, context) do
+    Enum.map(value, fn {type, str} ->
+      case type do
+        :expression ->
+          %Expression{ir: Reflection.ir(str, context)}
+
+        :literal ->
+          %TextNode{content: str}
+      end
+    end)
   end
 end
--- test/unit/hologram/template/transformers/element_node_transformer_test.exs
+++ test/unit/hologram/template/transformers/element_node_transformer_test.exs
@@ -11,7 +11,7 @@ defmodule Hologram.Template.ElementNodeTransformerTest do
   @tag_name "div"
 
   test "attr without modifiers" do
-    attrs = [{:literal, "test_key", "test_value"}]
+    attrs = [{"test_key", [literal: "test_value"]}]
     result = ElementNodeTransformer.transform(@tag_name, @children, attrs, @context)
 
     expected = %ElementNode{
@@ -29,7 +29,7 @@ defmodule Hologram.Template.ElementNodeTransformerTest do
   end
 
   test "attr with modifier" do
-    attrs = [{:literal, "test_key.modifier_1.modifier_2", "test_value"}]
+    attrs = [{"test_key.modifier_1.modifier_2", [literal: "test_value"]}]
     result = ElementNodeTransformer.transform(@tag_name, @children, attrs, @context)
 
     expected = %ElementNode{
@@ -47,7 +47,7 @@ defmodule Hologram.Template.ElementNodeTransformerTest do
   end
 
   test "boolean attr value" do
-    attrs = [{:boolean, "test_key", nil}]
+    attrs = [{"test_key", []}]
     result = ElementNodeTransformer.transform(@tag_name, @children, attrs, @context)
 
     expected = %ElementNode{
@@ -55,35 +55,7 @@ defmodule Hologram.Template.ElementNodeTransformerTest do
       children: @children,
       attrs: %{
         test_key: %{
-          value: nil,
-          modifiers: []
-        }
-      }
-    }
-
-    assert result == expected
-  end
-
-  test "literal attr value with embedded expression" do
-    attrs = [{:literal, "test_key", "abc{@k}xyz"}]
-    result = ElementNodeTransformer.transform(@tag_name, @children, attrs, @context)
-
-    expected = %ElementNode{
-      tag: @tag_name,
-      children: @children,
-      attrs: %{
-        test_key: %{
-          value: [
-            %TextNode{content: "abc"},
-            %Expression{
-              ir: %TupleType{
-                data: [
-                  %ModuleAttributeOperator{name: :k}
-                ]
-              }
-            },
-            %TextNode{content: "xyz"}
-          ],
+          value: [],
           modifiers: []
         }
       }
@@ -93,7 +65,7 @@ defmodule Hologram.Template.ElementNodeTransformerTest do
   end
 
   test "expression attr value" do
-    attrs = [{:expression, "test_key", "1 + 2"}]
+    attrs = [{"test_key", [expression: "{1 + 2}"]}]
     result = ElementNodeTransformer.transform(@tag_name, @children, attrs, @context)
 
     expected = %ElementNode{
@@ -112,6 +84,34 @@ defmodule Hologram.Template.ElementNodeTransformerTest do
                 ]
               }
             }
+          ],
+          modifiers: []
+        }
+      }
+    }
+
+    assert result == expected
+  end
+
+  test "multi-part attr value" do
+    attrs = [{"test_key", [literal: "abc", expression: "{@k}", literal: "xyz"]}]
+    result = ElementNodeTransformer.transform(@tag_name, @children, attrs, @context)
+
+    expected = %ElementNode{
+      tag: @tag_name,
+      children: @children,
+      attrs: %{
+        test_key: %{
+          value: [
+            %TextNode{content: "abc"},
+            %Expression{
+              ir: %TupleType{
+                data: [
+                  %ModuleAttributeOperator{name: :k}
+                ]
+              }
+            },
+            %TextNode{content: "xyz"}
           ],
           modifiers: []
         }
