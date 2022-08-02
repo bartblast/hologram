fa5bb9ddfcb9fcb9508be9ad689397b164cabc65
Bart Blast <bart@bartblast.com>
2022-08-02 12:14:36 +0200

    Fix expression node test data

--- test/unit/hologram/template/dom_tree_builder_test.exs
+++ test/unit/hologram/template/dom_tree_builder_test.exs
@@ -107,14 +107,14 @@ defmodule Hologram.Template.DOMTreeBuilderTest do
 
   test "element node attributes" do
     tags = [
-      start_tag: {"div", [{"id", [literal: "abc", expression: "@test", literal: "xyz"]}]},
+      start_tag: {"div", [{"id", [literal: "abc", expression: "{@test}", literal: "xyz"]}]},
       end_tag: "div"
     ]
 
     result = DOMTreeBuilder.build(tags)
 
     expected = [
-      {:element, "div", [{"id", [literal: "abc", expression: "@test", literal: "xyz"]}], []}
+      {:element, "div", [{"id", [literal: "abc", expression: "{@test}", literal: "xyz"]}], []}
     ]
 
     assert result == expected
@@ -122,14 +122,14 @@ defmodule Hologram.Template.DOMTreeBuilderTest do
 
   test "component node props" do
     tags = [
-      start_tag: {"Abc.Bcd", [{"id", [literal: "abc", expression: "@test", literal: "xyz"]}]},
+      start_tag: {"Abc.Bcd", [{"id", [literal: "abc", expression: "{@test}", literal: "xyz"]}]},
       end_tag: "Abc.Bcd"
     ]
 
     result = DOMTreeBuilder.build(tags)
 
     expected = [
-      {:component, "Abc.Bcd", [{"id", [literal: "abc", expression: "@test", literal: "xyz"]}], []}
+      {:component, "Abc.Bcd", [{"id", [literal: "abc", expression: "{@test}", literal: "xyz"]}], []}
     ]
 
     assert result == expected
