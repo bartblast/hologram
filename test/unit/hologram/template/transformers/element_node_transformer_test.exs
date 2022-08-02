b5c0d00dfe01b5b0c9eb24d88408b31d5325fd18
Bart Blast <bart@bartblast.com>
2022-08-02 13:09:30 +0200

    Simplify element node transformer tests

--- test/unit/hologram/template/transformers/element_node_transformer_test.exs
+++ test/unit/hologram/template/transformers/element_node_transformer_test.exs
@@ -2,15 +2,15 @@ defmodule Hologram.Template.ElementNodeTransformerTest do
   use Hologram.Test.UnitCase, async: true
 
   alias Hologram.Compiler.Context
-  alias Hologram.Compiler.IR.{AdditionOperator, IntegerType, ModuleAttributeOperator, TupleType}
-  alias Hologram.Template.VDOM.{ElementNode, Expression, TextNode}
   alias Hologram.Template.ElementNodeTransformer
+  alias Hologram.Template.VDOM.ElementNode
+  alias Hologram.Template.VDOM.TextNode
 
   @children :children_placeholder
   @context %Context{}
   @tag_name "div"
 
-  test "attr without modifiers" do
+  test "attribute without modifiers" do
     attrs = [{"test_key", [literal: "test_value"]}]
     result = ElementNodeTransformer.transform(@tag_name, @children, attrs, @context)
 
@@ -28,7 +28,7 @@ defmodule Hologram.Template.ElementNodeTransformerTest do
     assert result == expected
   end
 
-  test "attr with modifier" do
+  test "attribute with modifier" do
     attrs = [{"test_key.modifier_1.modifier_2", [literal: "test_value"]}]
     result = ElementNodeTransformer.transform(@tag_name, @children, attrs, @context)
 
@@ -39,81 +39,6 @@ defmodule Hologram.Template.ElementNodeTransformerTest do
         test_key: %{
           value: [%TextNode{content: "test_value"}],
           modifiers: [:modifier_1, :modifier_2]
-        }
-      }
-    }
-
-    assert result == expected
-  end
-
-  test "boolean attr value" do
-    attrs = [{"test_key", []}]
-    result = ElementNodeTransformer.transform(@tag_name, @children, attrs, @context)
-
-    expected = %ElementNode{
-      tag: @tag_name,
-      children: @children,
-      attrs: %{
-        test_key: %{
-          value: [],
-          modifiers: []
-        }
-      }
-    }
-
-    assert result == expected
-  end
-
-  test "expression attr value" do
-    attrs = [{"test_key", [expression: "{1 + 2}"]}]
-    result = ElementNodeTransformer.transform(@tag_name, @children, attrs, @context)
-
-    expected = %ElementNode{
-      tag: @tag_name,
-      children: @children,
-      attrs: %{
-        test_key: %{
-          value: [
-            %Expression{
-              ir: %TupleType{
-                data: [
-                  %AdditionOperator{
-                    left: %IntegerType{value: 1},
-                    right: %IntegerType{value: 2}
-                  }
-                ]
-              }
-            }
-          ],
-          modifiers: []
-        }
-      }
-    }
-
-    assert result == expected
-  end
-
-  test "multi-part attr value" do
-    attrs = [{"test_key", [literal: "abc", expression: "{@k}", literal: "xyz"]}]
-    result = ElementNodeTransformer.transform(@tag_name, @children, attrs, @context)
-
-    expected = %ElementNode{
-      tag: @tag_name,
-      children: @children,
-      attrs: %{
-        test_key: %{
-          value: [
-            %TextNode{content: "abc"},
-            %Expression{
-              ir: %TupleType{
-                data: [
-                  %ModuleAttributeOperator{name: :k}
-                ]
-              }
-            },
-            %TextNode{content: "xyz"}
-          ],
-          modifiers: []
         }
       }
     }
