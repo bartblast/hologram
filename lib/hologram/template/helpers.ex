828b7b228a3c9dc07a17c522b4a6403e0dbfb810
Bart Blast <bart@bartblast.com>
2022-11-18 11:21:08 +0100

    Move functions related to void elements to template helpers module

--- lib/hologram/template/helpers.ex
+++ lib/hologram/template/helpers.ex
@@ -1,4 +1,27 @@
 defmodule Hologram.Template.Helpers do
+  # see: https://html.spec.whatwg.org/multipage/syntax.html#void-elements
+  @void_html_elements [
+    "area",
+    "base",
+    "br",
+    "col",
+    "embed",
+    "hr",
+    "img",
+    "input",
+    "link",
+    "meta",
+    "param",
+    "source",
+    "track",
+    "wbr"
+  ]
+
+  # TODO: specify void SVG elements
+  # see: https://github.com/segmetric/hologram/issues/21
+  # see: https://developer.mozilla.org/en-US/docs/Web/SVG/Element
+  @void_svg_elements ["path", "rect"]
+
   def tag_type(tag_name) do
     first_char = String.at(tag_name, 0)
     downcased_first_char = String.downcase(first_char)
@@ -8,5 +31,17 @@ defmodule Hologram.Template.Helpers do
     else
       :component
     end
+  end
+
+  def void_element?(tag_name) do
+    void_html_element?(tag_name) || void_svg_element?(tag_name) || tag_name == "slot"
+  end
+
+  defp void_html_element?(tag_name) do
+    tag_name in @void_html_elements
+  end
+
+  defp void_svg_element?(tag_name) do
+    tag_name in @void_svg_elements
   end
 end
--- test/unit/hologram/template/helpers_test.exs
+++ test/unit/hologram/template/helpers_test.exs
@@ -11,4 +11,26 @@ defmodule Hologram.Template.HelpersTest do
       assert Helpers.tag_type("MyComponent") == :component
     end
   end
+
+  describe "void_element?/1" do
+    test "void HTML element" do
+      assert Helpers.void_element?("br")
+    end
+
+    test "void SVG element" do
+      assert Helpers.void_element?("path")
+    end
+
+    test "slot element" do
+      assert Helpers.void_element?("slot")
+    end
+
+    test "non-void HTML element" do
+      refute Helpers.void_element?("div")
+    end
+
+    test "non-void SVG element" do
+      refute Helpers.void_element?("g")
+    end
+  end
 end
