5d7fe0634408344ac0033ccd347cf6da17848686
Bart Blast <bart@bartblast.com>
2022-11-24 10:52:34 +0100

    Fix unused var warnings caused by tag assembler macros

--- lib/hologram/template/macros.ex
+++ lib/hologram/template/macros.ex
@@ -1,28 +1,17 @@
 defmodule Hologram.Template.Macros do
   defmacro assemble(context, status, tokens, do: body) do
-    context = Macro.escape(context, unquote: true)
-    status = Macro.escape(status, unquote: true)
-    tokens = Macro.escape(tokens, unquote: true)
-
-    body =
-      quote do
-        unquote(body)
-      end
-
-    body = Macro.escape(body, unquote: true)
-
-    quote bind_quoted: [context: context, status: status, tokens: tokens, body: body] do
-      def assemble(unquote(context), unquote(status), unquote(tokens)) do
+    quote do
+      def assemble(unquote(context) = evaluated_context, unquote(status) = evaluated_status, unquote(tokens) = evaluated_tokens) do
         if Application.get_env(:hologram, :debug) do
           IO.puts("\n........................................\n")
           IO.puts("context")
-          IO.inspect(unquote(context))
+          IO.inspect(evaluated_context)
           IO.puts("")
           IO.puts("status")
-          IO.inspect(unquote(status))
+          IO.inspect(evaluated_status)
           IO.puts("")
           IO.puts("tokens")
-          IO.inspect(unquote(tokens))
+          IO.inspect(evaluated_tokens)
           IO.puts("\n........................................")
         end
 
