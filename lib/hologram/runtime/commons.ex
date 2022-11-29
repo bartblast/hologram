3c08c1317586979e4a559a4af771e56b0a646a57
Bart Blast <bart@bartblast.com>
2022-11-29 11:16:22 +0100

    Build VDOM templates during compile time with sigil_H macro

--- lib/hologram/runtime/commons.ex
+++ lib/hologram/runtime/commons.ex
@@ -1,3 +1,29 @@
 defmodule Hologram.Runtime.Commons do
-  def sigil_H(str, _), do: String.trim(str)
+  alias Hologram.Compiler
+  alias Hologram.Template
+
+  defmacro sigil_H(str, _) do
+    module_def =
+      __CALLER__.file
+      |> Compiler.Parser.parse_file!()
+      |> Compiler.Normalizer.normalize()
+      |> Compiler.Transformer.transform(%Compiler.Context{})
+
+    context = %Compiler.Context{
+      aliases: module_def.aliases,
+      imports: module_def.imports
+    }
+
+    vdom =
+      str
+      |> Code.eval_quoted()
+      |> elem(0)
+      |> Template.Parser.parse!()
+      |> Template.Transformer.transform(context)
+      |> Macro.escape()
+
+    quote do
+      unquote(vdom)
+    end
+  end
 end
--- test/unit/hologram/runtime/commons_test.exs
+++ test/unit/hologram/runtime/commons_test.exs
@@ -1,8 +1,22 @@
 defmodule Hologram.Runtime.CommonsTest do
   use Hologram.Test.UnitCase, async: true
+  require Hologram.Runtime.Commons
+
   alias Hologram.Runtime.Commons
+  alias Hologram.Template.VDOM.ElementNode
+  alias Hologram.Template.VDOM.TextNode
 
   test "sigil_H/2" do
-    assert Commons.sigil_H(" \ntest \n", []) == "test"
+    result = Commons.sigil_H("<div>abc</div>", [])
+
+    expected = [
+      %ElementNode{
+        attrs: %{},
+        children: [%TextNode{content: "abc"}],
+        tag: "div"
+      }
+    ]
+
+    assert result == expected
   end
 end
