812c9274dc6b15de1e0bfeb817d2da3a516a3ed9
Bart Blast <bart@bartblast.com>
2022-12-12 10:27:14 +0100

    Temporarily disable templates compilation

--- lib/hologram/runtime/commons.ex
+++ lib/hologram/runtime/commons.ex
@@ -3,27 +3,30 @@ defmodule Hologram.Runtime.Commons do
   alias Hologram.Template
 
   defmacro sigil_H(str, _) do
-    module_def =
-      __CALLER__.file
-      |> Compiler.Parser.parse_file!()
-      |> Compiler.Normalizer.normalize()
-      |> Compiler.Transformer.transform(%Compiler.Context{})
+    str
 
-    context = %Compiler.Context{
-      aliases: module_def.aliases,
-      imports: module_def.imports
-    }
+    # TODO: uncomment
+    # module_def =
+    #   __CALLER__.file
+    #   |> Compiler.Parser.parse_file!()
+    #   |> Compiler.Normalizer.normalize()
+    #   |> Compiler.Transformer.transform(%Compiler.Context{})
 
-    vdom =
-      str
-      |> Code.eval_quoted()
-      |> elem(0)
-      |> Template.Parser.parse!()
-      |> Template.Transformer.transform(context)
-      |> Macro.escape()
+    # context = %Compiler.Context{
+    #   aliases: module_def.aliases,
+    #   imports: module_def.imports
+    # }
 
-    quote do
-      unquote(vdom)
-    end
+    # vdom =
+    #   str
+    #   |> Code.eval_quoted()
+    #   |> elem(0)
+    #   |> Template.Parser.parse!()
+    #   |> Template.Transformer.transform(context)
+    #   |> Macro.escape()
+
+    # quote do
+    #   unquote(vdom)
+    # end
   end
 end
