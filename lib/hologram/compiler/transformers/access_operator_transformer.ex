4eebcc44faf0c8d6b649f2acb3e9b54124a17e8e
Bart Blast <bart@bartblast.com>
2022-12-15 10:16:00 +0100

    Overhaul access operator transformer

--- lib/hologram/compiler/transformers/access_operator_transformer.ex
+++ lib/hologram/compiler/transformers/access_operator_transformer.ex
@@ -1,10 +1,12 @@
 defmodule Hologram.Compiler.AccessOperatorTransformer do
-  alias Hologram.Compiler.{Context, Transformer}
+  alias Hologram.Compiler.Context
   alias Hologram.Compiler.IR.AccessOperator
+  alias Hologram.Compiler.Transformer
 
   def transform({{:., _, [Access, :get]}, _, [data, key]}, %Context{} = context) do
     data = Transformer.transform(data, context)
     key = Transformer.transform(key, context)
+
     %AccessOperator{data: data, key: key}
   end
 end
