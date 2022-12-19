f80ee9c2a2ec7371af3257915874e5f67de5329e
Bart Blast <bart@bartblast.com>
2022-12-19 12:40:58 +0100

    Overhaul access operator and anonymous function call AST transformation

--- lib/hologram/compiler/overhaul_transformers/dot_operator_transformer.ex
+++ lib/hologram/compiler/overhaul_transformers/dot_operator_transformer.ex
@@ -1,11 +1,30 @@
 defmodule Hologram.Compiler.DotOperatorTransformer do
-  alias Hologram.Compiler.{Context, Transformer}
-  alias Hologram.Compiler.IR.DotOperator
+  alias Hologram.Compiler.AccessOperatorTransformer
+  alias Hologram.Compiler.AnonymousFunctionCallTransformer
+  alias Hologram.Compiler.Context
+  # alias Hologram.Compiler.IR.DotOperator
+  # alias Hologram.Compiler.Transformer
 
-  def transform({{:., _, [left, right]}, _, []}, %Context{} = context) do
-    %DotOperator{
-      left: Transformer.transform(left, context),
-      right: Transformer.transform(right, context)
-    }
+  def transform({{:., _, [Access, :get]}, _, _} = ast, %Context{} = context) do
+    AccessOperatorTransformer.transform(ast, context)
+  end
+
+  def transform({{:., _, [_]}, _, _} = ast, %Context{} = context) do
+    AnonymousFunctionCallTransformer.transform(ast, context)
   end
 end
+
+# def transform({{:., _, [left, right]}, _, []}, %Context{} = context) do
+#   %DotOperator{
+#     left: Transformer.transform(left, context),
+#     right: Transformer.transform(right, context)
+#   }
+# end
+
+# def transform({{:., _, _}, [no_parens: true, line: _], _} = ast, %Context{} = context) do
+#   DotOperatorTransformer.transform(ast, context)
+# end
+
+# def transform({{:., _, _}, _, _} = ast, %Context{} = context) do
+#   FunctionCallTransformer.transform(ast, context)
+# end
--- test/unit/hologram/compiler/transformers/dot_operator_transformer_test.exs
+++ test/unit/hologram/compiler/transformers/dot_operator_transformer_test.exs
@@ -1,20 +1,45 @@
 defmodule Hologram.Compiler.DotOperatorTransformerTest do
   use Hologram.Test.UnitCase, async: true
 
-  alias Hologram.Compiler.{Context, DotOperatorTransformer}
-  alias Hologram.Compiler.IR.{AtomType, DotOperator, Variable}
+  alias Hologram.Compiler.Context
+  alias Hologram.Compiler.DotOperatorTransformer
+  alias Hologram.Compiler.IR.AccessOperator
+  alias Hologram.Compiler.IR.AnonymousFunctionCall
+  # alias Hologram.Compiler.IR.AtomType
+  # alias Hologram.Compiler.IR.DotOperator
+  # alias Hologram.Compiler.IR.Variable
 
-  test "transform/3" do
-    code = "a.b"
+  test "access operator" do
+    code = "a[:b]"
     ast = ast(code)
 
-    result = DotOperatorTransformer.transform(ast, %Context{})
-
-    expected = %DotOperator{
-      left: %Variable{name: :a},
-      right: %AtomType{value: :b}
-    }
-
-    assert result == expected
+    assert %AccessOperator{} = DotOperatorTransformer.transform(ast, %Context{})
   end
+
+  test "anonymous function call" do
+    code = "test.()"
+    ast = ast(code)
+
+    assert %AnonymousFunctionCall{} = DotOperatorTransformer.transform(ast, %Context{})
+  end
+
+  # test "test" do
+  #   code = "my_fun()"
+  #   ast = ast(code)
+  #   IO.inspect(ast)
+  # end
+
+  # test "transform/3" do
+  #   code = "a.b"
+  #   ast = ast(code)
+
+  #   result = DotOperatorTransformer.transform(ast, %Context{})
+
+  #   expected = %DotOperator{
+  #     left: %Variable{name: :a},
+  #     right: %AtomType{value: :b}
+  #   }
+
+  #   assert result == expected
+  # end
 end
