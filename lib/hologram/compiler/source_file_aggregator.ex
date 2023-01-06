215342912750184392f408dd849493b02f998954
Bart Blast <bart@bartblast.com>
2023-01-06 13:43:56 +0100

    Do not use GenServer in source file aggregator

--- lib/hologram/compiler/source_file_aggregator.ex
+++ lib/hologram/compiler/source_file_aggregator.ex
@@ -1,23 +1,8 @@
 # TODO: implement SourceFileStore and test
 
 defmodule Hologram.Compiler.SourceFileAggregator do
-  use GenServer
-
   alias Hologram.Compiler.Reflection
   alias Hologram.Compiler.SourceFileStore
-
-  def run() do
-    start_link(nil)
-  end
-
-  def start_link(opts) do
-    GenServer.start_link(__MODULE__, opts, name: __MODULE__)
-  end
-
-  @impl true
-  def init(_opts) do
-    {:ok, nil}
-  end
 
   def aggregate(module) do
     source_path = Reflection.source_path(module)
