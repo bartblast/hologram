59a87237c78522e5d9a36e40865bbcd6f9870664
Bart Blast <bart@bartblast.com>
2023-01-17 10:48:18 +0100

    Expand aliases

--- lib/hologram/compiler/expander.ex
+++ lib/hologram/compiler/expander.ex
@@ -1,108 +1,47 @@
 defmodule Hologram.Compiler.Expander do
-  alias Hologram.Compiler.{Helpers, Normalizer, Reflection}
-  alias Hologram.Compiler.IR.{MacroDefinition, RequireDirective}
+  alias Hologram.Compiler.Context
+  alias Hologram.Compiler.Helpers
+  alias Hologram.Compiler.IR.Alias
+  alias Hologram.Compiler.IR.AliasDirective
+  alias Hologram.Compiler.IR.Block
+  alias Hologram.Compiler.IR.IgnoredExpression
+  alias Hologram.Compiler.IR.ModuleType
 
-  def expand_macro(%MacroDefinition{module: module, name: name}, args) do
-    expand_macro(module, name, args)
+  def expand(ir, context \\ %Context{})
+
+  def expand(%Alias{segments: segments}, %Context{aliases: defined_aliases} = context) do
+    expanded_alias_segs = expand_alias_segs(segments, defined_aliases)
+    module = Helpers.module(expanded_alias_segs)
+
+    {%ModuleType{module: module, segments: expanded_alias_segs}, context}
   end
 
-  def expand_macro(module, name, args) do
-    expanded =
-      apply(module, :"MACRO-#{name}", [__ENV__] ++ args)
-      |> Normalizer.normalize()
-
-    case expanded do
-      {:__block__, [], exprs} ->
-        exprs
-
-      _ ->
-        [expanded]
-    end
-  end
-
-  def expand_macros(ast, requires) do
-    expand_with_fun(ast, &expand_macros_in_expression(&1, requires))
-  end
-
-  defp expand_macros_in_expression({name, _, args} = expr, requires) do
-    args = if args, do: args, else: []
-    macro_def = find_macro_definition(name, args, requires)
-
-    if macro_def do
-      expand_macro(macro_def, args)
-    else
-      expr
-    end
-  end
-
-  defp expand_macros_in_expression(expr, _), do: expr
-
-  def expand_module_pseudo_variable(
-        {:defmodule, ast_1,
-         [{:__aliases__, ast_2, module_segs}, [do: {:__block__, ast_3, exprs}]]}
+  def expand(
+        %AliasDirective{alias_segs: alias_segs, as: as},
+        %Context{aliases: defined_aliases} = context
       ) do
-    exprs =
-      Enum.reduce(exprs, [], fn expr, acc ->
-        acc ++ [expand_module_pseudo_variable(expr, module_segs)]
+    expanded_alias_segs = expand_alias_segs(alias_segs, defined_aliases)
+    new_defined_aliases = Map.put(defined_aliases, as, expanded_alias_segs)
+    context = %{context | aliases: new_defined_aliases}
+
+    {%IgnoredExpression{}, context}
+  end
+
+  def expand(%Block{expressions: exprs}, %Context{} = context) do
+    {expanded_exprs, _new_context} =
+      Enum.reduce(exprs, {[], context}, fn expr, {expanded_exprs, new_context} ->
+        {expanded_expr, new_context} = expand(expr, new_context)
+        {expanded_exprs ++ [expanded_expr], new_context}
       end)
 
-    {:defmodule, ast_1, [{:__aliases__, ast_2, module_segs}, [do: {:__block__, ast_3, exprs}]]}
+    {%Block{expressions: expanded_exprs}, context}
   end
 
-  defp expand_module_pseudo_variable({:__MODULE__, line, _}, module_segs) do
-    {:__aliases__, line, module_segs}
-  end
-
-  defp expand_module_pseudo_variable(ast, module_segs) when is_tuple(ast) do
-    Tuple.to_list(ast)
-    |> expand_module_pseudo_variable(module_segs)
-    |> List.to_tuple()
-  end
-
-  defp expand_module_pseudo_variable(ast, module_segs) when is_list(ast) do
-    Enum.map(ast, &expand_module_pseudo_variable(&1, module_segs))
-  end
-
-  defp expand_module_pseudo_variable(ast, _), do: ast
-
-  defp expand_use_directive({:use, _, [{:__aliases__, _, module_segs}]}) do
-    Helpers.module(module_segs)
-    |> expand_macro(:__using__, [nil])
-  end
-
-  defp expand_use_directive(ast), do: ast
-
-  def expand_use_directives(ast) do
-    expand_with_fun(ast, &expand_use_directive/1)
-  end
-
-  defp expand_with_fun({:defmodule, line, [aliases, [do: {:__block__, _, exprs}]]}, fun) do
-    expanded =
-      Enum.reduce(exprs, [], fn expr, acc ->
-        case fun.(expr) do
-          # expanded expression is returned wrapped in a list
-          expr when is_list(expr) ->
-            acc ++ expr
-
-          # non-expandable expression
-          expr ->
-            acc ++ [expr]
-        end
-      end)
-
-    {:defmodule, line, [aliases, [do: {:__block__, [], expanded}]]}
-  end
-
-  defp find_macro_definition(name, args, requires) do
-    require_directive =
-      Enum.find(requires, fn %RequireDirective{module: module} ->
-        Reflection.has_macro?(module, name, Enum.count(args))
-      end)
-
-    if require_directive do
-      Reflection.macro_definition(require_directive.module, name, args)
+  defp expand_alias_segs([head | tail] = alias_segs, defined_aliases) do
+    if defined_aliases[head] do
+      defined_aliases[head] ++ tail
     else
-      nil
+      alias_segs
     end
   end
 end
--- test/unit/hologram/compiler/expander_test.exs
+++ test/unit/hologram/compiler/expander_test.exs
@@ -2,394 +2,101 @@ defmodule Hologram.Compiler.ExpanderTest do
   use Hologram.Test.UnitCase, async: true
 
   alias Hologram.Compiler.Expander
-  alias Hologram.Compiler.IR.{MacroDefinition, RequireDirective}
+  alias Hologram.Compiler.IR.Block
+  alias Hologram.Compiler.IR.IgnoredExpression
+  alias Hologram.Compiler.IR.ModuleType
 
-  @module_2 Hologram.Test.Fixtures.Compiler.Expander.Module2
-  @module_4 Hologram.Test.Fixtures.Compiler.Expander.Module4
-  @module_5 Hologram.Test.Fixtures.Compiler.Expander.Module5
-  @module_6 Hologram.Test.Fixtures.Compiler.Expander.Module6
-  @module_segs_1 [:Hologram, :Test, :Fixtures, :Compiler, :Expander, :Module1]
-  @module_segs_3 [:Hologram, :Test, :Fixtures, :Compiler, :Expander, :Module3]
-  @requires [%RequireDirective{module: Hologram.Test.Fixtures.Compiler.Expander.Module6}]
+  test "alias" do
+    code = "A"
+    ir = ir(code)
+    result = Expander.expand(ir)
 
-  describe "expand_macro/2" do
-    test "single expression / no params" do
-      macro_def = %MacroDefinition{module: @module_6, name: :test_macro_1}
+    assert {%ModuleType{module: A, segments: [:A]}, _context} = result
+  end
 
-      result = Expander.expand_macro(macro_def, [])
-      expected = [{:abc, [], @module_6}]
+  describe "alias directive" do
+    test "single alias directive" do
+      code = """
+      alias A.B, as: C
+      C
+      """
 
-      assert result == expected
+      ir = ir(code)
+      result = Expander.expand(ir)
+
+      assert {
+               %Block{
+                 expressions: [
+                   %IgnoredExpression{},
+                   %ModuleType{module: A.B, segments: [:A, :B]}
+                 ]
+               },
+               _context
+             } = result
     end
 
-    test "multiple expressions" do
-      macro_def = %MacroDefinition{module: @module_6, name: :test_macro_2}
+    test "alias directive that uses an alias defined before it" do
+      code = """
+      alias A.B, as: C
+      alias C.D, as: E
+      E
+      """
 
-      result = Expander.expand_macro(macro_def, [])
+      ir = ir(code)
+      result = Expander.expand(ir)
 
-      expected = [
-        {:abc, [], @module_6},
-        {:bcd, [], @module_6}
-      ]
-
-      assert result == expected
+      assert {
+               %Block{
+                 expressions: [
+                   %IgnoredExpression{},
+                   %IgnoredExpression{},
+                   %ModuleType{module: A.B.D, segments: [:A, :B, :D]}
+                 ]
+               },
+               _context
+             } = result
     end
 
-    test "params / unquote calculated value" do
-      macro_def = %MacroDefinition{module: @module_6, name: :test_macro_3}
+    test "alias directive that uses an alias defined after it" do
+      code = """
+      alias C.D, as: E
+      alias A.B, as: C
+      E
+      """
 
-      result = Expander.expand_macro(macro_def, [5, 6])
-      expected = [11]
+      ir = ir(code)
+      result = Expander.expand(ir)
 
-      assert result == expected
-    end
-
-    test "unquote param" do
-      macro_def = %MacroDefinition{module: @module_6, name: :test_macro_4}
-
-      result = Expander.expand_macro(macro_def, [5, 6])
-
-      expected = [
-        {:+, [context: @module_6, import: Kernel],
-         [{:+, [context: @module_6, import: Kernel], [{:z, [], @module_6}, 5]}, 6]}
-      ]
-
-      assert result == expected
-    end
-
-    test "__using__ macro" do
-      macro_def = %MacroDefinition{module: @module_6, name: :__using__}
-
-      result = Expander.expand_macro(macro_def, [nil])
-
-      expected = [
-        {:import, [context: @module_6],
-         [
-           {:__aliases__, [alias: false],
-            [:Hologram, :Test, :Fixtures, :Compiler, :Expander, :Module5]}
-         ]}
-      ]
-
-      assert result == expected
-    end
-
-    test "function definition, single expression" do
-      macro_def = %MacroDefinition{module: @module_6, name: :test_macro_5}
-
-      result = Expander.expand_macro(macro_def, [])
-
-      expected = [
-        {:def, [context: @module_6, import: Kernel],
-         [
-           {:test_function, [context: @module_6], @module_6},
-           [do: {:__block__, [], [123]}]
-         ]}
-      ]
-
-      assert result == expected
-    end
-
-    test "function definition, multiple expressions" do
-      macro_def = %MacroDefinition{module: @module_6, name: :test_macro_6}
-
-      result = Expander.expand_macro(macro_def, [])
-
-      expected = [
-        {:def, [context: @module_6, import: Kernel],
-         [
-           {:test_function, [context: @module_6], @module_6},
-           [do: {:__block__, [], [1, 2]}]
-         ]}
-      ]
-
-      assert result == expected
+      assert {
+               %Block{
+                 expressions: [
+                   %IgnoredExpression{},
+                   %IgnoredExpression{},
+                   %ModuleType{module: C.D, segments: [:C, :D]}
+                 ]
+               },
+               _context
+             } = result
     end
   end
 
-  test "expand_macro/3" do
-    result = Expander.expand_macro(@module_6, :test_macro_3, [5, 6])
-    expected = [11]
-
-    assert result == expected
-  end
-
-  describe "expand_macros/2" do
-    test "no macros / non-expandable expressions only" do
-      code = """
-      defmodule Test do
-        1
-        2
-      end
-      """
-
-      ast = ast(code)
-      result = Expander.expand_macros(ast, @requires)
-
-      assert result == ast
-    end
-
-    test "single macro" do
-      code = """
-      defmodule Test do
-        test_macro_1
-      end
-      """
-
-      ast = ast(code)
-
-      result = Expander.expand_macros(ast, @requires)
-
-      expected =
-        {:defmodule, [line: 1],
-         [
-           {:__aliases__, [line: 1], [:Test]},
-           [
-             do: {:__block__, [], [{:abc, [], @module_6}]}
-           ]
-         ]}
-
-      assert result == expected
-    end
-
-    test "multiple macros" do
-      code = """
-      defmodule Test do
-        test_macro_1
-        test_macro_7
-      end
-      """
-
-      ast = ast(code)
-
-      result = Expander.expand_macros(ast, @requires)
-
-      expected =
-        {:defmodule, [line: 1],
-         [
-           {:__aliases__, [line: 1], [:Test]},
-           [
-             do: {:__block__, [], [{:abc, [], @module_6}, 777]}
-           ]
-         ]}
-
-      assert result == expected
-    end
-
-    test "multiple expressions expanded" do
-      code = """
-      defmodule Test do
-        test_macro_2
-      end
-      """
-
-      ast = ast(code)
-
-      result = Expander.expand_macros(ast, @requires)
-
-      expected =
-        {:defmodule, [line: 1],
-         [
-           {:__aliases__, [line: 1], [:Test]},
-           [
-             do:
-               {:__block__, [],
-                [
-                  {:abc, [], @module_6},
-                  {:bcd, [], @module_6}
-                ]}
-           ]
-         ]}
-
-      assert result == expected
-    end
-
-    test "macro with params" do
-      code = """
-      defmodule Test do
-        test_macro_4(1, 2)
-      end
-      """
-
-      ast = ast(code)
-
-      result = Expander.expand_macros(ast, @requires)
-
-      expected =
-        {:defmodule, [line: 1],
-         [
-           {:__aliases__, [line: 1], [:Test]},
-           [
-             do:
-               {:__block__, [],
-                [
-                  {:+, [context: @module_6, import: Kernel],
-                   [
-                     {:+, [context: @module_6, import: Kernel],
-                      [
-                        {:z, [], @module_6},
-                        1
-                      ]},
-                     2
-                   ]}
-                ]}
-           ]
-         ]}
-
-      assert result == expected
-    end
-  end
-
-  test "expand_module_pseudo_variable/1" do
+  test "block" do
     code = """
-    defmodule Abc.Bcd do
-      def test(a, b) do
-        __MODULE__.some_fun(1, 2)
-      end
-    end
+    A
+    B
     """
 
-    ast = ast(code)
-    result = Expander.expand_module_pseudo_variable(ast)
+    ir = ir(code)
+    result = Expander.expand(ir)
 
-    expected =
-      {:defmodule, [line: 1],
-       [
-         {:__aliases__, [line: 1], [:Abc, :Bcd]},
-         [
-           do:
-             {:__block__, [],
-              [
-                {:def, [line: 2],
-                 [
-                   {:test, [line: 2], [{:a, [line: 2], nil}, {:b, [line: 2], nil}]},
-                   [
-                     do:
-                       {:__block__, [],
-                        [
-                          {{:., [line: 3], [{:__aliases__, [line: 3], [:Abc, :Bcd]}, :some_fun]},
-                           [line: 3], [1, 2]}
-                        ]}
-                   ]
-                 ]}
-              ]}
-         ]
-       ]}
-
-    assert result == expected
-  end
-
-  describe "expand_use_directives/1" do
-    test "no use directives / non-expandable expressions only" do
-      code = """
-      defmodule Test do
-        1
-        2
-      end
-      """
-
-      ast = ast(code)
-      result = Expander.expand_use_directives(ast)
-
-      assert result == ast
-    end
-
-    test "single use directive" do
-      code = """
-      defmodule Test do
-        use Hologram.Test.Fixtures.Compiler.Expander.Module2
-      end
-      """
-
-      ast = ast(code)
-
-      result = Expander.expand_use_directives(ast)
-
-      expected =
-        {:defmodule, [line: 1],
-         [
-           {:__aliases__, [line: 1], [:Test]},
-           [
-             do:
-               {:__block__, [],
-                [
-                  {:import, [context: @module_2],
-                   [
-                     {:__aliases__, [alias: false], @module_segs_1}
-                   ]}
-                ]}
-           ]
-         ]}
-
-      assert result == expected
-    end
-
-    test "multiple use directives" do
-      code = """
-      defmodule Test do
-        use Hologram.Test.Fixtures.Compiler.Expander.Module2
-        use Hologram.Test.Fixtures.Compiler.Expander.Module4
-      end
-      """
-
-      ast = ast(code)
-
-      result = Expander.expand_use_directives(ast)
-
-      expected =
-        {:defmodule, [line: 1],
-         [
-           {:__aliases__, [line: 1], [:Test]},
-           [
-             do:
-               {:__block__, [],
-                [
-                  {:import, [context: @module_2],
-                   [
-                     {:__aliases__, [alias: false], @module_segs_1}
-                   ]},
-                  {:import, [context: @module_4],
-                   [
-                     {:__aliases__, [alias: false], @module_segs_3}
-                   ]}
-                ]}
-           ]
-         ]}
-
-      assert result == expected
-    end
-
-    test "multiple expressions expanded" do
-      code = """
-      defmodule Test do
-        use Hologram.Test.Fixtures.Compiler.Expander.Module5
-      end
-      """
-
-      ast = ast(code)
-
-      result = Expander.expand_use_directives(ast)
-
-      expected =
-        {:defmodule, [line: 1],
-         [
-           {:__aliases__, [line: 1], [:Test]},
-           [
-             do:
-               {:__block__, [],
-                [
-                  {:import, [context: @module_5],
-                   [
-                     {:__aliases__, [alias: false], @module_segs_1}
-                   ]},
-                  {:import, [context: @module_5],
-                   [
-                     {:__aliases__, [alias: false], @module_segs_3}
-                   ]}
-                ]}
-           ]
-         ]}
-
-      assert result == expected
-    end
+    assert {
+             %Block{
+               expressions: [
+                 %ModuleType{module: A, segments: [:A]},
+                 %ModuleType{module: B, segments: [:B]}
+               ]
+             },
+             _context
+           } = result
   end
 end
