87b7f3518e939d69abb1d8da1dc2cb7c06f2d0ff
Bart Blast <bart@bartblast.com>
2023-01-23 12:19:42 +0100

    Evaluate addition operator

--- lib/hologram/compiler/evaluator.ex
+++ lib/hologram/compiler/evaluator.ex
@@ -1,5 +1,9 @@
 defmodule Hologram.Compiler.Evaluator do
   alias Hologram.Compiler.IR
 
+  def evaluate(%IR.AdditionOperator{left: left, right: right}) do
+    evaluate(left) + evaluate(right)
+  end
+
   def evaluate(%IR.IntegerType{value: value}), do: value
 end
--- test/unit/hologram/compiler/evaluator_test.exs
+++ test/unit/hologram/compiler/evaluator_test.exs
@@ -4,8 +4,21 @@ defmodule Hologram.Compiler.EvaluatorTest do
   alias Hologram.Compiler.Evaluator
   alias Hologram.Compiler.IR
 
+  test "addition operator" do
+    ir = %IR.AdditionOperator{
+      left: %IR.IntegerType{value: 1},
+      right: %IR.IntegerType{value: 2}
+    }
+
+    result = Evaluator.evaluate(ir)
+
+    assert result == 3
+  end
+
   test "integer type" do
     ir = %IR.IntegerType{value: 123}
-    assert Evaluator.evaluate(ir) == 123
+    result = Evaluator.evaluate(ir)
+
+    assert result == 123
   end
 end
