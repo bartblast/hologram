b5b8337e765e0de77771fbfbb53fab069c2962c1
Bart Blast <bart@bartblast.com>
2023-02-10 12:37:21 +0100

    Ungroup data type transformation tests

--- test/unit/hologram/compiler/transformer_test.exs
+++ test/unit/hologram/compiler/transformer_test.exs
@@ -3,30 +3,30 @@ defmodule Hologram.Compiler.TransformerTest do
   import Hologram.Compiler.Transformer
   alias Hologram.Compiler.IR
 
-  describe "data types" do
-    test "boolean" do
-      ast = ast("true")
-      assert transform(ast) == %IR.BooleanType{value: true}
-    end
+  # --- DATA TYPES --
 
-    test "float" do
-      ast = ast("1.0")
-      assert transform(ast) == %IR.FloatType{value: 1.0}
-    end
+  test "boolean type" do
+    ast = ast("true")
+    assert transform(ast) == %IR.BooleanType{value: true}
+  end
 
-    test "integer" do
-      ast = ast("1")
-      assert transform(ast) == %IR.IntegerType{value: 1}
-    end
+  test "float type" do
+    ast = ast("1.0")
+    assert transform(ast) == %IR.FloatType{value: 1.0}
+  end
 
-    test "nil" do
-      ast = ast("nil")
-      assert transform(ast) == %IR.NilType{}
-    end
+  test "integer type" do
+    ast = ast("1")
+    assert transform(ast) == %IR.IntegerType{value: 1}
+  end
 
-    test "string" do
-      ast = ast(~s("test"))
-      assert transform(ast) == %IR.StringType{value: "test"}
-    end
+  test "nil type" do
+    ast = ast("nil")
+    assert transform(ast) == %IR.NilType{}
+  end
+
+  test "string type" do
+    ast = ast(~s("test"))
+    assert transform(ast) == %IR.StringType{value: "test"}
   end
 end
