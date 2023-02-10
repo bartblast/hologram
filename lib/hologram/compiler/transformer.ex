54ab09e1a67171a008d1e99d7e9bd2de9a643682
Bart Blast <bart@bartblast.com>
2023-02-10 12:00:39 +0100

    Fix typo

--- lib/hologram/compiler/transformer.ex
+++ lib/hologram/compiler/transformer.ex
@@ -6,7 +6,7 @@ defmodule Hologram.Compiler.Transformer do
       iex> ast = quote do 1 + 2 end
       {:+, [context: Elixir, imports: [{1, Kernel}, {2, Kernel}]], [1, 2]}
       iex> Transformer.transform(ast)
-      %IR.AdditionOperator{left: %IR.IntegerType{value: 1}, right %IR.IntegerType{value: 2}}
+      %IR.AdditionOperator{left: %IR.IntegerType{value: 1}, right: %IR.IntegerType{value: 2}}
   """
   def transform(ast)
 end
