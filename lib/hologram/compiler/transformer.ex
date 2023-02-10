a8e65959f5bfb239752dcb62d71d3316709163f1
Bart Blast <bart@bartblast.com>
2023-02-10 11:59:43 +0100

    Add transform/2 function head and doc

---
+++ lib/hologram/compiler/transformer.ex
@@ -0,0 +1,12 @@
+defmodule Hologram.Compiler.Transformer do
+  @doc """
+  Transforms Elixir AST to Hologram IR.
+
+  ## Examples
+      iex> ast = quote do 1 + 2 end
+      {:+, [context: Elixir, imports: [{1, Kernel}, {2, Kernel}]], [1, 2]}
+      iex> Transformer.transform(ast)
+      %IR.AdditionOperator{left: %IR.IntegerType{value: 1}, right %IR.IntegerType{value: 2}}
+  """
+  def transform(ast)
+end
