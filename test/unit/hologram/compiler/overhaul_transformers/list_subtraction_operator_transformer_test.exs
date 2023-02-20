9ea944e5ee5cb3c3910ff1fd1cbe6f7b0d1a9ce5
Bart Blast <bart@bartblast.com>
2023-02-20 18:34:17 +0100

    Remove old test suite

--- test/unit/hologram/compiler/overhaul_transformers/list_subtraction_operator_transformer_test.exs
+++
@@ -1,6 +0,0 @@
-defmodule Hologram.Compiler.ListSubtractionOperatorTransformerTest do
-  use Hologram.Test.UnitCase, async: true
-
-  alias Hologram.Compiler.{Context, ListSubtractionOperatorTransformer}
-  alias Hologram.Compiler.IR.{IntegerType, ListSubtractionOperator, ListType}
-end
