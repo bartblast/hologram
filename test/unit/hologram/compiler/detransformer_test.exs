54b4509e350af2556ec0e4307e346c6700a942d9
Bart Blast <bart@bartblast.com>
2023-02-23 14:54:22 +0100

    Add comments in detransformer tests

--- test/unit/hologram/compiler/detransformer_test.exs
+++ test/unit/hologram/compiler/detransformer_test.exs
@@ -6,12 +6,14 @@ defmodule Hologram.Compiler.DetransformerTest do
   # --- OPERATORS ---
 
   test "addition operator" do
+    # 1 + 2
     ir = %IR.AdditionOperator{left: %IR.IntegerType{value: 1}, right: %IR.IntegerType{value: 2}}
 
     assert detransform(ir) == {:+, [line: 0], [1, 2]}
   end
 
   test "division operator" do
+    # 1 / 2
     ir = %IR.DivisionOperator{left: %IR.IntegerType{value: 1}, right: %IR.IntegerType{value: 2}}
 
     assert detransform(ir) == {:/, [line: 0], [1, 2]}
@@ -26,6 +28,7 @@ defmodule Hologram.Compiler.DetransformerTest do
   end
 
   test "equal to operator" do
+    # 1 == 2
     ir = %IR.EqualToOperator{left: %IR.IntegerType{value: 1}, right: %IR.IntegerType{value: 2}}
 
     assert detransform(ir) == {:==, [line: 0], [1, 2]}
@@ -34,36 +37,42 @@ defmodule Hologram.Compiler.DetransformerTest do
   # --- DATA TYPES ---
 
   test "atom type" do
+    # :abc
     ir = %IR.AtomType{value: :abc}
 
     assert detransform(ir) == :abc
   end
 
   test "boolean type" do
+    # true
     ir = %IR.BooleanType{value: true}
 
     assert detransform(ir) == true
   end
 
   test "float type" do
+    # 1.23
     ir = %IR.FloatType{value: 1.23}
 
     assert detransform(ir) == 1.23
   end
 
   test "integer type" do
+    # 123
     ir = %IR.IntegerType{value: 123}
 
     assert detransform(ir) == 123
   end
 
   test "list type" do
+    # [1, :b]
     ir = %IR.ListType{data: [%IR.IntegerType{value: 1}, %IR.AtomType{value: :b}]}
 
     assert detransform(ir) == [1, :b]
   end
 
   test "map type" do
+    # %{a: 1, b: 2}
     ir = %IR.MapType{
       data: [
         {%IR.AtomType{value: :a}, %IR.IntegerType{value: 1}},
@@ -75,18 +84,21 @@ defmodule Hologram.Compiler.DetransformerTest do
   end
 
   test "module type" do
+    # A.B
     ir = %IR.ModuleType{module: A.B, segments: [:A, :B]}
 
     assert detransform(ir) == {:__aliases__, [line: 0], [:A, :B]}
   end
 
   test "nil type" do
+    # nil
     ir = %IR.NilType{}
 
     assert detransform(ir) == nil
   end
 
   test "struct type" do
+    # %Hologram.Test.Fixtures.Struct{a: 1, b: 2}
     ir = %IR.StructType{
       module: %IR.ModuleType{
         module: Hologram.Test.Fixtures.Struct,
@@ -104,6 +116,7 @@ defmodule Hologram.Compiler.DetransformerTest do
   # --- CONTROL FLOW ---
 
   test "function call" do
+    # A.B.my_fun(1, 2)
     module = %IR.ModuleType{module: A.B, segments: [:A, :B]}
     args = [%IR.IntegerType{value: 1}, %IR.IntegerType{value: 2}]
     ir = %IR.FunctionCall{module: module, function: :my_fun, args: args}
@@ -113,6 +126,7 @@ defmodule Hologram.Compiler.DetransformerTest do
   end
 
   test "variable" do
+    # test
     ir = %IR.Variable{name: :test}
 
     assert detransform(ir) == {:test, [line: 0], nil}
