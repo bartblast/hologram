c9109843f22b9f351105ca1728f5c10b8579c345
Bart Blast <bart@bartblast.com>
2023-03-06 18:25:53 +0100

    Do not format specific module file fixture

--- .formatter.exs
+++ .formatter.exs
@@ -4,15 +4,21 @@ locals_without_parens = [
   route: 1
 ]
 
-[
-  export: [locals_without_parens: locals_without_parens],
-  import_deps: [:phoenix],
-  inputs: [
+# module fixture file is ignored, to be able to test cases when there are no parenthesis in aliased calls
+inputs =
+  [
     "*.{ex,exs}",
     "{config,lib}/**/*.{ex,exs}",
     "test/{js,unit}/**/*.{ex,exs}",
     "test/e2e/{config,lib,test}/**/*.{ex,exs}"
-  ],
+  ]
+  |> Enum.flat_map(&Path.wildcard(&1, match_dot: true))
+  |> Kernel.--(["test/unit/fixtures/compiler/transformer/module_1.ex"])
+
+[
+  export: [locals_without_parens: locals_without_parens],
+  import_deps: [:phoenix],
+  inputs: inputs,
   locals_without_parens: locals_without_parens,
   subdirectories: []
 ]
