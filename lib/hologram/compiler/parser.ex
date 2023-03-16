da6ebbb908176a8691e9f05272a882d8e1d5e0d9
Bart Blast <bart@bartblast.com>
2023-03-16 11:40:24 +0100

    Add doc for parse/1

--- lib/hologram/compiler/parser.ex
+++ lib/hologram/compiler/parser.ex
@@ -1,6 +1,13 @@
 defmodule Hologram.Compiler.Parser do
   use Hologram.Commons.Parser
 
+  @doc """
+  Parses Elixir code into Elixir AST.
+
+  ## Examples
+      iex> Parser.parse("1 + 2)
+      {:ok, {:+, [line: 1], [1, 2]}}
+  """
   @impl Hologram.Commons.Parser
   def parse(code) do
     Code.string_to_quoted(code)
