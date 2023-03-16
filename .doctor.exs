08db093322c2c21fb9e517a2113d7cc56da278c8
Bart Blast <bart@bartblast.com>
2023-03-16 18:25:43 +0100

    Run Doctor in CI

--- .doctor.exs
+++ .doctor.exs
@@ -1,15 +1,3 @@
 %Doctor.Config{
-  ignore_modules: [],
-  ignore_paths: [],
-  min_module_doc_coverage: 40,
-  min_module_spec_coverage: 0,
-  min_overall_doc_coverage: 50,
-  min_overall_moduledoc_coverage: 100,
-  min_overall_spec_coverage: 0,
-  exception_moduledoc_required: true,
-  raise: false,
-  reporter: Doctor.Reporters.Full,
-  struct_type_spec_required: true,
-  umbrella: false,
-  failed: false
+  min_overall_moduledoc_coverage: 0
 }
