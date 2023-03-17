1f3a37284be8607c7ccfff460d729b95deef629d
Bart Blast <bart@bartblast.com>
2023-03-17 11:00:54 +0100

    Add doc for AST.from_code/1

--- lib/hologram/compiler/ast.ex
+++ lib/hologram/compiler/ast.ex
@@ -2,6 +2,14 @@ defmodule Hologram.Compiler.AST do
   alias Hologram.Compiler.Normalizer
   alias Hologram.Compiler.Parser
 
+  @doc """
+  Converts Elixir code into Elixir AST.
+
+  ## Examples
+
+      iex> AST.from_code("1 + 2")
+      {:+, [line: 1], [1, 2]}
+  """
   def from_code(code) do
     Parser.parse!(code)
     |> Normalizer.normalize()
