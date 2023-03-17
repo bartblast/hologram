f1ebc0f5a9b210a0846668423bb4852315a21da9
Bart Blast <bart@bartblast.com>
2023-03-17 19:11:51 +0100

    Make mix doctor more strict

--- .doctor.exs
+++ .doctor.exs
@@ -1,6 +1,12 @@
 %Doctor.Config{
+  exception_moduledoc_required: true,
   # TODO: remove Hologram.Compiler.PatternMatching
   ignore_modules: [Hologram.Compiler.PatternMatching],
   ignore_paths: [~r(^test/*)],
-  min_overall_moduledoc_coverage: 0
+  min_module_doc_coverage: 100,
+  min_module_spec_coverage: 100,
+  min_overall_doc_coverage: 100,
+  min_overall_moduledoc_coverage: 0,
+  min_overall_spec_coverage: 100,
+  struct_type_spec_required: true
 }
