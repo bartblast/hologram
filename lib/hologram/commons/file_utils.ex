314ac56837a197ba1b00442fb807d2a94e333d6c
Bart Blast <bart@bartblast.com>
2023-03-20 13:54:48 +0100

    Add doc and spec for list_files_recursively/1

--- lib/hologram/commons/file_utils.ex
+++ lib/hologram/commons/file_utils.ex
@@ -1,4 +1,18 @@
 defmodule Hologram.Commons.FileUtils do
+  @doc """
+  Lists files nested in the given path or paths. The results are sorted in ascending order.
+
+  ## Examples
+
+      iex> FileUtils.list_files_recursively("test/elixir/fixtures/commons/file_utils/list_files_recursively/dir_1")
+      ["test/elixir/fixtures/commons/file_utils/list_files_recursively/dir_1/dir_3/file_5.txt",
+       "test/elixir/fixtures/commons/file_utils/list_files_recursively/dir_1/dir_3/file_6.txt",
+       "test/elixir/fixtures/commons/file_utils/list_files_recursively/dir_1/file_3.txt",
+       "test/elixir/fixtures/commons/file_utils/list_files_recursively/dir_1/file_4.txt"]
+  """
+  @spec list_files_recursively(binary | list(binary)) :: list(binary)
+  def list_files_recursively(path_or_paths)
+
   def list_files_recursively(paths) when is_list(paths) do
     paths
     |> Enum.map(&list_files_recursively/1)
