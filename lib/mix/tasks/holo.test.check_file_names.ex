4c9913ec3556433c4b2dd80f457366d6a14f6854
Bart Blast <bart@bartblast.com>
2023-03-20 14:23:06 +0100

    Print invalid file names in red

--- lib/mix/tasks/holo.test.check_file_names.ex
+++ lib/mix/tasks/holo.test.check_file_names.ex
@@ -3,18 +3,35 @@ defmodule Mix.Tasks.Holo.Test.CheckFileNames do
   alias Hologram.Commons.FileUtils
 
   @moduledoc """
-  Checks whether test scripts have valid file names (end with _test.exs),
-  so that they can be picked up by the mix test task.
+  Checks whether test scripts have valid file names,
+  i.e. they end with "_test" and have ".exs" file extension.
+
+  If there are any invalid file names, the task exits with code 1.
+
+  Scripts not adhering to the aforementioned rules wouldn't be picked up by the mix test task.
+
+  ## Examples
+
+      $ mix holo.test.check_file_names test/elixir/hologram/compiler test/elixir/hologram/template
   """
 
   @doc false
   def run(args) do
-    result =
+    invalid_file_names =
       args
       |> FileUtils.list_files_recursively()
-      |> Enum.all?(&String.ends_with?(&1, "_test.exs"))
+      |> Enum.reject(&String.ends_with?(&1, "_test.exs"))
 
-    unless result, do: exit({:shutdown, 1})
+    if Enum.any?(invalid_file_names) do
+      IO.puts(red("Found invalid file names:"))
+      Enum.each(invalid_file_names, &IO.puts(red(&1)))
+      exit({:shutdown, 1})
+    end
+
     :ok
+  end
+
+  defp red(text) do
+    IO.ANSI.red() <> text <> IO.ANSI.reset()
   end
 end
