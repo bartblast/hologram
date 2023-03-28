95f1173f779f1a0c2381e54d72d82c32b87d0a40
Bart Blast <bart@bartblast.com>
2023-03-28 14:22:19 +0200

    Remove old code

--- overhaul/test/unit/fixtures/compiler/transformer/module_1.ex
+++ overhaul/test/unit/fixtures/compiler/transformer/module_1.ex
@@ -144,10 +144,4 @@ defmodule Hologram.Test.Fixtures.Compiler.Transformer.Module1 do
       "#{my_var}"
     end
   end
-
-  defmacro macro_module_attribute_operator_1 do
-    quote do
-      @my_attr
-    end
-  end
 end
