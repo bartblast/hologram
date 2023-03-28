f9d7920e16151e5a1c7029429d06aea4a9d24901
Bart Blast <bart@bartblast.com>
2023-03-28 13:00:13 +0200

    Overhaul transformer interceptor setup

---
+++ lib/hologram/compiler/transformer.ex
@@ -0,0 +1,10 @@
+defmodule Hologram.Compiler.Transformer do
+  if Application.compile_env(:hologram, :debug_transformer) do
+    use Interceptor.Annotated,
+      config: %{
+        {Hologram.Compiler.Transformer, :transform, 1} => [
+          after: {Hologram.Compiler.Transformer, :debug, 2}
+        ]
+      }
+  end
+end
--- overhaul/lib/hologram/compiler/transformer2.ex
+++ overhaul/lib/hologram/compiler/transformer2.ex
@@ -1,13 +1,4 @@
 defmodule Hologram.Compiler.Transformer do
-  if Application.compile_env(:hologram, :debug_transformer) do
-    use Interceptor.Annotated,
-      config: %{
-        {Hologram.Compiler.Transformer, :transform, 1} => [
-          after: {Hologram.Compiler.Transformer, :debug, 2}
-        ]
-      }
-  end
-
   alias Hologram.Compiler.AST
   alias Hologram.Compiler.Helpers
   alias Hologram.Compiler.IR
