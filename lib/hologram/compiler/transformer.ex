23d3aaccbfd48a07542f7093a56f357cdd1f95d8
Bart Blast <bart@bartblast.com>
2023-03-30 12:54:23 +0200

    Overhaul transformer debug/2

---
+++ lib/hologram/compiler/transformer.ex
@@ -0,0 +1,26 @@
+defmodule Hologram.Compiler.Transformer do
+  if Application.compile_env(:hologram, :debug_transformer) do
+    use Interceptor.Annotated,
+      config: %{
+        {Hologram.Compiler.Transformer, :transform, 1} => [
+          after: {Hologram.Compiler.Transformer, :debug, 2}
+        ]
+      }
+  end
+
+  @doc """
+  Prints debug info for intercepted transform/1 call.
+  """
+  @spec debug({module, atom, [AST.t()]}, IR.t()) :: :ok
+  def debug({_module, _function, [ast] = _args}, result) do
+    IO.puts("\nTRANSFORM...............................\n")
+    IO.puts("ast")
+    # credo:disable-for-next-line
+    IO.inspect(ast)
+    IO.puts("")
+    IO.puts("result")
+    # credo:disable-for-next-line
+    IO.inspect(result)
+    IO.puts("\n........................................\n")
+  end
+end
--- overhaul/lib/hologram2/compiler/transformer.ex
+++ overhaul/lib/hologram2/compiler/transformer.ex
@@ -1,13 +1,4 @@
 defmodule Hologram.Compiler.Transformer do
-  if Application.compile_env(:hologram, :debug_transformer) do
-    use Interceptor.Annotated,
-      config: %{
-        {Hologram.Compiler.Transformer, :transform, 1} => [
-          after: {Hologram.Compiler.Transformer, :debug, 2}
-        ]
-      }
-  end
-
   alias Hologram.Compiler.Helpers
   alias Hologram.Compiler.IR
 
@@ -95,22 +86,6 @@ defmodule Hologram.Compiler.Transformer do
   end
 
   # --- PRESERVE ORDER (END) ---
-
-  @doc """
-  Prints debug info for intercepted transform/1 call.
-  """
-  @spec debug({module, atom, [AST.t()]}, IR.t()) :: :ok
-  def debug({_module, _function, [ast] = _args}, result) do
-    IO.puts("\nTRANSFORM...............................\n")
-    IO.puts("ast")
-    # credo:disable-for-next-line
-    IO.inspect(ast)
-    IO.puts("")
-    IO.puts("result")
-    # credo:disable-for-next-line
-    IO.inspect(result)
-    IO.puts("\n........................................\n")
-  end
 
   defp build_call_ir(module, function, args) do
     new_module =
