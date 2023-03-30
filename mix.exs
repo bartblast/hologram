308293b93f740617f3e83a26a971b876b802b56e
Bart Blast <bart@bartblast.com>
2023-03-30 10:56:50 +0200

    Install beam_file package

--- mix.exs
+++ mix.exs
@@ -9,6 +9,7 @@ defmodule Hologram.MixProject do
 
   defp deps do
     [
+      {:beam_file, "~> 0.4"},
       {:credo, "~> 1.0", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false},
       {:doctor, "~> 0.21", only: [:dev, :test]},
