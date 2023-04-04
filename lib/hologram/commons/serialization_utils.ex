efb254730a610aa4a30fdb1891bbf89fe478cb5b
Bart Blast <bart@bartblast.com>
2023-04-04 18:48:58 +0200

    Add docs and spec to serialization utils module

--- lib/hologram/commons/serialization_utils.ex
+++ lib/hologram/commons/serialization_utils.ex
@@ -1,10 +1,28 @@
 defmodule Hologram.Commons.SerializationUtils do
+  @doc """
+  Deserializes binary data to Elixir term.
+
+  ## Examples
+
+      iex> deserialize(<<131, 104, 3, 97, 1, 97, 2, 97, 3>>)
+      {1, 2, 3}
+  """
+  @spec deserialize(binary) :: term
   # sobelow_skip ["Misc.BinToTerm"]
-  def deserialize(data) do
-    :erlang.binary_to_term(data, [:safe])
+  def deserialize(binary) do
+    :erlang.binary_to_term(binary, [:safe])
   end
 
-  def serialize(data) do
-    :erlang.term_to_binary(data, compressed: 9)
+  @doc """
+  Serializes Elixir term to binary data.
+
+  ## Examples
+
+      iex> serialize({1, 2, 3})
+      <<131, 104, 3, 97, 1, 97, 2, 97, 3>>
+  """
+  @spec serialize(term) :: binary
+  def serialize(term) do
+    :erlang.term_to_binary(term, compressed: 9)
   end
 end
