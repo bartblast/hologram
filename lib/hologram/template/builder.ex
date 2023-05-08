2c9a191fceea95014fbd3adca92fd037c7a378f2
Bart Blast <bart@bartblast.com>
2023-05-08 19:18:22 +0200

    Render code for element and component nodes

--- lib/hologram/template/builder.ex
+++ lib/hologram/template/builder.ex
@@ -1,25 +1,42 @@
 defmodule Hologram.Template.Builder do
   alias Hologram.Compiler.AST
+  alias Hologram.Template.Helpers
 
   def build(tags) do
-    {code, _last_tag} =
+    {code, _last_tag_type} =
       tags
-      |> Enum.reduce({"", nil}, fn tag, {code_acc, last_tag} ->
-        tag_code = render_code(tag)
-        new_code_acc = append_code(code_acc, tag_code, last_tag)
+      |> Enum.reduce({"", nil}, fn tag, {code_acc, last_tag_type} ->
+        current_tag_type = elem(tag, 0)
+        current_tag_code = render_code(tag)
+        new_code_acc = append_code(code_acc, current_tag_code, last_tag_type)
 
-        {new_code_acc, :text}
+        {new_code_acc, current_tag_type}
       end)
 
     AST.for_code("[#{code}]")
   end
 
-  defp append_code(code_acc, code, last_tag) when last_tag in [nil, :end_tag] do
+  defp append_code(code_acc, code, last_tag_type) when last_tag_type in [:end_tag, :text] do
+    code_acc <> ", " <> code
+  end
+
+  defp append_code(code_acc, code, _last_tag_type) do
     code_acc <> code
   end
 
-  defp append_code(code_acc, code, _last_tag) do
-    code_acc <> ", " <> code
+  defp render_code({:end_tag, _tag_name}) do
+    "]}"
+  end
+
+  defp render_code({:start_tag, {tag_name, attributes}}) do
+    tag_type = Helpers.tag_type(tag_name)
+
+    attributes_code =
+      Enum.map_join(attributes, ", ", fn {name, value_parts} ->
+        "{\"#{name}\", [" <> Enum.map_join(value_parts, ", ", &render_code/1) <> "]}"
+      end)
+
+    "{:#{tag_type}, \"#{tag_name}\", [#{attributes_code}], ["
   end
 
   defp render_code({:text, str}) do
--- test/elixir/hologram/template/builder_test.exs
+++ test/elixir/hologram/template/builder_test.exs
@@ -5,4 +5,144 @@ defmodule Hologram.Template.BuilderTest do
   test "text node" do
     assert build([{:text, "abc"}]) == [{:text, "abc"}]
   end
+
+  describe "element node & component node" do
+    [
+      {:element, "attribute", "div"},
+      {:component, "property", "Aaa.Bbb"}
+    ]
+    |> Enum.each(fn {tag_type, attr_or_prop, tag_name} ->
+      test "#{tag_type} node without #{attr_or_prop}(s) or children" do
+        tag_name = unquote(tag_name)
+        tags = [{:start_tag, {tag_name, []}}, {:end_tag, tag_name}]
+
+        assert build(tags) == [
+                 {:{}, [line: 1], [unquote(tag_type), tag_name, [], []]}
+               ]
+      end
+
+      test "#{tag_type} node with single #{attr_or_prop}" do
+        tag_name = unquote(tag_name)
+        tags = [{:start_tag, {tag_name, [{"my_key", [text: "my_value"]}]}}, {:end_tag, tag_name}]
+
+        assert build(tags) ==
+                 [
+                   {:{}, [line: 1],
+                    [unquote(tag_type), tag_name, [{"my_key", [text: "my_value"]}], []]}
+                 ]
+      end
+
+      test "#{tag_type} node, with multiple #{attr_or_prop}(s)" do
+        tag_name = unquote(tag_name)
+
+        tags = [
+          {:start_tag,
+           {tag_name, [{"my_key_1", [text: "my_value_1"]}, {"my_key_2", [text: "my_value_2"]}]}},
+          {:end_tag, tag_name}
+        ]
+
+        assert build(tags) == [
+                 {:{}, [line: 1],
+                  [
+                    unquote(tag_type),
+                    tag_name,
+                    [{"my_key_1", [text: "my_value_1"]}, {"my_key_2", [text: "my_value_2"]}],
+                    []
+                  ]}
+               ]
+      end
+
+      test "#{tag_type} node #{attr_or_prop} with multiple value parts" do
+        tag_name = unquote(tag_name)
+
+        tags = [
+          {:start_tag, {tag_name, [{"my_key", [text: "my_value_1", text: "my_value_2"]}]}},
+          {:end_tag, tag_name}
+        ]
+
+        assert build(tags) == [
+                 {:{}, [line: 1],
+                  [
+                    unquote(tag_type),
+                    tag_name,
+                    [{"my_key", [text: "my_value_1", text: "my_value_2"]}],
+                    []
+                  ]}
+               ]
+      end
+
+      test "#{tag_type} node with text child" do
+        tag_name = unquote(tag_name)
+        tags = [{:start_tag, {tag_name, []}}, {:text, "abc"}, {:end_tag, tag_name}]
+
+        assert build(tags) == [
+                 {:{}, [line: 1], [unquote(tag_type), tag_name, [], [{:text, "abc"}]]}
+               ]
+      end
+
+      test "#{tag_type} node with element child" do
+        tag_name = unquote(tag_name)
+
+        tags = [
+          {:start_tag, {tag_name, []}},
+          {:start_tag, {"span", []}},
+          {:end_tag, "span"},
+          {:end_tag, tag_name}
+        ]
+
+        assert build(tags) == [
+                 {:{}, [line: 1],
+                  [
+                    unquote(tag_type),
+                    tag_name,
+                    [],
+                    [{:{}, [line: 1], [:element, "span", [], []]}]
+                  ]}
+               ]
+      end
+
+      test "#{tag_type} node with component child" do
+        tag_name = unquote(tag_name)
+
+        tags = [
+          {:start_tag, {tag_name, []}},
+          {:start_tag, {"Xxx.Yyy", []}},
+          {:end_tag, "Xxx.Yyy"},
+          {:end_tag, tag_name}
+        ]
+
+        assert build(tags) == [
+                 {:{}, [line: 1],
+                  [
+                    unquote(tag_type),
+                    tag_name,
+                    [],
+                    [{:{}, [line: 1], [:component, "Xxx.Yyy", [], []]}]
+                  ]}
+               ]
+      end
+
+      test "#{tag_type} node with multiple children" do
+        tag_name = unquote(tag_name)
+
+        tags = [
+          {:start_tag, {tag_name, []}},
+          {:start_tag, {"span", []}},
+          {:end_tag, "span"},
+          {:text, "abc"},
+          {:end_tag, tag_name}
+        ]
+
+        assert build(tags) == [
+                 {:{}, [line: 1],
+                  [
+                    unquote(tag_type),
+                    tag_name,
+                    [],
+                    [{:{}, [line: 1], [:element, "span", [], []]}, {:text, "abc"}]
+                  ]}
+               ]
+      end
+    end)
+  end
 end
