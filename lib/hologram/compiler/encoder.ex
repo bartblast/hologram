b8cb94e17d07792a81d88fec7cde635e3723f8f7
Bart Blast <bart@bartblast.com>
2023-05-11 19:19:16 +0200

    Encode float type

--- lib/hologram/compiler/encoder.ex
+++ lib/hologram/compiler/encoder.ex
@@ -1,6 +1,10 @@
 defmodule Hologram.Compiler.Encoder do
   alias Hologram.Compiler.IR
 
+  def encode(%IR.FloatType{value: value}) do
+    encode_primitive_type(:float, value)
+  end
+
   def encode(%IR.IntegerType{value: value}) do
     encode_primitive_type(:integer, value)
   end
--- test/elixir/hologram/compiler/encoder_test.exs
+++ test/elixir/hologram/compiler/encoder_test.exs
@@ -3,6 +3,10 @@ defmodule Hologram.Compiler.EncoderTest do
   import Hologram.Compiler.Encoder
   alias Hologram.Compiler.IR
 
+  test "float type" do
+    assert encode(%IR.FloatType{value: 1.23}) == "{type: 'float', value: 1.23}"
+  end
+
   test "integer type" do
     assert encode(%IR.IntegerType{value: 123}) == "{type: 'integer', value: 123}"
   end
