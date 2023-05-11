84cb7f07378b204f660c05bf11f13180c58dd786
Bart Blast <bart@bartblast.com>
2023-05-11 14:22:47 +0200

    Remove old resolver module

--- overhaul/test/unit/fixtures/compiler/resolver/module_1.ex
+++
@@ -1,5 +0,0 @@
-defmodule Hologram.Test.Fixtures.Compiler.Resolver.Module1 do
-  def test(_a, _b) do
-    :ok
-  end
-end
--- overhaul/test/unit/fixtures/compiler/resolver/module_2.ex
+++
@@ -1,5 +0,0 @@
-defmodule Hologram.Test.Fixtures.Compiler.Resolver.Module2 do
-  def test(_a, _b) do
-    :ok
-  end
-end
