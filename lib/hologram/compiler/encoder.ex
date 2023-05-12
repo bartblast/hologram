f7afe6386e4e7f3f127b50a980f9f0f3209e5a49
Bart Blast <bart@bartblast.com>
2023-05-12 12:46:27 +0200

    Refactor encoder helpers

--- lib/hologram/compiler/encoder.ex
+++ lib/hologram/compiler/encoder.ex
@@ -7,11 +7,11 @@ defmodule Hologram.Compiler.Encoder do
   end
 
   def encode(%IR.FloatType{value: value}) do
-    encode_primitive_type(:float, value)
+    encode_primitive_type(:float, value, false)
   end
 
   def encode(%IR.IntegerType{value: value}) do
-    encode_primitive_type(:integer, value)
+    encode_primitive_type(:integer, value, false)
   end
 
   def encode(%IR.ListType{data: data}) do
@@ -26,27 +26,35 @@ defmodule Hologram.Compiler.Encoder do
     "{type: 'tuple', data: #{encode_as_array(data)}}"
   end
 
-  def encode_as_string(value) do
-    value
-    |> to_string()
-    |> String.replace("'", "\\'")
-    |> String.replace("\n", "\\n")
-    |> StringUtils.wrap("'", "'")
-  end
-
   defp encode_as_array(data) do
     Enum.map(data, &encode/1)
     |> Enum.join(", ")
     |> StringUtils.wrap("[", "]")
   end
 
-  defp encode_primitive_type(type, value, as_string \\ false)
+  defp encode_as_string(value, wrap)
+
+  defp encode_as_string(value, false) do
+    value
+    |> to_string()
+    |> String.replace("'", "\\'")
+    |> String.replace("\n", "\\n")
+  end
+
+  defp encode_as_string(value, true) do
+    value
+    |> encode_as_string(false)
+    |> StringUtils.wrap("'", "'")
+  end
+
+  defp encode_primitive_type(type, value, as_string)
 
   defp encode_primitive_type(type, value, false) do
     "{type: '#{type}', value: #{value}}"
   end
 
   defp encode_primitive_type(type, value, true) do
-    encode_primitive_type(type, encode_as_string(value))
+    value_str = encode_as_string(value, true)
+    encode_primitive_type(type, value_str, false)
   end
 end
