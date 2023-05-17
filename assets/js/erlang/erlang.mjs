f5e983037d16c840337792ef5bf832fd42271fff
Bart Blast <bart@bartblast.com>
2023-05-17 19:24:46 +0200

    Update prettier paths

--- assets/js/erlang/erlang.mjs
+++ assets/js/erlang/erlang.mjs
@@ -5,13 +5,13 @@ import Type from "../type.mjs";
 export default class erlang {
   // start: is_float/1
   static is_float(term) {
-    return Type.boolean(Type.isFloat(term))
+    return Type.boolean(Type.isFloat(term));
   }
   // end: is_float/1
 
   // start: is_number/1
   static is_number(term) {
-    return Type.boolean(Type.isNumber(term))
+    return Type.boolean(Type.isNumber(term));
   }
   // end: is_number/1
 }
--- test/javascript/erlang/erlang_test.mjs
+++ test/javascript/erlang/erlang_test.mjs
@@ -1,34 +1,38 @@
 "use strict";
 
-import {assert, assertBoxedTrue, assertBoxedFalse} from "../../../assets/js/test_support.mjs";
+import {
+  assert,
+  assertBoxedTrue,
+  assertBoxedFalse,
+} from "../../../assets/js/test_support.mjs";
 import erlang from "../../../assets/js/erlang/erlang.mjs";
 import Type from "../../../assets/js/type.mjs";
 
 describe("is_float/1", () => {
   it("returns boxed true for boxed floats", () => {
     const result = Type.isFloat(Type.float(1.23));
-    assertBoxedTrue(result)
+    assertBoxedTrue(result);
   });
 
   it("returns boxed false for types other than boxed float", () => {
     const result = Type.isFloat(Type.atom("abc"));
-    assertBoxedFalse(result)
+    assertBoxedFalse(result);
   });
-})
+});
 
 describe("is_number/1", () => {
   it("returns boxed true for boxed floats", () => {
     const result = Type.isNumber(Type.float(1.23));
-    assertBoxedTrue(result)
+    assertBoxedTrue(result);
   });
 
   it("returns boxed true for boxed integers", () => {
     const result = Type.isNumber(Type.integer(123));
-    assertBoxedTrue(result)
+    assertBoxedTrue(result);
   });
 
   it("returns boxed false for types other than boxed float or boxed integer", () => {
     const result = Type.isNumber(Type.atom("abc"));
-    assertBoxedFalse(result)
+    assertBoxedFalse(result);
   });
-})
+});
