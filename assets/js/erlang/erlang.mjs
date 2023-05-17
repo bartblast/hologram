7b882759ba865f8fe2822a71c2a70a91da782d50
Bart Blast <bart@bartblast.com>
2023-05-17 19:05:31 +0200

    Implement JS version of :erlang.is_float/1

--- assets/js/erlang/erlang.mjs
+++ assets/js/erlang/erlang.mjs
@@ -3,6 +3,12 @@
 import Type from "../type.mjs";
 
 export default class erlang {
+  // start: is_float/1
+  static is_float(term) {
+    return Type.boolean(Type.isFloat(term))
+  }
+  // end: is_float/1
+
   // start: is_number/1
   static is_number(term) {
     return Type.isNumber(term)
--- test/javascript/erlang/erlang_test.mjs
+++ test/javascript/erlang/erlang_test.mjs
@@ -1,8 +1,20 @@
 "use strict";
 
-import {assert} from "../../../assets/js/test_support.mjs";
+import {assert, assertBoxedTrue, assertBoxedFalse} from "../../../assets/js/test_support.mjs";
 import erlang from "../../../assets/js/erlang/erlang.mjs";
 import Type from "../../../assets/js/type.mjs";
+
+describe("is_float/1", () => {
+  it("returns boxed true for boxed floats", () => {
+    const result = Type.isFloat(Type.float(1.23));
+    assertBoxedTrue(result)
+  });
+
+  it("returns boxed false for types other than boxed float", () => {
+    const result = Type.isFloat(Type.atom("abc"));
+    assertBoxedFalse(result)
+  });
+})
 
 describe("is_number/1", () => {
   it("delegates to runtime Type.isNumber/1", () => {
