8c54e5b484f6d19f16e4ea90cdd42934a2a3477d
Bart Blast <bart@bartblast.com>
2023-05-30 17:38:25 +0200

    Add function to_bit_list/1 doc and typespec

--- lib/hologram/commons/bitstring_utils.ex
+++ lib/hologram/commons/bitstring_utils.ex
@@ -1,4 +1,13 @@
 defmodule Hologram.Commons.BitstringUtils do
+  @doc """
+  Given a bitstring returns its corresponding list of bits (starting with the most significant bit at index 0).
+
+  ## Examples
+
+      iex> to_bit_list(<<25>>)
+      [1, 1, 0, 0, 1]
+  """
+  @spec to_bit_list(bitstring) :: list(integer)
   def to_bit_list(bitstring) do
     for <<bit::1*1 <- bitstring>>, do: bit
   end
