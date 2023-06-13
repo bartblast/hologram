c2b780b3f9738f988043701e64087e12205546bb
Bart Blast <bart@bartblast.com>
2023-06-13 13:01:46 +0200

    Implement starts_with_lowercase?/1 string util

--- lib/hologram/commons/string_utils.ex
+++ lib/hologram/commons/string_utils.ex
@@ -1,5 +1,39 @@
 defmodule Hologram.Commons.StringUtils do
   @doc """
+  Checks whether a string starts with a lowercase letter.
+
+  - This function uses the `String.next_grapheme/1` function to extract the first letter of the string.
+  - It converts the first letter to lowercase using `String.downcase/1` and compares it with the original first letter to determine if it is lowercase.
+  - If the input string is empty, the function returns `false`.
+
+  ## Parameters
+
+  - `str` - The input string to be checked.
+
+  ## Returns
+
+  Returns `true` if the string starts with a lowercase letter, and `false` otherwise.
+
+  ## Examples
+
+      iex> starts_with_lowercase?("Hello")
+      false
+
+      iex> starts_with_lowercase?("world")
+      true
+  """
+  @spec starts_with_lowercase?(String.t()) :: boolean()
+  def starts_with_lowercase?(str) do
+    case String.next_grapheme(str) do
+      {first_letter, _rest} ->
+        String.downcase(first_letter) == first_letter
+
+      nil ->
+        false
+    end
+  end
+
+  @doc """
   Wraps the given string with one string on the left side and another string on the right side.
 
   ## Examples
--- test/elixir/hologram/commons/string_utils_test.exs
+++ test/elixir/hologram/commons/string_utils_test.exs
@@ -2,6 +2,28 @@ defmodule Hologram.Commons.StringUtilsTest do
   use Hologram.Test.BasicCase, async: true
   import Hologram.Commons.StringUtils
 
+  describe "starts_with_lowercase?/1" do
+    test "returns true for a string starting with lowercase letter" do
+      assert starts_with_lowercase?("hello")
+    end
+
+    test "returns false for a string starting with uppercase letter" do
+      refute starts_with_lowercase?("World")
+    end
+
+    test "returns false for an empty string" do
+      refute starts_with_lowercase?("")
+    end
+
+    test "returns true for a string starting with a non-alphabetic character" do
+      assert starts_with_lowercase?("123abc")
+    end
+
+    test "returns true for a string containing only non-alphabetic characters" do
+      assert starts_with_lowercase?("!@#$")
+    end
+  end
+
   test "wrap/3" do
     assert wrap("ab", "cd", "ef") == "cdabef"
   end
