d7a0dee8f520553780bf906650f39da4ac204145
Bart Blast <bart@bartblast.com>
2023-06-14 19:28:24 +0200

    Add doc and typespec for IntegerUtils.count_digits/1

--- lib/hologram/commons/integer_utils.ex
+++ lib/hologram/commons/integer_utils.ex
@@ -1,4 +1,19 @@
 defmodule Hologram.Commons.IntegerUtils do
+  @doc """
+  Counts the number of digits in the given integer.
+
+  ## Examples
+
+      iex> count_digits(123)
+      3
+
+      iex> count_digits(0)
+      1
+
+      iex> count_digits(-123)
+      3
+  """
+  @spec count_digits(integer) :: non_neg_integer
   def count_digits(integer) do
     integer
     |> Integer.digits()
