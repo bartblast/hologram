37773b70d66980a6b3af91c410cf2413323f4af8
Bart Blast <bart@bartblast.com>
2023-06-23 20:09:02 +0200

    Cleanup

--- test/javascript/interpreter_test.mjs
+++ test/javascript/interpreter_test.mjs
@@ -925,7 +925,7 @@ describe("count()", () => {
   });
 });
 
-describe.only("defineFunction()", () => {
+describe("defineFunction()", () => {
   beforeEach(() => {
     // def my_fun_a(1), do: :expr_1
     // def my_fun_a(2), do: :expr_2
