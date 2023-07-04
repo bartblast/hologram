0e7c6dedf449f1865c0b737c132a68c091bdfddb
Bart Blast <bart@bartblast.com>
2023-07-04 15:49:31 +0200

    Cleanup

--- test/javascript/interpreter_test.mjs
+++ test/javascript/interpreter_test.mjs
@@ -10,7 +10,6 @@ import {
 import Erlang from "../../assets/js/erlang/erlang.mjs";
 import Interpreter from "../../assets/js/interpreter.mjs";
 import Type from "../../assets/js/type.mjs";
-import Utils from "../../assets/js/utils.mjs";
 
 before(() => linkModules());
 after(() => unlinkModules());
@@ -1710,7 +1709,7 @@ describe("matchOperator()", () => {
     });
 
     it("y = x + (x = 3) + x, (x = 11)", () => {
-      let vars = {
+      const vars = {
         a: Type.integer(9),
         x: Type.integer(11),
       };
