e1757860028388fe46f0d2006e44ef810476aadc
Bart Blast <bart@bartblast.com>
2023-07-06 20:24:11 +0200

    Add typespec and doc to js/1 test helper

--- test/elixir/support/helpers.ex
+++ test/elixir/support/helpers.ex
@@ -7,7 +7,16 @@ defmodule Hologram.Test.Helpers do
   defdelegate ast(code), to: AST, as: :for_code
   defdelegate ir(code, context), to: IR, as: :for_code
 
-  def encode(code) do
+  @doc """
+  Encodes Elixir source code to JavaScript source code.
+
+  ## Examples
+
+      iex> js("[1, :abc]")
+      "Type.list([Type.integer(1), Type.atom(\"abc\")])"
+  """
+  @spec js(String.t()) :: String.t()
+  def js(code) do
     code
     |> ir(%Context{})
     |> Encoder.encode(%Context{})
