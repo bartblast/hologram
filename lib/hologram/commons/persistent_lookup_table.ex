1c12a3fad45994441603e59b86cbe1ef3a5b1e3d
Bart Blast <bart@bartblast.com>
2023-07-11 20:25:27 +0200

    Implement persistent lookup table module

---
+++ lib/hologram/commons/persistent_lookup_table.ex
@@ -0,0 +1,163 @@
+defmodule Hologram.Commons.PersistentLookupTable do
+  use GenServer
+
+  alias Hologram.Commons.PersistentLookupTable
+  alias Hologram.Commons.SerializationUtils
+
+  defstruct pid: nil, name: nil
+  @type t :: %PersistentLookupTable{pid: pid, name: atom}
+
+  @doc """
+  Returns the value stored in the underlying ETS table under the given key.
+
+  ## Examples
+
+      iex> PersistentLookupTable.get(:my_plt, :my_key)
+      :my_value
+  """
+  @spec get(atom, atom) :: {:ok, term} | :error
+  def get(name, key) do
+    case :ets.lookup(name, key) do
+      [{^key, value}] ->
+        {:ok, value}
+
+      _fallback ->
+        :error
+    end
+  end
+
+  @doc """
+  Returns all items stored in the underlying ETS table.
+
+  ## Examples
+
+      iex> PersistentLookupTable.get_all(:my_plt)
+      %{key_1: :value_1, key_2: :value_2}
+  """
+  @spec get_all(atom) :: %{atom => term}
+  def get_all(name) do
+    name
+    |> :ets.tab2list()
+    |> Enum.into(%{})
+  end
+
+  @doc """
+  Creates the underlying ETS table if it doesn't exist yet,
+  truncates it, and populates it from the dump (if the dump exists).
+
+  ## Examples
+
+      iex> PersistentLookupTable.init(name: :my_plt, dump_path: "/my_dump_path")
+      {:ok, nil}
+  """
+  @impl GenServer
+  @spec init(keyword) :: {:ok, nil}
+  def init(opts) do
+    maybe_create_table(opts[:name])
+    reload_table(opts)
+    {:ok, nil}
+  end
+
+  @doc """
+  Puts multiple items into the underlying ETS table.
+
+  ## Examples
+
+      iex> PersistentLookupTable.put(:my_plt, [key_1: :value_1, key_2: :value_2])
+      true
+  """
+  @spec put(atom, keyword) :: true
+  def put(name, items) do
+    :ets.insert(name, items)
+  end
+
+  @doc """
+  Puts the given item into the underlying ETS table.
+
+  ## Examples
+
+      iex> PersistentLookupTable.put(%PersistentLookupTable{name: :my_plt}, :my_key, :my_value)
+      true
+
+      iex> PersistentLookupTable.put(:my_plt, :my_key, :my_value)
+      true
+  """
+  @spec put(PersistentLookupTable.t() | atom, atom, term) :: true
+
+  def put(%PersistentLookupTable{name: name}, key, value) do
+    put(name, key, value)
+  end
+
+  def put(name, key, value) do
+    :ets.insert(name, {key, value})
+  end
+
+  @doc """
+  Tells whether the underlying GenServer process is alive.
+
+  ## Examples
+
+      iex> PersistentLookupTable.running?(:my_plt)
+      true
+  """
+  @spec running?(atom) :: boolean
+  def running?(name) do
+    pid = Process.whereis(name)
+    if pid, do: Process.alive?(pid), else: false
+  end
+
+  @doc """
+  Starts the underlying GenServer process.
+
+  ## Examples
+
+      iex> PersistentLookupTable.start(name: :my_plt, dump_path: "/my_dump_path")
+      %PersistentLookupTable{pid: #PID<0.273.0>, name: :my_plt}
+  """
+  @spec start(keyword) :: PersistentLookupTable.t()
+  def start(opts) do
+    {:ok, pid} = GenServer.start_link(PersistentLookupTable, opts, name: opts[:name])
+    %PersistentLookupTable{pid: pid, name: opts[:name]}
+  end
+
+  @doc """
+  Tells whether the underlying ETS table exists.
+
+  ## Examples
+
+      iex> PersistentLookupTable.table_exists?(:my_plt)
+      true
+  """
+  @spec table_exists?(atom) :: boolean
+  def table_exists?(name) do
+    :ets.info(name) != :undefined
+  end
+
+  defp create_table(name) do
+    :ets.new(name, [:public, :named_table])
+  end
+
+  defp maybe_create_table(name) do
+    if !table_exists?(name), do: create_table(name)
+  end
+
+  defp populate_table(opts) do
+    opts[:dump_path]
+    |> File.read!()
+    |> SerializationUtils.deserialize()
+    |> Enum.each(fn {key, value} -> put(opts[:name], key, value) end)
+  end
+
+  defp reload_table(opts) do
+    truncate_table(opts[:name])
+
+    if opts[:dump_path] && File.exists?(opts[:dump_path]) do
+      populate_table_fun = opts[:populate_table_fun] || (&populate_table/1)
+      populate_table_fun.(opts)
+    end
+  end
+
+  defp truncate_table(name) do
+    :ets.delete_all_objects(name)
+  end
+end
---
+++ test/elixir/hologram/commons/persistent_lookup_table_test.exs
@@ -0,0 +1,161 @@
+defmodule Hologram.Commons.PersistentLookupTableTest do
+  use Hologram.Test.BasicCase, async: false
+  import Hologram.Commons.PersistentLookupTable
+
+  alias Hologram.Commons.PersistentLookupTable
+  alias Hologram.Commons.SerializationUtils
+
+  @dump_path "#{File.cwd!()}/tmp/plt_#{__MODULE__}.bin"
+  @items %{key_1: :value_1, key_2: :value_2}
+  @name :"plt_#{__MODULE__}"
+  @opts name: @name, dump_path: @dump_path
+
+  defp ets_table_exists?(table_name) do
+    table_name
+    |> :ets.info()
+    |> is_list()
+  end
+
+  defp dump_items do
+    binary = SerializationUtils.serialize(@items)
+    File.write!(@dump_path, binary)
+  end
+
+  defp wait_for_test_cleanup do
+    if running?(@name) || table_exists?(@name) do
+      :timer.sleep(1)
+      wait_for_test_cleanup()
+    end
+  end
+
+  setup do
+    wait_for_test_cleanup()
+    dump_items()
+
+    :ok
+  end
+
+  describe "get/2" do
+    test "key exists" do
+      start(@opts)
+      assert get(@name, :key_1) == {:ok, :value_1}
+    end
+
+    test "key doesn't exist" do
+      start(@opts)
+      assert get(@name, :key_invalid) == :error
+    end
+  end
+
+  test "get_all/1" do
+    start(@opts)
+    assert get_all(@name) == @items
+  end
+
+  test "put/2" do
+    start(@opts)
+
+    items = [
+      {:key_3, :value_3},
+      {:key_4, :value_4}
+    ]
+
+    put(@name, items)
+
+    assert get(@name, :key_3) == {:ok, :value_3}
+    assert get(@name, :key_4) == {:ok, :value_4}
+  end
+
+  describe "put/3" do
+    test "first arg is %PersistentLookupTable{} struct" do
+      plt = start(@opts)
+      put(plt, :key_3, :value_3)
+
+      assert get(@name, :key_3) == {:ok, :value_3}
+    end
+
+    test "first arg is PLT name" do
+      start(@opts)
+      put(@name, :key_3, :value_3)
+
+      assert get(@name, :key_3) == {:ok, :value_3}
+    end
+  end
+
+  describe "running?/1" do
+    test "is running" do
+      start(@opts)
+      assert running?(@name)
+    end
+
+    test "is not running" do
+      refute running?(@name)
+    end
+  end
+
+  describe "start/1" do
+    test "process name is registered" do
+      %PersistentLookupTable{pid: pid} = start(@opts)
+      assert Process.whereis(@name) == pid
+    end
+
+    test "ETS table is created if it doesn't exist yet" do
+      start(@opts)
+      assert ets_table_exists?(@name)
+    end
+
+    test "ETS table is not created if it already exists" do
+      :ets.new(@name, [:public, :named_table])
+      start(@opts)
+
+      assert ets_table_exists?(@name)
+    end
+
+    test "ETS table is truncated" do
+      :ets.new(@name, [:public, :named_table])
+      :ets.insert(@name, {:key_3, :value_3})
+      start(@opts)
+
+      assert get_all(@name) == @items
+    end
+
+    test "ETS table is populated when dump_path is given in opts and dump file exists" do
+      start(@opts)
+      assert get_all(@name) == @items
+    end
+
+    test "ETS table is not populated when dump_path is not given in opts" do
+      start(name: @name, dump_path: nil)
+      assert get_all(@name) == %{}
+    end
+
+    test "ETS table is not populated when dump_path is given in opts but dump file doesn't exist" do
+      File.rm!(@dump_path)
+      start(@opts)
+
+      assert get_all(@name) == %{}
+    end
+
+    test "applies custom populate table function if it is given in opts" do
+      populate_table_fun = fn _arg ->
+        put(@name, :custom_key_1, :custom_value_1)
+        put(@name, :custom_key_2, :custom_value_2)
+      end
+
+      start(name: @name, dump_path: @dump_path, populate_table_fun: populate_table_fun)
+
+      assert get_all(@name) == %{custom_key_1: :custom_value_1, custom_key_2: :custom_value_2}
+    end
+  end
+
+  describe "table_exists?/1" do
+    test "exists" do
+      :ets.new(@name, [:public, :named_table])
+      assert table_exists?(@name)
+    end
+
+    test "doesn't exist" do
+      refute table_exists?(@name)
+    end
+  end
+end
