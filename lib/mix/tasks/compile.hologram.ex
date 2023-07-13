feb41dd7275b898d7bd353cabd48c31fae60ef40
Bart Blast <bart@bartblast.com>
2023-07-13 19:58:28 +0200

    Draft compile mix task

---
+++ lib/mix/tasks/compile.hologram.ex
@@ -0,0 +1,43 @@
+# TODO: finish & test
+
+defmodule Mix.Tasks.Compile.Hologram do
+  use Mix.Task.Compiler
+
+  alias Hologram.Commons.PersistentLookupTable, as: PLT
+  alias Hologram.Compiler.Builder
+  alias Hologram.Compiler.Reflection
+
+  # @call_graph_dump_path Reflection.root_priv_path() <> "/call_graph.bin"
+  # @call_graph_name :hologram_call_graph
+
+  @ir_plt_dump_path Reflection.root_priv_path() <> "/plt_ir.bin"
+  @ir_plt_name :hologram_plt_ir
+
+  @module_digest_plt_dump_path Reflection.root_priv_path() <> "/plt_module_digest.bin"
+  @module_digest_plt_name :hologram_plt_module_digest
+
+  def run(_opts \\ []) do
+    new_module_digest_plt_name = :"new_#{@module_digest_plt_name}"
+    new_module_digest_plt = Builder.build_module_digest_plt(new_module_digest_plt_name)
+
+    old_module_digest_plt =
+      PLT.start(name: @module_digest_plt_name, dump_path: @module_digest_plt_dump_path)
+
+    diff = Builder.diff_module_digest_plts(old_module_digest_plt, new_module_digest_plt)
+
+    ir_plt = PLT.start(name: @ir_plt_name, dump_path: @ir_plt_dump_path)
+    Builder.patch_ir_plt(ir_plt, diff)
+
+    # call_graph = CallGraph.start(name: @call_graph_name, dump_path: @call_graph_dump_path)
+    # CallGraph.patch(call_graph, diff)
+
+    # Reflections.list_pages()
+    # |> Builder.build_page_js(&1, output_path, module_defs, call_graph))
+    # |> patch page digest plt
+    # |> create page-digest files
+
+    # write PLTs and call graph...
+
+    :ok
+  end
+end
