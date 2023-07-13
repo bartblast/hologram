2c8f0b3757dd7ae0e5f26acefd09d733e4f0d64d
Bart Blast <bart@bartblast.com>
2023-07-13 14:32:33 +0200

    Temporarily comment out code TODO

--- lib/hologram/compiler/call_graph.ex
+++ lib/hologram/compiler/call_graph.ex
@@ -1,50 +1,54 @@
+# TODO: remove comments
+
 defmodule Hologram.Compiler.CallGraph do
   use Agent
+
   alias Hologram.Compiler.CallGraph
+  # alias Hologram.Compiler.IR
 
   defstruct pid: nil, name: nil
   @type t :: %CallGraph{pid: pid, name: atom}
 
-  def add_edge(call_graph, from_vertex, to_vertex) do
-    Agent.update(call_graph.name, &Graph.add_edge(&1, from_vertex, to_vertex))
-  end
+  # def add_edge(call_graph, from_vertex, to_vertex) do
+  #   Agent.update(call_graph.name, &Graph.add_edge(&1, from_vertex, to_vertex))
+  # end
 
-  def add_vertex(call_graph, vertex) do
-    Agent.update(call_graph.name, &Graph.add_vertex(&1, vertex))
-  end
+  # def add_vertex(call_graph, vertex) do
+  #   Agent.update(call_graph.name, &Graph.add_vertex(&1, vertex))
+  # end
 
-  def data(call_graph) do
-    Agent.get(call_graph, & &1)
-  end
+  # def data(call_graph) do
+  #   Agent.get(call_graph, & &1)
+  # end
 
-  def edges(call_graph, vertex) do
-    Agent.get(call_graph.name, &Graph.edges(&1, vertex))
-  end
+  # def edges(call_graph, vertex) do
+  #   Agent.get(call_graph.name, &Graph.edges(&1, vertex))
+  # end
 
-  def has_edge?(call_graph, from_vertex, to_vertex) do
-    getter = fn graph ->
-      edges = Graph.edges(graph, from_vertex, to_vertex)
-      Enum.count(edges) == 1
-    end
+  # def has_edge?(call_graph, from_vertex, to_vertex) do
+  #   getter = fn graph ->
+  #     edges = Graph.edges(graph, from_vertex, to_vertex)
+  #     Enum.count(edges) == 1
+  #   end
 
-    Agent.get(call_graph.name, getter)
-  end
+  #   Agent.get(call_graph.name, getter)
+  # end
 
-  def has_vertex?(call_graph, vertex) do
-    Agent.get(call_graph, &Graph.has_vertex?(&1, vertex))
-  end
+  # def has_vertex?(call_graph, vertex) do
+  #   Agent.get(call_graph, &Graph.has_vertex?(&1, vertex))
+  # end
 
-  def num_edges(call_graph) do
-    Agent.get(call_graph, &Graph.num_edges/1)
-  end
+  # def num_edges(call_graph) do
+  #   Agent.get(call_graph, &Graph.num_edges/1)
+  # end
 
-  def num_vertices(call_graph) do
-    Agent.get(call_graph, &Graph.num_vertices/1)
-  end
+  # def num_vertices(call_graph) do
+  #   Agent.get(call_graph, &Graph.num_vertices/1)
+  # end
 
-  def reachable(call_graph, vertices) do
-    Agent.get(call_graph, &Graph.reachable(&1, vertices))
-  end
+  # def reachable(call_graph, vertices) do
+  #   Agent.get(call_graph, &Graph.reachable(&1, vertices))
+  # end
 
   @doc """
   Starts a new CallGraph agent with an initial empty graph.
@@ -63,7 +67,7 @@ defmodule Hologram.Compiler.CallGraph do
     %CallGraph{pid: pid, name: opts[:name]}
   end
 
-  def stop(call_graph) do
-    Agent.stop(call_graph.name)
-  end
+  # def stop(call_graph) do
+  #   Agent.stop(call_graph.name)
+  # end
 end
