e4dfcc571d9b1e5ce20ddc08508ea4fe83641084
Bart Blast <bart@bartblast.com>
2023-07-14 13:29:48 +0200

    Use imported functions

--- test/elixir/hologram/compiler/call_graph_test.exs
+++ test/elixir/hologram/compiler/call_graph_test.exs
@@ -17,8 +17,8 @@ defmodule Hologram.Compiler.CallGraphTest do
   end
 
   test "add_edge/3", %{call_graph: call_graph} do
-    :ok = CallGraph.add_edge(call_graph, :vertex_1, :vertex_2)
-    graph = CallGraph.graph(call_graph)
+    :ok = add_edge(call_graph, :vertex_1, :vertex_2)
+    graph = graph(call_graph)
 
     assert Graph.edge(graph, :vertex_1, :vertex_2) == %Graph.Edge{
              v1: :vertex_1,
@@ -29,8 +29,8 @@ defmodule Hologram.Compiler.CallGraphTest do
   end
 
   test "add_vertex/2", %{call_graph: call_graph} do
-    :ok = CallGraph.add_vertex(call_graph, :vertex_3)
-    graph = CallGraph.graph(call_graph)
+    :ok = add_vertex(call_graph, :vertex_3)
+    graph = graph(call_graph)
 
     assert Graph.has_vertex?(graph, :vertex_3)
   end
@@ -40,14 +40,14 @@ defmodule Hologram.Compiler.CallGraphTest do
       ir = %IR.AtomType{value: :abc}
       build(call_graph, ir, :vertex_1)
 
-      refute CallGraph.has_edge?(call_graph, :vertex_1, :abc)
+      refute has_edge?(call_graph, :vertex_1, :abc)
     end
 
     test "atom type ir, which is an alias", %{call_graph: call_graph} do
       ir = %IR.AtomType{value: Aaa}
       build(call_graph, ir, :vertex_1)
 
-      assert CallGraph.has_edge?(call_graph, :vertex_1, Aaa)
+      assert has_edge?(call_graph, :vertex_1, Aaa)
     end
 
     test "function definition ir", %{call_graph: call_graph} do
@@ -81,8 +81,8 @@ defmodule Hologram.Compiler.CallGraphTest do
       list = [%IR.AtomType{value: Aaa}, %IR.AtomType{value: Bbb}]
       build(call_graph, list, :vertex_1)
 
-      assert CallGraph.has_edge?(call_graph, :vertex_1, Aaa)
-      assert CallGraph.has_edge?(call_graph, :vertex_1, Bbb)
+      assert has_edge?(call_graph, :vertex_1, Aaa)
+      assert has_edge?(call_graph, :vertex_1, Bbb)
     end
 
     test "map", %{call_graph: call_graph} do
@@ -93,10 +93,10 @@ defmodule Hologram.Compiler.CallGraphTest do
 
       build(call_graph, map, :vertex_1)
 
-      assert CallGraph.has_edge?(call_graph, :vertex_1, Aaa)
-      assert CallGraph.has_edge?(call_graph, :vertex_1, Bbb)
-      assert CallGraph.has_edge?(call_graph, :vertex_1, Ccc)
-      assert CallGraph.has_edge?(call_graph, :vertex_1, Ddd)
+      assert has_edge?(call_graph, :vertex_1, Aaa)
+      assert has_edge?(call_graph, :vertex_1, Bbb)
+      assert has_edge?(call_graph, :vertex_1, Ccc)
+      assert has_edge?(call_graph, :vertex_1, Ddd)
     end
 
     test "module definition ir", %{call_graph: call_graph} do
@@ -112,31 +112,31 @@ defmodule Hologram.Compiler.CallGraphTest do
 
       build(call_graph, ir)
 
-      assert CallGraph.has_edge?(call_graph, Aaa, Bbb)
-      assert CallGraph.has_edge?(call_graph, Aaa, Ccc)
+      assert has_edge?(call_graph, Aaa, Bbb)
+      assert has_edge?(call_graph, Aaa, Ccc)
     end
 
     test "tuple", %{call_graph: call_graph} do
       tuple = {%IR.AtomType{value: Aaa}, %IR.AtomType{value: Bbb}}
       build(call_graph, tuple, :vertex_1)
 
-      assert CallGraph.has_edge?(call_graph, :vertex_1, Aaa)
-      assert CallGraph.has_edge?(call_graph, :vertex_1, Bbb)
+      assert has_edge?(call_graph, :vertex_1, Aaa)
+      assert has_edge?(call_graph, :vertex_1, Bbb)
     end
   end
 
   test "graph/1", %{call_graph: call_graph} do
-    assert %Graph{} = CallGraph.graph(call_graph)
+    assert %Graph{} = graph(call_graph)
   end
 
   describe "has_edge?/3" do
     test "doesn't have the given edge", %{call_graph: call_graph} do
-      refute CallGraph.has_edge?(call_graph, :vertex_1, :vertex_2)
+      refute has_edge?(call_graph, :vertex_1, :vertex_2)
     end
 
     test "has the given edge", %{call_graph: call_graph} do
-      CallGraph.add_edge(call_graph, :vertex_1, :vertex_2)
-      assert CallGraph.has_edge?(call_graph, :vertex_1, :vertex_2)
+      add_edge(call_graph, :vertex_1, :vertex_2)
+      assert has_edge?(call_graph, :vertex_1, :vertex_2)
     end
   end
 
