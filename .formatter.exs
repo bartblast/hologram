d01e1a4d136513e127a47c22e0ed4d239404e044
Bart Blast <bart@bartblast.com>
2023-07-17 19:12:12 +0200

    Overhaul formatter config

--- .formatter.exs
+++ .formatter.exs
@@ -1,3 +1,15 @@
+locals_without_parens = [
+  # Hologram.Page
+  layout: 1,
+  route: 1
+]
+
 [
-  inputs: ["*.exs", "{config,lib,test}/**/*.{ex,exs}"]
+  export: [locals_without_parens: locals_without_parens],
+  inputs: [
+    "*.{ex,exs}",
+    "{config,lib}/**/*.{ex,exs}",
+    "test/elixir/**/*.{ex,exs}"
+  ],
+  locals_without_parens: locals_without_parens
 ]
