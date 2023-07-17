2d55a27a1a3d73d90ba9b8d976c49444e0dfd8cc
Bart Blast <bart@bartblast.com>
2023-07-17 20:09:36 +0200

    Ignore Hologram.Page when running mix doctor

--- .doctor.exs
+++ .doctor.exs
@@ -1,6 +1,7 @@
 %Doctor.Config{
   exception_moduledoc_required: true,
-  ignore_modules: [],
+  # Hologram.Page is ignored because there are false positives.
+  ignore_modules: [Hologram.Page],
   ignore_paths: [~r(^test/*)],
   min_module_doc_coverage: 100,
   min_module_spec_coverage: 100,
