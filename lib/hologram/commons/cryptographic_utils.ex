47bedf1db45a7b8a130bee876494b2ac64d40c6c
Bart Blast <bart@bartblast.com>
2023-07-25 18:56:59 +0200

    Make digest util more generic

--- lib/hologram/commons/cryptographic_utils.ex
+++ lib/hologram/commons/cryptographic_utils.ex
@@ -1,27 +1,27 @@
 defmodule Hologram.Commons.CryptographicUtils do
   @doc """
-  Calculates the SHA-256 cryptographic digest of the input data.
-  If the `encode_as_hex` param is `true` then hexadecimal string is returned, or binary otherwise.
+  Calculates the cryptographic digest of the input data using the given algorithm (such as :md5, :sha256, etc.).
+  Output format can be either `:hex` (hexadecimal string) or `:binary` (binary data).
 
   ## Examples
 
-      iex> digest("Hologram", true)
+      iex> digest("Hologram", :sha256, :hex)
       "ddff15a2da596882cfd545132004c8e7355e457517a3874f4853cc6ff1110c2e"
 
       iex> digest("Hologram", false)
       <<221, 255, 21, 162, 218, 89, 104, 130, 207, 213, 69, 19, 32, 4, 200, 231, 53,
         94, 69, 117, 23, 163, 135, 79, 72, 83, 204, 111, 241, 17, 12, 46>>
   """
-  @spec digest(binary, boolean) :: String.t() | binary
-  def digest(data, encode_as_hex \\ true)
+  @spec digest(binary, atom, :binary | :hex) :: String.t() | binary
+  def digest(data, algo, format)
 
-  def digest(data, false) do
-    :crypto.hash(:sha256, data)
+  def digest(data, algo, :binary) do
+    :crypto.hash(algo, data)
   end
 
-  def digest(data, true) do
+  def digest(data, algo, :hex) do
     data
-    |> digest(false)
+    |> digest(algo, :binary)
     |> Base.encode16()
     |> String.downcase()
   end
--- test/elixir/hologram/commons/cryptographic_utils_test.exs
+++ test/elixir/hologram/commons/cryptographic_utils_test.exs
@@ -2,16 +2,15 @@ defmodule Hologram.Commons.CryptographicUtilsTest do
   use Hologram.Test.BasicCase, async: true
   import Hologram.Commons.CryptographicUtils
 
-  describe "digest/2" do
-    test "encode as hex string" do
-      assert digest("Hologram", true) ==
+  describe "digest/3" do
+    test "SHA256 algorithm with hex output format" do
+      assert digest("Hologram", :sha256, :hex) ==
                "ddff15a2da596882cfd545132004c8e7355e457517a3874f4853cc6ff1110c2e"
     end
 
-    test "encode as binary" do
-      assert digest("Hologram", false) ==
-               <<221, 255, 21, 162, 218, 89, 104, 130, 207, 213, 69, 19, 32, 4, 200, 231, 53, 94,
-                 69, 117, 23, 163, 135, 79, 72, 83, 204, 111, 241, 17, 12, 46>>
+    test "MD5 algorithm with binary output format" do
+      assert digest("Hologram", :md5, :binary) ==
+               <<8, 132, 19, 120, 33, 216, 230, 154, 210, 235, 180, 219, 210, 116, 125, 36>>
     end
   end
 end
