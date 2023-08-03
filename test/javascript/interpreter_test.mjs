f721593db7bb3dffb76dd47d508575b83d3cd895
Bart Blast <bart@bartblast.com>
2023-08-03 12:49:36 +0200

    Implement additional tests for cons pattern matching

--- test/javascript/interpreter_test.mjs
+++ test/javascript/interpreter_test.mjs
@@ -1958,6 +1958,98 @@ describe("matchOperator()", () => {
       });
     });
 
+    describe("[h | [2 | 3]]", () => {
+      let left;
+
+      beforeEach(() => {
+        left = Type.consPattern(
+          Type.variablePattern("h"),
+          Type.improperList([Type.integer(2), Type.integer(3)])
+        );
+      });
+
+      it("[h | [2 | 3]] = 3", () => {
+        const right = Type.integer(3);
+
+        assertError(
+          () => Interpreter.matchOperator(right, left, vars),
+          "MatchError",
+          "no match of right hand side value: 3"
+        );
+      });
+
+      it("[h | [2 | 3]] = []", () => {
+        const right = Type.list([]);
+
+        assertError(
+          () => Interpreter.matchOperator(right, left, vars),
+          "MatchError",
+          "no match of right hand side value: []"
+        );
+      });
+
+      it("[h | [2 | 3]] = [3]", () => {
+        const right = Type.list([Type.integer(3)]);
+
+        assertError(
+          () => Interpreter.matchOperator(right, left, vars),
+          "MatchError",
+          "no match of right hand side value: [3]"
+        );
+      });
+
+      it("[h | [2 | 3]] = [2, 3]", () => {
+        const right = Type.list([Type.integer(2), Type.integer(3)]);
+
+        assertError(
+          () => Interpreter.matchOperator(right, left, vars),
+          "MatchError",
+          "no match of right hand side value: [2, 3]"
+        );
+      });
+
+      it("[h | [2 | 3]] = [2 | 3]", () => {
+        const right = Type.improperList([Type.integer(2), Type.integer(3)]);
+
+        assertError(
+          () => Interpreter.matchOperator(right, left, vars),
+          "MatchError",
+          "no match of right hand side value: [2 | 3]"
+        );
+      });
+
+      it("[h | [2 | 3]] = [1, 2, 3]", () => {
+        const right = Type.list([
+          Type.integer(1),
+          Type.integer(2),
+          Type.integer(3),
+        ]);
+
+        assertError(
+          () => Interpreter.matchOperator(right, left, vars),
+          "MatchError",
+          "no match of right hand side value: [1, 2, 3]"
+        );
+      });
+
+      it("[h | [2 | 3]] = [1, 2 | 3]", () => {
+        const right = Type.improperList([
+          Type.integer(1),
+          Type.integer(2),
+          Type.integer(3),
+        ]);
+
+        const result = Interpreter.matchOperator(right, left, vars);
+
+        assert.deepStrictEqual(result, right);
+
+        assert.deepStrictEqual(vars, {
+          a: Type.integer(9),
+          h: Type.integer(1),
+        });
+      });
+    });
+
     // TODO: overhaul
     // it("left cons pattern == right proper list, cons pattern head and tail are variables", () => {
     //   const left = Type.consPattern(
