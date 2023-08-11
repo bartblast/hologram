5a86c25d822b8e31287af80067016731edb80bf6
Bart Blast <bart@bartblast.com>
2023-08-11 21:02:18 +0200

    Refactor PLT module struct and process starting

--- lib/hologram/commons/plt.ex
+++ lib/hologram/commons/plt.ex
@@ -5,210 +5,159 @@ defmodule Hologram.Commons.PLT do
   """
 
   use GenServer
-
   alias Hologram.Commons.PLT
-  alias Hologram.Commons.SerializationUtils
 
-  defstruct pid: nil, name: nil, dump_path: nil
-  @type t :: %PLT{pid: pid, name: atom, dump_path: String.t() | nil}
-
-  @doc """
-  Deletes a key-value pair from the give persistent lookup table.
-
-  ## Examples
-
-      iex> my_plt = %PLT{pid: #PID<0.273.0>, name: :my_plt}
-      iex> delete(my_plt, :my_key)
-      %PLT{pid: #PID<0.273.0>, name: :my_plt}
-  """
-  @spec delete(PLT.t(), atom) :: PLT.t()
-  def delete(plt, key) do
-    :ets.delete(plt.name, key)
-    plt
-  end
+  defstruct pid: nil, table_ref: nil
+  @type t :: %PLT{pid: pid, table_ref: :ets.tid()}
 
   @doc """
-  Serializes the contents of the given PLT's ETS table and writes them to a file.
-
-  ## Examples
-
-      iex> my_plt = %PLT{pid: #PID<0.273.0>, name: :my_plt, dump_path: "/my_dump_path"}
-      iex> dump(my_plt)
-      :ok
-  """
-  @spec dump(PLT.t()) :: :ok
-  def dump(%PLT{dump_path: dump_path} = plt) do
-    data =
-      plt
-      |> get_all()
-      |> SerializationUtils.serialize()
-
-    File.write!(dump_path, data)
-  end
-
-  @doc """
-  Returns the value stored in the underlying ETS table under the given key.
-  If the key doesn't exist the :error :atom is returned.
-
-  ## Examples
-
-      iex> get(%PLT{name: :my_plt}, :my_key)
-      :my_value
-  """
-  @spec get(PLT.t(), atom) :: {:ok, term} | :error
-  def get(plt, key) do
-    case :ets.lookup(plt.name, key) do
-      [{^key, value}] ->
-        {:ok, value}
-
-      _fallback ->
-        :error
-    end
-  end
-
-  @doc """
-  Returns the value stored in the underlying ETS table under the given key.
-  If the key doesn't exist a KeyError is raised.
-
-  ## Examples
-
-      iex> get!(%PLT{name: :my_plt}, :my_key)
-      :my_value
-
-      iex> get!(%PLT{name: :my_plt}, :invalid_key)
-      ** (KeyError) key :invalid_key not found in the PLT
-  """
-  @spec get!(PLT.t(), atom) :: term
-  def get!(plt, key) do
-    case get(plt, key) do
-      {:ok, value} -> value
-      _fallback -> raise KeyError, message: "key #{inspect(key)} not found in the PLT"
-    end
-  end
-
-  @doc """
-  Returns all items stored in the underlying ETS table.
-
-  ## Examples
-
-      iex> get_all(%PLT{name :my_plt})
-      %{key_1: :value_1, key_2: :value_2}
-  """
-  @spec get_all(PLT.t()) :: %{atom => term}
-  def get_all(plt) do
-    plt.name
-    |> :ets.tab2list()
-    |> Enum.into(%{})
-  end
-
-  @doc """
-  Creates the underlying ETS table if it doesn't exist yet,
-  truncates it, and populates it from the dump (if the dump exists).
-
-  ## Examples
-
-      iex> init(name: :my_plt, dump_path: "/my_dump_path")
-      {:ok, nil}
+  Returns the underlying ETS table's reference.
   """
   @impl GenServer
-  @spec init(keyword) :: {:ok, nil}
-  def init(opts) do
-    maybe_create_table(opts[:name])
-    reload_table(opts)
-    {:ok, nil}
+  @spec handle_call(:get_table_ref, GenServer.from(), :ets.tid()) ::
+          {:reply, :ets.tid(), :ets.tid()}
+  def handle_call(:get_table_ref, _from, table_ref) do
+    {:reply, table_ref, table_ref}
   end
 
   @doc """
-  Puts multiple items into the underlying ETS table.
-
-  ## Examples
-
-      iex> put(:my_plt, [key_1: :value_1, key_2: :value_2])
-      true
+  Creates the underlying ETS table.
   """
-  @spec put(atom, keyword) :: true
-  def put(name, items) do
-    :ets.insert(name, items)
-  end
-
-  @doc """
-  Puts the given item into the underlying ETS table.
-
-  ## Examples
-
-      iex> my_plt = %PLT{pid: #PID<0.273.0>, name: :my_plt}
-      iex> put(my_plt, :my_key, :my_value)
-      %PLT{pid: #PID<0.273.0>, name: :my_plt}
-
-      iex> put(:my_plt, :my_key, :my_value)
-      true
-  """
-  @spec put(PLT.t() | atom, atom, term) :: PLT.t() | true
-
-  def put(%PLT{name: name} = plt, key, value) do
-    put(name, key, value)
-    plt
-  end
-
-  def put(name, key, value) do
-    :ets.insert(name, {key, value})
+  @impl GenServer
+  @spec init(nil) :: {:ok, :ets.tid()}
+  def init(nil) do
+    table_ref = :ets.new(__MODULE__, [:public])
+    {:ok, table_ref}
   end
 
   @doc """
   Starts the underlying GenServer process.
-
-  ## Examples
-
-      iex> start(name: :my_plt, dump_path: "/my_dump_path")
-      %PLT{pid: #PID<0.273.0>, name: :my_plt, dump_path: "/my_dump_path"}
   """
-  @spec start(keyword) :: PLT.t()
-  def start(opts) do
-    {:ok, pid} = GenServer.start_link(PLT, opts, name: opts[:name])
-    %PLT{pid: pid, name: opts[:name], dump_path: opts[:dump_path]}
+  @spec start() :: PLT.t()
+  def start do
+    {:ok, pid} = GenServer.start_link(PLT, nil)
+    table_ref = GenServer.call(pid, :get_table_ref)
+
+    %PLT{pid: pid, table_ref: table_ref}
   end
 
-  @doc """
-  Tells whether the underlying ETS table exists.
+  # alias Hologram.Commons.SerializationUtils
 
-  ## Examples
+  # @doc """
+  # Deletes a key-value pair from the give persistent lookup table.
+  # """
+  # @spec delete(PLT.t(), atom) :: PLT.t()
+  # def delete(plt, key) do
+  #   :ets.delete(plt.table_ref, key)
+  #   plt
+  # end
 
-      iex> table_exists?(:my_plt)
-      true
-  """
-  @spec table_exists?(atom) :: boolean
-  def table_exists?(name) do
-    :ets.info(name) != :undefined
-  end
+  # @doc """
+  # Serializes the contents of the given PLT's ETS table and writes them to a file.
+  # """
+  # @spec dump(PLT.t()) :: :ok
+  # def dump(%PLT{dump_path: dump_path} = plt) do
+  #   data =
+  #     plt
+  #     |> get_all()
+  #     |> SerializationUtils.serialize()
 
-  defp create_table(name) do
-    :ets.new(name, [:public, :named_table])
-  end
+  #   File.write!(dump_path, data)
+  # end
 
-  defp maybe_create_table(name) do
-    if !table_exists?(name), do: create_table(name)
-  end
+  # @doc """
+  # Returns the value stored in the underlying ETS table under the given key.
+  # If the key doesn't exist the :error :atom is returned.
+  # """
+  # @spec get(PLT.t(), atom) :: {:ok, term} | :error
+  # def get(plt, key) do
+  #   case :ets.lookup(plt.table_ref, key) do
+  #     [{^key, value}] ->
+  #       {:ok, value}
 
-  defp populate_table(opts) do
-    items =
-      opts[:dump_path]
-      |> File.read!()
-      |> SerializationUtils.deserialize()
-      |> Map.to_list()
+  #     _fallback ->
+  #       :error
+  #   end
+  # end
 
-    put(opts[:name], items)
-  end
+  # @doc """
+  # Returns the value stored in the underlying ETS table under the given key.
+  # If the key doesn't exist a KeyError is raised.
+  # """
+  # @spec get!(PLT.t(), atom) :: term
+  # def get!(plt, key) do
+  #   case get(plt, key) do
+  #     {:ok, value} -> value
+  #     _fallback -> raise KeyError, message: "key #{inspect(key)} not found in the PLT"
+  #   end
+  # end
 
-  defp reload_table(opts) do
-    truncate_table(opts[:name])
+  # @doc """
+  # Returns all items stored in the underlying ETS table.
+  # """
+  # @spec get_all(PLT.t()) :: %{atom => term}
+  # def get_all(plt) do
+  #   plt.table_ref
+  #   |> :ets.tab2list()
+  #   |> Enum.into(%{})
+  # end
 
-    if opts[:dump_path] && File.exists?(opts[:dump_path]) do
-      populate_table_fun = opts[:populate_table_fun] || (&populate_table/1)
-      populate_table_fun.(opts)
-    end
-  end
+  # @doc """
+  # Puts multiple items into the underlying ETS table.
 
-  defp truncate_table(name) do
-    :ets.delete_all_objects(name)
-  end
+  # """
+  # @spec put(reference, keyword) :: true
+  # def put(table_ref, items) do
+  #   :ets.insert(table_ref, items)
+  # end
+
+  # @doc """
+  # Puts the given item into the underlying ETS table.
+  # """
+  # @spec put(PLT.t() | atom, atom, term) :: PLT.t() | true
+
+  # def put(%PLT{table_ref: table_ref} = plt, key, value) do
+  #   put(table_ref, key, value)
+  #   plt
+  # end
+
+  # def put(name, key, value) do
+  #   :ets.insert(name, {key, value})
+  # end
+
+  # @doc """
+  # Tells whether the underlying ETS table exists.
+
+  # ## Examples
+
+  #     iex> table_exists?(:my_plt)
+  #     true
+  # """
+  # @spec table_exists?(atom) :: boolean
+  # def table_exists?(name) do
+  #   :ets.info(name) != :undefined
+  # end
+
+  # defp populate_table(opts) do
+  #   items =
+  #     opts[:dump_path]
+  #     |> File.read!()
+  #     |> SerializationUtils.deserialize()
+  #     |> Map.to_list()
+
+  #   put(opts[:name], items)
+  # end
+
+  # defp reload_table(opts) do
+  #   truncate_table(opts[:name])
+
+  #   if opts[:dump_path] && File.exists?(opts[:dump_path]) do
+  #     populate_table_fun = opts[:populate_table_fun] || (&populate_table/1)
+  #     populate_table_fun.(opts)
+  #   end
+  # end
+
+  # defp truncate_table(name) do
+  #   :ets.delete_all_objects(name)
+  # end
 end
--- test/elixir/hologram/commons/plt_test.exs
+++ test/elixir/hologram/commons/plt_test.exs
@@ -1,198 +1,207 @@
 defmodule Hologram.Commons.PLTTest do
-  use Hologram.Test.BasicCase, async: false
+  use Hologram.Test.BasicCase, async: true
   import Hologram.Commons.PLT
-
   alias Hologram.Commons.PLT
-  alias Hologram.Commons.SerializationUtils
-  alias Hologram.Compiler.Reflection
 
-  @dump_path Reflection.tmp_path() <> "/plt_#{__MODULE__}.bin"
-  @items %{key_1: :value_1, key_2: :value_2}
-  @name :"plt_#{__MODULE__}"
-  @opts name: @name, dump_path: @dump_path
-
-  defp ets_table_exists?(table_name) do
-    table_name
+  defp ets_table_exists?(table_ref) do
+    table_ref
     |> :ets.info()
     |> is_list()
   end
 
-  defp dump_items do
-    binary = SerializationUtils.serialize(@items)
-    File.write!(@dump_path, binary)
+  test "start/0" do
+    assert %PLT{pid: pid, table_ref: table_ref} = start()
+
+    assert is_pid(pid)
+    assert is_reference(table_ref)
+
+    assert ets_table_exists?(table_ref)
   end
 
-  setup do
-    wait_for_plt_cleanup(@name)
-    dump_items()
+  # alias Hologram.Commons.SerializationUtils
+  # alias Hologram.Compiler.Reflection
 
-    :ok
-  end
+  # @dump_path Reflection.tmp_path() <> "/plt_#{__MODULE__}.bin"
+  # @items %{key_1: :value_1, key_2: :value_2}
+  # @name :"plt_#{__MODULE__}"
+  # @opts name: @name, dump_path: @dump_path
 
-  describe "delete/2" do
-    test "key exists" do
-      plt =
-        @opts
-        |> start()
-        |> put(:my_key, :my_value)
+  # defp dump_items do
+  #   binary = SerializationUtils.serialize(@items)
+  #   File.write!(@dump_path, binary)
+  # end
 
-      assert delete(plt, :my_key) == plt
-      assert get(plt, :my_key) == :error
-    end
+  # setup do
+  #   wait_for_plt_cleanup(@name)
+  #   dump_items()
 
-    test "key doesn't exist" do
-      plt = start(@opts)
+  #   :ok
+  # end
 
-      assert delete(plt, :my_key) == plt
-      assert get(plt, :my_key) == :error
-    end
-  end
+  # describe "delete/2" do
+  #   test "key exists" do
+  #     plt =
+  #       @opts
+  #       |> start()
+  #       |> put(:my_key, :my_value)
 
-  test "dump/1" do
-    @opts
-    |> start()
-    |> put(:dump_test, 123)
-    |> dump()
+  #     assert delete(plt, :my_key) == plt
+  #     assert get(plt, :my_key) == :error
+  #   end
 
-    items =
-      @dump_path
-      |> File.read!()
-      |> SerializationUtils.deserialize()
+  #   test "key doesn't exist" do
+  #     plt = start(@opts)
 
-    assert items.dump_test == 123
-  end
+  #     assert delete(plt, :my_key) == plt
+  #     assert get(plt, :my_key) == :error
+  #   end
+  # end
 
-  describe "get/2" do
-    test "key exists" do
-      plt = start(@opts)
-      assert get(plt, :key_1) == {:ok, :value_1}
-    end
+  # test "dump/1" do
+  #   @opts
+  #   |> start()
+  #   |> put(:dump_test, 123)
+  #   |> dump()
 
-    test "key doesn't exist" do
-      plt = start(@opts)
-      assert get(plt, :invalid_key) == :error
-    end
-  end
+  #   items =
+  #     @dump_path
+  #     |> File.read!()
+  #     |> SerializationUtils.deserialize()
 
-  describe "get!/2" do
-    test "key exists" do
-      plt = start(@opts)
-      assert get!(plt, :key_1) == :value_1
-    end
+  #   assert items.dump_test == 123
+  # end
 
-    test "key doesn't exist" do
-      plt = start(@opts)
+  # describe "get/2" do
+  #   test "key exists" do
+  #     plt = start(@opts)
+  #     assert get(plt, :key_1) == {:ok, :value_1}
+  #   end
 
-      assert_raise KeyError, "key :invalid_key not found in the PLT", fn ->
-        get!(plt, :invalid_key)
-      end
-    end
-  end
+  #   test "key doesn't exist" do
+  #     plt = start(@opts)
+  #     assert get(plt, :invalid_key) == :error
+  #   end
+  # end
 
-  test "get_all/1" do
-    plt = start(@opts)
-    assert get_all(plt) == @items
-  end
+  # describe "get!/2" do
+  #   test "key exists" do
+  #     plt = start(@opts)
+  #     assert get!(plt, :key_1) == :value_1
+  #   end
 
-  test "put/2" do
-    plt = start(@opts)
+  #   test "key doesn't exist" do
+  #     plt = start(@opts)
 
-    items = [
-      {:key_3, :value_3},
-      {:key_4, :value_4}
-    ]
+  #     assert_raise KeyError, "key :invalid_key not found in the PLT", fn ->
+  #       get!(plt, :invalid_key)
+  #     end
+  #   end
+  # end
 
-    put(@name, items)
+  # test "get_all/1" do
+  #   plt = start(@opts)
+  #   assert get_all(plt) == @items
+  # end
 
-    assert get(plt, :key_3) == {:ok, :value_3}
-    assert get(plt, :key_4) == {:ok, :value_4}
-  end
+  # test "put/2" do
+  #   plt = start(@opts)
 
-  describe "put/3" do
-    test "first arg is %PLT{} struct" do
-      plt =
-        @opts
-        |> start()
-        |> put(:key_3, :value_3)
+  #   items = [
+  #     {:key_3, :value_3},
+  #     {:key_4, :value_4}
+  #   ]
 
-      assert get(plt, :key_3) == {:ok, :value_3}
-    end
+  #   put(@name, items)
 
-    test "first arg is PLT name" do
-      plt = start(@opts)
-      put(@name, :key_3, :value_3)
+  #   assert get(plt, :key_3) == {:ok, :value_3}
+  #   assert get(plt, :key_4) == {:ok, :value_4}
+  # end
 
-      assert get(plt, :key_3) == {:ok, :value_3}
-    end
-  end
+  # describe "put/3" do
+  #   test "first arg is %PLT{} struct" do
+  #     plt =
+  #       @opts
+  #       |> start()
+  #       |> put(:key_3, :value_3)
 
-  describe "start/1" do
-    test "returns PLT struct with name and dump_path from opts" do
-      assert %PLT{name: @name, dump_path: @dump_path} = start(@opts)
-    end
+  #     assert get(plt, :key_3) == {:ok, :value_3}
+  #   end
 
-    test "process name is registered" do
-      %PLT{pid: pid} = start(@opts)
-      assert Process.whereis(@name) == pid
-    end
+  #   test "first arg is PLT name" do
+  #     plt = start(@opts)
+  #     put(@name, :key_3, :value_3)
 
-    test "ETS table is created if it doesn't exist yet" do
-      start(@opts)
-      assert ets_table_exists?(@name)
-    end
+  #     assert get(plt, :key_3) == {:ok, :value_3}
+  #   end
+  # end
 
-    test "ETS table is not created if it already exists" do
-      :ets.new(@name, [:public, :named_table])
-      start(@opts)
+  # describe "start/1" do
+  #   test "returns PLT struct with name and dump_path from opts" do
+  #     assert %PLT{name: @name, dump_path: @dump_path} = start(@opts)
+  #   end
 
-      assert ets_table_exists?(@name)
-    end
+  #   test "process name is registered" do
+  #     %PLT{pid: pid} = start(@opts)
+  #     assert Process.whereis(@name) == pid
+  #   end
 
-    test "ETS table is truncated" do
-      :ets.new(@name, [:public, :named_table])
-      :ets.insert(@name, {:key_3, :value_3})
-      plt = start(@opts)
+  #   test "ETS table is created if it doesn't exist yet" do
+  #     start(@opts)
+  #     assert ets_table_exists?(@name)
+  #   end
 
-      assert get_all(plt) == @items
-    end
+  #   test "ETS table is not created if it already exists" do
+  #     :ets.new(@name, [:public, :named_table])
+  #     start(@opts)
 
-    test "ETS table is populated when dump_path is given in opts and dump file exists" do
-      plt = start(@opts)
-      assert get_all(plt) == @items
-    end
+  #     assert ets_table_exists?(@name)
+  #   end
 
-    test "ETS table is not populated when dump_path is not given in opts" do
-      plt = start(name: @name, dump_path: nil)
-      assert get_all(plt) == %{}
-    end
+  #   test "ETS table is truncated" do
+  #     :ets.new(@name, [:public, :named_table])
+  #     :ets.insert(@name, {:key_3, :value_3})
+  #     plt = start(@opts)
 
-    test "ETS table is not populated when dump_path is given in opts but dump file doesn't exist" do
-      File.rm!(@dump_path)
-      plt = start(@opts)
+  #     assert get_all(plt) == @items
+  #   end
 
-      assert get_all(plt) == %{}
-    end
+  #   test "ETS table is populated when dump_path is given in opts and dump file exists" do
+  #     plt = start(@opts)
+  #     assert get_all(plt) == @items
+  #   end
 
-    test "applies custom populate table function if it is given in opts" do
-      populate_table_fun = fn _arg ->
-        put(@name, :custom_key_1, :custom_value_1)
-        put(@name, :custom_key_2, :custom_value_2)
-      end
+  #   test "ETS table is not populated when dump_path is not given in opts" do
+  #     plt = start(name: @name, dump_path: nil)
+  #     assert get_all(plt) == %{}
+  #   end
 
-      plt = start(name: @name, dump_path: @dump_path, populate_table_fun: populate_table_fun)
+  #   test "ETS table is not populated when dump_path is given in opts but dump file doesn't exist" do
+  #     File.rm!(@dump_path)
+  #     plt = start(@opts)
 
-      assert get_all(plt) == %{custom_key_1: :custom_value_1, custom_key_2: :custom_value_2}
-    end
-  end
+  #     assert get_all(plt) == %{}
+  #   end
 
-  describe "table_exists?/1" do
-    test "exists" do
-      :ets.new(@name, [:public, :named_table])
-      assert table_exists?(@name)
-    end
+  #   test "applies custom populate table function if it is given in opts" do
+  #     populate_table_fun = fn _arg ->
+  #       put(@name, :custom_key_1, :custom_value_1)
+  #       put(@name, :custom_key_2, :custom_value_2)
+  #     end
 
-    test "doesn't exist" do
-      refute table_exists?(@name)
-    end
-  end
+  #     plt = start(name: @name, dump_path: @dump_path, populate_table_fun: populate_table_fun)
+
+  #     assert get_all(plt) == %{custom_key_1: :custom_value_1, custom_key_2: :custom_value_2}
+  #   end
+  # end
+
+  # describe "table_exists?/1" do
+  #   test "exists" do
+  #     :ets.new(@name, [:public, :named_table])
+  #     assert table_exists?(@name)
+  #   end
+
+  #   test "doesn't exist" do
+  #     refute table_exists?(@name)
+  #   end
+  # end
 end
