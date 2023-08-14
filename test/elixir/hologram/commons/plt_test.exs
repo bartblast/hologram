fcd50957400aa890a527c84ea7564ddb16c09350
Bart Blast <bart@bartblast.com>
2023-08-14 16:07:11 +0200

    Refactor PLT tests

--- test/elixir/hologram/commons/plt_test.exs
+++ test/elixir/hologram/commons/plt_test.exs
@@ -8,123 +8,87 @@ defmodule Hologram.Commons.PLTTest do
 
   @dump_path Reflection.tmp_path() <> "/plt_#{__MODULE__}.bin"
 
+  @items [
+    {:my_key_1, :my_value_1},
+    {:my_key_2, :my_value_2}
+  ]
+
   defp ets_table_exists?(table_ref) do
     table_ref
     |> :ets.info()
     |> is_list()
   end
 
-  describe "delete/2" do
-    test "key exists" do
-      plt =
-        start()
-        |> put(:my_key, :my_value)
+  setup do
+    plt = put(start(), @items)
+    [plt: plt]
+  end
 
-      assert delete(plt, :my_key) == plt
-      assert get(plt, :my_key) == :error
+  describe "delete/2" do
+    test "key exists", %{plt: plt} do
+      assert delete(plt, :my_key_2) == plt
+      assert get(plt, :my_key_2) == :error
     end
 
-    test "key doesn't exist" do
-      plt = start()
-
-      assert delete(plt, :my_key) == plt
-      assert get(plt, :my_key) == :error
+    test "key doesn't exist", %{plt: plt} do
+      assert delete(plt, :my_key_3) == plt
+      assert get(plt, :my_key_3) == :error
     end
   end
 
-  test "dump/2" do
-    items = [
-      {:my_key_1, :my_value_1},
-      {:my_key_2, :my_value_2}
-    ]
-
-    start()
-    |> put(items)
-    |> dump(@dump_path)
+  test "dump/2", %{plt: plt} do
+    dump(plt, @dump_path)
 
     items =
       @dump_path
       |> File.read!()
       |> SerializationUtils.deserialize()
 
-    assert items == Enum.into(%{}, items)
+    assert items == Enum.into(@items, %{})
   end
 
   describe "get/2" do
-    test "key exists" do
-      plt =
-        start()
-        |> put(:my_key, :my_value)
-
-      assert get(plt, :my_key) == {:ok, :my_value}
+    test "key exists", %{plt: plt} do
+      assert get(plt, :my_key_2) == {:ok, :my_value_2}
     end
 
-    test "key doesn't exist" do
-      plt = start()
-      assert get(plt, :invalid_key) == :error
+    test "key doesn't exist", %{plt: plt} do
+      assert get(plt, :my_key_3) == :error
     end
   end
 
   describe "get!/2" do
-    test "key exists" do
-      plt =
-        start()
-        |> put(:my_key, :my_value)
-
-      assert get!(plt, :my_key) == :my_value
+    test "key exists", %{plt: plt} do
+      assert get!(plt, :my_key_2) == :my_value_2
     end
 
-    test "key doesn't exist" do
-      plt = start()
-
-      assert_raise KeyError, "key :invalid_key not found in the PLT", fn ->
-        get!(plt, :invalid_key)
+    test "key doesn't exist", %{plt: plt} do
+      assert_raise KeyError, "key :my_key_3 not found in the PLT", fn ->
+        get!(plt, :my_key_3)
       end
     end
   end
 
-  test "get_all/1" do
-    items = [
-      {:my_key_1, :my_value_1},
-      {:my_key_2, :my_value_2}
-    ]
-
-    plt =
-      start()
-      |> put(items)
-
-    assert get_all(plt) == %{my_key_1: :my_value_1, my_key_2: :my_value_2}
+  test "get_all/1", %{plt: plt} do
+    assert get_all(plt) == Enum.into(@items, %{})
   end
 
   test "put/2" do
-    items = [
-      {:my_key_1, :my_value_1},
-      {:my_key_2, :my_value_2}
-    ]
-
-    plt =
-      start()
-      |> put(items)
+    plt = put(start(), @items)
 
     assert get(plt, :my_key_1) == {:ok, :my_value_1}
     assert get(plt, :my_key_2) == {:ok, :my_value_2}
   end
 
   describe "put/3" do
-    test "first arg is a PLT struct" do
-      result =
-        start()
-        |> put(:my_key, :my_value)
-        |> get(:my_key)
-
-      assert result == {:ok, :my_value}
+    test "first arg is a PLT struct", %{plt: plt} do
+      assert put(plt, :my_key_3, :my_value_3) == plt
+      assert get(plt, :my_key_3) == {:ok, :my_value_3}
     end
 
-    test "first arg is an ETS table ref" do
-      %{table_ref: table_ref} = plt = start()
-      put(table_ref, :my_key, :my_value)
-
-      assert get(plt, :my_key) == {:ok, :my_value}
+    test "first arg is an ETS table ref", %{plt: plt} do
+      assert put(plt.table_ref, :my_key_3, :my_value_3) == true
+      assert get(plt, :my_key_3) == {:ok, :my_value_3}
     end
   end
 
