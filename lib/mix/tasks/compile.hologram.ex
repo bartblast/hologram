07e63e0a61ff8d5557c61f1c16aab86a36a2d9b9
Bart Blast <bart@bartblast.com>
2023-08-15 13:33:22 +0200

    Fix compile task

--- lib/mix/tasks/compile.hologram.ex
+++ lib/mix/tasks/compile.hologram.ex
@@ -14,42 +14,39 @@ defmodule Mix.Tasks.Compile.Hologram do
   alias Hologram.Commons.PLT
   alias Hologram.Compiler.Builder
   alias Hologram.Compiler.CallGraph
-  alias Hologram.Compiler.Reflection
+  # alias Hologram.Compiler.Reflection
 
-  @call_graph_dump_path Reflection.root_priv_path() <> "/call_graph.bin"
-  @call_graph_name :hologram_call_graph
-
-  @ir_plt_dump_path Reflection.root_priv_path() <> "/plt_ir.bin"
-  @ir_plt_name :hologram_plt_ir
-
-  @module_digest_plt_dump_path Reflection.root_priv_path() <> "/plt_module_digest.bin"
-  @new_module_digest_plt_name :hologram_plt_module_digest_tmp
-  @old_module_digest_plt_name :hologram_plt_module_digest
+  # @call_graph_dump_path Reflection.root_priv_path() <> "/call_graph.bin"
+  # @ir_plt_dump_path Reflection.root_priv_path() <> "/plt_ir.bin"
+  # @module_digest_plt_dump_path Reflection.root_priv_path() <> "/plt_module_digest.bin"
 
   @doc false
   @impl Mix.Task.Compiler
   def run(_opts \\ []) do
-    new_module_digest_plt = Builder.build_module_digest_plt(@new_module_digest_plt_name)
+    new_module_digest_plt = Builder.build_module_digest_plt()
 
-    old_module_digest_plt =
-      PLT.start(name: @old_module_digest_plt_name, dump_path: @module_digest_plt_dump_path)
+    # TODO: maybe load from dump
+    old_module_digest_plt = PLT.start()
 
     diff = Builder.diff_module_digest_plts(old_module_digest_plt, new_module_digest_plt)
 
-    ir_plt = PLT.start(name: @ir_plt_name, dump_path: @ir_plt_dump_path)
+    # TODO: maybe load from dump
+    ir_plt = PLT.start()
     Builder.patch_ir_plt(ir_plt, diff)
 
-    call_graph = CallGraph.start(name: @call_graph_name, dump_path: @call_graph_dump_path)
+    # TODO: maybe load from dump
+    call_graph = CallGraph.start()
     CallGraph.patch(call_graph, ir_plt, diff)
 
+    # TODO: bundle runtime and pages
     # Reflections.list_pages()
     # |> Builder.build_page_js(&1, output_path, module_defs, call_graph))
     # |> patch page digest plt
     # |> create page-digest files
 
-    # write PLTs and call graph...
-    PLT.dump(ir_plt)
-    PLT.dump(new_module_digest_plt)
+    # dump PLTs and call graph...
+    # PLT.dump(ir_plt)
+    # PLT.dump(new_module_digest_plt)
 
     :ok
   end
