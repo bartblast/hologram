971f4f16060f03549c3fb2c973392bd587cca5ba
Bart Blast <bart@bartblast.com>
2023-08-17 21:06:46 +0200

    Implement Hologram compile mix task

--- lib/mix/tasks/compile.hologram.ex
+++ lib/mix/tasks/compile.hologram.ex
@@ -10,8 +10,11 @@ defmodule Mix.Tasks.Compile.Hologram do
   alias Hologram.Compiler.CallGraph
   alias Hologram.Compiler.Reflection
 
+  require Logger
+
   @env Application.compile_env!(:hologram, :env)
   @root_path Reflection.root_path()
+  @tmp_path Reflection.tmp_path()
 
   @build_dir "#{@root_path}/_build/#{@env}/lib/hologram/priv"
   @bundle_dir "#{@root_path}/priv/static/assets"
@@ -22,12 +25,15 @@ defmodule Mix.Tasks.Compile.Hologram do
     build_dir: @build_dir,
     bundle_dir: @bundle_dir,
     esbuild_path: @esbuild_path,
-    js_source_dir: @js_source_dir
+    js_source_dir: @js_source_dir,
+    tmp_dir: @tmp_path
   ]
 
   @doc false
   @impl Mix.Task.Compiler
   def run(opts \\ @default_opts) do
+    Logger.info("Hologram: compiler started")
+
     new_module_digest_plt = Builder.build_module_digest_plt()
     old_module_digest_plt = PLT.start()
     module_digest_plt_dump_path = opts[:build_dir] <> "/module_digest.plt"
@@ -37,7 +43,7 @@ defmodule Mix.Tasks.Compile.Hologram do
 
     ir_plt = PLT.start()
     ir_plt_dump_path = opts[:build_dir] <> "/ir.plt"
-    PLT.maybe_load(ir_plt_dump_path, ir_plt_dump_path)
+    PLT.maybe_load(ir_plt, ir_plt_dump_path)
     Builder.patch_ir_plt(ir_plt, diff)
 
     call_graph = CallGraph.start()
@@ -45,25 +51,58 @@ defmodule Mix.Tasks.Compile.Hologram do
     CallGraph.maybe_load(call_graph, call_graph_dump_path)
     CallGraph.patch(call_graph, ir_plt, diff)
 
-    opts[:js_source_dir]
-    |> Builder.build_runtime_js(call_graph, ir_plt)
-    |> Builder.bundle(
-      "hologram.runtime",
-      opts[:esbuild_path],
-      opts[:build_dir],
-      opts[:bundle_dir]
-    )
+    bundle_runtime(call_graph, ir_plt, opts)
 
-    # TODO: bundle runtime and pages
-    # Reflections.list_pages()
-    # |> Builder.build_page_js(&1, output_path, module_defs, call_graph))
-    # |> patch page digest plt
-    # |> create page-digest files
+    bundle_pages(call_graph, ir_plt, opts)
 
-    # dump PLTs and call graph...
-    # PLT.dump(ir_plt)
-    # PLT.dump(new_module_digest_plt)
+    CallGraph.dump(call_graph, call_graph_dump_path)
+    PLT.dump(ir_plt, ir_plt_dump_path)
+    PLT.dump(new_module_digest_plt, module_digest_plt_dump_path)
+
+    Logger.info("Hologram: compiler finished")
 
     :ok
+  end
+
+  defp bundle_pages(call_graph, ir_plt, opts) do
+    Enum.each(Reflection.list_pages(), fn page_module ->
+      if !function_exported?(page_module, :__hologram_route__, 0) do
+        raise Hologram.CompileError,
+          message:
+            "Page '#{page_module}' doesn't have a route specified (use the route/1 macro to fix the issue)."
+      end
+
+      if !function_exported?(page_module, :__hologram_layout_module__, 0) do
+        raise Hologram.CompileError,
+          message:
+            "Page '#{page_module}' doesn't have a layout module specified (use the layout/1 macro to fix the issue)."
+      end
+
+      page_bundle_opts = [
+        esbuild_path: opts[:esbuild_path],
+        entry_name: to_string(page_module),
+        bundle_name: "hologram.page",
+        tmp_dir: opts[:tmp_dir],
+        bundle_dir: opts[:bundle_dir]
+      ]
+
+      page_module
+      |> Builder.build_page_js(call_graph, ir_plt, opts[:js_source_dir])
+      |> Builder.bundle(page_bundle_opts)
+    end)
+  end
+
+  defp bundle_runtime(call_graph, ir_plt, opts) do
+    runtime_bundle_opts = [
+      esbuild_path: opts[:esbuild_path],
+      entry_name: "hologram.runtime",
+      bundle_name: "hologram.runtime",
+      tmp_dir: opts[:tmp_dir],
+      bundle_dir: opts[:bundle_dir]
+    ]
+
+    opts[:js_source_dir]
+    |> Builder.build_runtime_js(call_graph, ir_plt)
+    |> Builder.bundle(runtime_bundle_opts)
   end
 end
--- test/elixir/mix/tasks/compile/hologram_test.exs
+++ test/elixir/mix/tasks/compile/hologram_test.exs
@@ -4,21 +4,68 @@ defmodule Mix.Tasks.Compile.HologramTest do
   alias Hologram.Compiler.Reflection
 
   @root_path Reflection.root_path()
-  @tmp_path "#{Reflection.tmp_path()}/#{__MODULE__}.run.1"
+  @tmp_path "#{Reflection.tmp_path()}/#{__MODULE__}/run_1"
 
   setup do
     clean_dir(@tmp_path)
     :ok
   end
 
+  defp test_build_artifacts do
+    call_graph_dump_path = "#{@tmp_path}/build/call_graph.bin"
+    assert File.exists?(call_graph_dump_path)
+
+    ir_plt_dump_path = "#{@tmp_path}/build/ir.plt"
+    assert File.exists?(ir_plt_dump_path)
+
+    module_digest_plt_dump_path = "#{@tmp_path}/build/module_digest.plt"
+    assert File.exists?(module_digest_plt_dump_path)
+
+    num_page_bundles =
+      "#{@tmp_path}/bundle/hologram.page.????????????????????????????????.js"
+      |> Path.wildcard()
+      |> Enum.count()
+
+    assert num_page_bundles > 1
+
+    num_page_source_maps =
+      "#{@tmp_path}/bundle/hologram.page.????????????????????????????????.js.map"
+      |> Path.wildcard()
+      |> Enum.count()
+
+    assert num_page_source_maps == num_page_bundles
+
+    num_runtime_bundles =
+      "#{@tmp_path}/bundle/hologram.runtime.????????????????????????????????.js"
+      |> Path.wildcard()
+      |> Enum.count()
+
+    assert num_runtime_bundles == 1
+
+    num_runtime_source_maps =
+      "#{@tmp_path}/bundle/hologram.runtime.????????????????????????????????.js.map"
+      |> Path.wildcard()
+      |> Enum.count()
+
+    assert num_runtime_source_maps == 1
+  end
+
+  # There are two tests in one test block here, because setup for the second test is expensive.
   test "run/1" do
     opts = [
-      build_dir: @tmp_path,
-      bundle_dir: @tmp_path,
       esbuild_path: "#{@root_path}/assets/node_modules/.bin/esbuild",
-      js_source_dir: "#{@root_path}/assets/js"
+      js_source_dir: "#{@root_path}/assets/js",
+      tmp_dir: "#{@tmp_path}/tmp",
+      build_dir: "#{@tmp_path}/build",
+      bundle_dir: "#{@tmp_path}/bundle"
     ]
 
+    # Test case 1: when there are no previous build artifacts
     run(opts)
+    test_build_artifacts()
+
+    # Test case 2: when there are previous build artifacts
+    run(opts)
+    test_build_artifacts()
   end
 end
