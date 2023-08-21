bc59ac5afd9cf283ba34c4793fedbadc12966ec0
Bart Blast <bart@bartblast.com>
2023-08-21 20:31:37 +0200

    Add missing doc and typespec

--- lib/hologram/template/renderer.ex
+++ lib/hologram/template/renderer.ex
@@ -1,4 +1,10 @@
 defmodule Hologram.Template.Renderer do
+  alias Hologram.Template.VDOMTree
+
+  @doc """
+  Renders the given VDOM node or nodes.
+  """
+  @spec render(VDOMTree.vdom_node() | list(VDOMTree.vdom_node())) :: String.t()
   def render(node_or_nodes)
 
   def render({:text, text}), do: text
