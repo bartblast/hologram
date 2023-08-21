5b4a648dcb60d7a9b5d88a77f6c6a3f864b02fd3
Bart Blast <bart@bartblast.com>
2023-08-21 20:10:28 +0200

    Implement ActionResult struct

---
+++ lib/hologram/runtime/action_result.ex
@@ -0,0 +1,12 @@
+defmodule Hologram.ActionResult do
+  alias Hologram.Operation
+
+  defstruct command: nil, context: nil, navigate: nil, state: nil
+
+  @type t :: %__MODULE__{
+          command: Operation.t(),
+          context: %{atom => any},
+          navigate: module | {module, keyword},
+          state: %{atom => any}
+        }
+end
