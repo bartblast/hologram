a0b2deb0f59dcc2b766b8f3f4dc6765f3f9e20d0
Bart Blast <bart@bartblast.com>
2023-08-23 12:55:18 +0200

    Render multiple nodes

--- lib/hologram/template/renderer.ex
+++ lib/hologram/template/renderer.ex
@@ -7,5 +7,10 @@ defmodule Hologram.Template.Renderer do
   @spec render(VDOMTree.vdom_node() | list(VDOMTree.vdom_node())) :: String.t()
   def render(node_or_nodes)
 
+  def render(nodes) when is_list(nodes) do
+    Enum.map(nodes, &render/1)
+    |> Enum.join()
+  end
+
   def render({:text, text}), do: text
 end
--- test/elixir/hologram/template/renderer_test.exs
+++ test/elixir/hologram/template/renderer_test.exs
@@ -2,6 +2,11 @@ defmodule Hologram.Template.RendererTest do
   use Hologram.Test.BasicCase, async: true
   import Hologram.Template.Renderer
 
+  test "multiple nodes" do
+    nodes = [{:text, "abc"}, {:text, "xyz"}]
+    assert render(nodes) == "abcxyz"
+  end
+
   test "text" do
     node = {:text, "abc"}
     assert render(node) == "abc"
