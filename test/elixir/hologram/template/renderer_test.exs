25020efa1a604ee53656d0c869f567ea1d288fb8
Bart Blast <bart@bartblast.com>
2023-08-24 11:36:09 +0200

    Reorder tests

--- test/elixir/hologram/template/renderer_test.exs
+++ test/elixir/hologram/template/renderer_test.exs
@@ -2,9 +2,9 @@ defmodule Hologram.Template.RendererTest do
   use Hologram.Test.BasicCase, async: true
   import Hologram.Template.Renderer
 
-  test "text" do
-    node = {:text, "abc"}
-    assert render(node) == "abc"
+  test "multiple nodes" do
+    nodes = [{:text, "abc"}, {:text, "xyz"}]
+    assert render(nodes) == "abcxyz"
   end
 
   test "expression" do
@@ -12,8 +12,8 @@ defmodule Hologram.Template.RendererTest do
     assert render(node) == "123"
   end
 
-  test "multiple nodes" do
-    nodes = [{:text, "abc"}, {:text, "xyz"}]
-    assert render(nodes) == "abcxyz"
+  test "text" do
+    node = {:text, "abc"}
+    assert render(node) == "abc"
   end
 end
