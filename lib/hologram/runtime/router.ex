13d0be202f8aa1cfb6626b2ac8a3cfd573b4aae3
Bart Blast <bart@bartblast.com>
2023-09-05 20:54:50 +0200

    Resolve page module

--- lib/hologram/runtime/router.ex
+++ lib/hologram/runtime/router.ex
@@ -4,15 +4,27 @@ defmodule Hologram.Router do
   alias Hologram.Compiler.Reflection
   alias Hologram.Router.SearchTree
 
+  @persistent_term_key {__MODULE__, :search_tree}
+
   @impl GenServer
-  def init(persistent_term_name \\ {__MODULE__, :search_tree}) do
+  def init(persistent_term_key \\ @persistent_term_key) do
     search_tree =
       Enum.reduce(Reflection.list_pages(), %SearchTree.Node{}, fn page, acc ->
         SearchTree.add_route(acc, page.__hologram_route__(), page)
       end)
 
-    :persistent_term.put(persistent_term_name, search_tree)
+    :persistent_term.put(persistent_term_key, search_tree)
 
     {:ok, nil}
+  end
+
+  @doc """
+  Given a URL path returns the page module that handles it.
+  """
+  @spec resolve_page(String.t(), :persistent_term.key()) :: module
+  def resolve_page(url_path, persistent_term_key \\ @persistent_term_key) do
+    persistent_term_key
+    |> :persistent_term.get()
+    |> SearchTree.match_route(url_path)
   end
 end
--- test/elixir/hologram/runtime/router_test.exs
+++ test/elixir/hologram/runtime/router_test.exs
@@ -1,23 +1,44 @@
 defmodule Hologram.Runtime.RouterTest do
   use Hologram.Test.BasicCase, async: true
   import Hologram.Router
+
   alias Hologram.Router.SearchTree
+  alias Hologram.Test.Fixtures.Runtime.Router.Module1
 
   test "init/1" do
-    persistent_term_name = random_atom()
+    persistent_term_key = random_atom()
 
-    assert {:ok, nil} = init(persistent_term_name)
+    assert {:ok, nil} = init(persistent_term_key)
 
-    search_tree = :persistent_term.get(persistent_term_name)
+    search_tree = :persistent_term.get(persistent_term_key)
 
     assert %SearchTree.Node{
              value: nil,
              children: %{
                "hologram-test-fixtures-runtime-router-module1" => %SearchTree.Node{
-                 value: Hologram.Test.Fixtures.Runtime.Router.Module1,
+                 value: Module1,
                  children: %{}
                }
              }
            } = search_tree
+  end
+
+  describe "resolve_page/2" do
+    setup do
+      persistent_term_key = random_atom()
+      init(persistent_term_key)
+
+      [persistent_term_key: persistent_term_key]
+    end
+
+    test "there is a matching route", %{persistent_term_key: persistent_term_key} do
+      url_path = "/hologram-test-fixtures-runtime-router-module1"
+      assert resolve_page(url_path, persistent_term_key) == Module1
+    end
+
+    test "there is no matching route", %{persistent_term_key: persistent_term_key} do
+      url_path = "/unknown-path"
+      refute resolve_page(url_path, persistent_term_key)
+    end
   end
 end
