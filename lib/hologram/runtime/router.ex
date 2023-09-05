2830208abc5276c2e38abb336cf93f3a586c822d
Bart Blast <bart@bartblast.com>
2023-09-05 21:25:18 +0200

    Extract params from a URL path

--- lib/hologram/runtime/router.ex
+++ lib/hologram/runtime/router.ex
@@ -6,6 +6,26 @@ defmodule Hologram.Router do
 
   @persistent_term_key {__MODULE__, :search_tree}
 
+  @doc """
+  Extracts param values from the given URL path corresponding to the route of the given page module.
+  """
+  @spec extract_params(String.t(), module) :: %{atom => any}
+  def extract_params(url_path, page) do
+    route_segments = String.split(page.__hologram_route__(), "/")
+    url_path_segments = String.split(url_path, "/")
+
+    route_segments
+    |> Enum.zip(url_path_segments)
+    |> Enum.reduce([], fn
+      {":" <> key, value}, acc ->
+        [{String.to_existing_atom(key), value} | acc]
+
+      _non_param_segment, acc ->
+        acc
+    end)
+    |> Enum.into(%{})
+  end
+
   @impl GenServer
   def init(persistent_term_key \\ @persistent_term_key) do
     search_tree =
