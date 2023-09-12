572f344833eaf9bebb5aa550d43d91fb0528c628
Bart Blast <bart@bartblast.com>
2023-09-12 14:33:26 +0200

    Add missing page fixture

---
+++ test/elixir/fixtures/template/renderer/module_39.ex
@@ -0,0 +1,21 @@
+defmodule Hologram.Test.Fixtures.Template.Renderer.Module39 do
+  use Hologram.Page
+  alias Hologram.Test.Fixtures.Template.Renderer.Module38
+
+  route "/hologram-test-fixtures-template-renderer-module39"
+
+  layout Hologram.Test.Fixtures.LayoutFixture
+
+  @impl Page
+  def init(_params, client, _server) do
+    client
+    |> put_context({:my_scope, :my_key}, 123)
+  end
+
+  @impl Page
+  def template do
+    ~H"""
+    <Module38 />
+    """
+  end
+end
