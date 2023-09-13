d12d327d1fa8eb82d8ce4b11dde2e5aede263d1c
Bart Blast <bart@bartblast.com>
2023-09-13 15:17:40 +0200

    Render runtime initial client data placeholder only if it hasn't been loaded yet

--- lib/hologram/ui/runtime.ex
+++ lib/hologram/ui/runtime.ex
@@ -1,13 +1,16 @@
 defmodule Hologram.UI.Runtime do
   use Hologram.Component
 
-  prop :initial_client_data, :string, from_context: {Hologram.Runtime, :initial_client_data}
+  prop :initial_client_data_loaded?, :boolean,
+    from_context: {Hologram.Runtime, :initial_client_data_loaded?}
 
   @impl Component
   def template do
     ~H"""
     <script>
-      window.__hologram_runtime_initial_client_data__ = "{@initial_client_data}";
+      {%if !@initial_client_data_loaded?}
+        window.__hologram_runtime_initial_client_data__ = "...";
+      {/if}
     </script>
     """
   end
--- test/elixir/hologram/ui/runtime_test.exs
+++ test/elixir/hologram/ui/runtime_test.exs
@@ -2,14 +2,28 @@ defmodule Hologram.UI.RuntimeTest do
   use Hologram.Test.BasicCase, async: true
   alias Hologram.UI.Runtime
 
-  test "output" do
+  test "initial_client_data_loaded? = false" do
     context = %{
-      {Hologram.Runtime, :initial_client_data} => "PLACEHOLDER"
+      {Hologram.Runtime, :initial_client_data_loaded?} => false
     }
 
     assert render_component(Runtime, %{}, context) == """
            <script>
-             window.__hologram_runtime_initial_client_data__ = "PLACEHOLDER";
+             
+               window.__hologram_runtime_initial_client_data__ = \"...\";
+             
+           </script>\
+           """
+  end
+
+  test "initial_client_data_loaded? = true" do
+    context = %{
+      {Hologram.Runtime, :initial_client_data_loaded?} => true
+    }
+
+    assert render_component(Runtime, %{}, context) == """
+           <script>
+             
            </script>\
            """
   end
