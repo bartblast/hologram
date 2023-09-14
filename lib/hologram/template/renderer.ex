ecaf30e548793687ae0272d040ff12acce217f46
Bart Blast <bart@bartblast.com>
2023-09-14 12:48:33 +0200

    Refactor renderer

--- lib/hologram/template/renderer.ex
+++ lib/hologram/template/renderer.ex
@@ -12,24 +12,24 @@ defmodule Hologram.Template.Renderer do
   @void_elems ~w(area base br col embed hr img input link meta param source track wbr)
 
   @doc """
-  Renders the given DOM node or DOM tree.
+  Renders the given DOM.
 
   ## Examples
 
-      iex> node = {:component, Module3, [{"id", [text: "my_component"]}], []}
-      iex> render(node, %{}, [])
+      iex> dom = {:component, Module3, [{"id", [text: "my_component"]}], []}
+      iex> render(dom, %{}, [])
       {
         "<div>state_a = 1, state_b = 2</div>",
         %{"my_component" => %Component.Client{state: %{a: 1, b: 2}}}
       }
   """
-  @spec render(DOM.dom_node() | DOM.tree(), %{(atom | {any, atom}) => any}, keyword(DOM.tree())) ::
+  @spec render(DOM.t(), %{(atom | {any, atom}) => any}, keyword(DOM.t())) ::
           {String.t(), %{atom => Component.Client.t()}}
-  def render(node_or_tree, context, slots)
+  def render(dom, context, slots)
 
   def render(nodes, context, slots) when is_list(nodes) do
     nodes
-    # There may be nil nodes resulting from if blocks, e.g. {%if false}abc{/if}
+    # There may be nil DOM nodes resulting from if blocks, e.g. {%if false}abc{/if}
     |> Enum.filter(& &1)
     |> Enum.reduce({"", %{}}, fn node, {acc_html, acc_clients} ->
       {html, clients} = render(node, context, slots)
@@ -37,11 +37,11 @@ defmodule Hologram.Template.Renderer do
     end)
   end
 
-  def render({:component, module, props_dom_tree, children}, context, slots) do
+  def render({:component, module, props_dom, children}, context, slots) do
     children = expand_slots(children, slots)
 
     props =
-      props_dom_tree
+      props_dom
       |> cast_props(module)
       |> inject_context_props(module, context)
 
@@ -87,25 +87,25 @@ defmodule Hologram.Template.Renderer do
 
   # TODO: Refactor once there is something akin to {...@var} syntax
   # (it would be possible to pass page state as layout props this way).
-  def render({:page, page_module, params_dom_tree, []}, context, _slots) do
-    params = cast_props(params_dom_tree, page_module)
+  def render({:page, page_module, params_dom, []}, context, _slots) do
+    params = cast_props(params_dom, page_module)
     {page_client, _server} = init_component(page_module, params)
 
     vars = aggregate_vars(params, page_client.state)
-    new_context = Map.merge(context, page_client.context)
+    context = Map.merge(context, page_client.context)
 
     layout_module = page_module.__hologram_layout_module__()
-    layout_props_dom_tree = build_layout_props_dom_tree(page_module, page_client)
+    layout_props_dom = build_layout_props_dom(page_module, page_client)
 
-    page_dom_tree = page_module.template().(vars)
-    node = {:component, layout_module, layout_props_dom_tree, page_dom_tree}
-    {html_without_initial_data, clients_without_page} = render(node, new_context, [])
+    page_dom = page_module.template().(vars)
+    layout_dom = {:component, layout_module, layout_props_dom, page_dom}
+    {html, clients} = render(layout_dom, context, [])
 
-    page_client_after_initial_data_loaded =
+    page_client =
       Templatable.put_context(page_client, {Hologram.Runtime, :initial_client_data_loaded?}, true)
 
-    clients = Map.put(clients_without_page, "page", page_client_after_initial_data_loaded)
-    html = inject_initial_data(html_without_initial_data, clients)
+    clients = Map.put(clients, "page", page_client)
+    html = inject_initial_client_data(html, clients)
 
     {html, clients}
   end
@@ -118,15 +118,15 @@ defmodule Hologram.Template.Renderer do
     Map.merge(props, state)
   end
 
-  defp build_layout_props_dom_tree(page_module, page_client) do
+  defp build_layout_props_dom(page_module, page_client) do
     page_module.__hologram_layout_props__()
     |> Enum.into(%{id: "layout"})
     |> aggregate_vars(page_client.state)
     |> Enum.map(fn {name, value} -> {to_string(name), [expression: {value}]} end)
   end
 
-  defp cast_props(props_dom_tree, module) do
-    props_dom_tree
+  defp cast_props(props_dom, module) do
+    props_dom
     |> filter_allowed_props(module)
     |> Stream.map(&evaluate_prop_value/1)
     |> Stream.map(&normalize_prop_name/1)
@@ -142,7 +142,7 @@ defmodule Hologram.Template.Renderer do
     {name, text}
   end
 
-  defp expand_slots(node_or_dom_tree, slots)
+  defp expand_slots(dom, slots)
 
   defp expand_slots(nodes, slots) when is_list(nodes) do
     nodes
@@ -164,7 +164,7 @@ defmodule Hologram.Template.Renderer do
 
   defp expand_slots(node, _slots), do: node
 
-  defp filter_allowed_props(props_dom_tree, module) do
+  defp filter_allowed_props(props_dom, module) do
     registered_prop_names =
       module.__props__()
       |> Enum.reject(fn {_name, _type, opts} -> opts[:from_context] end)
@@ -172,7 +172,7 @@ defmodule Hologram.Template.Renderer do
 
     allowed_props = ["id" | registered_prop_names]
 
-    Enum.filter(props_dom_tree, fn {name, _value_parts} -> name in allowed_props end)
+    Enum.filter(props_dom, fn {name, _value_parts} -> name in allowed_props end)
   end
 
   defp has_id_prop?(props) do
@@ -204,8 +204,8 @@ defmodule Hologram.Template.Renderer do
     Map.merge(props_from_template, props_from_context)
   end
 
-  defp inject_initial_data(html, clients) do
-    initial_client_data =
+  defp inject_initial_client_data(html, clients) do
+    data =
       clients
       |> Macro.escape()
       |> Normalizer.normalize()
@@ -217,7 +217,7 @@ defmodule Hologram.Template.Renderer do
     replacement = """
     window.__hologram_runtime_initial_client_data__ = (typeClass) => {
       const Type = typeClass;
-      return #{initial_client_data};
+      return #{data};
     };\
     """
 
@@ -231,9 +231,9 @@ defmodule Hologram.Template.Renderer do
   defp render_stateful_component(module, props, children, context) do
     {client, _server} = init_component(module, props)
     vars = aggregate_vars(props, client.state)
-    new_context = Map.merge(context, client.context)
+    context = Map.merge(context, client.context)
 
-    {html, children_clients} = render_template(module, vars, children, new_context)
+    {html, children_clients} = render_template(module, vars, children, context)
     clients = Map.put(children_clients, vars.id, client)
 
     {html, clients}
