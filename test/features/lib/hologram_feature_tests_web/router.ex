b7398f8e32c333bb52d155a5acda7941a2802f8b
Bart Blast <bart@bartblast.com>
2023-09-14 21:06:46 +0200

    Fix Dialyzer warning

--- test/features/lib/hologram_feature_tests_web/router.ex
+++ test/features/lib/hologram_feature_tests_web/router.ex
@@ -7,5 +7,9 @@ defmodule HologramFeatureTestsWeb.Router do
 
   scope "/api", HologramFeatureTestsWeb do
     pipe_through :api
+    
+    # This route is here only to prevent Dialyzer warning: "The pattern can never match the type.".
+    # See: https://github.com/phoenixframework/phoenix/issues/5375
+    get "/", PageController, :home
   end
 end
