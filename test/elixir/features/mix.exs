186f41dba84c2b52c0a3821a069004f41b03698a
Bart Blast <bart@bartblast.com>
2023-09-14 19:50:44 +0200

    Install hologram package in feature tests app

--- test/elixir/features/mix.exs
+++ test/elixir/features/mix.exs
@@ -36,7 +36,8 @@ defmodule HologramFeatureTests.MixProject do
       {:telemetry_metrics, "~> 0.6"},
       {:telemetry_poller, "~> 1.0"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:hologram, git: "https://github.com/bartblast/hologram.git"}
     ]
   end
 
