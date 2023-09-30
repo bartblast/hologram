bb90f7685879d9baaaf3776fdd8a2bc5063613ca
Bart Blast <bart@bartblast.com>
2023-09-30 13:52:52 +0200

    Add missing doc and typespec

--- lib/hologram/commons/ets.ex
+++ lib/hologram/commons/ets.ex
@@ -1,4 +1,8 @@
 defmodule Hologram.Commons.ETS do
+  @doc """
+  Creates a named, public ETS table.
+  """
+  @spec create_named_tabled(atom) :: :ets.tid()
   def create_named_tabled(table_name) do
     :ets.new(table_name, [:named_table, :public])
     :ets.whereis(table_name)
