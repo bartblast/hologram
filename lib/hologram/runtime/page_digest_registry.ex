f0f12acc09fc1cafca9ace00b980a01fcef07d1c
Bart Blast <bart@bartblast.com>
2023-10-06 14:40:42 +0200

    Add missing docs and typespecs

--- lib/hologram/runtime/page_digest_registry.ex
+++ lib/hologram/runtime/page_digest_registry.ex
@@ -34,10 +34,18 @@ defmodule Hologram.Runtime.PageDigestRegistry do
     |> PLT.get!(page_module)
   end
 
+  @doc """
+  Returns the implementation of the page digest registry's dump path.
+  """
+  @spec dump_path() :: String.t()
   def dump_path do
     Path.join([Reflection.build_dir(), Reflection.page_digest_plt_dump_file_name()])
   end
 
+  @doc """
+  Returns the implementation of the page digest registry's ETS table name.
+  """
+  @spec ets_table_name() :: atom
   def ets_table_name do
     __MODULE__
   end
