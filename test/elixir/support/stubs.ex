f286d976b016db76e4e8649b5f82cfc97990ec73
Bart Blast <bart@bartblast.com>
2023-10-09 20:36:33 +0200

    Fix random values in stubs

--- test/elixir/support/stubs.ex
+++ test/elixir/support/stubs.ex
@@ -1,22 +1,41 @@
 defmodule Hologram.Test.Stubs do
   alias Hologram.Commons.Reflection
   alias Hologram.Runtime.AssetPathRegistry
+  alias Hologram.Runtime.PageDigestRegistry
+
+  import Hologram.Test.Helpers, only: [random_atom: 0, random_module: 0, random_string: 0]
 
   defmacro use_module_stub(:asset_path_registry) do
-    caller_module = __CALLER__.module
+    random_module = random_module()
 
     quote do
-      defmodule alias!(unquote(caller_module).AssetPathRegistryStub) do
+      defmodule alias!(unquote(random_module).AssetPathRegistryStub) do
         @behaviour AssetPathRegistry
 
-        def static_dir_path, do: "#{Reflection.tmp_path()}/#{unquote(caller_module)}"
+        def static_dir_path, do: "#{Reflection.tmp_path()}/#{unquote(random_string())}"
 
-        def ets_table_name, do: unquote(caller_module)
+        def ets_table_name, do: unquote(random_atom())
 
-        def process_name, do: unquote(caller_module)
+        def process_name, do: unquote(random_atom())
       end
 
-      alias alias!(unquote(caller_module).AssetPathRegistryStub)
+      alias alias!(unquote(random_module).AssetPathRegistryStub)
+    end
+  end
+
+  defmacro use_module_stub(:page_digest_registry) do
+    random_module = random_module()
+
+    quote do
+      defmodule alias!(unquote(random_module).PageDigestRegistryStub) do
+        @behaviour PageDigestRegistry
+
+        def dump_path, do: "#{Reflection.tmp_path()}/#{unquote(random_string())}.plt"
+
+        def ets_table_name, do: unquote(random_atom())
+      end
+
+      alias alias!(unquote(random_module).PageDigestRegistryStub)
     end
   end
 end
