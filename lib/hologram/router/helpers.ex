6b4a2808b601116fc7601a4ca3a7db4615cbfae5
Bart Blast <bart@bartblast.com>
2023-10-10 11:27:12 +0200

    Implement asset_path/1 helper

---
+++ lib/hologram/router/helpers.ex
@@ -0,0 +1,18 @@
+defmodule Hologram.Router.Helpers do
+  alias Hologram.Runtime.AssetPathRegistry
+
+  @doc """
+  Retrieves the asset path, including the digest, for the specified static file within the static directory.
+  If there's no corresponding entry for the provided static file, the static file path itself will be returned.
+  """
+  @spec asset_path(String.t()) :: String.t()
+  def asset_path(static_path) do
+    case AssetPathRegistry.lookup(static_path) do
+      {:ok, asset_path} ->
+        asset_path
+
+      :error ->
+        static_path
+    end
+  end
+end
---
+++ test/elixir/hologram/router/helpers_test.exs
@@ -0,0 +1,34 @@
+defmodule Hologram.Router.HelpersTest do
+  use Hologram.Test.BasicCase, async: false
+
+  import Hologram.Router.Helpers
+  import Hologram.Test.Stubs
+  import Mox
+
+  alias Hologram.Runtime.AssetPathRegistry
+
+  use_module_stub :asset_path_registry
+
+  setup :set_mox_global
+
+  setup do
+    stub_with(AssetPathRegistryMock, AssetPathRegistryStub)
+    setup_asset_fixtures(AssetPathRegistryStub.static_dir_path())
+  end
+
+  describe "asset_path/1" do
+    setup do
+      AssetPathRegistry.start_link([])
+      :ok
+    end
+
+    test "asset exists" do
+      assert asset_path("/test_dir_1/test_dir_2/test_file_1.css") ==
+               "/test_dir_1/test_dir_2/test_file_1-11111111111111111111111111111111.css"
+    end
+
+    test "asset doesn't exist" do
+      assert asset_path("/invalid_file.css") == "/invalid_file.css"
+    end
+  end
+end
