53754824fded388f47a0389920f58cb11a31d539
Bart Blast <bart@bartblast.com>
2023-10-17 18:54:17 +0200

    Set page mount data

--- assets/js/hologram.mjs
+++ assets/js/hologram.mjs
@@ -4,7 +4,10 @@ import Interpreter from "./interpreter.mjs";
 import Type from "./type.mjs";
 
 export default class Hologram {
+  static clientsData = null;
   static isInitiated = false;
+  static pageModule = null;
+  static pageParams = null;
 
   // TODO: implement
   static init() {}
@@ -12,6 +15,20 @@ export default class Hologram {
   // TODO: implement
   static mountPage() {
     window.__hologramPageReachableFunctionDefs__(Interpreter, Type);
+
+    const mountData = window.__hologramPageMountData__(Type);
+    Hologram.clientsData = mountData.clientsData;
+    Hologram.pageModule = mountData.pageModule;
+    Hologram.pageParams = mountData.pageParams;
+
+    console.log("Hologram.clientsData =");
+    console.debug(Hologram.clientsData);
+
+    console.log("Hologram.pageModule =");
+    console.debug(Hologram.pageModule);
+
+    console.log("Hologram.pageParams =");
+    console.debug(Hologram.pageParams);
   }
 
   static onReady(callback) {
