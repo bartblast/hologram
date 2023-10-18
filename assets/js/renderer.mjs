c35695fdf7acafb3133150dd52799296ca21a42d
Bart Blast <bart@bartblast.com>
2023-10-18 14:22:57 +0200

    Move transpiled function proxy into the class

--- assets/js/renderer.mjs
+++ assets/js/renderer.mjs
@@ -3,19 +3,19 @@
 import Interpreter from "./interpreter.mjs";
 import Type from "./type.mjs";
 
-const mapFetch = Elixir_Map["fetch!/2"];
-
 // Based on Hologram.Template.Renderer
 export default class Renderer {
   static buildLayoutPropsDOM =
     Elixir_Hologram_Template_Renderer["build_layout_props_dom/2"];
+
+  static mapFetch = Elixir_Map["fetch!/2"];
 
   // TODO: implement
   static renderPage(pageModule, _pageParams, clientsData) {
     const _layoutModule =
       Interpreter.module(pageModule)["__layout_module__/0"]();
 
-    const pageClient = mapFetch(clientsData, Type.bitstring("page"));
+    const pageClient = Renderer.mapFetch(clientsData, Type.bitstring("page"));
 
     const layoutPropsDOM = Renderer.buildLayoutPropsDOM(pageModule, pageClient);
     console.dir(layoutPropsDOM);
