a61a52d74bd7b0a4dad8c365bc5940da8013fd38
Bart Blast <bart@bartblast.com>
2023-10-18 13:11:45 +0200

    Explain why the functions are included in the runtime

--- lib/hologram/compiler.ex
+++ lib/hologram/compiler.ex
@@ -268,6 +268,8 @@ defmodule Hologram.Compiler do
 
       # Interpreter.comprehension()
       {Enum, :to_list, 1},
+
+      # Functions used both on the client and the server.
       {Hologram.Template.Renderer, :aggregate_vars, 2},
       {Hologram.Template.Renderer, :build_layout_props_dom, 2},
 
