7b2c929dc910cd22eac2aec0c3e030804e322fb1
Bart Blast <bart@bartblast.com>
2023-10-20 14:46:00 +0200

    Cleanup

--- test/javascript/interpreter_test.mjs
+++ test/javascript/interpreter_test.mjs
@@ -1133,7 +1133,7 @@ describe("consOperator()", () => {
   });
 });
 
-describe.only("defineElixirFunction()", () => {
+describe("defineElixirFunction()", () => {
   beforeEach(() => {
     // def my_fun_a(1), do: :expr_1
     // def my_fun_a(2), do: :expr_2
