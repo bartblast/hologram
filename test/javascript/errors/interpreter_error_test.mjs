89ee381daacfcef1c2e98adff5fd894961df0811
Bart Blast <bart@bartblast.com>
2023-10-20 13:09:07 +0200

    Implement HologramInterpreterError class

---
+++ test/javascript/errors/interpreter_error_test.mjs
@@ -0,0 +1,13 @@
+"use strict";
+
+import {assert} from "../../../assets/js/test_support.mjs";
+import HologramInterpreterError from "../../../assets/js/errors/interpreter_error.mjs";
+
+it.only("HologramInterpreterError", () => {
+  try {
+    throw new HologramInterpreterError("my message");
+  } catch (error) {
+    assert.instanceOf(error, HologramInterpreterError);
+    assert.deepStrictEqual(error.message, "my message");
+  }
+});
