fa4d5f98940d301c980b7eeab9e3415d9528ca93
Bart Blast <bart@bartblast.com>
2023-10-23 20:08:00 +0200

    Add debug code

--- assets/js/interpreter.mjs
+++ assets/js/interpreter.mjs
@@ -130,6 +130,14 @@ export default class Interpreter {
     }
 
     globalThis[moduleName][`${functionName}/${functionArity}`] = function () {
+      console.log(
+        `${Interpreter.inspectModuleName(
+          moduleName,
+        )}.${functionName}/${functionArity}`,
+      );
+      console.dir(arguments);
+      console.log("----------");
+
       const args = Type.list([...arguments]);
       const arity = arguments.length;
 
