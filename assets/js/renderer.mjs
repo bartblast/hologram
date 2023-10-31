14c3b1b6d5cedc7a68895c61a921a3a9d2ac4bbc
Bart Blast <bart@bartblast.com>
2023-10-31 11:22:27 +0100

    Inspect renderer

--- assets/js/renderer.mjs
+++ assets/js/renderer.mjs
@@ -16,7 +16,8 @@ export default class Renderer {
       pageModule,
       pageClient,
     );
-    console.dir(layoutPropsDOM);
+
+    console.inspect(layoutPropsDOM);
   }
 
   static #buildLayoutPropsDOM(pageModule, pageClient) {
