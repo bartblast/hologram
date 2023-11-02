90cab0b39c3df94375ae492b0221f7fe7e514b69
Bart Blast <bart@bartblast.com>
2023-11-02 19:20:58 +0100

    Fix JS unit tests

--- test/javascript/sequence_test.mjs
+++ test/javascript/sequence_test.mjs
@@ -10,6 +10,14 @@ import Sequence from "../../assets/js/sequence.mjs";
 before(() => linkModules());
 after(() => unlinkModules());
 
+beforeEach(() => {
+  Sequence.value = 0;
+});
+
+afterEach(() => {
+  Sequence.value = 0;
+});
+
 it("next()", () => {
   assert.equal(Sequence.next(), 1);
   assert.equal(Sequence.next(), 2);
