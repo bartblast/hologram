e5ad997c82a33d715f649f6e3d0a3b5b604cca1b
Bart Blast <bart@bartblast.com>
2023-11-09 20:17:54 +0100

    Improve boxed errors

--- assets/js/errors/boxed_error.mjs
+++ assets/js/errors/boxed_error.mjs
@@ -1,8 +1,15 @@
+import Interpreter from "../interpreter.mjs";
+
 export default class HologramBoxedError extends Error {
   constructor(struct) {
     super("");
 
     this.name = "HologramBoxedError";
     this.struct = struct;
+
+    const boxedType = Interpreter.fetchErrorType(this);
+    const boxedMessage = Interpreter.fetchErrorMessage(this);
+
+    this.message = `(${boxedType}) ${boxedMessage}`;
   }
 }
