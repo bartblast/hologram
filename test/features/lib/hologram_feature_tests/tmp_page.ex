8824684f25f4bebb3aaf5c7d92667a325d187436
Bart Blast <bart@bartblast.com>
2023-11-15 18:04:35 +0100

    Update features test app tmp page

--- test/features/lib/hologram_feature_tests/tmp_page.ex
+++ test/features/lib/hologram_feature_tests/tmp_page.ex
@@ -1,13 +1,15 @@
 defmodule HologramFeatureTests.TmpPage do
   use Hologram.Page
 
-  route "/abc"
+  route "/abc/:xyz"
+
+  param :xyz
 
   layout HologramFeatureTests.TmpLayout
 
   def template do
     ~H"""
-    page
+    page with param: {@xyz}
     """
   end
 end
