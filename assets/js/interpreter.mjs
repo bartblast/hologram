2e5a7de5afc1883d9e06aeb4c4b31f46d0e66d93
Bart Blast <bart@bartblast.com>
2023-11-17 12:31:49 +0100

    Lowercase labels

--- assets/js/interpreter.mjs
+++ assets/js/interpreter.mjs
@@ -669,7 +669,7 @@ export default class Interpreter {
     console.group(mfa);
 
     if (args.length > 0) {
-      console.log("%cARGS", "color: blue");
+      console.log("%args", "color: blue");
 
       for (let i = 0; i < args.length; ++i) {
         console.log(
@@ -682,7 +682,7 @@ export default class Interpreter {
   }
 
   static #logFunctionResult(mfa, result) {
-    console.log("%cRESULT", "color: blue");
+    console.log("%result", "color: blue");
     console.log(Interpreter.inspect(result));
     console.groupEnd(mfa);
   }
