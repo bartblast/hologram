f0bc46e16f728756cd358d684ca71f6432ad46b2
Bart Blast <bart@bartblast.com>
2023-11-17 12:32:41 +0100

    Fix string spec

--- assets/js/interpreter.mjs
+++ assets/js/interpreter.mjs
@@ -669,7 +669,7 @@ export default class Interpreter {
     console.group(mfa);
 
     if (args.length > 0) {
-      console.log("%args", "color: blue");
+      console.log("%cargs", "color: blue");
 
       for (let i = 0; i < args.length; ++i) {
         console.log(
@@ -682,7 +682,7 @@ export default class Interpreter {
   }
 
   static #logFunctionResult(mfa, result) {
-    console.log("%result", "color: blue");
+    console.log("%cresult", "color: blue");
     console.log(Interpreter.inspect(result));
     console.groupEnd(mfa);
   }
