387946f65af2f9a348522d47b4fbbd7dd98c3b02
Bart Blast <bart@bartblast.com>
2023-11-24 12:53:37 +0100

    Test client rendering of void elements with attributes

--- test/javascript/renderer_test.mjs
+++ test/javascript/renderer_test.mjs
@@ -126,6 +126,44 @@ describe("element node", () => {
 
     assert.deepStrictEqual(result, expected);
   });
+
+  it("void element, with attributes", () => {
+    const node = Type.tuple([
+      Type.atom("element"),
+      Type.bitstring("img"),
+      Type.list([
+        Type.tuple([
+          Type.bitstring("attr_1"),
+          Type.keywordList([[Type.atom("text"), Type.bitstring("aaa")]]),
+        ]),
+        Type.tuple([
+          Type.bitstring("attr_2"),
+          Type.keywordList([
+            [Type.atom("expression"), Type.tuple([Type.integer(123)])],
+          ]),
+        ]),
+        Type.tuple([
+          Type.bitstring("attr_3"),
+          Type.keywordList([
+            [Type.atom("text"), Type.bitstring("ccc")],
+            [Type.atom("expression"), Type.tuple([Type.integer(987)])],
+            [Type.atom("text"), Type.bitstring("eee")],
+          ]),
+        ]),
+      ]),
+      Type.list([]),
+    ]);
+
+    const result = Renderer.renderDom(node, context, slots);
+
+    const expected = vnode(
+      "img",
+      {attrs: {attr_1: "aaa", attr_2: "123", attr_3: "ccc987eee"}},
+      [],
+    );
+
+    assert.deepStrictEqual(result, expected);
+  });
 });
 
 describe("node list", () => {
