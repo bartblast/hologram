59bfc2f823cbe69cdb1124cceff1f3c146ffe832
Bart Blast <bart@bartblast.com>
2024-02-24 22:01:37 +0100

    Implement IntegerUtils.parse!/2

--- lib/hologram/commons/integer_utils.ex
+++ lib/hologram/commons/integer_utils.ex
@@ -19,4 +19,24 @@ defmodule Hologram.Commons.IntegerUtils do
     |> Integer.digits()
     |> Enum.count()
   end
+
+  @doc """
+  Parses a text representation of an integer.
+
+  Raises an error if the text representation can't be parsed,
+  or if the base is less than 2 or more than 36,
+  or if only part of the text representation can be parsed.
+  """
+  def parse!(binary, base \\ 10) do
+    case Integer.parse(binary, base) do
+      {integer, ""} ->
+        integer
+
+      :error ->
+        raise ArgumentError, message: "invalid text representation"
+
+      {_integer, _remainder} ->
+        raise ArgumentError, message: "only part of the text representation can be parsed"
+    end
+  end
 end
--- test/elixir/hologram/commons/integer_utils_test.exs
+++ test/elixir/hologram/commons/integer_utils_test.exs
@@ -5,4 +5,42 @@ defmodule Hologram.Commons.IntegerUtilsTest do
   test "count_digits?/1" do
     assert count_digits(123) == 3
   end
+
+  describe "parse!/2" do
+    test "base 1" do
+      assert_raise ArgumentError, "invalid base 1", fn ->
+        parse!("123", 1)
+      end
+    end
+
+    test "base 2" do
+      assert parse!("1010", 2) == 10
+    end
+
+    test "base 10 (default)" do
+      assert parse!("123") == 123
+    end
+
+    test "base 36" do
+      assert parse!("abc", 36) == 13368
+    end
+
+    test "base 37" do
+      assert_raise ArgumentError, "invalid base 37", fn ->
+        parse!("abc", 37)
+      end
+    end
+
+    test "invalid text representation" do
+      assert_raise ArgumentError, "invalid text representation", fn ->
+        parse!("abc", 10)
+      end
+    end
+
+    test "only part of the text representation can be parsed" do
+      assert_raise ArgumentError, "only part of the text representation can be parsed", fn ->
+        parse!("123abc", 10)
+      end
+    end
+  end
 end
