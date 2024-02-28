313286fd0a02d497195d47ed32eb5de8c6014231
Bart Blast <bart@bartblast.com>
2024-02-28 21:12:51 +0100

    Refactor UI.Runtime component tests and test initial_page? prop

--- test/elixir/hologram/ui/runtime_test.exs
+++ test/elixir/hologram/ui/runtime_test.exs
@@ -31,49 +31,36 @@ defmodule Hologram.UI.RuntimeTest do
     ]
   end
 
-  test "initial_page? = false", %{context: context} do
+  test "initial_page? prop = false", %{context: context} do
     markup = render_component(Runtime, %{}, context)
     refute String.contains?(markup, "window.__hologramAssetManifest__")
   end
 
-  test "initial_page? = true", %{context: context} do
+  test "initial_page? prop = true", %{context: context} do
     context = Map.put(context, {Hologram.Runtime, :initial_page?}, true)
     markup = render_component(Runtime, %{}, context)
 
     assert String.contains?(markup, "window.__hologramAssetManifest__")
   end
 
-  test "page_mounted? = false", %{context: context} do
-    assert render_component(Runtime, %{}, context) == """
-           <script>
-             
-               
-                 window.__hologramPageMountData__ = (deps) => {
-                   const Type = deps.Type;
-                   
-                   return {
-                     componentStructs: $COMPONENT_STRUCTS_JS_PLACEHOLDER,
-                     pageModule: $PAGE_MODULE_JS_PLACEHOLDER,
-                     pageParams: $PAGE_PARAMS_JS_PLACEHOLDER
-                   };
-                 };
-               
-             
-           </script>
-           <script async src="/hologram/runtime.js"></script>
-           <script async src="/hologram/page-102790adb6c3b1956db310be523a7693.js"></script>\
-           """
+  test "page_mounted? prop = false", %{context: context} do
+    markup = render_component(Runtime, %{}, context)
+    assert String.contains?(markup, "window.__hologramPageMountData__")
   end
 
-  test "page_mounted? = true", %{context: context} do
+  test "page_mounted? prop = true", %{context: context} do
     context = Map.put(context, {Hologram.Runtime, :page_mounted?}, true)
+    markup = render_component(Runtime, %{}, context)
 
-    assert render_component(Runtime, %{}, context) == """
-           <script>
-             
-           </script>
-           <script async src="/hologram/runtime.js"></script>
-           <script async src="/hologram/page-102790adb6c3b1956db310be523a7693.js"></script>\
-           """
+    refute String.contains?(markup, "window.__hologramPageMountData__")
+  end
+
+  test "page_digest prop", %{context: context} do
+    markup = render_component(Runtime, %{}, context)
+
+    assert String.contains?(
+             markup,
+             ~s'<script async src="/hologram/page-102790adb6c3b1956db310be523a7693.js">'
+           )
   end
 end
