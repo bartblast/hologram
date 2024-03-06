c781cc6a5f384f38a535e7471b3a356a1c0d5ad9
Bart Blast <bart@bartblast.com>
2024-03-06 20:47:15 +0100

    Update error message

--- assets/js/elixir/hologram/router/helpers.mjs
+++ assets/js/elixir/hologram/router/helpers.mjs
@@ -1,16 +1,19 @@
 "use strict";
 
 import AssetPathRegistry from "../../../asset_path_registry.mjs";
-import Bitstring from "../../../bitstring.mjs";
+import Interpreter from "../../../interpreter.mjs";
 import Type from "../../../type.mjs";
 
 const Elixir_Hologram_Router_Helpers = {
   "asset_path/1": (staticPath) => {
     const assetPath = AssetPathRegistry.lookup(staticPath);
 
-    return !Type.isNil(assetPath)
-      ? assetPath
-      : Bitstring.merge([Type.bitstring("/"), staticPath]);
+    if (Type.isNil(assetPath)) {
+      const message = `there is no such asset: ${staticPath}`;
+      return Interpreter.raiseError("Hologram.AssetNotFoundError", message);
+    }
+
+    return assetPath;
   },
 };
 
--- test/javascript/elixir/hologram/router/helpers_test.mjs
+++ test/javascript/elixir/hologram/router/helpers_test.mjs
@@ -37,6 +37,14 @@ describe("asset_path/1", () => {
   });
 
   it("entry for static path doesn't exists", () => {
+    // assertBoxedError(
+    //   () => Elixir_Hologram_Router_Helpers["asset_path/1"](
+    //     Type.bitstring("static-path-4"),
+    //   )
+    //   "Hologram.AssetNotFound",
+    //   "there is no such asset: ",
+    // );
+
     const result = Elixir_Hologram_Router_Helpers["asset_path/1"](
       Type.bitstring("static-path-4"),
     );
