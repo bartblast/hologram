f2a6170e5635bc27727c8235a72b78ee8322c032
Bart Blast <bart@bartblast.com>
2024-03-10 11:17:28 +0100

    Change comment wording

--- lib/hologram/template.ex
+++ lib/hologram/template.ex
@@ -34,7 +34,7 @@ defmodule Hologram.Template do
   defp build_h_sigil_ast(markup) do
     quote do
       fn var!(vars) ->
-        # Fix unused var warning
+        # Fixes unused var warning
         # credo:disable-for-next-line Credo.Check.Consistency.UnusedVariableNames
         _ = var!(vars)
         unquote(dom_ast(markup))
