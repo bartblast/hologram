471bc35be219a1da282a5ff01aa2b5af14406bba
Bart Blast <bart@bartblast.com>
2024-03-12 20:55:05 +0100

    Port :code.ensure_loaded/1 to JS

---
+++ assets/js/erlang/code.mjs
@@ -0,0 +1,28 @@
+"use strict";
+
+import Interpreter from "../interpreter.mjs";
+import Type from "../type.mjs";
+
+// IMPORTANT!
+// If the given ported Erlang function calls other Erlang functions, then list such dependencies in a "deps" comment (see :erlang./=/2 for an example).
+// Also, in such case add respective call graph edges in Hologram.Compiler.list_runtime_mfas/1.
+
+const Erlang_Code = {
+  // start ensure_loaded/1
+  // This function is simplified - it returns either {:module, MyModule} or {:error, :nofile}.
+  "ensure_loaded/1": (module) => {
+    if (!Type.isAtom(module)) {
+      Interpreter.raiseFunctionClauseError(
+        "no function clause matching in :code.ensure_loaded/1/1",
+      );
+    }
+
+    return typeof Interpreter.moduleRef(module) === "undefined"
+      ? Type.tuple([Type.atom("error"), Type.atom("nofile")])
+      : Type.tuple([Type.atom("module"), module]);
+  },
+  // end ensure_loaded/1
+  // deps: []
+};
+
+export default Erlang_Code;
---
+++ test/javascript/erlang/code_test.mjs
@@ -0,0 +1,33 @@
+"use strict";
+
+import {assert, linkModules, unlinkModules} from "../support/helpers.mjs";
+
+import Erlang_Code from "../../../assets/js/erlang/code.mjs";
+import Type from "../../../assets/js/type.mjs";
+
+before(() => linkModules());
+after(() => unlinkModules());
+
+// IMPORTANT!
+// Each JavaScript test has a related Elixir consistency test in test/elixir/hologram/ex_js_consistency/erlang/code_test.exs
+// Always update both together.
+
+describe("ensure_loaded/1", () => {
+  const fun = Erlang_Code["ensure_loaded/1"];
+
+  it("loaded module", () => {
+    const module = Type.alias("String.Chars");
+    const result = fun(module);
+    const expected = Type.tuple([Type.atom("module"), module]);
+
+    assert.deepStrictEqual(result, expected);
+  });
+
+  it("not loaded, non-existing module", () => {
+    const module = Type.alias("MyModule");
+    const result = fun(module);
+    const expected = Type.tuple([Type.atom("error"), Type.atom("nofile")]);
+
+    assert.deepStrictEqual(result, expected);
+  });
+});
