632f4504516f19052ab418ac4df0078b4eb7dfed
Bart Blast <bart@bartblast.com>
2024-03-13 11:00:46 +0100

    List manually ported Kernel.inspect/1 deps

--- assets/js/elixir/kernel.mjs
+++ assets/js/elixir/kernel.mjs
@@ -4,6 +4,7 @@ import Interpreter from "../interpreter.mjs";
 import Type from "../type.mjs";
 
 const Elixir_Kernel = {
+  // deps: [Kernel.inspect/2]
   "inspect/1": (term) => {
     return Elixir_Kernel["inspect/2"](term, Type.list([]));
   },
