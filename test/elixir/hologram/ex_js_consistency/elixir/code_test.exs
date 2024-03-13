fbe8fcd742adf4157f7f64517241560f037ab5b6
Bart Blast <bart@bartblast.com>
2024-03-13 12:41:42 +0100

    Implement Elixir constistency tests for Code.ensure_compiled/1

---
+++ test/elixir/hologram/ex_js_consistency/elixir/code_test.exs
@@ -0,0 +1,27 @@
+defmodule Hologram.ExJsConsistency.Elixir.CodeTest do
+  @moduledoc """
+  IMPORTANT!
+  Each Elixir consistency test has a related JavaScript test in test/javascript/elixir/code_test.mjs
+  Always update both together.
+  """
+
+  use Hologram.Test.BasicCase, async: true
+
+  describe "ensure_compiled/1" do
+    test "compiled module" do
+      assert Code.ensure_compiled(String.Chars) == {:module, String.Chars}
+    end
+
+    test "not compiled, non-existing module" do
+      assert Code.ensure_compiled(MyModule) == {:error, :nofile}
+    end
+
+    test "raises FunctionClauseError if the argument is not an atom" do
+      assert_raise FunctionClauseError,
+                   "no function clause matching in Code.ensure_compiled/1",
+                   fn ->
+                     Code.ensure_compiled(1)
+                   end
+    end
+  end
+end
