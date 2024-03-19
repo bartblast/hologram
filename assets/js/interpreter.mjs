b0bd0c25e7b2b2d46fb9a9221383ac5dd7a54618
Bart Blast <bart@bartblast.com>
2024-03-19 15:07:08 +0100

    Remove redundant deserialize() function

--- assets/js/interpreter.mjs
+++ assets/js/interpreter.mjs
@@ -315,15 +315,6 @@ export default class Interpreter {
     };
   }
 
-  static deserialize(json) {
-    return JSON.parse(json, (_key, value) => {
-      if (typeof value === "string" && /^__bigint__:-?\d+$/.test(value)) {
-        return BigInt(value.substring(11, value.length));
-      }
-      return value;
-    });
-  }
-
   // deps: [:maps.get/2]
   static dotOperator(left, right) {
     // if left argument is a boxed atom, treat the operator as a remote function call
