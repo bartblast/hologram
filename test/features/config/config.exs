ca92ca5bf625f2ba6094760a4665a910c0b237be
Bart Blast <bart@bartblast.com>
2024-04-04 16:46:49 +0200

    Cleanup feature tests app configs

--- test/features/config/config.exs
+++ test/features/config/config.exs
@@ -1,30 +1,21 @@
-# This file is responsible for configuring your application
-# and its dependencies with the aid of the Config module.
-#
-# This configuration file is loaded before any dependency and
-# is restricted to this project.
-
-# General application configuration
 import Config
 
-# Configures the endpoint
 config :hologram_feature_tests, HologramFeatureTestsWeb.Endpoint,
-  url: [host: "localhost"],
+  live_view: [signing_salt: "mNbBbt8f"],
+  pubsub_server: HologramFeatureTests.PubSub,
   render_errors: [
     formats: [json: HologramFeatureTestsWeb.ErrorJSON],
     layout: false
   ],
-  pubsub_server: HologramFeatureTests.PubSub,
-  live_view: [signing_salt: "mNbBbt8f"]
+  url: [host: "localhost"]
 
-# Configures Elixir's Logger
 config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
 
-# Use Jason for JSON parsing in Phoenix
-config :phoenix, :json_library, Jason
+config :phoenix,
+  json_library: Jason,
+  # Initialize plugs at runtime for faster test compilation
+  plug_init_mode: :runtime
 
-# Import environment specific config. This must remain at the bottom
-# of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
