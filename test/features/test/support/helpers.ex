6b52dd04e459a4b64da702e6a58bbee4ac7dc9ea
Bart Blast <bart@bartblast.com>
2024-04-05 21:42:26 +0200

    Implement visit/1 and visit/2 feature test helpers

---
+++ test/features/test/support/helpers.ex
@@ -0,0 +1,13 @@
+defmodule HologramFeatureTestsWeb.Test.Helpers do
+  alias Hologram.Router
+  alias Wallaby.Browser
+
+  def visit(session, page_module) do
+    visit(session, page_module, [])
+  end
+
+  def visit(session, page_module, params) do
+    path = Router.Helpers.page_path(page_module, params)
+    Browser.visit(session, path)
+  end
+end
