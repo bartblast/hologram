ae6dba8ceb848b8a586ba2a7c1548218237ecf9d
Bart Blast <bart@bartblast.com>
2024-04-07 20:19:12 +0200

    Fix feature test

--- test/features/test/operators_test.exs
+++ test/features/test/operators_test.exs
@@ -99,7 +99,7 @@ defmodule HologramFeatureTests.OperatorsTest do
     session
     |> visit(OperatorsPage)
     |> click(css("button[id='not']"))
-    |> assert_text(css("#result"), inspect(not @boolean_b))
+    |> assert_text(css("#result"), inspect(not @boolean_a))
   end
 
   feature "!", %{session: session} do
