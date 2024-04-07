08743489c4183682c1d0b501b305bfb534bd7e9b
Bart Blast <bart@bartblast.com>
2024-04-07 17:47:38 +0200

    Copy build_value/1 test helper

--- test/features/test/support/helpers.ex
+++ test/features/test/support/helpers.ex
@@ -2,6 +2,15 @@ defmodule HologramFeatureTests.Helpers do
   alias Hologram.Router
   alias Wallaby.Browser
 
+  @doc """
+  Returns the given argument.
+  It prevents compiler warnings in tests when the given value is not permitted is specific situation.
+  """
+  @spec build_value(any) :: any
+  def build_value(value) do
+    value
+  end
+
   def visit(session, page_module) do
     visit(session, page_module, [])
   end
