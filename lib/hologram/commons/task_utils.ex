c1b49c34fe6ff6f24c2d725d11eb5bda22d26c42
Bart Blast <bart@bartblast.com>
2024-04-08 20:10:17 +0200

    Implement await_tasks/1

--- lib/hologram/commons/task_utils.ex
+++ lib/hologram/commons/task_utils.ex
@@ -1,4 +1,8 @@
 defmodule Hologram.Commons.TaskUtils do
+  def await_tasks(tasks) do
+    Enum.map(tasks, &Task.await(&1, :infinity))
+  end
+
   def map_async(enumerable, fun) do
     Enum.map(enumerable, fn elem ->
       Task.async(fn -> fun.(elem) end)
--- test/elixir/hologram/commons/task_utils_test.exs
+++ test/elixir/hologram/commons/task_utils_test.exs
@@ -2,6 +2,15 @@ defmodule Hologram.Commons.TaskUtilsTest do
   use Hologram.Test.BasicCase, async: true
   import Hologram.Commons.TaskUtils
 
+  test "await_tasks/1" do
+    res =
+      1..3
+      |> map_async(&(&1 + &1))
+      |> await_tasks()
+
+    assert res == [2, 4, 6]
+  end
+
   test "map_async/2" do
     res = map_async(1..3, &(&1 + &1))
 
