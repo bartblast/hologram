445252571676f20cd3734c9a23e769ca2f574e3a
Bart Blast <bart@bartblast.com>
2024-04-08 17:25:08 +0200

    Log compiler debug info

--- lib/mix/tasks/compile.hologram.ex
+++ lib/mix/tasks/compile.hologram.ex
@@ -39,36 +39,64 @@ defmodule Mix.Tasks.Compile.Hologram do
   """
   @spec compile(keyword) :: :ok
   def compile(opts) do
-    Logger.info("Hologram: compiler started")
+    Logger.info("Hologram: start compiling")
+
+    Logger.debug("Hologram: start building module digest PLTs")
 
     {new_module_digest_plt, old_module_digest_plt, module_digest_plt_dump_path} =
       build_module_digest_plts(opts)
 
+    Logger.debug("Hologram: finished building module digest PLTs")
+
+    Logger.debug("Hologram: start diffing module digest PLTs")
+
     diff = Compiler.diff_module_digest_plts(old_module_digest_plt, new_module_digest_plt)
+
+    Logger.debug("Hologram: finished diffing module digest PLTs")
+
+    Logger.debug("Hologram: start building IR PLT")
 
     {ir_plt, ir_plt_dump_path} = build_ir_plt(opts, diff)
 
+    Logger.debug("Hologram: finished building IR PLT")
+
+    Logger.debug("Hologram: start building call graph")
+
     {call_graph, call_graph_dump_path} = build_call_graph(opts, ir_plt, diff)
+
+    Logger.debug("Hologram: finished building call graph")
+
+    Logger.debug("Hologram: start installing JS deps")
 
     Compiler.install_js_deps(opts[:assets_source_dir])
 
+    Logger.debug("Hologram: finished installing JS deps")
+
+    Logger.debug("Hologram: start runtime bundling")
+
     bundle_runtime(call_graph, ir_plt, opts)
+
+    Logger.debug("Hologram: finished runtime bundling")
 
     page_digest_plt = PLT.start()
 
     page_digest_plt_dump_path =
       Path.join([opts[:build_dir], Reflection.page_digest_plt_dump_file_name()])
 
+    Logger.debug("Hologram: start pages bundling")
+
     call_graph
     |> bundle_pages(ir_plt, opts)
     |> Enum.each(fn {module, digest} -> PLT.put(page_digest_plt, module, digest) end)
+
+    Logger.debug("Hologram: finished pages bundling")
 
     PLT.dump(page_digest_plt, page_digest_plt_dump_path)
     CallGraph.dump(call_graph, call_graph_dump_path)
     PLT.dump(ir_plt, ir_plt_dump_path)
     PLT.dump(new_module_digest_plt, module_digest_plt_dump_path)
 
-    Logger.info("Hologram: compiler finished")
+    Logger.info("Hologram: finished compiling")
 
     :ok
   end
