b2ed77e6ba2e51a36aa6629693cd730c497b1c76
Bart Blast <bart@bartblast.com>
2024-04-11 18:09:34 +0200

    Add doc and typespec for async_many/2

--- lib/hologram/commons/task_utils.ex
+++ lib/hologram/commons/task_utils.ex
@@ -1,4 +1,8 @@
 defmodule Hologram.Commons.TaskUtils do
+  @doc """
+  Starts multiple tasks.
+  """
+  @spec async_many(Enum.t(), fun) :: list(Task.t())
   def async_many(enumerable, fun) do
     Enum.map(enumerable, fn elem ->
       Task.async(fn -> fun.(elem) end)
