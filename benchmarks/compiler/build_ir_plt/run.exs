e0854df75b9ff6a47da45a00d3284540ff1771ef
Bart Blast <bart@bartblast.com>
2024-04-22 18:33:19 +0200

    Benchmark build_ir_plt/1

---
+++ benchmarks/compiler/build_ir_plt/run.exs
@@ -0,0 +1,24 @@
+alias Hologram.Benchmarks
+alias Hologram.Commons.PLT
+alias Hologram.Compiler
+
+Benchee.run(
+  %{
+    "build_ir_plt/1" =>
+      {fn module_beam_path_plt ->
+         Compiler.build_ir_plt(module_beam_path_plt)
+       end,
+       before_scenario: fn _input ->
+         Benchmarks.build_module_beam_path_plt()
+       end,
+       after_each: fn ir_plt ->
+         PLT.stop(ir_plt)
+       end}
+  },
+  formatters: [
+    Benchee.Formatters.Console,
+    {Benchee.Formatters.Markdown,
+     description: "build_ir_plt/1", file: Path.join(__DIR__, "README.md")}
+  ],
+  time: 60
+)
