f01765313fc618c9daedc93cb552c2bd81ae6e19
Bart Blast <bart@bartblast.com>
2024-04-23 22:06:17 +0200

    Fix param values

--- benchmarks/compiler/diff_module_digest_plts/run.exs
+++ benchmarks/compiler/diff_module_digest_plts/run.exs
@@ -31,14 +31,14 @@ Benchee.run(
          Compiler.diff_module_digest_plts(old_module_digest_plt, new_module_digest_plt)
        end,
        before_scenario: fn _input ->
-         Benchmarks.generate_module_digest_plts(0, 0, 0)
+         Benchmarks.generate_module_digest_plts(0, 0, 100)
        end},
     "1/3 added, 1/3 removed, 1/3 updated" => {
       fn {old_module_digest_plt, new_module_digest_plt} ->
         Compiler.diff_module_digest_plts(old_module_digest_plt, new_module_digest_plt)
       end,
       before_scenario: fn _input ->
-        Benchmarks.generate_module_digest_plts(33, 33, 0)
+        Benchmarks.generate_module_digest_plts(33, 33, 34)
       end
     }
   },
