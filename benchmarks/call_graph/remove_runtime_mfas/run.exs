bacd16602a12072e15c13b0a16095e31726301d2
Bart Blast <bart@bartblast.com>
2024-04-24 18:26:16 +0200

    Benchmark CallGraph.remove_runtime_mfas/2

---
+++ benchmarks/call_graph/remove_runtime_mfas/run.exs
@@ -0,0 +1,31 @@
+alias Hologram.Compiler
+alias Hologram.Compiler.CallGraph
+
+Benchee.run(
+  %{
+    "remove_runtime_mfas/2" =>
+      {fn {call_graph, runtime_mfas} ->
+         CallGraph.remove_runtime_mfas(call_graph, runtime_mfas)
+       end,
+       before_scenario: fn _input ->
+         module_beam_path_plt = Compiler.build_module_beam_path_plt()
+         ir_plt = Compiler.build_ir_plt(module_beam_path_plt)
+         call_graph = CallGraph.build_from_ir_plt(ir_plt)
+         runtime_mfas = Compiler.list_runtime_mfas(call_graph)
+
+         {call_graph, runtime_mfas}
+       end,
+       before_each: fn {call_graph, runtime_mfas} ->
+         {CallGraph.clone(call_graph), runtime_mfas}
+       end,
+       after_each: fn call_graph ->
+         CallGraph.stop(call_graph)
+       end}
+  },
+  formatters: [
+    Benchee.Formatters.Console,
+    {Benchee.Formatters.Markdown,
+     description: "remove_runtime_mfas/2", file: Path.join(__DIR__, "README.md")}
+  ],
+  time: 60
+)
