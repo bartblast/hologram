96e479841149e227023eecfcc29137bdb7809773
Bart Blast <bart@bartblast.com>
2024-04-26 16:24:19 +0200

    Benchmark CallGraph.clone/1

---
+++ benchmarks/call_graph/clone/run.exs
@@ -0,0 +1,22 @@
+alias Hologram.Compiler
+alias Hologram.Compiler.CallGraph
+
+Benchee.run(
+  %{
+    "clone/1" =>
+      {fn call_graph ->
+         CallGraph.clone(call_graph)
+       end,
+       after_each: fn call_graph ->
+         CallGraph.stop(call_graph)
+       end}
+  },
+  before_scenario: fn _input ->
+    Compiler.build_call_graph()
+  end,
+  formatters: [
+    Benchee.Formatters.Console,
+    {Benchee.Formatters.Markdown, description: "clone/1", file: Path.join(__DIR__, "README.md")}
+  ],
+  time: 60
+)
