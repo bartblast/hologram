e30260c659d6fe2e08677631098a15a9307a62e4
Bart Blast <bart@bartblast.com>
2024-04-29 19:09:44 +0200

    Benchmark CallGraph.dump/1

---
+++ benchmarks/call_graph/dump/run.exs
@@ -0,0 +1,51 @@
+alias Hologram.Commons.FileUtils
+alias Hologram.Commons.Reflection
+alias Hologram.Compiler
+alias Hologram.Compiler.CallGraph
+
+Benchee.run(
+  %{
+    "dump dir exists, dump file exist" =>
+      {fn {call_graph, dump_path} ->
+         CallGraph.dump(call_graph, dump_path)
+       end,
+       before_scenario: fn {call_graph, _dump_dir, dump_path} ->
+         CallGraph.dump(call_graph, dump_path)
+         {call_graph, dump_path}
+       end},
+    "dump dir exists, dump file doesn't exist" =>
+      {fn {call_graph, dump_path} ->
+         CallGraph.dump(call_graph, dump_path)
+       end,
+       before_scenario: fn {call_graph, dump_dir, dump_path} ->
+         FileUtils.recreate_dir(dump_dir)
+         {call_graph, dump_path}
+       end,
+       before_each: fn {call_graph, dump_path} ->
+         File.rm(dump_path)
+         {call_graph, dump_path}
+       end},
+    "dump dir doesn't exists" =>
+      {fn {call_graph, dump_path} ->
+         CallGraph.dump(call_graph, dump_path)
+       end,
+       before_each: fn {call_graph, dump_dir, dump_path} ->
+         File.rmdir(dump_dir)
+         {call_graph, dump_path}
+       end}
+  },
+  before_scenario: fn _input ->
+    call_graph = Compiler.build_call_graph()
+
+    dump_dir = Path.join([Reflection.tmp_dir(), "benchmarks", "call_graph", "dump"])
+    dump_path = Path.join(dump_dir, "call_graph.bin")
+
+    {call_graph, dump_dir, dump_path}
+  end,
+  formatters: [
+    Benchee.Formatters.Console,
+    {Benchee.Formatters.Markdown,
+     description: "CallGraph.dump/2", file: Path.join(__DIR__, "README.md")}
+  ],
+  time: 60
+)
