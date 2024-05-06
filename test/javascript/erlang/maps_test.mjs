c5ea8ee1f7490297e8e90a9db8678e3b5253292b
Bart Blast <bart@bartblast.com>
2024-05-06 22:54:23 +0200

    Refactor ported :maps module tests

--- test/javascript/erlang/maps_test.mjs
+++ test/javascript/erlang/maps_test.mjs
@@ -38,6 +38,8 @@ describe("Erlang_Maps", () => {
   after(() => unlinkModules());
 
   describe("fold/3", () => {
+    const fold = Erlang_Maps["fold/3"];
+
     let fun, map;
 
     beforeEach(() => {
@@ -69,23 +71,18 @@ describe("Erlang_Maps", () => {
     });
 
     it("reduces empty map", () => {
-      const result = Erlang_Maps["fold/3"](fun, Type.integer(10), Type.map([]));
+      const result = fold(fun, Type.integer(10), Type.map([]));
       assert.deepStrictEqual(result, Type.integer(10));
     });
 
     it("reduces non-empty map", () => {
-      const result = Erlang_Maps["fold/3"](fun, Type.integer(10), map);
+      const result = fold(fun, Type.integer(10), map);
       assert.deepStrictEqual(result, Type.integer(331));
     });
 
     it("raises ArgumentError if the first argument is not an anonymous function", () => {
       assertBoxedError(
-        () =>
-          Erlang_Maps["fold/3"](
-            Type.atom("abc"),
-            Type.integer(10),
-            Type.map([]),
-          ),
+        () => fold(Type.atom("abc"), Type.integer(10), Type.map([])),
         "ArgumentError",
         Interpreter.buildErrorsFoundMsg(
           1,
@@ -108,7 +105,7 @@ describe("Erlang_Maps", () => {
       );
 
       assertBoxedError(
-        () => Erlang_Maps["fold/3"](fun, Type.integer(10), Type.map([])),
+        () => fold(fun, Type.integer(10), Type.map([])),
         "ArgumentError",
         Interpreter.buildErrorsFoundMsg(
           1,
@@ -119,7 +116,7 @@ describe("Erlang_Maps", () => {
 
     it("raises BadMapError if the third argument is not a map", () => {
       assertBoxedError(
-        () => Erlang_Maps["fold/3"](fun, Type.integer(10), Type.atom("abc")),
+        () => fold(fun, Type.integer(10), Type.atom("abc")),
         "BadMapError",
         "expected a map, got: :abc",
       );
@@ -127,13 +124,15 @@ describe("Erlang_Maps", () => {
   });
 
   describe("from_list/1", () => {
+    const from_list = Erlang_Maps["from_list/1"];
+
     it("builds a map from the given list of key-value tuples", () => {
       const list = Type.list([
         Type.tuple([Type.atom("a"), Type.integer(2)]),
         Type.tuple([Type.integer(3), Type.float(4.0)]),
       ]);
 
-      const result = Erlang_Maps["from_list/1"](list);
+      const result = from_list(list);
 
       const expected = Type.map([
         [Type.atom("a"), Type.integer(2)],
@@ -153,7 +152,7 @@ describe("Erlang_Maps", () => {
         Type.tuple([Type.atom("b"), Type.integer(6)]),
       ]);
 
-      const result = Erlang_Maps["from_list/1"](list);
+      const result = from_list(list);
 
       const expected = Type.map([
         [Type.atom("a"), Type.integer(5)],
@@ -165,7 +164,7 @@ describe("Erlang_Maps", () => {
 
     it("raises ArgumentError if the argument is not a list", () => {
       assertBoxedError(
-        () => Erlang_Maps["from_list/1"](Type.integer(123)),
+        () => from_list(Type.integer(123)),
         "ArgumentError",
         Interpreter.buildErrorsFoundMsg(1, "not a list"),
       );
@@ -173,6 +172,8 @@ describe("Erlang_Maps", () => {
   });
 
   describe("get/2", () => {
+    const get = Erlang_Maps["get/2"];
+
     it("returns the value assiociated with the given key if map contains the key", () => {
       const key = Type.atom("b");
       const value = Type.integer(2);
@@ -182,14 +183,14 @@ describe("Erlang_Maps", () => {
         [key, value],
       ]);
 
-      const result = Erlang_Maps["get/2"](key, map);
+      const result = get(key, map);
 
       assert.deepStrictEqual(result, value);
     });
 
     it("raises BadMapError if the second argument is not a map", () => {
       assertBoxedError(
-        () => Erlang_Maps["get/2"](Type.atom("a"), Type.integer(1)),
+        () => get(Type.atom("a"), Type.integer(1)),
         "BadMapError",
         "expected a map, got: 1",
       );
@@ -197,7 +198,7 @@ describe("Erlang_Maps", () => {
 
     it("raises KeyError if the map doesn't contain the given key", () => {
       assertBoxedError(
-        () => Erlang_Maps["get/2"](Type.atom("a"), Type.map([])),
+        () => get(Type.atom("a"), Type.map([])),
         "KeyError",
         "key :a not found in: %{}",
       );
@@ -206,6 +207,7 @@ describe("Erlang_Maps", () => {
 
   describe("get/3", () => {
     const defaultValue = Type.atom("default_value");
+    const get = Erlang_Maps["get/3"];
 
     it("returns the value assiociated with the given key if map contains the key", () => {
       const key = Type.atom("b");
@@ -216,44 +218,39 @@ describe("Erlang_Maps", () => {
         [key, value],
       ]);
 
-      const result = Erlang_Maps["get/3"](key, map, defaultValue);
+      const result = get(key, map, defaultValue);
 
       assert.deepStrictEqual(result, value);
     });
 
     it("raises BadMapError if the second argument is not a map", () => {
       assertBoxedError(
-        () =>
-          Erlang_Maps["get/3"](Type.atom("a"), Type.integer(1), defaultValue),
+        () => get(Type.atom("a"), Type.integer(1), defaultValue),
         "BadMapError",
         "expected a map, got: 1",
       );
     });
 
     it("returns the default value if the map doesn't contain the given key", () => {
-      const result = Erlang_Maps["get/3"](
-        Type.atom("a"),
-        Type.map([]),
-        defaultValue,
-      );
+      const result = get(Type.atom("a"), Type.map([]), defaultValue);
       assert.deepStrictEqual(result, defaultValue);
     });
   });
 
   describe("is_key/2", () => {
-    const fun = Erlang_Maps["is_key/2"];
+    const is_key = Erlang_Maps["is_key/2"];
 
     it("returns true if the given map has the given key", () => {
-      assertBoxedTrue(fun(atomB, mapA1B2));
+      assertBoxedTrue(is_key(atomB, mapA1B2));
     });
 
     it("returns false if the given map doesn't have the given key", () => {
-      assertBoxedFalse(fun(atomC, mapA1B2));
+      assertBoxedFalse(is_key(atomC, mapA1B2));
     });
 
     it("raises BadMapError if the second argument is not a map", () => {
       assertBoxedError(
-        () => fun(atomA, atomAbc),
+        () => is_key(atomA, atomAbc),
         "BadMapError",
         "expected a map, got: :abc",
       );
@@ -261,10 +258,10 @@ describe("Erlang_Maps", () => {
   });
 
   describe("keys/1", () => {
-    const fun = Erlang_Maps["keys/1"];
+    const keys = Erlang_Maps["keys/1"];
 
     it("empty map", () => {
-      assert.deepStrictEqual(fun(Type.map([])), Type.list([]));
+      assert.deepStrictEqual(keys(Type.map([])), Type.list([]));
     });
 
     it("non-empty map", () => {
@@ -273,12 +270,12 @@ describe("Erlang_Maps", () => {
         [atomB, integer2],
       ]);
 
-      assert.deepStrictEqual(fun(map), Type.list([atomA, atomB]));
+      assert.deepStrictEqual(keys(map), Type.list([atomA, atomB]));
     });
 
     it("not a map", () => {
       assertBoxedError(
-        () => fun(atomAbc),
+        () => keys(atomAbc),
         "BadMapError",
         "expected a map, got: :abc",
       );
@@ -303,13 +300,15 @@ describe("Erlang_Maps", () => {
       contextFixture(),
     );
 
+    const map = Erlang_Maps["map/2"];
+
     it("maps empty map", () => {
-      const result = Erlang_Maps["map/2"](fun, Type.map([]));
+      const result = map(fun, Type.map([]));
       assert.deepStrictEqual(result, Type.map([]));
     });
 
     it("maps non-empty map", () => {
-      const result = Erlang_Maps["map/2"](
+      const result = map(
         fun,
         Type.map([
           [Type.atom("a"), Type.integer(1)],
@@ -330,7 +329,7 @@ describe("Erlang_Maps", () => {
 
     it("raises ArgumentError if the first argument is not an anonymous function", () => {
       assertBoxedError(
-        () => Erlang_Maps["map/2"](Type.atom("abc"), Type.map([])),
+        () => map(Type.atom("abc"), Type.map([])),
         "ArgumentError",
         Interpreter.buildErrorsFoundMsg(
           1,
@@ -355,7 +354,7 @@ describe("Erlang_Maps", () => {
       );
 
       assertBoxedError(
-        () => Erlang_Maps["map/2"](fun, Type.map([])),
+        () => map(fun, Type.map([])),
         "ArgumentError",
         Interpreter.buildErrorsFoundMsg(
           1,
@@ -366,7 +365,7 @@ describe("Erlang_Maps", () => {
 
     it("raises BadMapError if the second argument is not a map", () => {
       assertBoxedError(
-        () => Erlang_Maps["map/2"](fun, Type.atom("abc")),
+        () => map(fun, Type.atom("abc")),
         "BadMapError",
         "expected a map, got: :abc",
       );
@@ -374,6 +373,8 @@ describe("Erlang_Maps", () => {
   });
 
   describe("merge/2", () => {
+    const merge = Erlang_Maps["merge/2"];
+
     it("merges two maps", () => {
       const map1 = Type.map([
         [Type.atom("a"), Type.integer(1)],
@@ -385,7 +386,7 @@ describe("Erlang_Maps", () => {
         [Type.atom("d"), Type.integer(4)],
       ]);
 
-      const result = Erlang_Maps["merge/2"](map1, map2);
+      const result = merge(map1, map2);
 
       const expected = Type.map([
         [Type.atom("a"), Type.integer(1)],
@@ -410,7 +411,7 @@ describe("Erlang_Maps", () => {
         [Type.atom("a"), Type.integer(6)],
       ]);
 
-      const result = Erlang_Maps["merge/2"](map1, map2);
+      const result = merge(map1, map2);
 
       const expected = Type.map([
         [Type.atom("a"), Type.integer(6)],
@@ -426,7 +427,7 @@ describe("Erlang_Maps", () => {
       const map = Type.map([[Type.atom("a"), Type.integer(1)]]);
 
       assertBoxedError(
-        () => Erlang_Maps["merge/2"](Type.integer(123), map),
+        () => merge(Type.integer(123), map),
         "BadMapError",
         "expected a map, got: 123",
       );
@@ -436,7 +437,7 @@ describe("Erlang_Maps", () => {
       const map = Type.map([[Type.atom("a"), Type.integer(1)]]);
 
       assertBoxedError(
-        () => Erlang_Maps["merge/2"](map, Type.integer(123)),
+        () => merge(map, Type.integer(123)),
         "BadMapError",
         "expected a map, got: 123",
       );
@@ -444,9 +445,11 @@ describe("Erlang_Maps", () => {
   });
 
   describe("put/3", () => {
+    const put = Erlang_Maps["put/3"];
+
     it("when the map doesn't have the given key", () => {
       const map = Type.map([[Type.atom("a"), Type.integer(1)]]);
-      const result = Erlang_Maps["put/3"](Type.atom("b"), Type.integer(2), map);
+      const result = put(Type.atom("b"), Type.integer(2), map);
 
       const expected = Type.map([
         [Type.atom("a"), Type.integer(1)],
@@ -462,7 +465,7 @@ describe("Erlang_Maps", () => {
         [Type.atom("b"), Type.integer(2)],
       ]);
 
-      const result = Erlang_Maps["put/3"](Type.atom("b"), Type.integer(3), map);
+      const result = put(Type.atom("b"), Type.integer(3), map);
 
       const expected = Type.map([
         [Type.atom("a"), Type.integer(1)],
@@ -474,12 +477,7 @@ describe("Erlang_Maps", () => {
 
     it("raises BadMapError if the third argument is not a map", () => {
       assertBoxedError(
-        () =>
-          Erlang_Maps["put/3"](
-            Type.atom("a"),
-            Type.integer(1),
-            Type.atom("abc"),
-          ),
+        () => put(Type.atom("a"), Type.integer(1), Type.atom("abc")),
         "BadMapError",
         "expected a map, got: :abc",
       );
@@ -487,8 +485,10 @@ describe("Erlang_Maps", () => {
   });
 
   describe("to_list/1", () => {
+    const to_list = Erlang_Maps["to_list/1"];
+
     it("doesn't mutate its arguments", () => {
-      Erlang_Maps["to_list/1"](
+      to_list(
         freeze(
           Type.map([
             [Type.atom("a"), Type.integer(1)],
@@ -499,7 +499,7 @@ describe("Erlang_Maps", () => {
     });
 
     it("returns an empty list if given an empty map", () => {
-      const result = Erlang_Maps["to_list/1"](Type.map([]));
+      const result = to_list(Type.map([]));
       assert.deepStrictEqual(result, Type.list([]));
     });
 
@@ -509,7 +509,7 @@ describe("Erlang_Maps", () => {
         [Type.atom("b"), Type.integer(2)],
       ]);
 
-      const result = Erlang_Maps["to_list/1"](map);
+      const result = to_list(map);
 
       const expected = Type.list([
         Type.tuple([Type.atom("a"), Type.integer(1)]),
@@ -521,7 +521,7 @@ describe("Erlang_Maps", () => {
 
     it("raises BadMapError if the argument is not a map", () => {
       assertBoxedError(
-        () => Erlang_Maps["to_list/1"](Type.atom("abc")),
+        () => to_list(Type.atom("abc")),
         "BadMapError",
         "expected a map, got: :abc",
       );
@@ -536,7 +536,7 @@ describe("Erlang_Maps", () => {
       ]);
 
       assertBoxedError(
-        () => Erlang_Maps["to_list/1"](iterator),
+        () => to_list(iterator),
         "BadMapError",
         "expected a map, got: {:a, 1, :none}",
       );
