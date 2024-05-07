1e71593ab478b73d5b2373647f234600dba3a01a
Bart Blast <bart@bartblast.com>
2024-05-07 17:37:37 +0200

    Update layout

--- test/features/app/components/layouts/default_layout.ex
+++ test/features/app/components/layouts/default_layout.ex
@@ -4,8 +4,11 @@ defmodule HologramFeatureTests.Components.DefaultLayout do
 
   def template do
     ~H"""
-    <html>
+    <!DOCTYPE html>
+    <html lang="en">
       <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Runtime />
       </head>
       <body style="padding: 25px">
