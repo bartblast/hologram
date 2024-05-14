310c15201f5d6a2cf6dabd63322e83ca55ec5eca
Bart Blast <bart@bartblast.com>
2024-05-14 22:22:32 +0200

    Implement Client.push()

--- assets/js/client.mjs
+++ assets/js/client.mjs
@@ -4,18 +4,39 @@ import {Socket} from "phoenix";
 
 // Tested implicitely in feature tests.
 export default class Client {
+  static channel = null;
+
   static async connect() {
     const socket = new Socket("/hologram");
     socket.connect();
 
-    socket
-      .channel("hologram")
+    Client.channel = socket.channel("hologram");
+
+    Client.channel
       .join()
       .receive("ok", (_resp) => {
         console.debug("Joined Hologram channel");
       })
       .receive("error", (_resp) => {
         console.error("Unable to join Hologram channel");
+      });
+  }
+
+  static async push(event, payload, callback) {
+    Client.channel
+      .push(event, payload)
+      .receive("ok", callback)
+      .receive("error", (_resp) => {
+        console.error(
+          "Unable to push an event to the server (reason: error)",
+          arguments,
+        );
+      })
+      .receive("timeout", (_resp) => {
+        console.error(
+          "Unable to push an event to the server (reason: timeout)",
+          arguments,
+        );
       });
   }
 }
