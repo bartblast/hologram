8eb8a391a5d1d687cc7270cf91ff552dc3ca2252
Bart Blast <bart@bartblast.com>
2024-05-20 17:33:31 +0200

    Test Websocket client encoder

--- test/javascript/client_test.mjs
+++ test/javascript/client_test.mjs
@@ -1,12 +1,32 @@
 "use strict";
 
-import {assert, linkModules, unlinkModules} from "./support/helpers.mjs";
+import {assert, linkModules, sinon, unlinkModules} from "./support/helpers.mjs";
 
 import Client from "../../assets/js/client.mjs";
+import Type from "../../assets/js/type.mjs";
 
 describe("Client", () => {
   before(() => linkModules());
   after(() => unlinkModules());
+
+  it("encoder()", () => {
+    const callbackSpy = sinon.spy();
+
+    const msg = {
+      event: "dummy_event",
+      join_ref: "dummy_join_ref",
+      payload: Type.integer(123),
+      ref: "dummy_ref",
+      topic: "dummy_topic",
+    };
+
+    Client.encoder(msg, callbackSpy);
+
+    const expected =
+      '["dummy_join_ref","dummy_ref","dummy_topic","dummy_event",{"type":"integer","value":"__bigint__:123"}]';
+
+    sinon.assert.calledOnceWithExactly(callbackSpy, expected);
+  });
 
   describe("isConnected()", () => {
     it("socket is null", () => {
