ec52d74a6ae53310694994240dbf82659b4d6ceb
Bart Blast <bart@bartblast.com>
2024-06-10 20:33:14 +0200

    Start live reload process in dev env

--- lib/hologram/application.ex
+++ lib/hologram/application.ex
@@ -3,14 +3,26 @@ defmodule Hologram.Application do
 
   @impl Application
   def start(_type, _args) do
-    children = [
+    opts = [strategy: :one_for_one, name: Hologram.Supervisor]
+
+    Hologram.env()
+    |> children()
+    |> Supervisor.start_link(opts)
+  end
+
+  defp all_envs_children do
+    [
       Hologram.Router.PageModuleResolver,
       Hologram.Assets.PathRegistry,
       Hologram.Assets.ManifestCache,
       Hologram.Assets.PageDigestRegistry
     ]
-
-    opts = [strategy: :one_for_one, name: Hologram.Supervisor]
-    Supervisor.start_link(children, opts)
   end
+
+  defp children(:dev) do
+    # credo:disable-for-next-line Credo.Check.Refactor.AppendSingleItem
+    all_envs_children() ++ [Hologram.LiveReload]
+  end
+
+  defp children(_env), do: all_envs_children()
 end
