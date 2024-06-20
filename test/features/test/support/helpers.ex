3d0ace2077b39ccccafb61a6a9eb957c859e89be
Bart Blast <bart@bartblast.com>
2024-06-20 18:16:28 +0200

    Order functions

--- test/features/test/support/helpers.ex
+++ test/features/test/support/helpers.ex
@@ -2,15 +2,6 @@ defmodule HologramFeatureTests.Helpers do
   alias Hologram.Router
   alias Wallaby.Browser
 
-  @doc """
-  Returns the given argument.
-  It prevents compiler warnings in tests when the given value is not permitted is specific situation.
-  """
-  @spec wrap_value(any) :: any
-  def wrap_value(value) do
-    value
-  end
-
   def visit(session, page_module) do
     visit(session, page_module, [])
   end
@@ -18,5 +9,14 @@ defmodule HologramFeatureTests.Helpers do
   def visit(session, page_module, params) do
     path = Router.Helpers.page_path(page_module, params)
     Browser.visit(session, path)
+  end
+
+  @doc """
+  Returns the given argument.
+  It prevents compiler warnings in tests when the given value is not permitted is specific situation.
+  """
+  @spec wrap_value(any) :: any
+  def wrap_value(value) do
+    value
   end
 end
