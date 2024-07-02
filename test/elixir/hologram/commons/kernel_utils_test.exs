21490e60059841e774d84e1c9c7a8e2e7f33909a
Bart Blast <bart@bartblast.com>
2024-07-02 23:27:51 +0200

    Fix flaky test

--- test/elixir/hologram/commons/kernel_utils_test.exs
+++ test/elixir/hologram/commons/kernel_utils_test.exs
@@ -3,7 +3,7 @@ defmodule Hologram.Commons.KernelUtilsTest do
   alias Hologram.Commons.KernelUtils
 
   test "inspect/1" do
-    assert Kernel.inspect(%{x: 1, y: 2}) == "%{y: 2, x: 1}"
+    assert Kernel.inspect(%{x: 1, y: 2}) in ["%{x: 1, y: 2}", "%{y: 2, x: 1}"]
     assert KernelUtils.inspect(%{x: 1, y: 2}) == "%{x: 1, y: 2}"
   end
 end
