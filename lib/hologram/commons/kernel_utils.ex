81deb50a085a20f44eb418a2197f26ec681a3577
Bart Blast <bart@bartblast.com>
2024-07-02 21:05:17 +0200

    Implement KernelUtils.inspect/1

---
+++ lib/hologram/commons/kernel_utils.ex
@@ -0,0 +1,10 @@
+defmodule Hologram.Commons.KernelUtils do
+  @doc """
+  Inspects the given argument according to the Inspect protocol.
+  Maps inspection is deterministic by sorting their key-value pairs.
+  """
+  @spec inspect(any) :: String.t()
+  def inspect(term) do
+    Kernel.inspect(term, custom_options: [sort_maps: true])
+  end
+end
---
+++ test/elixir/hologram/commons/kernel_utils_test.exs
@@ -0,0 +1,9 @@
+defmodule Hologram.Commons.KernelUtilsTest do
+  use Hologram.Test.BasicCase, async: true
+  alias Hologram.Commons.KernelUtils
+
+  test "inspect/1" do
+    assert Kernel.inspect(%{x: 1, y: 2}) == "%{y: 2, x: 1}"
+    assert KernelUtils.inspect(%{x: 1, y: 2}) == "%{x: 1, y: 2}"
+  end
+end
