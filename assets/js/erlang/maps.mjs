e1ba8a32eb1ab56fb65b6b26d78281ee14002b73
Bart Blast <bart@bartblast.com>
2024-07-04 20:22:33 +0200

    Fix grammar error

--- assets/js/erlang/maps.mjs
+++ assets/js/erlang/maps.mjs
@@ -5,7 +5,7 @@ import Type from "../type.mjs";
 import Utils from "../utils.mjs";
 
 // IMPORTANT!
-// If the given ported Erlang function calls other Erlang functions, then list such dependencies in a "Deps" comment (see :erlang./=/2 for an example).
+// If the given ported Erlang function calls other Erlang functions, then list such dependencies in the "Deps" comment (see :erlang./=/2 for an example).
 // Also, in such case add respective call graph edges in Hologram.CallGraph.list_runtime_mfas/1.
 
 const Erlang_Maps = {
--- assets/js/erlang/persistent_term.mjs
+++ assets/js/erlang/persistent_term.mjs
@@ -4,7 +4,7 @@ import MemoryStorage from "../memory_storage.mjs";
 import Type from "../type.mjs";
 
 // IMPORTANT!
-// If the given ported Erlang function calls other Erlang functions, then list such dependencies in a "Deps" comment (see :erlang./=/2 for an example).
+// If the given ported Erlang function calls other Erlang functions, then list such dependencies in the "Deps" comment (see :erlang./=/2 for an example).
 // Also, in such case add respective call graph edges in Hologram.CallGraph.list_runtime_mfas/1.
 
 const Erlang_Persistent_Term = {
