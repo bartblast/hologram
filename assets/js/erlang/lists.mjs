3a5987a20564d082704088a64b1d517e0e88d0c4
Bart Blast <bart@bartblast.com>
2024-07-04 20:21:26 +0200

    Fix comments

--- assets/js/erlang/lists.mjs
+++ assets/js/erlang/lists.mjs
@@ -4,7 +4,7 @@ import Interpreter from "../interpreter.mjs";
 import Type from "../type.mjs";
 
 // IMPORTANT!
-// If the given ported Erlang function calls other Erlang functions, then list such dependencies in a "deps" comment (see :erlang./=/2 for an example).
+// If the given ported Erlang function calls other Erlang functions, then list such dependencies in a "Deps" comment (see :erlang./=/2 for an example).
 // Also, in such case add respective call graph edges in Hologram.CallGraph.list_runtime_mfas/1.
 
 const Erlang_Lists = {
