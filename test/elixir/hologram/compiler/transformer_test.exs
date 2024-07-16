f1e54053d77001054602f370ae4972f35dc2e6c1
Bart Blast <bart@bartblast.com>
2024-07-16 15:54:42 +0200

    Add transformer test case for: cons operator, nested, impproper (AST from BEAM file)

--- test/elixir/hologram/compiler/transformer_test.exs
+++ test/elixir/hologram/compiler/transformer_test.exs
@@ -67,6 +67,7 @@ defmodule Hologram.Compiler.TransformerTest do
   alias Hologram.Test.Fixtures.Compiler.Tranformer.Module62
   alias Hologram.Test.Fixtures.Compiler.Tranformer.Module63
   alias Hologram.Test.Fixtures.Compiler.Tranformer.Module64
+  alias Hologram.Test.Fixtures.Compiler.Tranformer.Module65
   alias Hologram.Test.Fixtures.Compiler.Tranformer.Module7
   alias Hologram.Test.Fixtures.Compiler.Tranformer.Module8
   alias Hologram.Test.Fixtures.Compiler.Tranformer.Module9
@@ -3234,10 +3235,20 @@ defmodule Hologram.Compiler.TransformerTest do
              }
     end
 
-    test "nested, impproper" do
+    test "nested, impproper (AST from source code)" do
       ast = ast("[1 | [2 | 3]]")
 
       assert transform(ast, %Context{}) == %IR.ConsOperator{
+               head: %IR.IntegerType{value: 1},
+               tail: %IR.ConsOperator{
+                 head: %IR.IntegerType{value: 2},
+                 tail: %IR.IntegerType{value: 3}
+               }
+             }
+    end
+
+    test "nested, impproper (AST from BEAM file)" do
+      assert transform_module_and_fetch_expr(Module65) == %IR.ConsOperator{
                head: %IR.IntegerType{value: 1},
                tail: %IR.ConsOperator{
                  head: %IR.IntegerType{value: 2},
---
+++ test/elixir/support/fixtures/compiler/transformer/module_65.ex
@@ -0,0 +1,6 @@
+# credo:disable-for-this-file Credo.Check.Readability.Specs
+defmodule Hologram.Test.Fixtures.Compiler.Tranformer.Module65 do
+  def test do
+    [1 | [2 | 3]]
+  end
+end
