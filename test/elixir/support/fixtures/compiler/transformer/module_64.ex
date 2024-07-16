8b8d4b71a133356f0c1f177deaf3e6ce3bfb4e88
Bart Blast <bart@bartblast.com>
2024-07-16 16:55:42 +0200

    Ignore Dialyzer warnings about improper list construction

--- test/elixir/support/fixtures/compiler/transformer/module_64.ex
+++ test/elixir/support/fixtures/compiler/transformer/module_64.ex
@@ -1,5 +1,7 @@
 # credo:disable-for-this-file Credo.Check.Readability.Specs
 defmodule Hologram.Test.Fixtures.Compiler.Tranformer.Module64 do
+  @dialyzer {:no_improper_lists, test: 0}
+
   def test do
     [1, 2 | 3]
   end
--- test/elixir/support/fixtures/compiler/transformer/module_65.ex
+++ test/elixir/support/fixtures/compiler/transformer/module_65.ex
@@ -1,5 +1,7 @@
 # credo:disable-for-this-file Credo.Check.Readability.Specs
 defmodule Hologram.Test.Fixtures.Compiler.Tranformer.Module65 do
+  @dialyzer {:no_improper_lists, test: 0}
+
   def test do
     [1 | [2 | 3]]
   end
