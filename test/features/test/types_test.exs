ca230a8ceddb75e47d446f7d9559ba5b6185002a
Bart Blast <bart@bartblast.com>
2024-07-21 15:51:19 +0200

    Test screenshots archiving

--- test/features/test/types_test.exs
+++ test/features/test/types_test.exs
@@ -176,4 +176,11 @@ defmodule HologramFeatureTests.TypesTest do
     |> click(css("button[id='tuple']"))
     |> assert_text(css("#result"), inspect({123, :abc}))
   end
+
+  feature "fail" do
+    session
+    |> visit(TypesPage)
+    |> click(css("button[id='tuple']"))
+    |> assert_text(css("#result"), "fail")
+  end
 end
