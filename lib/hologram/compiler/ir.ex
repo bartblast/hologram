a413c9250b555f476f7a74eb292e6b851c91e588
Bart Blast <bart@bartblast.com>
2024-09-23 23:30:51 +0200

    Fix aggregate_module_funs/1 doc

--- lib/hologram/compiler/ir.ex
+++ lib/hologram/compiler/ir.ex
@@ -316,25 +316,21 @@ defmodule Hologram.Compiler.IR do
   end
 
   @doc """
-  Aggregates function definitions from a module definition into a map.
-
-  This function takes a module definition IR and returns a map where:
-  - Keys are tuples of {function_name, arity}
-  - Values are tuples of {visibility, list_of_clauses}
+  Aggregates function clauses from a module definition.
 
   ## Returns
 
-  A map of function definitions, where each entry is in the format:
+  A list, where each entry is in the format:
   {{function_name, arity}, {visibility, [clause_1, clause_2, ...]}}
 
   ## Example
 
       iex> module_def = %ModuleDefinition{...}
       iex> aggregate_module_funs(module_def)
-      %{
-        {:my_function_1, 3} => {:public, [%IR.FunctionClause{...}, %IR.FunctionClause{...}]},
-        {:my_function_2, 1} => {:private, [%IR.FunctionClause{...}]}
-      }
+      [
+        {{:my_function_1, 3}, {:public, [%IR.FunctionClause{...}, %IR.FunctionClause{...}]}},
+        {{:my_function_2, 1}, {:private, [%IR.FunctionClause{...}]}}
+      ]
   """
   @spec aggregate_module_funs(ModuleDefinition.t()) ::
           list({{atom, non_neg_integer}, {:public | :private, list(FunctionClause.t())}})
