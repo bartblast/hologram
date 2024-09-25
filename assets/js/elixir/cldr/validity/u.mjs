2292bd268c142fd14a9937e67f33d0942875b218
Bart Blast <bart@bartblast.com>
2024-09-25 21:08:29 +0200

    Update comment

--- assets/js/elixir/cldr/validity/u.mjs
+++ assets/js/elixir/cldr/validity/u.mjs
@@ -4,9 +4,7 @@ import HologramInterpreterError from "../../../errors/interpreter_error.mjs";
 import Interpreter from "../../../interpreter.mjs";
 
 const Elixir_Cldr_Validity_U = {
-  // TODO: port manually to JavaScript
-  // {Cldr.Validity.U, :encode_key, 2} transpiles to a huge, deeply nested JS code,
-  // so only a placeholder which raises an error is used here instead (temporarily).
+  // TODO: port manually to JavaScript (transpiled output is too big and deeply nested)
   "encode_key/2": (_key, _value) => {
     throw new HologramInterpreterError(
       Interpreter.buildTooBigOutputErrorMsg(
