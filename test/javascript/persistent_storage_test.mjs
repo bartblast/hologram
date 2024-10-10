04eae96bcb30b9b17a1aef42f268b9e82c5f4718
Bart Blast <bart@bartblast.com>
2024-10-10 16:56:55 +0200

    Test putPageSnapshot()

--- test/javascript/persistent_storage_test.mjs
+++ test/javascript/persistent_storage_test.mjs
@@ -8,11 +8,13 @@ import {
 import PersistentStorage from "../../assets/js/persistent_storage.mjs";
 
 import FDBDatabase from "../../assets/node_modules/fake-indexeddb/build/esm/FDBDatabase.js";
+import FDBOpenDBRequest from "../../assets/node_modules/fake-indexeddb/build/esm/FDBOpenDBRequest.js";
 
 defineGlobalErlangAndElixirModules();
 
 describe("PersistentStorage", () => {
   it("init()", async () => {
+    await PersistentStorage.reset();
     const result = await PersistentStorage.init("dev");
 
     assert.instanceOf(result, FDBDatabase);
@@ -36,5 +38,35 @@ describe("PersistentStorage", () => {
     assert.equal(index.keyPath, "createdAt");
     assert.isFalse(index.multiEntry);
     assert.isFalse(index.unique);
+  });
+
+  it("putPageSnapshot()", async () => {
+    await PersistentStorage.reset();
+    await PersistentStorage.init("dev");
+
+    const data = {a: 1, b: 2};
+    const id = await PersistentStorage.putPageSnapshot(data);
+    assert.equal(id, 1);
+
+    const objects = await new Promise((resolve) => {
+      PersistentStorage.db
+        .transaction(
+          PersistentStorage.PAGE_SNAPSHOTS_OBJ_STORE_NAME,
+          "readonly",
+        )
+        .objectStore(PersistentStorage.PAGE_SNAPSHOTS_OBJ_STORE_NAME)
+        .getAll().onsuccess = (event) => {
+        resolve(event.target.result);
+      };
+    });
+
+    const createdAt = objects[0].createdAt;
+    assert.instanceOf(createdAt, Date);
+    assert.deepStrictEqual(objects, [{data: data, createdAt: createdAt}]);
+
+    const nowMs = Date.now();
+    const createdAtMs = createdAt.getTime();
+    assert.isAbove(nowMs, createdAtMs);
+    assert.isAtMost(Math.abs(nowMs - createdAtMs), 3000);
   });
 });
