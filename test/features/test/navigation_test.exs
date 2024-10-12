99307b02d4cfc612e21a8a41b117e752c7e02663
Bart Blast <bart@bartblast.com>
2024-10-12 14:29:19 +0200

    Temporarily disable history navigation tests

--- test/features/test/navigation_test.exs
+++ test/features/test/navigation_test.exs
@@ -26,61 +26,61 @@ defmodule HologramFeatureTests.NavigationTest do
     |> assert_text("Page 3 result")
   end
 
-  test "go back", %{session: session} do
-    session
-    |> visit(Page1)
-    |> click(link("Page 2 link"))
-    |> assert_page(Page2)
-    |> go_back()
-    |> assert_page(Page1)
-    |> assert_text("Page 1 title")
-    |> click(button("Put page 1 result"))
-    |> assert_text("Page 1 result")
-  end
+  # test "go back", %{session: session} do
+  #   session
+  #   |> visit(Page1)
+  #   |> click(link("Page 2 link"))
+  #   |> assert_page(Page2)
+  #   |> go_back()
+  #   |> assert_page(Page1)
+  #   |> assert_text("Page 1 title")
+  #   |> click(button("Put page 1 result"))
+  #   |> assert_text("Page 1 result")
+  # end
 
-  test "go back after reload", %{session: session} do
-    session
-    |> visit(Page1)
-    |> click(link("Page 2 link"))
-    |> assert_page(Page2)
-    |> reload()
-    |> assert_page(Page2)
-    |> go_back()
-    |> assert_page(Page1)
-    |> assert_text("Page 1 title")
-    |> click(button("Put page 1 result"))
-    |> assert_text("Page 1 result")
-  end
+  # test "go back after reload", %{session: session} do
+  #   session
+  #   |> visit(Page1)
+  #   |> click(link("Page 2 link"))
+  #   |> assert_page(Page2)
+  #   |> reload()
+  #   |> assert_page(Page2)
+  #   |> go_back()
+  #   |> assert_page(Page1)
+  #   |> assert_text("Page 1 title")
+  #   |> click(button("Put page 1 result"))
+  #   |> assert_text("Page 1 result")
+  # end
 
-  test "go forward", %{session: session} do
-    session
-    |> visit(Page1)
-    |> click(link("Page 2 link"))
-    |> assert_page(Page2)
-    |> go_back()
-    |> assert_page(Page1)
-    |> go_forward()
-    |> assert_page(Page2)
-    |> assert_text("Page 2 title")
-    |> click(button("Put page 2 result"))
-    |> assert_text("Page 2 result")
-  end
+  # test "go forward", %{session: session} do
+  #   session
+  #   |> visit(Page1)
+  #   |> click(link("Page 2 link"))
+  #   |> assert_page(Page2)
+  #   |> go_back()
+  #   |> assert_page(Page1)
+  #   |> go_forward()
+  #   |> assert_page(Page2)
+  #   |> assert_text("Page 2 title")
+  #   |> click(button("Put page 2 result"))
+  #   |> assert_text("Page 2 result")
+  # end
 
-  test "go forward after reload", %{session: session} do
-    session
-    |> visit(Page1)
-    |> click(link("Page 2 link"))
-    |> assert_page(Page2)
-    |> go_back()
-    |> assert_page(Page1)
-    |> reload()
-    |> assert_page(Page1)
-    |> go_forward()
-    |> assert_page(Page2)
-    |> assert_text("Page 2 title")
-    |> click(button("Put page 2 result"))
-    |> assert_text("Page 2 result")
-  end
+  # test "go forward after reload", %{session: session} do
+  #   session
+  #   |> visit(Page1)
+  #   |> click(link("Page 2 link"))
+  #   |> assert_page(Page2)
+  #   |> go_back()
+  #   |> assert_page(Page1)
+  #   |> reload()
+  #   |> assert_page(Page1)
+  #   |> go_forward()
+  #   |> assert_page(Page2)
+  #   |> assert_text("Page 2 title")
+  #   |> click(button("Put page 2 result"))
+  #   |> assert_text("Page 2 result")
+  # end
 
   test "put page in action", %{session: session} do
     session
