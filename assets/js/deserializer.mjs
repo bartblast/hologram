f1e05458c157db0762e87938aa746adc751218cb
Bart Blast <bart@bartblast.com>
2024-10-12 15:34:14 +0200

    Deserialize boxed bitstrings that are binaries

--- assets/js/deserializer.mjs
+++ assets/js/deserializer.mjs
@@ -5,8 +5,14 @@ import Type from "./type.mjs";
 export default class Deserializer {
   static deserialize(data) {
     return JSON.parse(data, (_key, value) => {
-      if (typeof value === "string" && value.startsWith("__integer__:")) {
-        return Type.integer(BigInt(value.slice(12)));
+      if (typeof value === "string") {
+        if (value.startsWith("__integer__:")) {
+          return Type.integer(BigInt(value.slice(12)));
+        }
+
+        if (value.startsWith("__binary__:")) {
+          return Type.bitstring(value.slice(11));
+        }
       }
 
       return value;
--- test/javascript/deserializer_test.mjs
+++ test/javascript/deserializer_test.mjs
@@ -21,6 +21,14 @@ describe("Deserializer", () => {
       assert.deepStrictEqual(result, obj);
     });
 
+    it("boxed bitstring that is a binary", () => {
+      const term = Type.bitstring("abc");
+      const data = JsonEncoder.encode(term);
+      const result = Deserializer.deserialize(data);
+
+      assert.deepStrictEqual(result, term);
+    });
+
     it("boxed integer", () => {
       const term = Type.integer(123);
       const data = JsonEncoder.encode(term);
