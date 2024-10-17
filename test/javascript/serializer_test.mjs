4ad58c7eb8e62d486655e7c35f54c51b6aeffe3a
Bart Blast <bart@bartblast.com>
2024-10-17 13:49:21 +0200

    Test boxed atom serialization

--- test/javascript/serializer_test.mjs
+++ test/javascript/serializer_test.mjs
@@ -6,6 +6,7 @@ import {
 } from "./support/helpers.mjs";
 
 import Serializer from "../../assets/js/serializer.mjs";
+import Type from "../../assets/js/type.mjs";
 
 defineGlobalErlangAndElixirModules();
 
@@ -13,18 +14,29 @@ describe("Serializer", () => {
   describe("serialize()", () => {
     const serialize = Serializer.serialize;
 
-    it("JS BigInt", () => {
-      const term = 123n;
-      const expected = "__bigint__:123";
+    describe("boxed terms", () => {
+      it("atom", () => {
+        const term = Type.atom('a"bc');
+        const expected = '{"type":"atom","value":"a\\"bc"}';
 
-      assert.equal(serialize(term), expected);
+        assert.equal(serialize(term), expected);
+      });
     });
 
-    it("JS BigInt nested in object", () => {
-      const term = {a: 123n, b: 2};
-      const expected = '{"a":"__bigint__:123","b":2}';
+    describe("JS terms", () => {
+      it("JS BigInt", () => {
+        const term = 123n;
+        const expected = "__bigint__:123";
 
-      assert.equal(serialize(term), expected);
+        assert.equal(serialize(term), expected);
+      });
+
+      it("JS BigInt nested in object", () => {
+        const term = {a: 123n, b: 2};
+        const expected = '{"a":"__bigint__:123","b":2}';
+
+        assert.equal(serialize(term), expected);
+      });
     });
   });
 });
