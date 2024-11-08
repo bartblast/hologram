d11827d8f606de055e5a42b3e508eca565103bf0
Bart Blast <bart@bartblast.com>
2024-11-08 19:21:24 +0100

    Implement feature tests of case expression

---
+++ test/features/app/pages/control_flow_page.ex
@@ -0,0 +1,170 @@
+defmodule HologramFeatureTests.ControlFlow.CasePage do
+  use Hologram.Page
+
+  import Hologram.Commons.KernelUtils, only: [inspect: 1]
+  import Hologram.Commons.TestUtils, only: [wrap_term: 1]
+  import Kernel, except: [inspect: 1]
+
+  route "/control-flow/case"
+
+  layout HologramFeatureTests.Components.DefaultLayout
+
+  def init(_params, component, _server) do
+    put_state(component, :result, nil)
+  end
+
+  def template do
+    ~H"""
+    <p>
+      <button $click="reset"> Reset </button>
+    </p>
+    <p>
+      <button $click="basic_case"> Basic case </button>
+      <button $click="multiple_expression_condition"> Multiple-expression condition </button>
+      <button $click="multiple_clauses"> Multiple clauses </button>
+      <button $click="multiple_expression_clause_body"> Multiple-expression clause body </button>
+      <button $click="single_guard"> Single guard </button>
+      <button $click="multiple_guards"> Multiple guards </button>
+      <button $click="vars_matching"> Vars matching </button>
+      <button $click="vars_scoping"> Vars scoping </button>
+      <button $click="var_match_in_condition"> Var match in condition </button>
+      <button $click="no_matching_clause"> No matching clause </button>
+      <button $click="error_in_clause_body"> Error in clause body </button>
+    </p>
+    <p>
+      Result: <strong id="result"><code>{inspect(@result)}</code></strong>
+    </p>
+    """
+  end
+
+  # single-expression condition / single clause / single-expression clause body
+  def action(:basic_case, _params, component) do
+    result =
+      case 1 do
+        1 -> :a
+      end
+
+    put_state(component, :result, result)
+  end
+
+  def action(:multiple_expression_condition, _params, component) do
+    result =
+      case (
+             wrap_term(2)
+             1
+           ) do
+        1 -> :a
+      end
+
+    put_state(component, :result, result)
+  end
+
+  def action(:multiple_clauses, _params, component) do
+    result =
+      case 2 do
+        1 -> :a
+        2 -> :b
+        3 -> :c
+      end
+
+    put_state(component, :result, result)
+  end
+
+  def action(:multiple_expression_clause_body, _params, component) do
+    result =
+      case 2 do
+        1 ->
+          :x
+          :a
+
+        2 ->
+          :y
+          :b
+
+        3 ->
+          :z
+          :c
+      end
+
+    put_state(component, :result, result)
+  end
+
+  def action(:single_guard, _params, component) do
+    result =
+      case wrap_term(2) do
+        x when x == 1 -> :a
+        x when x == 2 -> :b
+        x when x == 3 -> :c
+      end
+
+    put_state(component, :result, result)
+  end
+
+  def action(:multiple_guards, _params, component) do
+    result =
+      case wrap_term(25) do
+        x when x > 0 and x < 10 -> :a
+        x when x > 10 and x < 20 -> :b
+        x when x > 10 and x < 30 -> :c
+        x when x > 10 and x < 40 -> :d
+      end
+
+    put_state(component, :result, result)
+  end
+
+  def action(:vars_matching, _params, component) do
+    result =
+      case [2, 3] do
+        [4, 5] -> :a
+        [x, y] -> {1, x, y}
+      end
+
+    put_state(component, :result, result)
+  end
+
+  def action(:vars_scoping, _params, component) do
+    x = 1
+    y = 2
+
+    result =
+      case wrap_term(3) do
+        x when x > 10 -> :a
+        y -> {x, y}
+      end
+
+    put_state(component, :result, {x, y, result})
+  end
+
+  def action(:var_match_in_condition, _params, component) do
+    result =
+      case (
+             x = 1
+             y = 2
+             3
+           ) do
+        x -> {x, y}
+      end
+
+    put_state(component, :result, {x, y, result})
+  end
+
+  def action(:no_matching_clause, _params, component) do
+    _result =
+      case wrap_term(3) do
+        1 -> :a
+        2 -> :b
+      end
+
+    put_state(component, :result, "abc")
+  end
+
+  def action(:error_in_clause_body, _params, _component) do
+    case 1 do
+      1 -> raise ArgumentError, "my message"
+    end
+  end
+
+  def action(:reset, _params, component) do
+    put_state(component, :result, nil)
+  end
+end
---
+++ test/features/test/control_flow/case_test.exs
@@ -0,0 +1,88 @@
+defmodule HologramFeatureTests.ControlFlow.CaseTest do
+  use HologramFeatureTests.TestCase, async: true
+  alias HologramFeatureTests.ControlFlow.CasePage
+
+  # single-expression condition / single clause / single-expression clause body
+  feature "basic case", %{session: session} do
+    session
+    |> visit(CasePage)
+    |> click(button("Basic case"))
+    |> assert_text(css("#result"), ":a")
+  end
+
+  feature "multiple-expression condition", %{session: session} do
+    session
+    |> visit(CasePage)
+    |> click(button("Multiple-expression condition"))
+    |> assert_text(css("#result"), ":a")
+  end
+
+  feature "multiple clauses", %{session: session} do
+    session
+    |> visit(CasePage)
+    |> click(button("Multiple clauses"))
+    |> assert_text(css("#result"), ":b")
+  end
+
+  feature "multiple-expression clause body", %{session: session} do
+    session
+    |> visit(CasePage)
+    |> click(button("Multiple-expression clause body"))
+    |> assert_text(css("#result"), ":b")
+  end
+
+  feature "single guard", %{session: session} do
+    session
+    |> visit(CasePage)
+    |> click(button("Single guard"))
+    |> assert_text(css("#result"), ":b")
+  end
+
+  feature "multiple guards", %{session: session} do
+    session
+    |> visit(CasePage)
+    |> click(button("Multiple guard"))
+    |> assert_text(css("#result"), ":c")
+  end
+
+  feature "vars matching", %{session: session} do
+    session
+    |> visit(CasePage)
+    |> click(button("Vars matching"))
+    |> assert_text(css("#result"), "{1, 2, 3}")
+  end
+
+  feature "vars scoping", %{session: session} do
+    session
+    |> visit(CasePage)
+    |> click(button("Vars scoping"))
+    |> assert_text(css("#result"), "{1, 2, {1, 3}}")
+  end
+
+  feature "var match in condition", %{session: session} do
+    session
+    |> visit(CasePage)
+    |> click(button("Var match in condition"))
+    |> assert_text(css("#result"), "{1, 2, {3, 2}}")
+  end
+
+  feature "no matching clause", %{session: session} do
+    assert_js_error session,
+                    "(CaseClauseError) no case clause matching: 3",
+                    fn ->
+                      session
+                      |> visit(CasePage)
+                      |> click(button("No matching clause"))
+                    end
+  end
+
+  feature "error_in_clause_body", %{session: session} do
+    assert_js_error session,
+                    "(ArgumentError) my message",
+                    fn ->
+                      session
+                      |> visit(CasePage)
+                      |> click(button("Error in clause body"))
+                    end
+  end
+end
