014cafa59f478a9e879e9c1eedc2b4ac62185c84
Bart Blast <bart@bartblast.com>
2024-11-11 22:06:15 +0100

    Implement feature tests for unless expression

---
+++ test/features/app/pages/control_flow/unless_page.ex
@@ -0,0 +1,159 @@
+defmodule HologramFeatureTests.ControlFlow.UnlessPage do
+  use Hologram.Page
+
+  import Hologram.Commons.KernelUtils, only: [inspect: 1]
+  import Kernel, except: [inspect: 1]
+
+  route "/control-flow/unless"
+
+  layout HologramFeatureTests.Components.DefaultLayout
+
+  def init(_params, component, _server) do
+    put_state(component, :result, nil)
+  end
+
+  def template do
+    ~H"""
+    <p>
+      <button $click="reset"> Reset </button>
+    </p>
+    <p>
+      <button $click="basic_case"> Basic case </button>
+      <button $click="multiple_expression_condition"> Multiple-expression condition </button>
+      <button $click="multiple_expression_unless_body"> Multiple-expression unless body </button>
+      <button $click="unmet_condition_no_else_body"> Unmet condition, no else body </button>
+      <button $click="single_expression_else_body"> Single-expression else body </button>
+      <button $click="multiple_expression_else_body"> Multiple-expression else body </button>
+      <button $click="vars_scoping_in_unless_body"> Vars scoping in unless body </button>
+      <button $click="vars_scoping_in_else_body"> Vars scoping in else body </button>
+      <button $click="error_in_condition"> Error in condition </button>
+      <button $click="error_in_unless_body"> Error in unless body </button>
+      <button $click="error_in_else_body"> Error in else body </button>
+    </p>
+    <p>
+      Result: <strong id="result"><code>{inspect(@result)}</code></strong>
+    </p>
+    """
+  end
+
+  # single-expression condition / single-expression unless body / no else expression
+  def action(:basic_case, _params, component) do
+    result =
+      unless false do
+        :a
+      end
+
+    put_state(component, :result, result)
+  end
+
+  def action(:multiple_expression_condition, _params, component) do
+    result =
+      unless (
+               true
+               false
+             ) do
+        :a
+      end
+
+    put_state(component, :result, result)
+  end
+
+  def action(:multiple_expression_unless_body, _params, component) do
+    result =
+      unless false do
+        :x
+        :a
+      end
+
+    put_state(component, :result, result)
+  end
+
+  def action(:unmet_condition_no_else_body, _params, component) do
+    result =
+      unless true do
+        :a
+      end
+
+    put_state(component, :result, result)
+  end
+
+  def action(:single_expression_else_body, _params, component) do
+    result =
+      unless true do
+        :a
+      else
+        :b
+      end
+
+    put_state(component, :result, result)
+  end
+
+  def action(:multiple_expression_else_body, _params, component) do
+    result =
+      unless true do
+        :a
+      else
+        :b
+        :c
+      end
+
+    put_state(component, :result, result)
+  end
+
+  def action(:vars_scoping_in_unless_body, _params, component) do
+    z = 3
+
+    result =
+      unless (
+               x = 1
+               y = 2
+               false
+             ) do
+        x = x + 10
+        {x, y, z}
+      end
+
+    put_state(component, :result, {x, y, z, result})
+  end
+
+  def action(:vars_scoping_in_else_body, _params, component) do
+    z = 3
+
+    result =
+      unless (
+               x = 1
+               y = 2
+             ) do
+        :a
+      else
+        x = x + 10
+        {x, y, z}
+      end
+
+    put_state(component, :result, {x, y, z, result})
+  end
+
+  def action(:error_in_condition, _params, _component) do
+    unless raise ArgumentError, "my message" do
+      :a
+    end
+  end
+
+  def action(:error_in_unless_body, _params, _component) do
+    unless false do
+      raise ArgumentError, "my message"
+    end
+  end
+
+  def action(:error_in_else_body, _params, _component) do
+    unless true do
+      :a
+    else
+      raise ArgumentError, "my message"
+    end
+  end
+
+  def action(:reset, _params, component) do
+    put_state(component, :result, nil)
+  end
+end
---
+++ test/features/test/control_flow/unless_test.exs
@@ -0,0 +1,91 @@
+defmodule HologramFeatureTests.ControlFlow.UnlessTest do
+  use HologramFeatureTests.TestCase, async: true
+  alias HologramFeatureTests.ControlFlow.UnlessPage
+
+  # single-expression condition / single-expression unless body / no else expression
+  feature "basic case", %{session: session} do
+    session
+    |> visit(UnlessPage)
+    |> click(button("Basic case"))
+    |> assert_text(css("#result"), ":a")
+  end
+
+  feature "multiple-expression condition", %{session: session} do
+    session
+    |> visit(UnlessPage)
+    |> click(button("Multiple-expression condition"))
+    |> assert_text(css("#result"), ":a")
+  end
+
+  feature "multiple-expression unless body", %{session: session} do
+    session
+    |> visit(UnlessPage)
+    |> click(button("Multiple-expression unless body"))
+    |> assert_text(css("#result"), ":a")
+  end
+
+  feature "unmet condition, no else body", %{session: session} do
+    session
+    |> visit(UnlessPage)
+    |> click(button("Unmet condition, no else body"))
+    |> assert_text(css("#result"), "nil")
+  end
+
+  feature "single-expression else body", %{session: session} do
+    session
+    |> visit(UnlessPage)
+    |> click(button("Single-expression else body"))
+    |> assert_text(css("#result"), ":b")
+  end
+
+  feature "multiple-expression else body", %{session: session} do
+    session
+    |> visit(UnlessPage)
+    |> click(button("Multiple-expression else body"))
+    |> assert_text(css("#result"), ":c")
+  end
+
+  feature "vars scoping in unless body", %{session: session} do
+    session
+    |> visit(UnlessPage)
+    |> click(button("Vars scoping in unless body"))
+    |> assert_text(css("#result"), "{1, 2, 3, {11, 2, 3}}")
+  end
+
+  feature "vars scoping in else body", %{session: session} do
+    session
+    |> visit(UnlessPage)
+    |> click(button("Vars scoping in else body"))
+    |> assert_text(css("#result"), "{1, 2, 3, {11, 2, 3}}")
+  end
+
+  feature "error in condition", %{session: session} do
+    assert_js_error session,
+                    "(ArgumentError) my message",
+                    fn ->
+                      session
+                      |> visit(UnlessPage)
+                      |> click(button("Error in condition"))
+                    end
+  end
+
+  feature "error in unless body", %{session: session} do
+    assert_js_error session,
+                    "(ArgumentError) my message",
+                    fn ->
+                      session
+                      |> visit(UnlessPage)
+                      |> click(button("Error in unless body"))
+                    end
+  end
+
+  feature "error in else body", %{session: session} do
+    assert_js_error session,
+                    "(ArgumentError) my message",
+                    fn ->
+                      session
+                      |> visit(UnlessPage)
+                      |> click(button("Error in else body"))
+                    end
+  end
+end
