8613061ee708a2575dabc05959973dc056288a50
Bart Blast <bart@bartblast.com>
2024-12-08 19:00:19 +0100

    Implement interpolation feature tests

---
+++ test/features/app/pages/template_markup/interpolation_page.ex
@@ -0,0 +1,14 @@
+defmodule HologramFeatureTests.TemplateMarkup.InterpolationPage do
+  use Hologram.Page
+
+  route "/template-markup/interpolation"
+
+  layout HologramFeatureTests.Components.DefaultLayout
+
+  def template do
+    ~H"""
+    <span class="node_1">a{1 + 1}c</div>
+    <span class="node_{1 + 1}">xyz</span>
+    """
+  end
+end
--- test/features/test/template_markup_test.exs
+++ test/features/test/template_markup_test.exs
@@ -1,11 +1,26 @@
 defmodule HologramFeatureTests.TemplateMarkupTest do
   use HologramFeatureTests.TestCase, async: true
 
+  alias HologramFeatureTests.TemplateMarkup.InterpolationPage
   alias HologramFeatureTests.TemplateMarkup.TextAndElementPage
 
   feature "text and element", %{session: session} do
     session
     |> visit(TextAndElementPage)
     |> assert_has(css("div.parent_elem span.child_elem", text: "my text"))
+  end
+
+  describe "interpolation" do
+    feature "in text", %{session: session} do
+      session
+      |> visit(InterpolationPage)
+      |> assert_has(css("span.node_1", text: "a2c"))
+    end
+
+    feature "in attribute value", %{session: session} do
+      session
+      |> visit(InterpolationPage)
+      |> assert_has(css("span.node_2", text: "xyz"))
+    end
   end
 end
