ff02729e85e25db15604be11263f7e17cdec6144
Bart Blast <bart@bartblast.com>
2024-12-12 22:37:02 +0100

    Implement feature test for page with template defined in file

---
+++ test/features/app/pages/pages/template_file_page.ex
@@ -0,0 +1,7 @@
+defmodule HologramFeatureTests.Pages.TemplateFilePage do
+  use Hologram.Page
+
+  route "/pages/template-file"
+
+  layout HologramFeatureTests.Components.DefaultLayout
+end
--- test/features/test/page_test.exs
+++ test/features/test/page_test.exs
@@ -1,10 +1,18 @@
 defmodule HologramFeatureTests.PagesTest do
   use HologramFeatureTests.TestCase, async: true
+
+  alias HologramFeatureTests.Pages.TemplateFilePage
   alias HologramFeatureTests.Pages.TemplateFunctionPage
 
   feature "template defined in function", %{session: session} do
     session
     |> visit(TemplateFunctionPage)
     |> assert_text("Template defined in function")
+  end
+
+  feature "template defined in file", %{session: session} do
+    session
+    |> visit(TemplateFilePage)
+    |> assert_text("Template defined in file")
   end
 end
