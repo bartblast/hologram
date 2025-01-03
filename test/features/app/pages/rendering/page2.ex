e31bdb05b01813e3b9412c8dd8b1b8e45d2fc3aa
Bart Blast <bart@bartblast.com>
2025-01-03 13:25:22 +0100

    Implement feature test for rendering root element with single attribute

---
+++ test/features/app/pages/rendering/page2.ex
@@ -0,0 +1,27 @@
+defmodule HologramFeatureTests.Rendering.Page2 do
+  use Hologram.Page
+
+  alias Hologram.UI.Runtime
+  alias HologramFeatureTests.Components.CommonLayoutStyles
+
+  route "/rendering/2"
+
+  layout HologramFeatureTests.Components.EmptyLayout
+
+  def template do
+    ~H"""
+    <!DOCTYPE html>
+    <html attr_1="value_1">
+      <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <Runtime />
+        <CommonLayoutStyles />
+      </head>
+      <body>
+        Page2
+      </body>
+    </html>
+    """
+  end
+end
--- test/features/test/rendering_test.exs
+++ test/features/test/rendering_test.exs
@@ -2,10 +2,17 @@ defmodule HologramFeatureTests.RenderingTest do
   use HologramFeatureTests.TestCase, async: true
 
   alias HologramFeatureTests.Rendering.Page1
+  alias HologramFeatureTests.Rendering.Page2
 
   feature "root element without attributes", %{session: session} do
     session
     |> visit(Page1)
     |> assert_has(css("html"))
+  end
+
+  feature "root element with single attribute", %{session: session} do
+    session
+    |> visit(Page2)
+    |> assert_has(css("html[attr_1='value_1']"))
   end
 end
