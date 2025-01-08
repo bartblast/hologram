c576c816843dccc17d6d35f93f8233f11132a6c9
Bart Blast <bart@bartblast.com>
2025-01-08 13:45:34 +0100

    Fix Hologram.env/0

--- lib/hologram.ex
+++ lib/hologram.ex
@@ -1,9 +1,17 @@
 defmodule Hologram do
-  @env Mix.env()
+  @mix_env Mix.env()
 
   @doc """
   Returns the current environment.
   """
   @spec env() :: atom
-  def env, do: @env
+  def env do
+    regex = ~r"^.+/_build/([^/]+)/.+$"
+    lib_dir = to_string(:code.lib_dir(:hologram))
+
+    case Regex.run(regex, lib_dir) do
+      [_lib_dir, env] -> String.to_existing_atom(env)
+      _fallback -> @mix_env
+    end
+  end
 end
