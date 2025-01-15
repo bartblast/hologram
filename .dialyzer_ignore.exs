4ba769c80647f3b7a16f7f1dfde0686a4b755bcd
Bart Blast <bart@bartblast.com>
2025-01-15 17:03:23 +0100

    Ignore vendored libgraph Dialyzer warnings

---
+++ .dialyzer_ignore.exs
@@ -0,0 +1,8 @@
+[
+  {"lib/libgraph/graph.ex", "Invalid type specification for function bellman_ford."},
+  {"lib/libgraph/graph.ex", "Invalid type specification for function a_star."},
+  {"lib/libgraph/graph.ex", "Invalid type specification for function in_edges."},
+  {"lib/libgraph/graph.ex", "Invalid type specification for function out_edges."},
+  {"lib/libgraph/graph/pathfindings/bellman_ford.ex",
+   "Invalid type specification for function edge_weight."}
+]
