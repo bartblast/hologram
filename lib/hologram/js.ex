274973a45ab539cc79a6c4b64d04fc103e55c896
Bart Blast <bart@bartblast.com>
2025-01-19 19:55:15 +0100

    Add missing spec

--- lib/hologram/js.ex
+++ lib/hologram/js.ex
@@ -14,5 +14,6 @@ defmodule Hologram.JS do
   Provides a convenient syntax for executing JavaScript code using the ~JS sigil.
   """
   @doc since: "1.15.0"
+  @spec sigil_JS(String.t(), []) :: String.t()
   def sigil_JS(code, []), do: exec(code)
 end
