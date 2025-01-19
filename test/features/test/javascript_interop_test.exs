f3bb83c1eca0ca7d79627c615f0f7de621e30382
Bart Blast <bart@bartblast.com>
2025-01-19 23:03:38 +0100

    Fix unused alias warning

--- test/features/test/javascript_interop_test.exs
+++ test/features/test/javascript_interop_test.exs
@@ -1,10 +1,11 @@
 defmodule HologramFeatureTests.JavaScriptInteropTest do
   use HologramFeatureTests.TestCase, async: true
-  alias HologramFeatureTests.JavaScriptInteropPage
 
   # Multi-letter uppercase sigils were introduced in Elixir 1.15.0,
   # see: https://github.com/elixir-lang/elixir/blob/v1.15/CHANGELOG.md
   if Version.match?(System.version(), ">= 1.15.0") do
+    alias HologramFeatureTests.JavaScriptInteropPage
+
     feature "~JS sigil", %{session: session} do
       session
       |> visit(JavaScriptInteropPage)
