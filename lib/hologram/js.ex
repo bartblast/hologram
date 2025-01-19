b9238b81f72a5c54c40dbad221d6cd8658f803f8
Bart Blast <bart@bartblast.com>
2025-01-19 17:44:50 +0100

    Implement ~JS sigil

--- lib/hologram/js.ex
+++ lib/hologram/js.ex
@@ -7,6 +7,14 @@ defmodule Hologram.JS do
   Executes JavaScript code.
   Server-side implementation is just a dummy. The actual implementation is on the client-side.
   """
-  @spec exec(String.t()) :: nil
-  def exec(_code), do: nil
+  @spec exec(String.t()) :: String.t()
+  def exec(code), do: code
+
+  if Version.compare(System.version(), "1.15.0") in [:gt, :eq] do
+    @doc """
+    Provides a convenient syntax for executing JavaScript code using the ~JS sigil.
+    """
+    @doc since: "1.15.0"
+    def sigil_JS(code, []), do: exec(code)
+  end
 end
--- test/elixir/hologram/js_test.exs
+++ test/elixir/hologram/js_test.exs
@@ -3,6 +3,13 @@ defmodule Hologram.JSTest do
   import Hologram.JS
 
   test "exec/1" do
-    assert exec("console.log('Hello, world!');") == nil
+    code = "console.log('Hello, world!');"
+    assert exec(code) == code
+  end
+
+  if Version.compare(System.version(), "1.15.0") in [:gt, :eq] do
+    test "sigil_JS/1" do
+      assert ~JS"console.log('Hello, world!');" == "console.log('Hello, world!');"
+    end
   end
 end
