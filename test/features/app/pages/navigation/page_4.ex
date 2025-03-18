973ecd654b1480b841d3d6fb0e103babdf0acbcf
Bart Blast <bart@bartblast.com>
2025-03-18 18:43:13 +0100

    Change link text

--- test/features/app/pages/navigation/page_4.ex
+++ test/features/app/pages/navigation/page_4.ex
@@ -13,7 +13,7 @@ defmodule HologramFeatureTests.Navigation.Page4 do
     <h1>Page 4 title</h1>
     <div style="width: 10000px; height: 500px; background-color: blue"></div>
     <Link to={Page2} style="margin-left: 500px">Page 2 link</Link>
-    <a href="https://www.wikipedia.org/">Wikipedia</a>
+    <a href="https://www.wikipedia.org/">External link</a>
     <div style="width: 10000px; height: 10000px; background-color: blue"></div>
     """
   end
