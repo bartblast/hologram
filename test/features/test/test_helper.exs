12af27228092541998b3f2497873624c1437387e
Bart Blast <bart@bartblast.com>
2025-03-25 14:32:05 +0100

    Set max tests cases to 2 in feature tests when running GitHub Actions

--- test/features/test/test_helper.exs
+++ test/features/test/test_helper.exs
@@ -1,4 +1,7 @@
-ExUnit.configure(max_cases: 1)
+if System.get_env("GITHUB_ACTIONS") == "true" do
+  ExUnit.configure(max_cases: 2)
+end
+
 ExUnit.start()
 
 {:ok, _apps} = Application.ensure_all_started(:wallaby)
