dfd0d344e9a36553a84be271e7d86019043f9647
Bart Blast <bart@bartblast.com>
2025-03-27 18:05:00 +0100

    Benchmark Type.bitstring() with a string consisting of 100 Unicode chars arg

---
+++ benchmarks/javascript/type_bitstring_100_unicode/run.mjs
@@ -0,0 +1,11 @@
+"use strict";
+
+import Type from "../../../assets/js/type.mjs";
+
+import {benchmark} from "../support/helpers.mjs";
+
+benchmark(() => {
+  Type.bitstring(
+    "全息图全息图全息图全全息图全息图全息图全全息图全息图全息图全全息图全息图全息图全全息图全息图全息图全全息图全息图全息图全全息图全息图全息图全全息图全息图全息图全全息图全息图全息图全全息图全息图全息图全",
+  );
+});
