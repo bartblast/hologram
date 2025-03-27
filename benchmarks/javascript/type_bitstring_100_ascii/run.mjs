969058265a4bf1386d7bd9536ceb8636004c1974
Bart Blast <bart@bartblast.com>
2025-03-27 17:55:41 +0100

    Benchmark Type.bitstring() with a string consisting of 100 ASCII chars arg

---
+++ benchmarks/javascript/type_bitstring_100_ascii/run.mjs
@@ -0,0 +1,11 @@
+"use strict";
+
+import Type from "../../../assets/js/type.mjs";
+
+import {benchmark} from "../support/helpers.mjs";
+
+benchmark(() => {
+  Type.bitstring(
+    "abcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghijabcdefghij",
+  );
+});
