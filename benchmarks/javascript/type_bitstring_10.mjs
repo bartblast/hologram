d4d41199c1f0b7bb9a59962675e0071fbc80ec91
Bart Blast <bart@bartblast.com>
2025-03-27 14:39:56 +0100

    Implement JS benchmark helper

---
+++ benchmarks/javascript/type_bitstring_10.mjs
@@ -0,0 +1,10 @@
+"use strict";
+
+import Type from "../../assets/js/type.mjs"
+
+import { benchmark } from "./support/helpers.mjs";
+
+benchmark(() => {
+  // Type.bitstring("abcdefghij");
+  Type.atom("abcdefghij");
+});
