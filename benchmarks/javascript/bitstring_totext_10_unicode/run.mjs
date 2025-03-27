fe31b626b73ff4062735c73b96c4fe7ea80a1f76
Bart Blast <bart@bartblast.com>
2025-03-27 19:17:07 +0100

    Benchmark Bitstring.toText() with a bitstring consisting of 10 Unicode chars arg

---
+++ benchmarks/javascript/bitstring_totext_10_unicode/run.mjs
@@ -0,0 +1,12 @@
+"use strict";
+
+import Bitstring from "../../../assets/js/bitstring.mjs";
+import Type from "../../../assets/js/type.mjs";
+
+import {benchmark} from "../support/helpers.mjs";
+
+const bitstring = Type.bitstring("全息图全息图全息图全");
+
+benchmark(() => {
+  Bitstring.toText(bitstring);
+});
