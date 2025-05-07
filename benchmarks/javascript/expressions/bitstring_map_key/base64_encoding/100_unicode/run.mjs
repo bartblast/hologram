c8042666543b9a9e799e7099927290aef5745d4e
Bart Blast <bart@bartblast.com>
2025-05-07 12:50:15 +0200

    Benchmark different encodings of bitstring map keys

---
+++ benchmarks/javascript/expressions/bitstring_map_key/base64_encoding/100_unicode/run.mjs
@@ -0,0 +1,22 @@
+"use strict";
+
+import Bitstring2 from "../../../../../../assets/js/bitstring2.mjs";
+import Type from "../../../../../../assets/js/type.mjs";
+
+import {benchmark} from "../../../../support/helpers.mjs";
+
+const bitstring = Type.bitstring2(
+  "全息图全息图全息图全全息图全息图全息图全全息图全息图全息图全全息图全息图全息图全全息图全息图全息图全全息图全息图全息图全全息图全息图全息图全全息图全息图全息图全全息图全息图全息图全全息图全息图全息图全",
+);
+
+Bitstring2.maybeSetBytesFromText(bitstring);
+
+benchmark(() => {
+  let binaryString = "";
+
+  for (let i = 0; i < bitstring.bytes.length; i++) {
+    binaryString += String.fromCharCode(bitstring.bytes[i]);
+  }
+
+  btoa(binaryString);
+});
---
+++ benchmarks/javascript/expressions/bitstring_map_key/hex_encoding/100_unicode/run.mjs
@@ -0,0 +1,20 @@
+"use strict";
+
+import Bitstring2 from "../../../../../../assets/js/bitstring2.mjs";
+import Type from "../../../../../../assets/js/type.mjs";
+
+import {benchmark} from "../../../../support/helpers.mjs";
+
+const bitstring = Type.bitstring2(
+  "全息图全息图全息图全全息图全息图全息图全全息图全息图全息图全全息图全息图全息图全全息图全息图全息图全全息图全息图全息图全全息图全息图全息图全全息图全息图全息图全全息图全息图全息图全全息图全息图全息图全",
+);
+
+Bitstring2.maybeSetBytesFromText(bitstring);
+
+benchmark(() => {
+  let hexString = "";
+
+  for (let i = 0; i < bitstring.bytes.length; i++) {
+    hexString += bitstring.bytes[i].toString(16);
+  }
+});
