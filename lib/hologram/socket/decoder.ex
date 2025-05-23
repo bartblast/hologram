4f46ccf8945269339afe1df32fbedd9365497db9
Bart Blast <bart@bartblast.com>
2025-05-23 18:54:21 +0200

    Use serialization format version in server decoder

--- lib/hologram/socket/decoder.ex
+++ lib/hologram/socket/decoder.ex
@@ -18,16 +18,25 @@ defmodule Hologram.Socket.Decoder do
   def atoms_whitelist, do: @atoms_whitelist
 
   @doc """
-  Decodes a term serialized by the client and pre-decoded from JSON.
+  Decodes the top-level term serialized by the client that has been pre-decoded from JSON.
+  The term is expected to be a list in the format [version, data] where version is an integer
+  and data is the serialized content to be decoded. This is the entry point for decoding,
+  which then recursively uses decode/2 for nested structures.
   """
-  @spec decode(map | String.t()) :: any
-  def decode(term)
+  @spec decode(list()) :: any
+  def decode(list)
 
-  def decode([_version, data]) do
-    decode(data)
+  def decode([version, data]) do
+    decode(version, data)
   end
 
-  def decode(%{
+  @doc """
+  Decodes a term serialized by the client and pre-decoded from JSON.
+  """
+  @spec decode(integer, map | String.t()) :: any
+  def decode(version, term)
+
+  def decode(_version, %{
         "type" => "anonymous_function",
         "capturedModule" => module_str,
         "capturedFunction" => function_str,
@@ -39,53 +48,53 @@ defmodule Hologram.Socket.Decoder do
     Function.capture(module, function, arity)
   end
 
-  def decode("__atom__:" <> value) do
+  def decode(version, %{"type" => "list", "data" => data}) do
+    Enum.map(data, &decode(version, &1))
+  end
+
+  def decode(_version, %{"type" => "pid", "segments" => [x, y, z]}) do
+    IEx.Helpers.pid(x, y, z)
+  end
+
+  def decode(_version, %{"type" => "port", "value" => value}) do
+    IEx.Helpers.port(value)
+  end
+
+  def decode(_version, %{"type" => "reference", "value" => value}) do
+    IEx.Helpers.ref(value)
+  end
+
+  def decode(version, %{"type" => "tuple", "data" => data}) do
+    data
+    |> Enum.map(&decode(version, &1))
+    |> List.to_tuple()
+  end
+
+  def decode(1, "__atom__:" <> value) do
     String.to_existing_atom(value)
   end
 
-  def decode("__binary__:" <> value) do
+  def decode(1, "__binary__:" <> value) do
     value
   end
 
-  def decode(%{"type" => "bitstring", "bits" => bits}) do
+  def decode(1, %{"type" => "bitstring", "bits" => bits}) do
     BitstringUtils.from_bit_list(bits)
   end
 
-  def decode("__float__:" <> value) do
+  def decode(1, "__float__:" <> value) do
     value
     |> Float.parse()
     |> elem(0)
   end
 
-  def decode("__integer__:" <> value) do
+  def decode(1, "__integer__:" <> value) do
     IntegerUtils.parse!(value)
   end
 
-  def decode(%{"type" => "list", "data" => data}) do
-    Enum.map(data, &decode/1)
-  end
-
-  def decode(%{"type" => "map", "data" => data}) do
+  def decode(1, %{"type" => "map", "data" => data}) do
     data
-    |> Enum.map(fn [key, value] -> {decode(key), decode(value)} end)
+    |> Enum.map(fn [key, value] -> {decode(1, key), decode(1, value)} end)
     |> Enum.into(%{})
-  end
-
-  def decode(%{"type" => "pid", "segments" => [x, y, z]}) do
-    IEx.Helpers.pid(x, y, z)
-  end
-
-  def decode(%{"type" => "port", "value" => value}) do
-    IEx.Helpers.port(value)
-  end
-
-  def decode(%{"type" => "reference", "value" => value}) do
-    IEx.Helpers.ref(value)
-  end
-
-  def decode(%{"type" => "tuple", "data" => data}) do
-    data
-    |> Enum.map(&decode/1)
-    |> List.to_tuple()
   end
 end
--- test/elixir/hologram/socket/decoder_test.exs
+++ test/elixir/hologram/socket/decoder_test.exs
@@ -2,78 +2,79 @@ defmodule Hologram.Socket.DecoderTest do
   use Hologram.Test.BasicCase, async: true
   import Hologram.Socket.Decoder
 
-  test "top-level data" do
-    assert decode([1, "__atom__:abc"]) == :abc
-  end
+  describe "version 1" do
+    test "top-level data" do
+      assert decode([1, "__atom__:abc"]) == :abc
+    end
 
-  test "anonymous function" do
-    assert decode(%{
-             "type" => "anonymous_function",
-             "capturedModule" => "Calendar.ISO",
-             "capturedFunction" => "parse_date",
-             "arity" => 2
-           }) == (&Calendar.ISO.parse_date/2)
-  end
+    test "anonymous function" do
+      assert decode(1, %{
+               "type" => "anonymous_function",
+               "capturedModule" => "Calendar.ISO",
+               "capturedFunction" => "parse_date",
+               "arity" => 2
+             }) == (&Calendar.ISO.parse_date/2)
+    end
 
-  test "atom" do
-    assert decode("__atom__:abc") == :abc
-  end
+    test "atom" do
+      assert decode(1, "__atom__:abc") == :abc
+    end
 
-  test "binary bitstring" do
-    assert decode("__binary__:a\"bc") == "a\"bc"
-  end
+    test "binary bitstring" do
+      assert decode(1, "__binary__:a\"bc") == "a\"bc"
+    end
 
-  test "non-binary bitstring" do
-    assert decode(%{"type" => "bitstring", "bits" => [1, 0, 1, 0]}) == <<1::1, 0::1, 1::1, 0::1>>
-  end
+    test "non-binary bitstring" do
+      assert decode(1, %{"type" => "bitstring", "bits" => [1, 0, 1, 0]}) ==
+               <<1::1, 0::1, 1::1, 0::1>>
+    end
 
-  describe "float" do
-    test "float" do
-      assert decode("__float__:1.23") === 1.23
+    test "float, encoded as float" do
+      assert decode(1, "__float__:1.23") === 1.23
+    end
+
+    test "float, encoded as integer" do
+      assert decode(1, "__float__:1") === 1.0
     end
 
     test "integer" do
-      assert decode("__float__:1") === 1.0
+      assert decode(1, "__integer__:123") == 123
     end
-  end
 
-  test "integer" do
-    assert decode("__integer__:123") == 123
-  end
+    test "list" do
+      assert decode(1, %{
+               "type" => "list",
+               "data" => ["__integer__:1", "__float__:2.34"]
+             }) == [1, 2.34]
+    end
 
-  test "list" do
-    assert decode(%{
-             "type" => "list",
-             "data" => ["__integer__:1", "__float__:2.34"]
-           }) == [1, 2.34]
-  end
+    test "map" do
+      assert decode(1, %{
+               "type" => "map",
+               "data" => [
+                 ["__atom__:a", "__integer__:1"],
+                 ["__binary__:b", "__float__:2.34"]
+               ]
+             }) == %{:a => 1, "b" => 2.34}
+    end
 
-  test "map" do
-    assert decode(%{
-             "type" => "map",
-             "data" => [
-               ["__atom__:a", "__integer__:1"],
-               ["__binary__:b", "__float__:2.34"]
-             ]
-           }) == %{:a => 1, "b" => 2.34}
-  end
+    test "pid" do
+      assert decode(1, %{"type" => "pid", "segments" => [0, 11, 222]}) == pid("0.11.222")
+    end
 
-  test "pid" do
-    assert decode(%{"type" => "pid", "segments" => [0, 11, 222]}) == pid("0.11.222")
-  end
+    test "port" do
+      assert decode(1, %{"type" => "port", "value" => "0.11"}) == port("0.11")
+    end
 
-  test "port" do
-    assert decode(%{"type" => "port", "value" => "0.11"}) == port("0.11")
-  end
+    test "reference" do
+      assert decode(1, %{"type" => "reference", "value" => "0.1.2.3"}) == ref("0.1.2.3")
+    end
 
-  test "reference" do
-    assert decode(%{"type" => "reference", "value" => "0.1.2.3"}) == ref("0.1.2.3")
-  end
-
-  test "tuple" do
-    assert decode(%{
-             "type" => "tuple",
-             "data" => ["__integer__:1", "__float__:2.34"]
-           }) == {1, 2.34}
+    test "tuple" do
+      assert decode(1, %{
+               "type" => "tuple",
+               "data" => ["__integer__:1", "__float__:2.34"]
+             }) == {1, 2.34}
+    end
   end
 end
