29fa9eccd5abe258b67d4ee33f76b3fc4a736b62
Bart Blast <bart@bartblast.com>
2025-05-24 15:03:46 +0200

    Test deserializing boxed anonymous functions encoded in v1 serialization format

--- test/javascript/deserializer_test.mjs
+++ test/javascript/deserializer_test.mjs
@@ -62,134 +62,9 @@ function testTopLevelBitstringDeserialization(term) {
   assert.isTrue(Interpreter.isStrictlyEqual(deserialized, term));
 }
 
-describe("Deserializer", () => {
+describe.only("Deserializer", () => {
   describe("deserialize()", () => {
-    describe("boxed terms", () => {
-      describe("anonymous_function", () => {
-        const term = Type.anonymousFunction(
-          1,
-          [
-            {
-              params: (_context) => [Type.variablePattern("x")],
-              guards: [
-                (context) => Erlang["/=/2"](context.vars.x, Type.integer(1)),
-              ],
-              // prettier-ignore
-              body: (context) => { return Type.list([Type.atom("a"), context.vars.x, context.vars.i]) },
-            },
-            {
-              params: (_context) => [Type.variablePattern("x")],
-              guards: [
-                (context) => Erlang["/=/2"](context.vars.x, Type.integer(2)),
-              ],
-              // prettier-ignore
-              body: (context) => { return Type.list([Type.atom("b"), context.vars.x, context.vars.j]) },
-            },
-          ],
-          contextFixture({vars: {i: Type.integer(10), j: Type.integer(20)}}),
-        );
-
-        it("top-level", () => {
-          const serialized = serialize(term);
-          const deserialized = deserialize(serialized);
-
-          assert.deepStrictEqual(deserialized, {
-            ...term,
-            clauses: deserialized.clauses,
-          });
-
-          assert.equal(deserialized.clauses.length, 2);
-
-          assert.isFunction(deserialized.clauses[0].params);
-          assert.isFunction(deserialized.clauses[0].guards[0]);
-          assert.isFunction(deserialized.clauses[0].body);
-
-          assert.isFunction(deserialized.clauses[1].params);
-          assert.isFunction(deserialized.clauses[1].guards[0]);
-          assert.isFunction(deserialized.clauses[1].body);
-
-          const callResult = Interpreter.callAnonymousFunction(deserialized, [
-            Type.integer(1),
-          ]);
-
-          const expectedCallResult = Type.list([
-            Type.atom("b"),
-            Type.integer(1),
-            Type.integer(20),
-          ]);
-
-          assert.deepStrictEqual(callResult, expectedCallResult);
-        });
-
-        it("nested", () => {
-          const nestedTerm = {x: term, y: 2};
-          const serialized = serialize(nestedTerm);
-          const deserialized = deserialize(serialized);
-
-          assert.deepStrictEqual(deserialized, {
-            x: {
-              ...term,
-              clauses: deserialized.x.clauses,
-            },
-            y: 2,
-          });
-
-          assert.equal(deserialized.x.clauses.length, 2);
-
-          assert.isFunction(deserialized.x.clauses[0].params);
-          assert.isFunction(deserialized.x.clauses[0].guards[0]);
-          assert.isFunction(deserialized.x.clauses[0].body);
-
-          assert.isFunction(deserialized.x.clauses[1].params);
-          assert.isFunction(deserialized.x.clauses[1].guards[0]);
-          assert.isFunction(deserialized.x.clauses[1].body);
-
-          const callResult = Interpreter.callAnonymousFunction(deserialized.x, [
-            Type.integer(1),
-          ]);
-
-          const expectedCallResult = Type.list([
-            Type.atom("b"),
-            Type.integer(1),
-            Type.integer(20),
-          ]);
-
-          assert.deepStrictEqual(callResult, expectedCallResult);
-        });
-
-        it("not versioned", () => {
-          const serialized = serialize(term, true, false);
-          const deserialized = deserialize(serialized, false);
-
-          assert.deepStrictEqual(deserialized, {
-            ...term,
-            clauses: deserialized.clauses,
-          });
-
-          assert.equal(deserialized.clauses.length, 2);
-
-          assert.isFunction(deserialized.clauses[0].params);
-          assert.isFunction(deserialized.clauses[0].guards[0]);
-          assert.isFunction(deserialized.clauses[0].body);
-
-          assert.isFunction(deserialized.clauses[1].params);
-          assert.isFunction(deserialized.clauses[1].guards[0]);
-          assert.isFunction(deserialized.clauses[1].body);
-
-          const callResult = Interpreter.callAnonymousFunction(deserialized, [
-            Type.integer(1),
-          ]);
-
-          const expectedCallResult = Type.list([
-            Type.atom("b"),
-            Type.integer(1),
-            Type.integer(20),
-          ]);
-
-          assert.deepStrictEqual(callResult, expectedCallResult);
-        });
-      });
-
+    describe("OVERHAUL: boxed terms", () => {
       describe("float", () => {
         const term = Type.float(1.23);
 
@@ -322,7 +197,7 @@ describe("Deserializer", () => {
       });
     });
 
-    describe("JS terms", () => {
+    describe("OVERHAUL: JS terms", () => {
       describe("array", () => {
         const term = [123, Type.float(2.34), Type.atom("abc")];
 
@@ -519,6 +394,132 @@ describe("Deserializer", () => {
 
     describe("version 2 (current)", () => {
       describe("boxed terms", () => {
+        describe("anonymous_function", () => {
+          const term = Type.anonymousFunction(
+            1,
+            [
+              {
+                params: (_context) => [Type.variablePattern("x")],
+                guards: [
+                  (context) => Erlang["/=/2"](context.vars.x, Type.integer(1)),
+                ],
+                // prettier-ignore
+                body: (context) => { return Type.list([Type.atom("a"), context.vars.x, context.vars.i]) },
+              },
+              {
+                params: (_context) => [Type.variablePattern("x")],
+                guards: [
+                  (context) => Erlang["/=/2"](context.vars.x, Type.integer(2)),
+                ],
+                // prettier-ignore
+                body: (context) => { return Type.list([Type.atom("b"), context.vars.x, context.vars.j]) },
+              },
+            ],
+            contextFixture({vars: {i: Type.integer(10), j: Type.integer(20)}}),
+          );
+
+          it("top-level", () => {
+            const serialized = serialize(term);
+            const deserialized = deserialize(serialized);
+
+            assert.deepStrictEqual(deserialized, {
+              ...term,
+              clauses: deserialized.clauses,
+            });
+
+            assert.equal(deserialized.clauses.length, 2);
+
+            assert.isFunction(deserialized.clauses[0].params);
+            assert.isFunction(deserialized.clauses[0].guards[0]);
+            assert.isFunction(deserialized.clauses[0].body);
+
+            assert.isFunction(deserialized.clauses[1].params);
+            assert.isFunction(deserialized.clauses[1].guards[0]);
+            assert.isFunction(deserialized.clauses[1].body);
+
+            const callResult = Interpreter.callAnonymousFunction(deserialized, [
+              Type.integer(1),
+            ]);
+
+            const expectedCallResult = Type.list([
+              Type.atom("b"),
+              Type.integer(1),
+              Type.integer(20),
+            ]);
+
+            assert.deepStrictEqual(callResult, expectedCallResult);
+          });
+
+          it("nested", () => {
+            const nestedTerm = {x: term, y: 2};
+            const serialized = serialize(nestedTerm);
+            const deserialized = deserialize(serialized);
+
+            assert.deepStrictEqual(deserialized, {
+              x: {
+                ...term,
+                clauses: deserialized.x.clauses,
+              },
+              y: 2,
+            });
+
+            assert.equal(deserialized.x.clauses.length, 2);
+
+            assert.isFunction(deserialized.x.clauses[0].params);
+            assert.isFunction(deserialized.x.clauses[0].guards[0]);
+            assert.isFunction(deserialized.x.clauses[0].body);
+
+            assert.isFunction(deserialized.x.clauses[1].params);
+            assert.isFunction(deserialized.x.clauses[1].guards[0]);
+            assert.isFunction(deserialized.x.clauses[1].body);
+
+            const callResult = Interpreter.callAnonymousFunction(
+              deserialized.x,
+              [Type.integer(1)],
+            );
+
+            const expectedCallResult = Type.list([
+              Type.atom("b"),
+              Type.integer(1),
+              Type.integer(20),
+            ]);
+
+            assert.deepStrictEqual(callResult, expectedCallResult);
+          });
+
+          it("not versioned", () => {
+            const serialized = serialize(term, true, false);
+            const deserialized = deserialize(serialized, false);
+
+            assert.deepStrictEqual(deserialized, {
+              ...term,
+              clauses: deserialized.clauses,
+            });
+
+            assert.equal(deserialized.clauses.length, 2);
+
+            assert.isFunction(deserialized.clauses[0].params);
+            assert.isFunction(deserialized.clauses[0].guards[0]);
+            assert.isFunction(deserialized.clauses[0].body);
+
+            assert.isFunction(deserialized.clauses[1].params);
+            assert.isFunction(deserialized.clauses[1].guards[0]);
+            assert.isFunction(deserialized.clauses[1].body);
+
+            const callResult = Interpreter.callAnonymousFunction(deserialized, [
+              Type.integer(1),
+            ]);
+
+            const expectedCallResult = Type.list([
+              Type.atom("b"),
+              Type.integer(1),
+              Type.integer(20),
+            ]);
+
+            assert.deepStrictEqual(callResult, expectedCallResult);
+          });
+        });
+
         describe("atom", () => {
           const term = Type.atom("abc");
 
@@ -589,7 +590,112 @@ describe("Deserializer", () => {
 
     describe("old versions", () => {
       describe("version 1", () => {
-        describe("boxded terms", () => {
+        describe("boxed terms", () => {
+          describe("anonymous_function", () => {
+            const term = Type.anonymousFunction(
+              1,
+              [
+                {
+                  params: (_context) => [Type.variablePattern("x")],
+                  guards: [
+                    (context) =>
+                      Erlang["/=/2"](context.vars.x, Type.integer(1)),
+                  ],
+                  // prettier-ignore
+                  body: (context) => { return Type.list([Type.atom("a"), context.vars.x, context.vars.i]) },
+                },
+                {
+                  params: (_context) => [Type.variablePattern("x")],
+                  guards: [
+                    (context) =>
+                      Erlang["/=/2"](context.vars.x, Type.integer(2)),
+                  ],
+                  // prettier-ignore
+                  body: (context) => { return Type.list([Type.atom("b"), context.vars.x, context.vars.j]) },
+                },
+              ],
+              contextFixture({
+                vars: {i: Type.integer(10), j: Type.integer(20)},
+              }),
+            );
+
+            it("top-level", () => {
+              const serialized =
+                '[1,{"type":"anonymous_function","arity":1,"capturedFunction":null,"capturedModule":null,"clauses":[{"params":"__function__:(_context) => [Type.variablePattern(\\"x\\")]","guards":["__function__:(context) =>\\n                      Erlang[\\"/=/2\\"](context.vars.x, Type.integer(1))"],"body":"__function__:(context) => { return Type.list([Type.atom(\\"a\\"), context.vars.x, context.vars.i]) }"},{"params":"__function__:(_context) => [Type.variablePattern(\\"x\\")]","guards":["__function__:(context) =>\\n                      Erlang[\\"/=/2\\"](context.vars.x, Type.integer(2))"],"body":"__function__:(context) => { return Type.list([Type.atom(\\"b\\"), context.vars.x, context.vars.j]) }"}],"context":{"module":"__atom__:Elixir.MyModule","vars":{"i":"__integer__:10","j":"__integer__:20"}},"uniqueId":2}]';
+              const deserialized = deserialize(serialized);
+
+              assert.deepStrictEqual(deserialized, {
+                ...term,
+                clauses: deserialized.clauses,
+              });
+
+              assert.equal(deserialized.clauses.length, 2);
+
+              assert.isFunction(deserialized.clauses[0].params);
+              assert.isFunction(deserialized.clauses[0].guards[0]);
+              assert.isFunction(deserialized.clauses[0].body);
+
+              assert.isFunction(deserialized.clauses[1].params);
+              assert.isFunction(deserialized.clauses[1].guards[0]);
+              assert.isFunction(deserialized.clauses[1].body);
+
+              const callResult = Interpreter.callAnonymousFunction(
+                deserialized,
+                [Type.integer(1)],
+              );
+
+              const expectedCallResult = Type.list([
+                Type.atom("b"),
+                Type.integer(1),
+                Type.integer(20),
+              ]);
+
+              assert.deepStrictEqual(callResult, expectedCallResult);
+            });
+
+            it("nested", () => {
+              // nestedTerm = {x: term, y: 2};
+              const serialized =
+                '[1,{"x":{"type":"anonymous_function","arity":1,"capturedFunction":null,"capturedModule":null,"clauses":[{"params":"__function__:(_context) => [Type.variablePattern(\\"x\\")]","guards":["__function__:(context) =>\\n                      Erlang[\\"/=/2\\"](context.vars.x, Type.integer(1))"],"body":"__function__:(context) => { return Type.list([Type.atom(\\"a\\"), context.vars.x, context.vars.i]) }"},{"params":"__function__:(_context) => [Type.variablePattern(\\"x\\")]","guards":["__function__:(context) =>\\n                      Erlang[\\"/=/2\\"](context.vars.x, Type.integer(2))"],"body":"__function__:(context) => { return Type.list([Type.atom(\\"b\\"), context.vars.x, context.vars.j]) }"}],"context":{"module":"__atom__:Elixir.MyModule","vars":{"i":"__integer__:10","j":"__integer__:20"}},"uniqueId":2},"y":2}]';
+
+              const deserialized = deserialize(serialized);
+
+              assert.deepStrictEqual(deserialized, {
+                x: {
+                  ...term,
+                  clauses: deserialized.x.clauses,
+                },
+                y: 2,
+              });
+
+              assert.equal(deserialized.x.clauses.length, 2);
+
+              assert.isFunction(deserialized.x.clauses[0].params);
+              assert.isFunction(deserialized.x.clauses[0].guards[0]);
+              assert.isFunction(deserialized.x.clauses[0].body);
+
+              assert.isFunction(deserialized.x.clauses[1].params);
+              assert.isFunction(deserialized.x.clauses[1].guards[0]);
+              assert.isFunction(deserialized.x.clauses[1].body);
+
+              const callResult = Interpreter.callAnonymousFunction(
+                deserialized.x,
+                [Type.integer(1)],
+              );
+
+              const expectedCallResult = Type.list([
+                Type.atom("b"),
+                Type.integer(1),
+                Type.integer(20),
+              ]);
+
+              assert.deepStrictEqual(callResult, expectedCallResult);
+            });
+
+            // Not applicable
+            // it("not versioned", () => {});
+          });
+
           describe("atom", () => {
             const term = Type.atom("abc");
 
