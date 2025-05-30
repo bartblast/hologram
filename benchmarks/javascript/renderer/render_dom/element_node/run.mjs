acd76cfdb56e95632d6e0dd02f8c53295267ad52
Bart Blast <bart@bartblast.com>
2025-05-30 17:55:51 +0200

    Benchmark Renderer.renderDom() for element nodes

---
+++ benchmarks/javascript/renderer/render_dom/element_node/run.mjs
@@ -0,0 +1,24 @@
+"use strict";
+
+import Renderer from "../../../../../assets/js/renderer.mjs";
+import Type from "../../../../../assets/js/type.mjs";
+
+import {benchmark} from "../../../support/helpers.mjs";
+import {defineGlobalErlangAndElixirModules} from "../../../../../test/javascript/support/helpers.mjs";
+
+defineGlobalErlangAndElixirModules();
+
+const context = Type.map();
+const defaultTarget = Type.bitstring("my_default_target");
+const slots = Type.keywordList();
+
+const node = Type.tuple([
+  Type.atom("element"),
+  Type.bitstring("div"),
+  Type.list(),
+  Type.list(),
+]);
+
+benchmark(() => {
+  Renderer.renderDom(node, context, slots, defaultTarget);
+});
