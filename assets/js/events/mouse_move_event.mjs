740f4f372243bf5e6c2960152344afb3863f742d
Bart Blast <bart@bartblast.com>
2025-06-12 23:09:53 +0200

    Add fields to mouse move events

--- assets/js/events/mouse_move_event.mjs
+++ assets/js/events/mouse_move_event.mjs
@@ -5,8 +5,16 @@ import Type from "../type.mjs";
 export default class MouseMoveEvent {
   static buildOperationParam(event) {
     return Type.map([
+      [Type.atom("client_x"), Type.float(event.clientX)],
+      [Type.atom("client_y"), Type.float(event.clientY)],
+      [Type.atom("movement_x"), Type.float(event.movementX)],
+      [Type.atom("movement_y"), Type.float(event.movementY)],
+      [Type.atom("offset_x"), Type.float(event.offsetX)],
+      [Type.atom("offset_y"), Type.float(event.offsetY)],
       [Type.atom("page_x"), Type.float(event.pageX)],
       [Type.atom("page_y"), Type.float(event.pageY)],
+      [Type.atom("screen_x"), Type.float(event.screenX)],
+      [Type.atom("screen_y"), Type.float(event.screenY)],
     ]);
   }
 
--- test/javascript/events/mouse_move_event_test.mjs
+++ test/javascript/events/mouse_move_event_test.mjs
@@ -12,14 +12,34 @@ defineGlobalErlangAndElixirModules();
 
 describe("MouseMoveEvent", () => {
   it("buildOperationParam()", () => {
-    const event = {pageX: 1, pageY: 2};
+    const event = {
+      clientX: 10,
+      clientY: 20,
+      movementX: 5,
+      movementY: 15,
+      offsetX: 30,
+      offsetY: 40,
+      pageX: 1,
+      pageY: 2,
+      screenX: 100,
+      screenY: 200,
+    };
+
     const result = MouseMoveEvent.buildOperationParam(event);
 
     assert.deepStrictEqual(
       result,
       Type.map([
+        [Type.atom("client_x"), Type.float(10)],
+        [Type.atom("client_y"), Type.float(20)],
+        [Type.atom("movement_x"), Type.float(5)],
+        [Type.atom("movement_y"), Type.float(15)],
+        [Type.atom("offset_x"), Type.float(30)],
+        [Type.atom("offset_y"), Type.float(40)],
         [Type.atom("page_x"), Type.float(1)],
         [Type.atom("page_y"), Type.float(2)],
+        [Type.atom("screen_x"), Type.float(100)],
+        [Type.atom("screen_y"), Type.float(200)],
       ]),
     );
   });
