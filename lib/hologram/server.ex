77de2caf019bddb7384c669231c9cb169ded1908
Bart Blast <bart@bartblast.com>
2025-06-19 16:53:46 +0200

    Move cookie store diff/2 function to the server module

--- lib/hologram/server.ex
+++ lib/hologram/server.ex
@@ -20,6 +20,57 @@ defmodule Hologram.Server do
           session: %{atom => any}
         }
 
+  @doc """
+  Computes the difference between two cookie sets.
+
+  Returns a list of update instructions needed to transform `old_cookies` 
+  into `new_cookies`. The result includes:
+
+  - New cookies (exist in new but not in old)
+  - Modified cookies (exist in both but with different values)  
+  - Deleted cookies (exist in old but not in new, marked with `nil`)
+
+  Unchanged cookies are omitted from the result.
+
+  ## Parameters
+
+  - `old_cookies` - The previous cookie state as a map
+  - `new_cookies` - The desired cookie state as a map
+
+  ## Returns
+
+  A list of `{key, value}` tuples representing update instructions.
+  Deleted cookies are represented with `nil` values.
+
+  ## Examples
+
+      iex> diff_cookies(%{}, %{"new" => "value"})
+      [{"new", "value"}]
+
+      iex> diff_cookies(%{"old" => "value"}, %{})
+      [{"old", nil}]
+
+      iex> diff_cookies(%{"key" => "old"}, %{"key" => "new"})
+      [{"key", "new"}]
+
+      iex> diff_cookies(%{"same" => "value"}, %{"same" => "value"})
+      []
+  """
+  @spec diff_cookies(%{String.t() => any()}, %{String.t() => any()}) :: [{String.t(), any()}]
+  def diff_cookies(old_cookies, new_cookies) do
+    changed_or_new =
+      for {key, value} <- new_cookies,
+          old_cookies[key] != value,
+          do: {key, value}
+
+    deleted =
+      for {key, _value} <- old_cookies,
+          not Map.has_key?(new_cookies, key),
+          do: {key, nil}
+
+    changed_or_new ++ deleted
+  end
+
   @impl WebSock
   def init(http_conn) do
     {:ok, http_conn}
