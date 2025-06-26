d66bb9f87d3911959707b3d10e38b45392e1c6fb
Bart Blast <bart@bartblast.com>
2025-06-27 00:21:36 +0200

    Define Digraph struct

--- lib/hologram/compiler/digraph.ex
+++ lib/hologram/compiler/digraph.ex
@@ -7,4 +7,13 @@ defmodule Hologram.Compiler.Digraph do
   # - Vertices in an ETS set
   # - Edges in an ETS bag (allows duplicate edges with same source)
   # - Reverse edges in another ETS bag for efficient inbound edges queries
+
+  defstruct [:vertices_table, :edges_table, :reverse_edges_table, :ref]
+
+  @type t :: %__MODULE__{
+          vertices_table: :ets.tid(),
+          edges_table: :ets.tid(),
+          reverse_edges_table: :ets.tid(),
+          ref: reference
+        }
 end
