ac3d7146e490ac1f4819557ce6e349372605d113
Bart Blast <bart@bartblast.com>
2025-06-29 17:17:21 +0200

    Fix flaky test

--- test/elixir/libgraph/utils_test.exs
+++ test/elixir/libgraph/utils_test.exs
@@ -1,5 +1,6 @@
 defmodule Graph.UtilsTest do
   use ExUnit.Case, async: true
+  alias Hologram.Commons.SystemUtils
 
   defp sizeof(term) do
     Graph.Utils.sizeof(term)
@@ -7,9 +8,18 @@ defmodule Graph.UtilsTest do
 
   test "sizeof/1" do
     assert 64 = sizeof({1, :foo, "bar"})
-    assert 456 = sizeof(String.duplicate("bar", 128))
     assert 8 = sizeof([])
     assert 24 = sizeof([1 | 2])
     assert 56 = sizeof([1, 2, 3])
+
+    expected =
+      if SystemUtils.otp_version() >= 27 do
+        456
+      else
+        440
+      end
+
+    result = String.duplicate("bar", 128)
+    assert sizeof(result) == expected
   end
 end
