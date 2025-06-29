911cdbc238bfdb419df51d26df5664e84c2f3431
Bart Blast <bart@bartblast.com>
2025-06-29 20:22:52 +0200

    Add vertex to digraph

--- lib/hologram/compiler/digraph2.ex
+++ lib/hologram/compiler/digraph2.ex
@@ -14,6 +14,14 @@ defmodule Hologram.Compiler.Digraph2 do
   @type edge :: {vertex, vertex}
   @type vertex :: any
 
+  @doc """
+  Adds a vertex to the graph.
+  """
+  @spec add_vertex(t, vertex) :: t
+  def add_vertex(%Digraph2{vertices: vertices} = graph, vertex) do
+    %{graph | vertices: Map.put(vertices, vertex, true)}
+  end
+
   def new do
     %Digraph2{vertices: %{}, edges: %{}, reverse_edges: %{}}
   end
--- test/elixir/hologram/compiler/digraph2_test.exs
+++ test/elixir/hologram/compiler/digraph2_test.exs
@@ -3,6 +3,30 @@ defmodule Hologram.Compiler.Digraph2Test do
   import Hologram.Compiler.Digraph2
   alias Hologram.Compiler.Digraph2
 
+  setup do
+    [empty_graph: new()]
+  end
+
+  describe "add_vertex/2" do
+    test "adds a single vertex when it doesn't exist yet", %{empty_graph: graph} do
+      assert add_vertex(graph, :a) == %Digraph2{
+               vertices: %{a: true},
+               edges: %{},
+               reverse_edges: %{}
+             }
+    end
+
+    test "adds a single vertex when it already exists", %{empty_graph: graph} do
+      graph_with_vetex_a = add_vertex(graph, :a)
+
+      assert add_vertex(graph_with_vetex_a, :a) == %Digraph2{
+               vertices: %{a: true},
+               edges: %{},
+               reverse_edges: %{}
+             }
+    end
+  end
+
   describe "new/0" do
     test "creates a new digraph" do
       assert new() == %Digraph2{vertices: %{}, edges: %{}, reverse_edges: %{}}
