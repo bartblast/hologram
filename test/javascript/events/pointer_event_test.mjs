82e51e8717929b8fed56b0e6ba8cc9dded660092
Bart Blast <bart@bartblast.com>
2025-07-24 21:47:32 +0200

    Fix pointer event unit tests

--- test/javascript/events/pointer_event_test.mjs
+++ test/javascript/events/pointer_event_test.mjs
@@ -15,31 +15,71 @@ defineGlobalErlangAndElixirModules();
 describe("PointerEvent", () => {
   describe("buildOperationParam()", () => {
     it("known pointer type", () => {
-      const event = {pageX: 1, pageY: 2, pointerType: "mouse"};
+      const event = {
+        clientX: 10,
+        clientY: 20,
+        movementX: 5,
+        movementY: 15,
+        offsetX: 30,
+        offsetY: 40,
+        pageX: 1,
+        pageY: 2,
+        pointerType: "mouse",
+        screenX: 100,
+        screenY: 200,
+      };
+
       const result = PointerEvent.buildOperationParam(event);
 
-      assert.deepStrictEqual(
-        result,
-        Type.map([
-          [Type.atom("page_x"), Type.float(1)],
-          [Type.atom("page_y"), Type.float(2)],
-          [Type.atom("pointer_type"), Type.atom("mouse")],
-        ]),
-      );
+      const expected = Type.map([
+        [Type.atom("client_x"), Type.float(10)],
+        [Type.atom("client_y"), Type.float(20)],
+        [Type.atom("movement_x"), Type.float(5)],
+        [Type.atom("movement_y"), Type.float(15)],
+        [Type.atom("offset_x"), Type.float(30)],
+        [Type.atom("offset_y"), Type.float(40)],
+        [Type.atom("page_x"), Type.float(1)],
+        [Type.atom("page_y"), Type.float(2)],
+        [Type.atom("screen_x"), Type.float(100)],
+        [Type.atom("screen_y"), Type.float(200)],
+        [Type.atom("pointer_type"), Type.atom("mouse")],
+      ]);
+
+      assert.deepStrictEqual(result, expected);
     });
 
     it("unknown pointer type", () => {
-      const event = {pageX: 1, pageY: 2, pointerType: ""};
+      const event = {
+        clientX: 10,
+        clientY: 20,
+        movementX: 5,
+        movementY: 15,
+        offsetX: 30,
+        offsetY: 40,
+        pageX: 1,
+        pageY: 2,
+        pointerType: "",
+        screenX: 100,
+        screenY: 200,
+      };
+
       const result = PointerEvent.buildOperationParam(event);
 
-      assert.deepStrictEqual(
-        result,
-        Type.map([
-          [Type.atom("page_x"), Type.float(1)],
-          [Type.atom("page_y"), Type.float(2)],
-          [Type.atom("pointer_type"), Type.nil()],
-        ]),
-      );
+      const expected = Type.map([
+        [Type.atom("client_x"), Type.float(10)],
+        [Type.atom("client_y"), Type.float(20)],
+        [Type.atom("movement_x"), Type.float(5)],
+        [Type.atom("movement_y"), Type.float(15)],
+        [Type.atom("offset_x"), Type.float(30)],
+        [Type.atom("offset_y"), Type.float(40)],
+        [Type.atom("page_x"), Type.float(1)],
+        [Type.atom("page_y"), Type.float(2)],
+        [Type.atom("screen_x"), Type.float(100)],
+        [Type.atom("screen_y"), Type.float(200)],
+        [Type.atom("pointer_type"), Type.nil()],
+      ]);
+
+      assert.deepStrictEqual(result, expected);
     });
   });
 
