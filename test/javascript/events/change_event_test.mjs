5467c24d54646b663fcc29aad33fa7d62c25c66b
Bart Blast <bart@bartblast.com>
2025-08-05 20:02:51 +0200

    Update change event unit tests

--- test/javascript/events/change_event_test.mjs
+++ test/javascript/events/change_event_test.mjs
@@ -11,18 +11,50 @@ import Type from "../../../assets/js/type.mjs";
 defineGlobalErlangAndElixirModules();
 
 describe("ChangeEvent", () => {
-  const event = {target: {value: "my_value"}};
+  describe("buildOperationParam()", () => {
+    it("handles text inputs", () => {
+      const event = {target: {type: "text", value: "my_value"}};
+      const result = ChangeEvent.buildOperationParam(event);
 
-  it("buildOperationParam()", () => {
-    const result = ChangeEvent.buildOperationParam(event);
+      assert.deepStrictEqual(
+        result,
+        Type.map([[Type.atom("value"), Type.bitstring("my_value")]]),
+      );
+    });
 
-    assert.deepStrictEqual(
-      result,
-      Type.map([[Type.atom("value"), Type.bitstring("my_value")]]),
-    );
+    it("handles checkbox inputs", () => {
+      const event = {target: {type: "checkbox", checked: true}};
+      const result = ChangeEvent.buildOperationParam(event);
+
+      assert.deepStrictEqual(
+        result,
+        Type.map([[Type.atom("value"), Type.boolean(true)]]),
+      );
+    });
+
+    it("handles radio inputs", () => {
+      const event = {target: {type: "radio", checked: false}};
+      const result = ChangeEvent.buildOperationParam(event);
+
+      assert.deepStrictEqual(
+        result,
+        Type.map([[Type.atom("value"), Type.boolean(false)]]),
+      );
+    });
+
+    it("handles other input types as text", () => {
+      const event = {target: {type: "email", value: "test@example.com"}};
+      const result = ChangeEvent.buildOperationParam(event);
+
+      assert.deepStrictEqual(
+        result,
+        Type.map([[Type.atom("value"), Type.bitstring("test@example.com")]]),
+      );
+    });
   });
 
   it("isEventIgnored()", () => {
+    const event = {target: {value: "my_value"}};
     assert.isFalse(ChangeEvent.isEventIgnored(event));
   });
 });
