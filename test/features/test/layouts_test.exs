c9783b0e90b2ad6f7014e2a19d1d1f1e41620229
Bart Blast <bart@bartblast.com>
2025-09-02 19:27:23 +0200

    Fix layouts feature tests

--- test/features/test/layouts_test.exs
+++ test/features/test/layouts_test.exs
@@ -8,18 +8,24 @@ defmodule HologramFeatureTests.LayoutsTest do
   feature "no props", %{session: session} do
     session
     |> visit(NoPropsPage)
-    |> assert_text(css("#layout_result"), inspect(%{cid: "layout"}))
+    |> assert_text(css("#layout_result"), "%{cid: &quot;layout&quot;}")
   end
 
   feature "props passed explicitely", %{session: session} do
     session
     |> visit(PropsPassedExplicitelyPage)
-    |> assert_text(css("#layout_result"), inspect(%{a: "abc", b: 123, cid: "layout"}))
+    |> assert_text(
+      css("#layout_result"),
+      "%{a: &quot;abc&quot;, b: 123, cid: &quot;layout&quot;}"
+    )
   end
 
   feature "props passed implicitely", %{session: session} do
     session
     |> visit(PropsPassedImplicitelyPage)
-    |> assert_text(css("#layout_result"), inspect(%{a: "abc", b: 123, cid: "layout"}))
+    |> assert_text(
+      css("#layout_result"),
+      "%{a: &quot;abc&quot;, b: 123, cid: &quot;layout&quot;}"
+    )
   end
 end
