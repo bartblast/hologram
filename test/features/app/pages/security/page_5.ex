87c7a3f57c10abaa01529c35ae11395dab1e478e
Bart Blast <bart@bartblast.com>
2025-09-09 01:33:37 +0200

    Test that form input controlled attributes are not escaped

---
+++ test/features/app/pages/security/page_5.ex
@@ -0,0 +1,44 @@
+defmodule HologramFeatureTests.Security.Page5 do
+  use Hologram.Page
+
+  route "/security/5"
+
+  layout HologramFeatureTests.Components.DefaultLayout
+
+  def init(_params, component, _server) do
+    put_state(component, email: nil, select: nil, text: nil, textarea: nil)
+  end
+
+  def template do
+    ~HOLO"""
+    <form>
+      <input type="text" id="text_input" value={@text} />
+      <div id="text_result">{@text}</div>
+      <br /><br />
+      
+      <input type="email" id="email_input" value={@email} />
+      <div id="email_result">{@email}</div>
+      <br /><br />
+      
+      <input type="textarea" id="textarea_input" value={@textarea} />
+      <div id="textarea_result">{@textarea}</div>
+      <br /><br />
+      
+      <select id="select_input" value={@select}>
+        <option id="select_option_1" value="option_1">Option 1</option>
+        <option id="select_option_2" value="b < c">Option 2</option>
+        <option id="select_option_3" value="option_2">Option 2</option>
+      </select>
+      <div id="select_result">{@select}</div>
+    </form>
+
+    <p>
+      <button $click="set_values">Set values</button>
+    </p>
+    """
+  end
+
+  def action(:set_values, _params, component) do
+    put_state(component, email: "c < d", select: "b < c", text: "a < b", textarea: "d < e")
+  end
+end
--- test/features/test/security_test.exs
+++ test/features/test/security_test.exs
@@ -5,6 +5,7 @@ defmodule HologramFeatureTests.SecurityTest do
   alias HologramFeatureTests.Security.Page2
   alias HologramFeatureTests.Security.Page3
   alias HologramFeatureTests.Security.Page4
+  alias HologramFeatureTests.Security.Page5
 
   describe "CSRF protection" do
     feature "initial page request is successful", %{session: session} do
@@ -70,6 +71,20 @@ defmodule HologramFeatureTests.SecurityTest do
       |> assert_text(css("#my_div"), "a &amp; b")
       |> assert_has(css("#my_div[class='c &lt; d']"))
       |> assert_inline_script("#my_script", "window.myVar = `1 &lt; 2`;")
+    end
+
+    feature "form inputs controlled attributes are not escaped", %{session: session} do
+      session
+      |> visit(Page5)
+      |> click(button("Set values"))
+      |> assert_text(css("#text_result"), "a &lt; b")
+      |> assert_input_value("#text_input", "a < b")
+      |> assert_text(css("#email_result"), "c &lt; d")
+      |> assert_input_value("#email_input", "c < d")
+      |> assert_text(css("#textarea_result"), "d &lt; e")
+      |> assert_input_value("#textarea_input", "d < e")
+      |> assert_text(css("#select_result"), "b &lt; c")
+      |> assert_input_value("#select_input", "b < c")
     end
   end
 end
