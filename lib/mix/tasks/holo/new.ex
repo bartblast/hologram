44244b8d5f733fa7a6d46b4dd8598e931dbade14
Bart Blast <bart@bartblast.com>
2025-09-19 21:49:30 +0200

    Generate config/config.exs

--- lib/mix/tasks/holo/new.ex
+++ lib/mix/tasks/holo/new.ex
@@ -16,11 +16,36 @@ defmodule Mix.Tasks.Holo.New do
   @import "tailwindcss" source("../../app");
   """
 
+  @config_exs_template """
+  import Config
+
+  config :my_app,
+    ecto_repos: [MyApp.Repo],
+    generators: [timestamp_type: :utc_datetime]
+
+  config :my_app, Hologram.Endpoint,
+    url: [host: "localhost"],
+    adapter: Bandit.PhoenixAdapter
+
+  config :tailwind,
+    version: "4.1.7",
+    my_app: [
+      args: ~w(
+        --input=assets/css/app.css
+        --output=priv/static/assets/css/app.css
+      ),
+      cd: Path.expand("..", __DIR__)
+    ]
+
+  import_config "\#{config_env()}.exs"
+  """
+
   @doc false
   @impl Mix.Task
   def run([project_name]) when is_binary(project_name) do
     create_project_dir(project_name)
     create_assets(project_name)
+    create_config(project_name)
 
     print_info("")
     print_info("Your Hologram project was created successfully.")
@@ -52,6 +77,16 @@ defmodule Mix.Tasks.Holo.New do
     File.write!(app_css_path, @app_css_template)
   end
 
+  defp create_config(project_name) do
+    print_info("* creating #{project_name}/config/config.exs")
+
+    config_dir = Path.join(project_name, "config")
+    File.mkdir_p!(config_dir)
+
+    config_exs_path = Path.join(config_dir, "config.exs")
+    File.write!(config_exs_path, replace_placeholders(project_name, @config_exs_template))
+  end
+
   defp create_project_dir(project_name) do
     print_info("* creating #{project_name}/")
 
@@ -69,5 +104,11 @@ defmodule Mix.Tasks.Holo.New do
 
   defp print_info(info) do
     Mix.shell().info(info)
+  end
+
+  defp replace_placeholders(project_name, template) do
+    template
+    |> String.replace("my_app", project_name)
+    |> String.replace("MyApp", Macro.camelize(project_name))
   end
 end
