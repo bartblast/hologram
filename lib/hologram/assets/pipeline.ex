c87fb845c5aa75c6b2b96291a9956f5c72149268
Bart Blast <bart@bartblast.com>
2025-09-26 00:11:53 +0200

    List old dist files

---
+++ lib/hologram/assets/pipeline.ex
@@ -0,0 +1,22 @@
+defmodule Hologram.Assets.Pipeline do
+  @moduledoc false
+
+  alias Hologram.Commons.FileUtils
+  alias Hologram.Commons.PathUtils
+
+  def run(opts) do
+    _assets_dir = opts[:assets_dir]
+    dist_dir = opts[:dist_dir]
+    _tmp_dir = opts[:tmp_dir]
+
+    _old_dist_files = list_old_dist_files(dist_dir)
+  end
+
+  defp list_old_dist_files(dist_dir) do
+    hologram_dir_prefix = Path.join(dist_dir, "hologram") <> PathUtils.path_separator()
+
+    dist_dir
+    |> FileUtils.list_files_recursively()
+    |> Enum.reject(&String.starts_with?(&1, hologram_dir_prefix))
+  end
+end
