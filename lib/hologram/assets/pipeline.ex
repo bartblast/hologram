9c2baa206a1080bf24508add7158ba49be50bb2f
Bart Blast <bart@bartblast.com>
2025-09-30 21:36:34 +0200

    Copy image assets

--- lib/hologram/assets/pipeline.ex
+++ lib/hologram/assets/pipeline.ex
@@ -5,13 +5,18 @@ defmodule Hologram.Assets.Pipeline do
   alias Hologram.Commons.PathUtils
 
   def run(opts) do
-    _assets_dir = opts[:assets_dir]
+    assets_dir = opts[:assets_dir]
     dist_dir = opts[:dist_dir]
     tmp_dist_dir = Path.join(opts[:tmp_dir], "dist")
 
     _old_dist_files = list_old_dist_files(dist_dir)
+    copy_assets(assets_dir, tmp_dist_dir)
 
     FileUtils.recreate_dir(tmp_dist_dir)
+  end
+
+  defp copy_assets(assets_dir, tmp_dist_dir) do
+    maybe_copy_images(assets_dir, tmp_dist_dir)
   end
 
   defp list_old_dist_files(dist_dir) do
@@ -20,5 +25,14 @@ defmodule Hologram.Assets.Pipeline do
     dist_dir
     |> FileUtils.list_files_recursively()
     |> Enum.reject(&String.starts_with?(&1, hologram_dir_prefix))
+  end
+
+  defp maybe_copy_images(assets_dir, tmp_dist_dir) do
+    images_source_path = Path.join(assets_dir, "images")
+
+    if File.exists?(images_source_path) do
+      images_target_path = Path.join(tmp_dist_dir, "images")
+      File.cp_r!(images_source_path, images_target_path)
+    end
   end
 end
