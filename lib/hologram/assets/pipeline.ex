a30cd416612c61534b6998712bc6c53071a0d510
Bart Blast <bart@bartblast.com>
2025-10-01 01:56:48 +0200

    Remove old dist files

--- lib/hologram/assets/pipeline.ex
+++ lib/hologram/assets/pipeline.ex
@@ -9,15 +9,25 @@ defmodule Hologram.Assets.Pipeline do
     assets_dir = opts[:assets_dir]
     dist_dir = opts[:dist_dir]
 
-    _old_dist_files = list_old_dist_files(dist_dir)
+    old_dist_paths = list_old_dist_paths(dist_dir)
 
-    assets_dir
-    |> list_assets()
-    |> stream_extract_asset_infos(assets_dir)
-    |> stream_read_assets()
-    |> stream_digest_assets(dist_dir)
-    |> stream_compress_assets()
-    |> Stream.run()
+    new_dist_paths =
+      assets_dir
+      |> list_assets()
+      |> stream_extract_asset_infos(assets_dir)
+      |> stream_read_assets()
+      |> stream_digest_assets(dist_dir)
+      |> stream_compress_assets()
+      |> Enum.to_list()
+      |> collect_new_dist_paths()
+
+    remove_old_dist_files(old_dist_paths, new_dist_paths)
+  end
+
+  defp collect_new_dist_paths(assets) do
+    Enum.flat_map(assets, fn asset ->
+      [asset.digested_asset_path, asset.compressed_asset_path]
+    end)
   end
 
   defp list_assets(assets_dir) do
@@ -34,12 +44,22 @@ defmodule Hologram.Assets.Pipeline do
     FileUtils.list_files_recursively(images_dir)
   end
 
-  defp list_old_dist_files(dist_dir) do
+  defp list_old_dist_paths(dist_dir) do
     hologram_dir_prefix = Path.join(dist_dir, "hologram") <> PathUtils.path_separator()
 
     dist_dir
     |> FileUtils.list_files_recursively()
     |> Enum.reject(&String.starts_with?(&1, hologram_dir_prefix))
+  end
+
+  defp remove_old_dist_files(old_dist_paths, new_dist_paths) do
+    # Kernel.--/2 can be more performant for small lists due to lower overhead,
+    # but MapSet.difference/2 becomes faster as size grows.
+    files_to_remove = old_dist_paths -- new_dist_paths
+
+    Enum.each(files_to_remove, fn file_path ->
+      File.rm!(file_path)
+    end)
   end
 
   defp stream_compress_assets(assets) do
