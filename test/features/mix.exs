2839f9c8b0c75315df4af61f06dca605791d0334
Bart Blast <bart@bartblast.com>
2025-10-11 17:09:43 +0200

    Bump version

--- test/features/mix.exs
+++ test/features/mix.exs
@@ -20,7 +20,7 @@ defmodule HologramFeatureTests.MixProject do
       {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false},
       {:hologram,
        git: "https://github.com/bartblast/hologram.git",
-       ref: "278efcc02b0d4e4d8c3bb8c4bd8afe3fe69186b0"},
+       ref: "2d5f4aba24dae045726533d18812c8de8aa5c0a5"},
       {:jason, "~> 1.0"},
       {:phoenix, "~> 1.7"},
       {:plug_cowboy, "~> 2.0"},
