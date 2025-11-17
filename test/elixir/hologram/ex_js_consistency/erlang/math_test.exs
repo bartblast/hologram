7c3971bddf1c612f7055c1246a93cd793522fc85
Simon Landry <simon.landry@rumandcode.io>
2025-11-17 12:37:56 -0500

    Remove leftover comments

--- test/elixir/hologram/ex_js_consistency/erlang/math_test.exs
+++ test/elixir/hologram/ex_js_consistency/erlang/math_test.exs
@@ -9,61 +9,6 @@ defmodule Hologram.ExJsConsistency.Erlang.MathTest do
 
   @moduletag :consistency
 
-  # describe("Erlang_Math", () => {
-  #   it("exp/1", () => {
-  #     const exp = Erlang_Math["exp/1"];
-
-  #     describe("returns correct value", () => {
-  #       const number = Type.float(2);
-
-  #       const result = exp(number);
-  #       const expected = Type.float(7.38905609893065);
-
-  #       assert.deepStrictEqual(result, expected);
-  #     });
-
-  #     describe("returns correct value if passing negative value", () => {
-  #       const number = Type.float(-2);
-
-  #       const result = exp(number);
-  #       const expected = Type.float(0.1353352832366127);
-
-  #       assert.deepStrictEqual(result, expected);
-  #     });
-
-  #     describe("returns correct value if passing one", () => {
-  #       const number = Type.float(1.0);
-
-  #       const result = exp(number);
-  #       const expected = Type.float(2.718281828459045);
-
-  #       assert.deepStrictEqual(result, expected);
-  #     });
-
-  #     describe("returns correct value if passing zero", () => {
-  #       const number = Type.float(0);
-
-  #       const result = exp(number);
-  #       const expected = Type.float(1.0);
-
-  #       assert.deepStrictEqual(result, expected);
-  #     });
-
-  #     describe("raises ArgumentError if the argument is a string", () => {
-  #       const integerString = Type.string("12345");
-
-  #       assertBoxedError(() => exp(integerString), "ArgumentError", Interpreter.buildArgumentErrorMsg(1, "not a float or integer"));
-  #     });
-
-  #     describe("raises ArgumentError if the argument is a list", () => {
-  #       const list = Type.list([Type.integer(1), Type.integer(2)]);
-
-  #       assertBoxedError(() => exp(list), "ArgumentError", Interpreter.buildArgumentErrorMsg(1, "not a float or integer"));
-  #     });
-  #   });
-  # });
-
-
   describe "exp/1" do
     test "returns correct value" do
       number = 2.0
