a5396a0d89870fd011964c64f5980eec72d9e6aa
Aaron Tavistock <aaron@geekshow.com>
2025-11-18 17:31:19 -0800

    Resolve my bad merge

--- assets/js/erlang/binary.mjs
+++ assets/js/erlang/binary.mjs
@@ -4,6 +4,11 @@ import Bitstring from "../bitstring.mjs";
 import Interpreter from "../interpreter.mjs";
 import Type from "../type.mjs";
 
+// IMPORTANT!
+// If the given ported Erlang function calls other Erlang functions, then list such dependencies in the "Deps" comment (see :erlang./=/2 for an example).
+// Also, in such case add respective call graph edges in Hologram.CallGraph.list_runtime_mfas/1.
+
+
 // Base class and factory
 class Matcher {
   // Create pattern object from pattern or compiled pattern
@@ -624,10 +629,41 @@ class AhoCorasickMatcher extends Matcher {
 
     return null;
   }
-
 }
 
 const Erlang_Binary = {
+  // Start at/2
+  "at/2": function (subject, pos) {
+    if (!Type.isBinary(subject)) {
+      const msg = Type.isBitstring(subject)
+        ? "is a bitstring (expected a binary)"
+        : "not a binary";
+      Interpreter.raiseArgumentError(Interpreter.buildArgumentErrorMsg(1, msg));
+    }
+
+    if (!Type.isInteger(pos)) {
+      Interpreter.raiseArgumentError(
+        Interpreter.buildArgumentErrorMsg(2, "not an integer"),
+      );
+    }
+
+    if (pos.value < 0n) {
+      Interpreter.raiseArgumentError(
+        Interpreter.buildArgumentErrorMsg(2, "out of range"),
+      );
+    }
+
+    Bitstring.maybeSetBytesFromText(subject);
+
+    if (pos.value >= subject.bytes.length) {
+      Interpreter.raiseArgumentError("argument error");
+    }
+
+    return Type.integer(subject.bytes[pos.value]);
+  },
+  // End at/2
+  // Deps: []
+
   "compile_pattern/1": (pattern) => {
     const patternObj = Matcher.create(pattern);
     return patternObj.toTuple();
