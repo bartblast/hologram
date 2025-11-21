8e60b14ca11a04a461aca17cb2d4638bb162fbc5
ideaMarcos <571086+ideaMarcos@users.noreply.github.com>
2025-11-20 23:19:22 -0500

    match erlang default option

--- test/javascript/erlang/erlang_test.mjs
+++ test/javascript/erlang/erlang_test.mjs
@@ -2966,6 +2966,7 @@ describe("Erlang", () => {
     });
 
     it("raises ArgumentError if the second argument is not a proper list", () => {
+      const floatVal = Type.float(0.1 + 0.2);
       const opts = Type.improperList([
         Type.tuple([Type.atom("decimals"), Type.integer(4)]),
         Type.atom("compact"),
