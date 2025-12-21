e06481365e7ffe4bbae5b41a00817977063b1b4f
ryan <ryanpauley@gmail.com>
2025-12-22 00:22:43 +1100

    Use charlist sigil instead of single-quotes

--- test/elixir/hologram/ex_js_consistency/erlang/string_test.exs
+++ test/elixir/hologram/ex_js_consistency/erlang/string_test.exs
@@ -11,38 +11,38 @@ defmodule Hologram.ExJsConsistency.Erlang.StringTest do
 
   describe "join/2" do
     test "single element" do
-      assert :string.join(['hello'], ', ') == 'hello'
+      assert :string.join([~c"hello"], ~c", ") == ~c"hello"
     end
 
     test "multiple elements" do
-      assert :string.join(['one', 'two', 'three'], ', ') == 'one, two, three'
+      assert :string.join([~c"one", ~c"two", ~c"three"], ~c", ") == ~c"one, two, three"
     end
 
     test "empty separator" do
-      assert :string.join(['hello', 'world'], '') == 'helloworld'
+      assert :string.join([~c"hello", ~c"world"], ~c"") == ~c"helloworld"
     end
 
     test "empty strings (charlists) in list" do
-      assert :string.join(['', 'hello', '', 'world', ''], '-') == '-hello--world-'
+      assert :string.join([~c"", ~c"hello", ~c"", ~c"world", ~c""], ~c"-") == ~c"-hello--world-"
     end
 
     test "multi-character separator" do
-      assert :string.join(['apple', 'banana', 'cherry'], ' and ') == 'apple and banana and cherry'
+      assert :string.join([~c"apple", ~c"banana", ~c"cherry"], ~c" and ") == ~c"apple and banana and cherry"
     end
 
     test "empty list" do
       assert_error FunctionClauseError,
-                   build_function_clause_error_msg(":string.join/2", [[], ', ']),
+                   build_function_clause_error_msg(":string.join/2", [[], ~c", "]),
                    fn ->
-                     :string.join([], ', ')
+                     :string.join([], ~c", ")
                    end
     end
 
     test "first argument is not a list" do
       assert_error FunctionClauseError,
-                   build_function_clause_error_msg(":string.join/2", [:not_a_list, ', ']),
+                   build_function_clause_error_msg(":string.join/2", [:not_a_list, ~c", "]),
                    fn ->
-                     :string.join(:not_a_list, ', ')
+                     :string.join(:not_a_list, ~c", ")
                    end
     end
   end
