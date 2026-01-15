60d1cc6c62ed1c1c8fcb2accd698d206bb73c697
Bart Blast <bart@bartblast.com>
2026-01-15 20:50:59 +0100

    Simplify tests and add missing call graph edge

--- lib/hologram/compiler/call_graph.ex
+++ lib/hologram/compiler/call_graph.ex
@@ -29,6 +29,7 @@ defmodule Hologram.Compiler.CallGraph do
     {{:erlang, :binary_to_existing_atom, 2}, {:erlang, :binary_to_atom, 2}},
     {{:erlang, :binary_to_integer, 1}, {:erlang, :binary_to_integer, 2}},
     {{:erlang, :error, 1}, {:erlang, :error, 2}},
+    {{:erlang, :float_to_list, 2}, {:erlang, :float_to_binary, 2}},
     {{:erlang, :integer_to_binary, 1}, {:erlang, :integer_to_binary, 2}},
     {{:erlang, :integer_to_list, 1}, {:erlang, :integer_to_list, 2}},
     {{:erlang, :iolist_to_binary, 1}, {:lists, :flatten, 1}},
