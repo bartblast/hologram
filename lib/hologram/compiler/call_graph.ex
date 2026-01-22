2c475f6bbe4eb311e4f46151042b8ca4ca3724a7
Brett Fincher <brettf@simpli.fi>
2026-01-22 07:38:13 -0600

    add sets size/1 entry to call_graph

--- lib/hologram/compiler/call_graph.ex
+++ lib/hologram/compiler/call_graph.ex
@@ -59,6 +59,7 @@ defmodule Hologram.Compiler.CallGraph do
     {{:sets, :is_subset, 2}, {:sets, :to_list, 1}},
     {{:sets, :new, 1}, {:sets, :_validate_opts, 1}},
     {{:sets, :to_list, 1}, {:maps, :keys, 1}},
+    {{:sets, :size, 1}, {:maps, :keys, 1}},
     {{:string, :titlecase, 1}, {:lists, :flatten, 1}},
     {{:string, :titlecase, 1}, {:unicode_util, :cp, 1}},
     {{:unicode, :characters_to_binary, 1}, {:unicode, :characters_to_binary, 3}},
