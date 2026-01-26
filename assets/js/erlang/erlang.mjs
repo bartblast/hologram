d8e7578ce7ac48405f262751193d0182ac5b1374
Michael Ward <michael@scripthungry.com>
2026-01-26 21:19:10 +0000

    Fixed function ordring

--- assets/js/erlang/erlang.mjs
+++ assets/js/erlang/erlang.mjs
@@ -1477,26 +1477,6 @@ const Erlang = {
   // End localtime/0
   // Deps: []
 
-  // Start ref_to_list/1
-  "ref_to_list/1": (reference) => {
-    if (!Type.isReference(reference)) {
-      Interpreter.raiseArgumentError(
-        Interpreter.buildArgumentErrorMsg(1, "not a reference"),
-      );
-    }
-
-    const localIncarnationId = ERTS.nodeTable.getLocalIncarnationId(
-      reference.node,
-      reference.creation,
-    );
-
-    const text = `#Ref<${localIncarnationId}.${reference.idWords.toReversed().join(".")}>`;
-
-    return Bitstring.toCodepoints(Type.bitstring(text));
-  },
-  // End ref_to_list/1
-  // Deps: []
-
   // Start make_ref/0
   "make_ref/0": () => {
     const node = ERTS.nodeTable.CLIENT_NODE;
@@ -1614,6 +1594,26 @@ const Erlang = {
     return Bitstring.toCodepoints(Type.bitstring(pidText));
   },
   // End pid_to_list/1
+  // Deps: []
+
+  // Start ref_to_list/1
+  "ref_to_list/1": (reference) => {
+    if (!Type.isReference(reference)) {
+      Interpreter.raiseArgumentError(
+        Interpreter.buildArgumentErrorMsg(1, "not a reference"),
+      );
+    }
+
+    const localIncarnationId = ERTS.nodeTable.getLocalIncarnationId(
+      reference.node,
+      reference.creation,
+    );
+
+    const text = `#Ref<${localIncarnationId}.${reference.idWords.toReversed().join(".")}>`;
+
+    return Bitstring.toCodepoints(Type.bitstring(text));
+  },
+  // End ref_to_list/1
   // Deps: []
 
   // Start rem/2
