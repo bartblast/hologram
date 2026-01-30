4fcead0007d75078353382e8880d6f2d4906ffaf
Simon Tirant <simon.tirant@gmail.com>
2026-01-30 16:23:56 +0100

    chore: mix format

--- assets/js/erlang/string.mjs
+++ assets/js/erlang/string.mjs
@@ -347,7 +347,7 @@ const Erlang_String = {
       const codepointNum = Number(firstCodepoint.value);
       const rest = cpResult.data.slice(1);
 
-      return { codepointNum, rest };
+      return {codepointNum, rest};
     };
 
     // Helper: Uppercase a single codepoint and return array of uppercased codepoints
@@ -377,7 +377,7 @@ const Erlang_String = {
         return Type.bitstring("");
       }
 
-      const { codepointNum, rest } = extraction;
+      const {codepointNum, rest} = extraction;
       const restBinary = rest[0]; // Tail of the improper list
       const restText = Bitstring.toText(restBinary);
 
@@ -395,7 +395,7 @@ const Erlang_String = {
         return Type.list();
       }
 
-      const { codepointNum, rest } = extraction;
+      const {codepointNum, rest} = extraction;
       const uppercasedCodepoints = uppercaseCodepoint(codepointNum).map((cp) =>
         Type.integer(cp),
       );
