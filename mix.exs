c59258f6d23bb952651c4493729938f9ec64b9b8
Bart Blast <bart@bartblast.com>
2026-02-11 21:30:35 +0100

    Bump version

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@
 defmodule Hologram.MixProject do
   use Mix.Project
 
-  @version "0.6.6"
+  @version "0.7.0"
 
   # Copied from Hologram.Commons.SystemUtils
   @windows_exec_suffixes [".bat", ".cmd", ".exe"]
