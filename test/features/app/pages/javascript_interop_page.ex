096a2bf8d341a3aad4c112b81258a372e748bc38
Bart Blast <bart@bartblast.com>
2026-02-19 21:47:45 +0100

    Implement feature test for global fun call JS interop

--- test/features/app/pages/javascript_interop_page.ex
+++ test/features/app/pages/javascript_interop_page.ex
@@ -1,24 +1,45 @@
 defmodule HologramFeatureTests.JavaScriptInteropPage do
   use Hologram.Page
 
+  import Hologram.Commons.KernelUtils, only: [inspect: 1]
+  import Kernel, except: [inspect: 1]
+
+  alias Hologram.JS
+
   route "/javascript-interop"
 
   layout HologramFeatureTests.Components.DefaultLayout
 
+  def init(_params, component, _server) do
+    put_state(component, :result, nil)
+  end
+
   def template do
     ~HOLO"""
+    <p>
+      <button $click="call_global_fun"> Call global fun </button>
+    </p>
     <p>
       <button $click="run_js_snippet"> Run JavaScript snippet </button>
     </p>
     <p>
-      Result: <strong id="result"><code>nil</code></strong>
+      Call result: <strong id="call_result"><code>{inspect(@result)}</code></strong>
+    </p>
+    <p>
+      JS snippet result: <strong id="js_snippet_result"><code>nil</code></strong>
     </p>
     """
   end
 
+  def action(:call_global_fun, _params, component) do
+    result = JS.call("Math", "round", [3.7])
+
+    put_state(component, :result, result)
+  end
+
   def action(:run_js_snippet, _params, component) do
     ~JS"""
-    document.getElementById('result').querySelector('code').textContent = 'Hologram';
+    document.getElementById('js_snippet_result').querySelector('code').textContent = 'Hologram';
     """
 
     component
--- test/features/test/javascript_interop_test.exs
+++ test/features/test/javascript_interop_test.exs
@@ -1,10 +1,19 @@
 defmodule HologramFeatureTests.JavaScriptInteropTest do
   use HologramFeatureTests.TestCase, async: true
 
+  describe "JS.call/3" do
+    feature "global fun", %{session: session} do
+      session
+      |> visit(HologramFeatureTests.JavaScriptInteropPage)
+      |> click(button("Call global fun"))
+      |> assert_text(css("#call_result"), "4")
+    end
+  end
+
   feature "~JS sigil", %{session: session} do
     session
     |> visit(HologramFeatureTests.JavaScriptInteropPage)
     |> click(button("Run JavaScript snippet"))
-    |> assert_text(css("#result"), "Hologram")
+    |> assert_text(css("#js_snippet_result"), "Hologram")
   end
 end
