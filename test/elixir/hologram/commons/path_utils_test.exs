5b9f9b403e716375a3f9c88c177fbe1e720594ac
Bart Blast <bart@bartblast.com>
2026-03-06 21:33:33 +0100

    Implement env_path_separator/0 path util

--- test/elixir/hologram/commons/path_utils_test.exs
+++ test/elixir/hologram/commons/path_utils_test.exs
@@ -2,6 +2,18 @@ defmodule Hologram.Commons.PathUtilsTest do
   use Hologram.Test.BasicCase, async: true
   import Hologram.Commons.PathUtils
 
+  test "env_path_separator/0" do
+    result = env_path_separator()
+
+    case :os.type() do
+      {:unix, _name} ->
+        assert result == ":"
+
+      _fallback ->
+        assert result == ";"
+    end
+  end
+
   test "path_separator/0" do
     result = path_separator()
 
