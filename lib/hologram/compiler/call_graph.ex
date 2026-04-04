2120b64d4669c1ec002ba767ac5de87ebbe7ab41
Bart Blast <bart@bartblast.com>
2026-04-04 22:34:24 +0200

    Add Date.day_of_era/1 -> Calendar.ISO.day_of_era/3 dynamic dispatch edge

--- lib/hologram/compiler/call_graph.ex
+++ lib/hologram/compiler/call_graph.ex
@@ -18,6 +18,7 @@ defmodule Hologram.Compiler.CallGraph do
 
   # These edges can't be discovered from static IR analysis.
   @dynamic_dispatch_edges [
+    {{Date, :day_of_era, 1}, {Calendar.ISO, :day_of_era, 3}},
     {{Date, :day_of_week, 2}, {Calendar.ISO, :day_of_week, 4}},
     {{Date, :days_in_month, 1}, {Calendar.ISO, :days_in_month, 2}},
     {{Date, :leap_year?, 1}, {Calendar.ISO, :leap_year?, 1}},
