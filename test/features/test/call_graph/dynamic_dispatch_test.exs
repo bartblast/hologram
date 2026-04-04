eaa02dbe8f62a32d80c900641ddc1ce9315ee482
Bart Blast <bart@bartblast.com>
2026-04-05 00:32:08 +0200

    Fix feature test

--- test/features/test/call_graph/dynamic_dispatch_test.exs
+++ test/features/test/call_graph/dynamic_dispatch_test.exs
@@ -4,9 +4,11 @@ defmodule HologramFeatureTests.DynamicDispatchTest do
   alias HologramFeatureTests.CallGraph.DynamicDispatchPage
 
   feature "Date.new/3", %{session: session} do
+    expected = "{:ok, %{__struct__: Date, calendar: Calendar.ISO, day: 15, month: 6, year: 2024}}"
+
     session
     |> visit(DynamicDispatchPage)
     |> click(button("Date.new"))
-    |> assert_text(css("#result"), "{:ok, ~D[2024-06-15]}")
+    |> assert_text(css("#result"), expected)
   end
 end
