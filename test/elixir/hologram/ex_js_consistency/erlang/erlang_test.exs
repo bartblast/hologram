190913576d9ee0ca1151b99ad5c0bd44e7363f6b
Bart Blast <bart@bartblast.com>
2026-04-30 16:13:35 +0200

    Add Elixir consistency test for unsupported ETF tag 50

--- test/elixir/hologram/ex_js_consistency/erlang/erlang_test.exs
+++ test/elixir/hologram/ex_js_consistency/erlang/erlang_test.exs
@@ -2594,6 +2594,15 @@ defmodule Hologram.ExJsConsistency.Erlang.ErlangTest do
                    {:erlang, :binary_to_term, [binary]}
     end
 
+    test "raises ArgumentError for unsupported ETF tag (tag 50)" do
+      # Tag 50 is not a valid ETF tag
+      binary = <<131, 50>>
+
+      assert_error ArgumentError,
+                   build_argument_error_msg(1, "invalid external representation of a term"),
+                   {:erlang, :binary_to_term, [binary]}
+    end
+
     # Consistency tests for specific external term formats
 
     test "STRING_EXT format with various byte values" do
