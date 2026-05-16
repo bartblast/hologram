1d48efe1be52f6f85a813bf8f4d426dbcb88367a
Bart Blast <bart@bartblast.com>
2026-05-16 20:04:02 +0200

    Expose put_broadcast in handler scope

--- lib/hologram/component.ex
+++ lib/hologram/component.ex
@@ -72,6 +72,8 @@ defmodule Hologram.Component do
             prop: 3,
             put_action: 2,
             put_action: 3,
+            put_broadcast: 3,
+            put_broadcast: 4,
             put_command: 2,
             put_command: 3,
             put_context: 3,
