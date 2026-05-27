282cc9a728571dd730e570a00d496100358d7d49
Bart Blast <bart@bartblast.com>
2026-05-27 12:46:57 +0200

    Drop obsolete Bandit read_timeout TODO from SSE prep

--- lib/hologram/realtime/sse.ex
+++ lib/hologram/realtime/sse.ex
@@ -78,10 +78,6 @@ defmodule Hologram.Realtime.SSE do
   @doc false
   @spec prepare(Plug.Conn.t()) :: Plug.Conn.t()
   def prepare(conn) do
-    # TODO: When Bandit ships a per-connection read_timeout setter (see
-    # bandit_sse_timeout_note.md / upstream issue), call it here so individual
-    # SSE connections survive past Bandit's 60s read_timeout. Until then, the
-    # 60s reap is absorbed by the JS-driven reconnect path.
     conn
     |> Plug.Conn.put_resp_header("cache-control", "no-cache")
     |> Plug.Conn.put_resp_header("connection", "keep-alive")
