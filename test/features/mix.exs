1e7f6f13a8d29f0c94a2ea0c2fa505464bba285c
Bart Blast <bart@bartblast.com>
2026-06-04 14:29:25 +0200

    Add a feature test for event debouncing

--- test/features/mix.exs
+++ test/features/mix.exs
@@ -24,7 +24,7 @@ defmodule HologramFeatureTests.MixProject do
       {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false},
       {:hologram,
        git: "https://github.com/bartblast/hologram.git",
-       ref: "f0bd0f251e7ee74ce7bf8e76579f2e09948e4040"},
+       ref: "acedaa0f7d4d9d337b4067c6bf8eaecc664cc7db"},
       {:jason, "~> 1.0"},
       {:phoenix, "~> 1.7"},
       {:plug_cowboy, "~> 2.0"},
