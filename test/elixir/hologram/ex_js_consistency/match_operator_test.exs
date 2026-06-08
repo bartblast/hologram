a8ebba526215b2b53c5f0ccf0477a173f74bc3bb
Bart Blast <bart@bartblast.com>
2026-06-08 17:59:13 +0200

    Pin size variable in match_operator bitstring patterns for Elixir 1.20

--- test/elixir/hologram/ex_js_consistency/match_operator_test.exs
+++ test/elixir/hologram/ex_js_consistency/match_operator_test.exs
@@ -292,7 +292,7 @@ defmodule Hologram.ExJsConsistency.MatchOperatorTest do
       # Use `size` variable to prevent compilation error in Elixir/OTP versions that don't support size 16.
       size = wrap_term(16)
 
-      result = <<value::float-size(size)-signed>> = <<123.45::size(size)>>
+      result = <<value::float-size(^size)-signed>> = <<123.45::size(size)>>
 
       assert result == <<123.45::size(size)>>
       assert value == 123.4375
@@ -304,7 +304,7 @@ defmodule Hologram.ExJsConsistency.MatchOperatorTest do
 
       # 170 == 0b10101010
       assert_error MatchError, build_match_error_msg(<<170>>), fn ->
-        <<_value::float-size(size)-signed>> = <<1::1, 0::1, 1::1, 0::1, 1::1, 0::1, 1::1, 0::1>>
+        <<_value::float-size(^size)-signed>> = <<1::1, 0::1, 1::1, 0::1, 1::1, 0::1, 1::1, 0::1>>
       end
     end
 
@@ -359,7 +359,7 @@ defmodule Hologram.ExJsConsistency.MatchOperatorTest do
       # Use `size` variable to prevent compilation error in Elixir/OTP versions that don't support size 16.
       size = wrap_term(16)
 
-      result = <<value::float-size(size)-unsigned>> = <<123.45::size(size)>>
+      result = <<value::float-size(^size)-unsigned>> = <<123.45::size(size)>>
 
       assert result == <<123.45::size(size)>>
       assert value == 123.4375
@@ -371,7 +371,8 @@ defmodule Hologram.ExJsConsistency.MatchOperatorTest do
 
       # 170 == 0b10101010
       assert_error MatchError, build_match_error_msg(<<170>>), fn ->
-        <<_value::float-size(size)-unsigned>> = <<1::1, 0::1, 1::1, 0::1, 1::1, 0::1, 1::1, 0::1>>
+        <<_value::float-size(^size)-unsigned>> =
+          <<1::1, 0::1, 1::1, 0::1, 1::1, 0::1, 1::1, 0::1>>
       end
     end
 
