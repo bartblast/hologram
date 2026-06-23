401a18ac0092b6695e24cacbfbea96b90d746722
Bart Blast <bart@bartblast.com>
2026-06-24 00:13:23 +0200

    Test allow_default on form submission

--- test/features/mix.exs
+++ test/features/mix.exs
@@ -24,7 +24,7 @@ defmodule HologramFeatureTests.MixProject do
       {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false},
       {:hologram,
        git: "https://github.com/bartblast/hologram.git",
-       ref: "c025e0492bd857e68991ef3f91b3f87c216264b1"},
+       ref: "75bed6f3f6a3e8e6a66cb6cc01e6f7b0aadfb06f"},
       {:jason, "~> 1.0"},
       {:phoenix, "~> 1.7"},
       {:plug_cowboy, "~> 2.0"},
