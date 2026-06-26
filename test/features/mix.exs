c5b3c1b43d24d5399c4d3c140352f881996ed4b0
Bart Blast <bart@bartblast.com>
2026-06-26 17:03:40 +0200

    Update Hologram dep in feature tests app

--- test/features/mix.exs
+++ test/features/mix.exs
@@ -24,7 +24,7 @@ defmodule HologramFeatureTests.MixProject do
       {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false},
       {:hologram,
        git: "https://github.com/bartblast/hologram.git",
-       ref: "eb3b49ede541a509697fa2010fca0058d958724c"},
+       ref: "bdc158e8cb289ff2589f425545b89920b56ff760"},
       {:jason, "~> 1.0"},
       {:phoenix, "~> 1.7"},
       {:plug_cowboy, "~> 2.0"},
