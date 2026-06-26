3000040e1a9fbcbe0ed0f5f60d41153f85520c42
Bart Blast <bart@bartblast.com>
2026-06-27 01:29:01 +0200

    Add try rescue-clause consistency tests

--- test/elixir/hologram/ex_js_consistency/try_test.exs
+++ test/elixir/hologram/ex_js_consistency/try_test.exs
@@ -8,6 +8,51 @@ defmodule Hologram.ExJsConsistency.TryTest do
 
   @moduletag :consistency
 
+  describe "rescue clauses" do
+    test "rescues without a module" do
+      result =
+        try do
+          raise RuntimeError, "my message"
+        rescue
+          _exception -> :rescued_any
+        end
+
+      assert result == :rescued_any
+    end
+
+    test "rescues with a single module" do
+      result =
+        try do
+          raise ArgumentError, "my message"
+        rescue
+          e in ArgumentError -> {:rescued_single, e.message}
+        end
+
+      assert result == {:rescued_single, "my message"}
+    end
+
+    test "rescues with multiple modules" do
+      result =
+        try do
+          raise RuntimeError, "my message"
+        rescue
+          e in [ArgumentError, RuntimeError] -> {:rescued_multiple, e.message}
+        end
+
+      assert result == {:rescued_multiple, "my message"}
+    end
+
+    test "re-raises when no rescue clause matches" do
+      assert_error RuntimeError, "my message", fn ->
+        try do
+          raise RuntimeError, "my message"
+        rescue
+          e in ArgumentError -> {:rescued_single, e.message}
+        end
+      end
+    end
+  end
+
   describe "else clauses" do
     test "raises TryClauseError when no else clause matches" do
       value = wrap_term(:no_match)
