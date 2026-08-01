4280eee90466a8bd410fb0f48bc7208d22451068
Bart Blast <bart@bartblast.com>
2026-08-01 11:47:42 +0200

    Bump the Hologram dep ref in feature tests

--- test/features/mix.exs
+++ test/features/mix.exs
@@ -24,7 +24,7 @@ defmodule HologramFeatureTests.MixProject do
       {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false},
       {:hologram,
        git: "https://github.com/bartblast/hologram.git",
-       ref: "c3cdf48a4b131e7bfba83c178e593d3685a02060"},
+       ref: "135c3bc30a8b2dbf7237ca15d187e8ca91021baf"},
       {:jason, "~> 1.0"},
       {:phoenix, "~> 1.7"},
       {:plug_cowboy, "~> 2.0"},
