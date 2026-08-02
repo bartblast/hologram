ad535d498cc8e1b99307e43339cfe83b282364df
Bart Blast <bart@bartblast.com>
2026-08-02 14:06:34 +0200

    Bump Hologram dep in feature tests app

--- test/features/mix.exs
+++ test/features/mix.exs
@@ -24,7 +24,7 @@ defmodule HologramFeatureTests.MixProject do
       {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false},
       {:hologram,
        git: "https://github.com/bartblast/hologram.git",
-       ref: "1f955a31d958ed80b55167576dc83c21f1cc20eb"},
+       ref: "8d4c579eefc57300b41971c86d36631153f65336"},
       {:jason, "~> 1.0"},
       {:phoenix, "~> 1.7"},
       {:plug_cowboy, "~> 2.0"},
