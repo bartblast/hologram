1b8505e7e0577eb247c148b30f965aca20ba6e99
Bart Blast <bart@bartblast.com>
2026-08-04 20:59:36 +0200

    Remove the vendored Snabbdom

--- assets/js/vendor/snabbdom/build/vnode.js
+++
@@ -1,5 +0,0 @@
-export function vnode(sel, data, children, text, elm) {
-  const key = data === undefined ? undefined : data.key;
-  return {sel, data, children, text, elm, key};
-}
-//# sourceMappingURL=vnode.js.map
