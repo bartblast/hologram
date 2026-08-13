67cfc821150022f98160e3b396918877c60fca38
Bart Blast <bart@bartblast.com>
2026-08-14 00:01:00 +0200

    Bump Hologram dep refs

--- test/umbrella/apps/app_1/mix.exs
+++ test/umbrella/apps/app_1/mix.exs
@@ -14,7 +14,7 @@ defmodule App1.MixProject do
       {:app_3, in_umbrella: true},
       {:hologram,
        git: "https://github.com/bartblast/hologram.git",
-       ref: "9a151aa90c2cba1635d262c28c0896162d5be232"},
+       ref: "7e37613c1cf35cc7f4e381f38bef3fcfafe5000d"},
       {:jason, "~> 1.0"},
       {:phoenix, "~> 1.7"},
       {:plug_cowboy, "~> 2.0"},
