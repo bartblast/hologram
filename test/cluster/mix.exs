a0faa2a4ee8f94cf8e9793f06bb988d5a620f788
Bart Blast <bart@bartblast.com>
2026-08-13 21:11:05 +0200

    Point the cluster tests app at the latest build

--- test/cluster/mix.exs
+++ test/cluster/mix.exs
@@ -24,7 +24,7 @@ defmodule HologramClusterTests.MixProject do
       {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false},
       {:hologram,
        git: "https://github.com/bartblast/hologram.git",
-       ref: "9a151aa90c2cba1635d262c28c0896162d5be232"},
+       ref: "644d46385ca5cb7ab44458f2146a8a414d0a4d57"},
       {:jason, "~> 1.0"},
       {:mint, "~> 1.0", only: :test},
       {:phoenix, "~> 1.7"},
