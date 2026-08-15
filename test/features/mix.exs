0fe4890e91b7df8964af19c233dbdc0bb6355cd4
Bart Blast <bart@bartblast.com>
2026-08-15 19:35:03 +0200

    Point test apps at the latest build

--- test/features/mix.exs
+++ test/features/mix.exs
@@ -24,7 +24,7 @@ defmodule HologramFeatureTests.MixProject do
       {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false},
       {:hologram,
        git: "https://github.com/bartblast/hologram.git",
-       ref: "fc48fc61ff6442aa182cc30064c96595bad42e2e"},
+       ref: "935dc617b505a82d510523800a4789dfdca60cf1"},
       {:jason, "~> 1.0"},
       {:phoenix, "~> 1.7"},
       {:plug_cowboy, "~> 2.0"},
