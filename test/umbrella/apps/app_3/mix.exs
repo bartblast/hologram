a29e8930dddd2ab2656194594d741896efba9cb7
Bart Blast <bart@bartblast.com>
2026-08-18 23:03:05 +0200

    Point the test apps at the current lib commit

--- test/umbrella/apps/app_3/mix.exs
+++ test/umbrella/apps/app_3/mix.exs
@@ -5,7 +5,7 @@ defmodule App3.MixProject do
     [
       {:hologram,
        git: "https://github.com/bartblast/hologram.git",
-       ref: "6afc3f282cea38e7c7bdae489380322c75a5763e"}
+       ref: "5526c2eff74d14b92c751eed4afd8eff5e7eeca4"}
     ]
   end
 
