54ddf7f051cddf07330cc1fdf5dba21ea3d3595e
Bart Blast <bart@bartblast.com>
2026-08-24 02:51:25 +0200

    Point the test apps at the branch

--- test/features/mix.exs
+++ test/features/mix.exs
@@ -24,7 +24,7 @@ defmodule HologramFeatureTests.MixProject do
       {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false},
       {:hologram,
        git: "https://github.com/bartblast/hologram.git",
-       ref: "3f9febd899eeff9af9130b86d2c227557a5bbcea"},
+       ref: "2e0020b41760c5439aa2f6a192366e27f0ee9276"},
       {:jason, "~> 1.0"},
       {:phoenix, "~> 1.7"},
       {:plug_cowboy, "~> 2.0"},
--- test/umbrella/apps/app_3/mix.exs
+++ test/umbrella/apps/app_3/mix.exs
@@ -5,7 +5,7 @@ defmodule App3.MixProject do
     [
       {:hologram,
        git: "https://github.com/bartblast/hologram.git",
-       ref: "3f9febd899eeff9af9130b86d2c227557a5bbcea"}
+       ref: "2e0020b41760c5439aa2f6a192366e27f0ee9276"}
     ]
   end
 
