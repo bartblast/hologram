2a08c165818aaca5f15c767e5f95cc3b820fde0b
Bart Blast <bart@bartblast.com>
2026-08-24 19:58:17 +0200

    Add a page exercising claimed writes in the browser

---
+++ test/features/app/entities/note.ex
@@ -0,0 +1,19 @@
+defmodule HologramFeatureTests.Entities.Note do
+  use Hologram.Entity
+
+  alias HologramFeatureTests.Entities.User
+
+  attribute :body, :string
+  attribute :pinned, :boolean, default: false
+
+  relationship :author, User
+
+  role :editor
+  role :owner, extends: :editor, creator: true
+
+  allow :read, to: :editor
+  allow :create, author_id: user_id()
+  allow :delete, to: :owner
+  allow :manage_roles, to: :owner
+  allow :pin, to: :editor, pinned: false
+end
