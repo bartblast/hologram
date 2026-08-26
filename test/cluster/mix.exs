1f338b8cb97c09eed0bc61e0efc6809060f26370
Bart Blast <bart@bartblast.com>
2026-08-26 21:24:47 +0200

    Point every test app at the map equality fix

--- test/cluster/mix.exs
+++ test/cluster/mix.exs
@@ -24,7 +24,7 @@ defmodule HologramClusterTests.MixProject do
       {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false},
       {:hologram,
        git: "https://github.com/bartblast/hologram.git",
-       ref: "ef875b8313beff52c151df1828784c507843f4d7"},
+       ref: "6d558ab346c5ef305742b64f4e5416786a42643a"},
       {:jason, "~> 1.0"},
       {:mint, "~> 1.0", only: :test},
       {:phoenix, "~> 1.7"},
--- test/umbrella/apps/app_1/mix.exs
+++ test/umbrella/apps/app_1/mix.exs
@@ -14,7 +14,7 @@ defmodule App1.MixProject do
       {:app_3, in_umbrella: true},
       {:hologram,
        git: "https://github.com/bartblast/hologram.git",
-       ref: "ef875b8313beff52c151df1828784c507843f4d7"},
+       ref: "6d558ab346c5ef305742b64f4e5416786a42643a"},
       {:jason, "~> 1.0"},
       {:phoenix, "~> 1.7"},
       {:plug_cowboy, "~> 2.0"},
