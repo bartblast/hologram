5c7c09e9bfb4ef4062fddf4f543fdcda976e1792
Bart Blast <bart@bartblast.com>
2026-08-27 00:27:53 +0200

    Box counter deltas on client-side rows

--- test/javascript/model_test.mjs
+++ test/javascript/model_test.mjs
@@ -104,6 +104,7 @@ describe("Model", () => {
         Type.map([
           [Type.atom("__struct__"), Type.alias("Hologram.Entity.Metadata")],
           [Type.atom("attribute_changes"), Type.map([])],
+          [Type.atom("attribute_deltas"), Type.map([])],
           [Type.atom("claim"), Type.nil()],
           [Type.atom("relationship_ops"), Type.map([])],
           [
@@ -125,6 +126,7 @@ describe("Model", () => {
         Type.map([
           [Type.atom("__struct__"), Type.alias("Hologram.Entity.Metadata")],
           [Type.atom("attribute_changes"), Type.map([])],
+          [Type.atom("attribute_deltas"), Type.map([])],
           [Type.atom("claim"), Type.nil()],
           [Type.atom("relationship_ops"), Type.map([])],
           [Type.atom("revisions"), Type.map([])],
