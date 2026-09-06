d6838690a0d21c65201dadf6894e1fef44c6a584
Bart Blast <bart@bartblast.com>
2026-09-06 21:42:21 +0200

    Point the test apps at the house digest

--- test/cluster/mix.exs
+++ test/cluster/mix.exs
@@ -24,7 +24,7 @@ defmodule HologramClusterTests.MixProject do
       {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false},
       {:hologram,
        git: "https://github.com/bartblast/hologram.git",
-       ref: "31f3522836eac7c2c89ea5b3d042c59bce01b6c9"},
+       ref: "eb7d39c21088347d61cb2cda97cda59fc63c3519"},
       {:jason, "~> 1.0"},
       {:mint, "~> 1.0", only: :test},
       {:phoenix, "~> 1.7"},
--- test/umbrella/apps/app_1/mix.exs
+++ test/umbrella/apps/app_1/mix.exs
@@ -14,7 +14,7 @@ defmodule App1.MixProject do
       {:app_3, in_umbrella: true},
       {:hologram,
        git: "https://github.com/bartblast/hologram.git",
-       ref: "31f3522836eac7c2c89ea5b3d042c59bce01b6c9"},
+       ref: "eb7d39c21088347d61cb2cda97cda59fc63c3519"},
       {:jason, "~> 1.0"},
       {:phoenix, "~> 1.7"},
       {:plug_cowboy, "~> 2.0"},
