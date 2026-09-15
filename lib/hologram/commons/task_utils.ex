28799c0b70e5dd8454b75339f7f8f6c756937f0d
Bart Blast <bart@bartblast.com>
2026-09-15 22:51:15 +0200

    Remove the unbounded fan-out

--- lib/hologram/commons/task_utils.ex
+++ lib/hologram/commons/task_utils.ex
@@ -2,16 +2,6 @@ defmodule Hologram.Commons.TaskUtils do
   @moduledoc false
 
   @doc """
-  Starts multiple tasks.
-  """
-  @spec async_many(Enum.t(), fun) :: list(Task.t())
-  def async_many(enumerable, fun) do
-    Enum.map(enumerable, fn elem ->
-      Task.async(fn -> fun.(elem) end)
-    end)
-  end
-
-  @doc """
   Maps the function over the enumerable in concurrent tasks, at most `:max_concurrency` at a time
   (default: the number of online schedulers), and returns the results in the enumerable's order.
   A task that raises takes the caller down, as with `Task.async/1` and `Task.await/2`.
--- test/elixir/hologram/commons/task_utils_test.exs
+++ test/elixir/hologram/commons/task_utils_test.exs
@@ -2,13 +2,6 @@ defmodule Hologram.Commons.TaskUtilsTest do
   use Hologram.Test.BasicCase, async: true
   import Hologram.Commons.TaskUtils
 
-  test "async_many/2" do
-    res = async_many(1..3, &(&1 + &1))
-
-    assert [%Task{}, %Task{}, %Task{}] = res
-    assert Enum.map(res, &Task.await/1) == [2, 4, 6]
-  end
-
   describe "map_concurrently/3" do
     test "returns results in the enumerable's order" do
       assert map_concurrently(1..5, &(&1 * 10)) == [10, 20, 30, 40, 50]
