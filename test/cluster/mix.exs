fbaaaa511936c2a03ed66564538dddd34eefc536
Bart Blast <bart@bartblast.com>
2026-09-18 19:15:40 +0200

    Bump Hologram dep refs in test apps

--- test/cluster/mix.exs
+++ test/cluster/mix.exs
@@ -24,7 +24,7 @@ defmodule HologramClusterTests.MixProject do
       {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false},
       {:hologram,
        git: "https://github.com/bartblast/hologram.git",
-       ref: "761ac188a681c26e318fd1f7ead6c74bfbd3af5d"},
+       ref: "0add5ce27df416855b65df0f0767d99b7037ce7f"},
       {:jason, "~> 1.0"},
       {:mint, "~> 1.0", only: :test},
       {:phoenix, "~> 1.7"},
--- test/umbrella/apps/app_1/mix.exs
+++ test/umbrella/apps/app_1/mix.exs
@@ -14,7 +14,7 @@ defmodule App1.MixProject do
       {:app_3, in_umbrella: true},
       {:hologram,
        git: "https://github.com/bartblast/hologram.git",
-       ref: "761ac188a681c26e318fd1f7ead6c74bfbd3af5d"},
+       ref: "0add5ce27df416855b65df0f0767d99b7037ce7f"},
       {:jason, "~> 1.0"},
       {:phoenix, "~> 1.7"},
       {:plug_cowboy, "~> 2.0"},
