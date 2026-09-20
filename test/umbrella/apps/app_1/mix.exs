69e69cbd0c889058400799622d80124e70712d84
Bart Blast <bart@bartblast.com>
2026-09-21 01:47:40 +0200

    Run the umbrella tests app on Bandit

--- test/umbrella/apps/app_1/mix.exs
+++ test/umbrella/apps/app_1/mix.exs
@@ -12,12 +12,12 @@ defmodule App1.MixProject do
     [
       {:app_2, in_umbrella: true},
       {:app_3, in_umbrella: true},
+      {:bandit, "~> 1.5"},
       {:hologram,
        git: "https://github.com/bartblast/hologram.git",
        ref: "728faa2c2440763647b4d88bcac5bb3757d10e70"},
       {:jason, "~> 1.0"},
       {:phoenix, "~> 1.7"},
-      {:plug_cowboy, "~> 2.0"},
       {:wallaby, "~> 0.30", only: :test}
     ]
   end
